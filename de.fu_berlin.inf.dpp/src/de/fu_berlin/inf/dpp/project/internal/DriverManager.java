b7d0d2859ae4be09b8028b19a6ca01877fdab7ce
Riad Djemili <djemili@users.sourceforge.net>
2006-09-24 23:51:53 +0000

    - DriverManager registers self at startup

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/internal/DriverManager.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/internal/DriverManager.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.xmlpull.v1.XmlPullParser;
 
+import de.fu_berlin.inf.dpp.Saros;
 import de.fu_berlin.inf.dpp.User;
 import de.fu_berlin.inf.dpp.activities.IActivity;
 import de.fu_berlin.inf.dpp.activities.RoleActivity;
@@ -15,11 +16,20 @@ import de.fu_berlin.inf.dpp.project.IActivityProvider;
 import de.fu_berlin.inf.dpp.project.ISharedProject;
 import de.fu_berlin.inf.dpp.project.ISharedProjectListener;
 
+/**
+ * This manager is responsible for handling driver changes.
+ * 
+ * @author rdjemili
+ */
 public class DriverManager implements IActivityProvider, ISharedProjectListener {
 	private List<IActivityListener> activityListeners = 
 		new LinkedList<IActivityListener>();
 	
 	private ISharedProject sharedProject;
+    
+    public DriverManager() {
+        Saros.getDefault().getSessionManager().addSessionListener(this);
+    }
 
 	/* (non-Javadoc)
 	 * @see de.fu_berlin.inf.dpp.project.ISessionListener
