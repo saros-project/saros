a7cfaa7669738f7969efca892294ab10afd777ba
Christopher Oezbek <coezbek@users.sourceforge.net>
2006-12-02 09:59:15 +0000

    Added Error Logging.

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/actions/NewSessionAction.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/actions/NewSessionAction.java
@@ -20,10 +20,13 @@
 package de.fu_berlin.inf.dpp.ui.actions;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.jivesoftware.smack.XMPPException;
@@ -31,51 +34,62 @@ import org.jivesoftware.smack.XMPPException;
 import de.fu_berlin.inf.dpp.Saros;
 import de.fu_berlin.inf.dpp.project.SessionManager;
 
+/**
+ * Start to share a project (a "session").
+ * 
+ * @author rdjemili
+ * 
+ */
 public class NewSessionAction implements IObjectActionDelegate {
-    private IProject selectedProject;
-    
-    /* (non-Javadoc)
-     * Defined in IActionDelegate
-     */
-    public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-    }
+	private IProject selectedProject;
 
-    /* (non-Javadoc)
-     * Defined in IActionDelegate
-     */
-    public void run(IAction action) {
-        try {
-            SessionManager sm = Saros.getDefault().getSessionManager();
-            sm.startSession(selectedProject);
-            
-        } catch (XMPPException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+	/*
+	 * (non-Javadoc) Defined in IActionDelegate
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
 
-    /* (non-Javadoc)
-     * Defined in IActionDelegate
-     */
-    public void selectionChanged(IAction action, ISelection selection) {
-        selectedProject = getProject(selection);
-        
-        SessionManager sm = Saros.getDefault().getSessionManager();
-        boolean running = sm.getSharedProject() != null;
-        boolean connected = Saros.getDefault().isConnected();
-        
-        action.setEnabled(connected && !running && selectedProject != null && 
-            selectedProject.isAccessible());
-    }
+	/*
+	 * (non-Javadoc) Defined in IActionDelegate
+	 */
+	public void run(IAction action) {
+		try {
+			SessionManager sm = Saros.getDefault().getSessionManager();
+			sm.startSession(selectedProject);
+		} catch (final XMPPException e) {
+			Display.getDefault().syncExec(new Runnable() {
+				public void run() {
+					ErrorDialog.openError(Display.getDefault().getActiveShell(),
+						"Error Starting Session", "Session could not be started", new Status(
+							Status.ERROR, "de.fu_berlin.inf.dpp", Status.ERROR,
+							e.getMessage(), e));
+				}
+			});
+		}
+	}
 
-    private IProject getProject(ISelection selection) {
-        Object element = ((IStructuredSelection)selection).getFirstElement();
-        if (element instanceof IProject) {
-            return (IProject)element;
-        } else if (element instanceof IJavaProject) {
-            return ((IJavaProject)element).getProject();
-        }
-        
-        return null;
-    }
+	/*
+	 * (non-Javadoc) Defined in IActionDelegate
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		selectedProject = getProject(selection);
+
+		SessionManager sm = Saros.getDefault().getSessionManager();
+		boolean running = sm.getSharedProject() != null;
+		boolean connected = Saros.getDefault().isConnected();
+
+		action.setEnabled(connected && !running && selectedProject != null
+			&& selectedProject.isAccessible());
+	}
+
+	private IProject getProject(ISelection selection) {
+		Object element = ((IStructuredSelection) selection).getFirstElement();
+		if (element instanceof IProject) {
+			return (IProject) element;
+		} else if (element instanceof IJavaProject) {
+			return ((IJavaProject) element).getProject();
+		}
+
+		return null;
+	}
 }
