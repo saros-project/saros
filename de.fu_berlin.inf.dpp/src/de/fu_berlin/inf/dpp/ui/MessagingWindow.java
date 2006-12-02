fb7419deba3deb17020088656cd4489c31a55c27
Christopher Oezbek <coezbek@users.sourceforge.net>
2006-12-02 10:03:18 +0000

    Format, clean-up, minimal documentation, error-handling.

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/MessagingWindow.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/MessagingWindow.java
@@ -36,144 +36,119 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 
 import de.fu_berlin.inf.dpp.MessagingManager;
+import de.fu_berlin.inf.dpp.Saros;
 
+/**
+ * TODO Very kludgy window for chat messages. Needs some work
+ * 
+ * @author rdjemili
+ *
+ */
 public class MessagingWindow extends ApplicationWindow {
-    private MessagingManager.SessionProvider session;
+	private MessagingManager.SessionProvider session;
 
-    private TextViewer                       historyViewer;
-    private StyledText                       textInput;
-    
-//    private TextViewer                       messageInput;
+	private TextViewer historyViewer;
 
-    
+	private StyledText textInput;
 
-    public MessagingWindow(MessagingManager.SessionProvider session) {
-        super(null); // top-level window
-        
-        this.session = session;
-    }
-    
-    @Override
-    protected Control createContents(Composite parent) {
-        // main composite
-        Composite composite = new Composite(parent, SWT.None);
-        FillLayout fillLayout = new FillLayout();
-//        fillLayout.marginHeight = 5;
-//        fillLayout.marginWidth = 5;
-        composite.setLayout(fillLayout);
-        
-        SashForm form = new SashForm(composite, SWT.VERTICAL);
-        
-        historyViewer = new TextViewer(form, SWT.READ_ONLY | SWT.V_SCROLL | SWT.BORDER);
-        historyViewer.setDocument(new Document());
-        
-//        messageInput = new TextViewer(form, SWT.V_SCROLL | SWT.BORDER);
-//        messageInput.addTextInputListener(new ITextInputListener() {
-//
-//            public void inputDocumentChanged(IDocument oldInput, IDocument newInput) {
-//                try {
-//                    if (newInput.getChar(newInput.getLength() - 1) == SWT.CR) { // HACK
-//                        sendInput();
-//                    }
-//                } catch (BadLocationException e) {
-//                    // TODO Auto-generated catch block
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//            public void inputDocumentAboutToBeChanged(IDocument oldInput, IDocument newInput) {
-//                // ignore
-//            }
-//        });
-//        
-        textInput = new StyledText(form, SWT.MULTI | SWT.BORDER);
-        textInput.addKeyListener(new KeyListener(){
-            public void keyPressed(KeyEvent e) {
-                if (e.character == SWT.CR) {
-                    e.doit = false;
-                    sendInput();
-                }
-            }
+	public MessagingWindow(MessagingManager.SessionProvider session) {
+		super(null); // top-level window
 
-            public void keyReleased(KeyEvent e) {
-                // ignore
-            }
-        });
-        textInput.setFocus();
-        
-        form.setWeights(new int[]{5, 1});
-        
-        // fill textViewer
-        for (MessagingManager.ChatLine chatLine : session.getHistory()) {
-            addChatLine(chatLine);
-        }
-        
-        return composite;
-    }
+		this.session = session;
+	}
 
-    @Override
-    protected void configureShell(Shell shell) {
-        shell.setText(session.getName());
-        shell.setSize(500, 400);
-        
-        super.configureShell(shell);
-    }
-    
-    private void sendInput() {
-        String msg = textInput.getText().trim();
-        textInput.setText("");
-        
-        session.sendMessage(msg);
-    }
-    
-    /**
-     * Add a chat line to the text viewer.
-     */
-    public void addChatLine(MessagingManager.ChatLine line) {
-        String sender = line.sender;
-        String text   = line.text;
-        
-        
-//        int length = document.getLength();
-//        
-//        StyledText styledText = (StyledText)historyViewer.getControl();
-//        
-//        StringBuffer newLine = new StringBuffer();
-//        
-//        try {
-//            document.replace(length, 0, newLine.toString());
-//        } catch (BadLocationException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-        
-        
-        IDocument document = historyViewer.getDocument();
-        int start = document.getLength();
-        
-        // build and append new chat line string
-        StringBuffer newLine = new StringBuffer();
-        if (start > 0) {
-            newLine.append('\n');
-            start++;
-        }
-        newLine.append(sender);
-        newLine.append(": ");
-        newLine.append(text);
-        
-        try {
-            document.replace(document.getLength(), 0, newLine.toString());
-        } catch (BadLocationException e) {
-            e.printStackTrace();
-        }
-//        styledText.append(newLine.toString());
-        
-        // show sender in bold
-        StyledText styledText = historyViewer.getTextWidget();
-        StyleRange styleRange = new StyleRange();
-        styleRange.start = start;
-        styleRange.length = sender.length();
-        styleRange.fontStyle = SWT.BOLD;
-        styledText.setStyleRange(styleRange);
-    }
+	@Override
+	protected Control createContents(Composite parent) {
+		// main composite
+		Composite composite = new Composite(parent, SWT.None);
+		FillLayout fillLayout = new FillLayout();
+		// fillLayout.marginHeight = 5;
+		// fillLayout.marginWidth = 5;
+		composite.setLayout(fillLayout);
+
+		SashForm form = new SashForm(composite, SWT.VERTICAL);
+
+		historyViewer = new TextViewer(form, SWT.READ_ONLY | SWT.V_SCROLL | SWT.BORDER);
+		historyViewer.setDocument(new Document());
+
+		textInput = new StyledText(form, SWT.MULTI | SWT.BORDER);
+		textInput.addKeyListener(new KeyListener() {
+			public void keyPressed(KeyEvent e) {
+				if (e.character == SWT.CR) {
+					e.doit = false;
+					sendInput();
+				}
+			}
+
+			public void keyReleased(KeyEvent e) {
+				// ignore
+			}
+		});
+		textInput.setFocus();
+
+		form.setWeights(new int[] { 5, 1 });
+
+		// fill textViewer
+		for (MessagingManager.ChatLine chatLine : session.getHistory()) {
+			addChatLine(chatLine);
+		}
+
+		return composite;
+	}
+
+	@Override
+	protected void configureShell(Shell shell) {
+
+		String sessionName = session.getName();
+		if (sessionName == null) {
+			sessionName = "Talking to Unknown";
+		}
+
+		shell.setText(sessionName);
+		shell.setSize(500, 400);
+
+		super.configureShell(shell);
+	}
+
+	private void sendInput() {
+		String msg = textInput.getText().trim();
+		textInput.setText("");
+
+		session.sendMessage(msg);
+	}
+
+	/**
+	 * Add a chat line to the text viewer.
+	 */
+	public void addChatLine(MessagingManager.ChatLine line) {
+		String sender = line.sender;
+		String text = line.text;
+
+		IDocument document = historyViewer.getDocument();
+		int start = document.getLength();
+
+		// build and append new chat line string
+		StringBuffer newLine = new StringBuffer();
+		if (start > 0) {
+			newLine.append('\n');
+			start++;
+		}
+		newLine.append(sender);
+		newLine.append(": ");
+		newLine.append(text);
+
+		try {
+			document.replace(document.getLength(), 0, newLine.toString());
+		} catch (BadLocationException e) {
+			Saros.log("Could not update chat window", e);
+		}
+
+		// Show sender in bold
+		StyledText styledText = historyViewer.getTextWidget();
+		StyleRange styleRange = new StyleRange();
+		styleRange.start = start;
+		styleRange.length = sender.length();
+		styleRange.fontStyle = SWT.BOLD;
+		styledText.setStyleRange(styleRange);
+	}
 }
