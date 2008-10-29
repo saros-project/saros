39891a9705f01c945dd2a5d1bae84be662574a7f
Christoph Jacob <chris_fu@users.sourceforge.net>
2008-10-29 15:12:42 +0000

    added TODO, review needed on IWizardPage2, interface not really needed

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/wizards/IWizardPage2.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/wizards/IWizardPage2.java
@@ -2,6 +2,7 @@ package de.fu_berlin.inf.dpp.ui.wizards;
 
 import org.eclipse.jface.wizard.IWizardPage;
 
+// TODO CJ: Review needed: Why this interface is needed???
 public interface IWizardPage2 extends IWizardPage {
 
     public boolean performFinish();
