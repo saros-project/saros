58de6fb17da0e8d06befd276321d378df9991136
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-02-21 15:44:54 +0000

    [DOCU] Clarified whether streams should be closed or not.

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/IDataReceiver.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/IDataReceiver.java
@@ -6,11 +6,26 @@ import org.eclipse.core.runtime.Path;
 
 import de.fu_berlin.inf.dpp.net.internal.TransferDescription;
 
+/**
+ * Implementors don't need to (but may) close the InputStream if they consumed
+ * the stream. But: Implementors are expected to have consumed all data from the
+ * stream when returning, because the caller will close or reuse the stream once
+ * the receiver returns.
+ */
 public interface IDataReceiver {
 
+    /**
+     * @return true if the input stream has been consumed by the receiver
+     */
     boolean receivedArchive(TransferDescription data, InputStream input);
 
+    /**
+     * @return true if the input stream has been consumed by the receiver
+     */
     boolean receivedResource(JID from, Path path, InputStream input, int time);
 
+    /**
+     * @return true if the input stream has been consumed by the receiver
+     */
     boolean receivedFileList(TransferDescription data, InputStream input);
 }
