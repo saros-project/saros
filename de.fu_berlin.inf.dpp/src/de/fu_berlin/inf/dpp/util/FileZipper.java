32930ceb32fbb17479700f375b7c2b4572794000
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-02-23 10:33:16 +0000

    Merged r.726 into trunk
    [INTERNAL] Cleaned-up FileZipper

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/FileZipper.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/FileZipper.java
@@ -3,9 +3,7 @@ package de.fu_berlin.inf.dpp.util;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 import java.util.zip.Adler32;
@@ -17,7 +15,6 @@ import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 
 /**
  * This class contains method to create a zip archive out of a list of files.
@@ -29,35 +26,6 @@ import org.eclipse.core.runtime.Path;
 public class FileZipper {
 
     private static Logger logger = Logger.getLogger(FileZipper.class);
-
-    protected static void addFile(String path, File file,
-        ZipOutputStream zipStream) throws IOException {
-
-        zipStream.putNextEntry(new ZipEntry(path + Path.SEPARATOR
-            + file.getName()));
-
-        IOUtils.copy(new FileInputStream(file), zipStream);
-
-        zipStream.closeEntry();
-    }
-
-    protected static void addFolder(String path, File file, ZipOutputStream zos)
-        throws Exception {
-
-        File[] files = file.listFiles();
-        for (File f : files) {
-            if (f.isDirectory()) {
-                FileZipper.logger.debug("Compress folder: " + file.getName());
-                FileZipper.addFolder(path + Path.SEPARATOR + file.getName(), f,
-                    zos);
-            } else {
-                FileZipper.logger.debug("Compress file : " + file.getName()
-                    + " path " + path);
-                FileZipper.addFile(path + Path.SEPARATOR + file.getName(), f,
-                    zos);
-            }
-        }
-    }
 
     /**
      * To create a checksum when unzipping one could use
@@ -80,28 +48,25 @@ public class FileZipper {
                 new Adler32());
         }
 
-        ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream);
+        ZipOutputStream zipStream = new ZipOutputStream(outputStream);
 
         for (IPath path : files) {
+
             FileZipper.logger.debug("Compress file: " + path);
 
-            File f = project.getFile(path).getLocation().toFile();
-            if (f.exists()) {
+            File file = project.getFile(path).getLocation().toFile();
+            if (file.exists()) {
+                zipStream.putNextEntry(new ZipEntry(path.toString()));
 
-                /* create project path in folder structure. */
-                String[] structure = path.segments();
-                String path_structure = "";
-                for (int j = 0; j < structure.length - 1; j++) {
-                    String s = structure[j];
-                    path_structure += s + Path.SEPARATOR;
-                }
+                IOUtils.copy(new FileInputStream(file), zipStream);
 
-                FileZipper.addFile(path_structure, f, zipOutputStream);
+                zipStream.closeEntry();
             } else {
-                throw new FileNotFoundException(path.toString());
+                FileZipper.logger
+                    .warn("File given to Zip which does not exist: " + path);
             }
         }
-        zipOutputStream.close();
+        zipStream.close();
 
         // Checksum
         if (cos != null) {
