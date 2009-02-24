ea99524569509ed84b8c230acaef816092c92d54
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-02-24 08:03:10 +0000

    [DOCU][DEBUG] Improved Documentation of RoleActivity.

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/activities/RoleActivity.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/activities/RoleActivity.java
@@ -22,13 +22,55 @@ package de.fu_berlin.inf.dpp.activities;
 import de.fu_berlin.inf.dpp.User.UserRole;
 import de.fu_berlin.inf.dpp.net.JID;
 
+/**
+ * A role activity indicates that a user has a new Role in the Driver/Observer
+ * schemes of things.
+ */
 public class RoleActivity implements IActivity {
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((role == null) ? 0 : role.hashCode());
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof RoleActivity))
+            return false;
+        RoleActivity other = (RoleActivity) obj;
+        if (role == null) {
+            if (other.role != null)
+                return false;
+        } else if (!role.equals(other.role))
+            return false;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
+        return true;
+    }
 
     private String source;
 
     private final JID user;
     private final UserRole role;
 
+    /**
+     * Creates a new RoleActivity which indicates that the given user should
+     * change into the given role.
+     * 
+     * @param user
+     * @param role
+     */
     public RoleActivity(JID user, UserRole role) {
         this.user = user;
         this.role = role;
@@ -43,17 +85,8 @@ public class RoleActivity implements IActivity {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof RoleActivity) {
-            RoleActivity activity = (RoleActivity) obj;
-            return activity.getUser().equals(this.user);
-        }
-        return false;
-    }
-
-    @Override
     public String toString() {
-        return "RoleActivity(user:" + this.user + ",role:" + this.getRole()
+        return "RoleActivity(user:" + this.user + ",new role:" + this.getRole()
             + ")";
     }
 
