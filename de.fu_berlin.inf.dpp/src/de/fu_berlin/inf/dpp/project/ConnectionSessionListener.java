0fe0cdaa7ed3d86fe6dfead47217f7e6de232e6f
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-04-06 09:09:39 +0000

    [DOCU] Improved documentation about the expected order in which calls to the ConnectionSessionListeners need to be made

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/ConnectionSessionListener.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/ConnectionSessionListener.java
@@ -5,6 +5,20 @@ package de.fu_berlin.inf.dpp.project;
 
 import org.jivesoftware.smack.XMPPConnection;
 
+/**
+ * The general contract of this listener is that the order of calls to these
+ * methods will always be in the following order:
+ * 
+ * prepare() -> start() <-> stop() -> dispose() -> prepare() ->...
+ * 
+ * So first is always prepare followed by start.
+ * 
+ * Then start() may lead to stop() from which the session may be disposed() or
+ * started again.
+ * 
+ * Once the session has been disposed it can only be prepared again.
+ * 
+ */
 public interface ConnectionSessionListener {
 
     public void prepare(XMPPConnection connection);
