2add674e8e0ee316483bbd5f28ef9a15e6343e48
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-04-17 09:30:04 +0000

    Patch by sszuecs
    [API] Converted OpenInviteInterface to using PicoContainer.

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/actions/OpenInviteInterface.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/actions/OpenInviteInterface.java
@@ -2,10 +2,13 @@ package de.fu_berlin.inf.dpp.ui.actions;
 
 import org.apache.log4j.Logger;
 import org.eclipse.jface.action.Action;
+import org.picocontainer.annotations.Inject;
 
 import de.fu_berlin.inf.dpp.Saros;
 import de.fu_berlin.inf.dpp.project.AbstractSessionListener;
+import de.fu_berlin.inf.dpp.project.ISessionListener;
 import de.fu_berlin.inf.dpp.project.ISharedProject;
+import de.fu_berlin.inf.dpp.project.SessionManager;
 import de.fu_berlin.inf.dpp.ui.SarosUI;
 import de.fu_berlin.inf.dpp.util.Util;
 
@@ -14,26 +17,31 @@ public class OpenInviteInterface extends Action {
     private static final Logger log = Logger
         .getLogger(OpenInviteInterface.class.getName());
 
+    protected ISessionListener sessionListener = new AbstractSessionListener() {
+        @Override
+        public void sessionStarted(ISharedProject sharedProject) {
+            setEnabled(sharedProject.isHost());
+        }
+
+        @Override
+        public void sessionEnded(ISharedProject sharedProject) {
+            setEnabled(false);
+        }
+    };
+
+    @Inject
+    protected SessionManager sessionManager;
+
     public OpenInviteInterface() {
         super();
         setImageDescriptor(SarosUI.getImageDescriptor("/icons/invites.png"));
         setToolTipText("Open invitation interface");
 
-        Saros.getDefault().getSessionManager().addSessionListener(
-            new AbstractSessionListener() {
-
-                public void sessionStarted(ISharedProject sharedProject) {
-                    setEnabled(sharedProject.isHost());
-                }
-
-                public void sessionEnded(ISharedProject sharedProject) {
-                    setEnabled(false);
-                }
-            });
+        Saros.getDefault().reinject(this);
+        sessionManager.addSessionListener(sessionListener);
 
         // Needed when the Interface is created during a session
-        ISharedProject project = Saros.getDefault().getSessionManager()
-            .getSharedProject();
+        ISharedProject project = sessionManager.getSharedProject();
         setEnabled((project != null) && project.isHost());
     }
 
@@ -44,8 +52,7 @@ public class OpenInviteInterface extends Action {
     public void run() {
         Util.runSafeSync(log, new Runnable() {
             public void run() {
-                Saros.getDefault().getSessionManager().getSharedProject()
-                    .startInvitation(null);
+                sessionManager.getSharedProject().startInvitation(null);
             }
         });
     }
