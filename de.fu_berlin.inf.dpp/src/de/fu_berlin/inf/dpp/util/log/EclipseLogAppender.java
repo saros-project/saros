cc7284f9fb9032bee368f2a5adca4cdcaccafd40
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-05-04 22:37:31 +0000

    [FIX] EclipseLogAppender does not get initialized.

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/log/EclipseLogAppender.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/log/EclipseLogAppender.java
@@ -1,5 +1,8 @@
 package de.fu_berlin.inf.dpp.util.log;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Level;
 import org.apache.log4j.Priority;
@@ -7,6 +10,7 @@ import org.apache.log4j.spi.LoggingEvent;
 import org.apache.log4j.spi.ThrowableInformation;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.picocontainer.annotations.Inject;
 import org.picocontainer.annotations.Nullable;
 
 import de.fu_berlin.inf.dpp.Saros;
@@ -25,24 +29,63 @@ public class EclipseLogAppender extends AppenderSkeleton {
         }
     }
 
-    protected Saros saros;
+    public static class LogEvent {
 
-    public void log(int level, String message, @Nullable Throwable t) {
-
-        if (saros == null) {
-            Saros.reinject(this);
+        public LogEvent(int level, String message, Throwable t) {
+            this.level = level;
+            this.message = message;
+            this.t = t;
         }
 
-        if (saros != null) {
-            IStatus logMessage = new Status(level, Saros.SAROS, IStatus.OK,
-                message, t);
+        protected int level;
+
+        protected String message;
+
+        protected Throwable t;
+
+        public int getLevel() {
+            return level;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public Throwable getT() {
+            return t;
+        }
+    }
+
+    @Inject
+    protected Saros saros;
+
+    protected List<LogEvent> queuedMessages = new LinkedList<LogEvent>();
+
+    protected void log(int level, String message, @Nullable Throwable t) {
+
+        if (saros == null) {
+            try {
+                Saros.reinject(this);
+            } catch (RuntimeException e) {
+                // ignore
+            }
+        }
+
+        queuedMessages.add(new LogEvent(level, message, t));
+        if (saros == null) {
+            return;
+        }
+
+        while (queuedMessages.size() > 0) {
+            LogEvent event = queuedMessages.remove(0);
+            IStatus logMessage = new Status(event.getLevel(), Saros.SAROS,
+                IStatus.OK, event.getMessage(), event.getT());
             saros.getLog().log(logMessage);
         }
     }
 
     @Override
     protected void append(LoggingEvent event) {
-
         if (event.getLevel().isGreaterOrEqual(Level.WARN)) {
 
             String message = this.layout.format(event);
@@ -53,6 +96,7 @@ public class EclipseLogAppender extends AppenderSkeleton {
                 log(IStatus.WARNING, message, getThrowable(event));
             }
         }
+
     }
 
     @Override
