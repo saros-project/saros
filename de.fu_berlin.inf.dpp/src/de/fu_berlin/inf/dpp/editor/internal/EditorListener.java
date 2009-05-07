680e978ef7adc8f811aa1d566458b15101030761
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-05-07 06:38:58 +0000

    [FIX] #1541294: Resizing eclipse does not resize viewport annotation

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/editor/internal/EditorListener.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/editor/internal/EditorListener.java
@@ -3,12 +3,17 @@
  */
 package de.fu_berlin.inf.dpp.editor.internal;
 
+import org.apache.log4j.Logger;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.IViewportListener;
 import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.text.source.LineRange;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.MouseEvent;
@@ -17,7 +22,14 @@ import org.eclipse.ui.IEditorPart;
 
 import de.fu_berlin.inf.dpp.editor.EditorManager;
 
+/**
+ * Listener for tracking the selection and viewport of an IEditorPart and
+ * reporting any changes to an EditorManager.
+ */
 public class EditorListener {
+
+    private static final Logger log = Logger.getLogger(EditorListener.class
+        .getName());
 
     protected EditorManager manager;
 
@@ -45,21 +57,65 @@ public class EditorListener {
                 "EditorPart does not provide an ITextViewer!");
         }
 
-        viewer.getTextWidget().addMouseListener(mouseListener);
-        viewer.getTextWidget().addKeyListener(keyListener);
+        StyledText textWidget = viewer.getTextWidget();
+        textWidget.addControlListener(controlListener);
+        textWidget.addMouseListener(mouseListener);
+        textWidget.addKeyListener(keyListener);
+
         viewer.getSelectionProvider().addSelectionChangedListener(
             selectionChangedListener);
         viewer.addViewportListener(viewportListener);
     }
 
+    /**
+     * Disconnects all listeners from the underlying EditorPart.
+     * 
+     * @throws IllegalStateException
+     *             if already disposed
+     */
+    public void unbind() {
+
+        if (part == null) {
+            throw new IllegalStateException();
+        }
+
+        StyledText textWidget = viewer.getTextWidget();
+        textWidget.removeControlListener(controlListener);
+        textWidget.removeMouseListener(mouseListener);
+        textWidget.removeKeyListener(keyListener);
+
+        viewer.getSelectionProvider().removeSelectionChangedListener(
+            selectionChangedListener);
+        viewer.removeViewportListener(viewportListener);
+
+        viewer = null;
+        part = null;
+    }
+
+    /**
+     * Listens to resize events of the control, because the IViewportListener
+     * does not report such events.
+     */
+    protected ControlListener controlListener = new ControlListener() {
+
+        public void controlMoved(ControlEvent e) {
+            generateViewport();
+        }
+
+        public void controlResized(ControlEvent e) {
+            generateViewport();
+        }
+
+    };
+
     protected final MouseListener mouseListener = new MouseListener() {
 
         public void mouseDown(MouseEvent e) {
-            checkSelection();
+            generateSelection();
         }
 
         public void mouseUp(MouseEvent e) {
-            checkSelection();
+            generateSelection();
         }
 
         public void mouseDoubleClick(MouseEvent e) {
@@ -69,7 +125,7 @@ public class EditorListener {
 
     protected final KeyListener keyListener = new KeyListener() {
         public void keyReleased(KeyEvent e) {
-            checkSelection();
+            generateSelection();
         }
 
         public void keyPressed(KeyEvent e) {
@@ -78,19 +134,22 @@ public class EditorListener {
     };
 
     protected IViewportListener viewportListener = new IViewportListener() {
-        // TODO why doesn't this react to window resizes?
+        /*
+         * This does not report window resizes because of
+         * https://bugs.eclipse.org/bugs/show_bug.cgi?id=171018
+         */
         public void viewportChanged(int verticalOffset) {
-            manager.generateViewport(part, EditorAPI.getViewport(viewer));
+            generateViewport();
         }
     };
 
     protected ISelectionChangedListener selectionChangedListener = new ISelectionChangedListener() {
         public void selectionChanged(SelectionChangedEvent event) {
-            checkSelection();
+            generateSelection();
         }
     };
 
-    protected void checkSelection() {
+    protected void generateSelection() {
 
         ITextSelection selection = (ITextSelection) viewer
             .getSelectionProvider().getSelection();
@@ -101,20 +160,14 @@ public class EditorListener {
         }
     }
 
-    public void unbind() {
+    protected void generateViewport() {
 
-        if (part == null) {
-            throw new IllegalStateException();
+        LineRange viewport = EditorAPI.getViewport(viewer);
+
+        if (log.isDebugEnabled()) {
+            log.debug("Viewport changed: " + viewport);
         }
 
-        viewer.getTextWidget().removeMouseListener(mouseListener);
-        viewer.getTextWidget().removeKeyListener(keyListener);
-        viewer.getSelectionProvider().removeSelectionChangedListener(
-            selectionChangedListener);
-        viewer.removeViewportListener(viewportListener);
-
-        viewer = null;
-        part = null;
-
+        manager.generateViewport(part, viewport);
     }
 }
