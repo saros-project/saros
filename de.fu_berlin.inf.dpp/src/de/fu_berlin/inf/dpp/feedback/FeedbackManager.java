a18d107c8b6574f88d794bf17f878418ffe0a93e
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-05-29 10:55:51 +0000

    Patch by ldohrmann
    [FEEDBACK] Activated our first survey targetting fast user feedback.
    
    Signed-off-by coezbek

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/feedback/FeedbackManager.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/feedback/FeedbackManager.java
@@ -36,7 +36,7 @@ import de.fu_berlin.inf.dpp.util.Util;
 @Component(module = "feedback")
 public class FeedbackManager extends AbstractFeedbackManager {
     /** the URL to the website that contains our survey */
-    public static final String SURVEY_URL = "https://www.inf.fu-berlin.de/w/SE/DPPSurveys"; //$NON-NLS-1$
+    public static final String SURVEY_URL = "http://survey.mi.fu-berlin.de/public/survey.php?name=SarosFastUserFeedback";
 
     /** the text to show in the first FeedbackDialog */
     public static final String FEEDBACK_REQUEST = Messages
