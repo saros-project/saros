3ff7bd68828b73a63d54065b867efa7141c541df
Sebastian Ziller <s-ziller@users.sourceforge.net>
2009-05-29 13:25:27 +0000

    [INTERNAL] generated hashcode and equals in StartHandle
    signed off by coezbek

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/synchronize/StartHandle.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/synchronize/StartHandle.java
@@ -50,4 +50,35 @@ public class StartHandle {
     public String getHandleID() {
         return id;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        StartHandle other = (StartHandle) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
+        return true;
+    }
 }
