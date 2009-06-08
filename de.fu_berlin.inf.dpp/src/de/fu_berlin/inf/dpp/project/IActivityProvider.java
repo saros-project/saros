3aaa9c218b8956e55ea9f6622cae5409ad32db2d
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-06-08 15:42:49 +0000

    [DOCU] Improve interface contract of IActivityProvider.

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/IActivityProvider.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/IActivityProvider.java
@@ -3,17 +3,35 @@ package de.fu_berlin.inf.dpp.project;
 import de.fu_berlin.inf.dpp.activities.IActivity;
 
 /**
- * Every activity provider is responsible for one or more activity types. It
- * handles creating (fromXML) and executing (exec) the activities of some type
- * and notifies listeners.
+ * Every activity provider is responsible for one or more activity types.
+ * 
+ * It can...
+ * 
+ * ...cause an activity to be executed locally (exec)
+ * 
+ * ...report an activity to the registered listeners
+ * {@link IActivityListener#activityCreated(IActivity)}
  * 
  * @author rdjemili
  */
 public interface IActivityProvider {
 
+    /**
+     * Will cause implementor of the interface to execute the given activity.
+     * 
+     * @swt The implementor may expect that this method is called from the SWT
+     *      thread.
+     */
     public void exec(IActivity activity);
 
+    /**
+     * Add the given listener to the list of listeners which are informed when
+     * an activity occurred locally.
+     */
     public void addActivityListener(IActivityListener listener);
 
+    /**
+     * Remove a listener previously registered with addActivityListener.
+     */
     public void removeActivityListener(IActivityListener listener);
 }
