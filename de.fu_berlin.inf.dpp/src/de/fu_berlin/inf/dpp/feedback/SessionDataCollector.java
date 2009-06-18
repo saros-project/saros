b18f27cca0319ee304ce2d2188493092974d1958
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-06-18 07:14:35 +0000

    Patch by ldohrmann
    [FEATURE] Statistic: General platform infos, feedback settings and
    session data (session time and ID) is collected
     
    Signed-off-by coezbek

---
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/feedback/SessionDataCollector.java
@@ -0,0 +1,72 @@
+package de.fu_berlin.inf.dpp.feedback;
+
+import de.fu_berlin.inf.dpp.Saros;
+import de.fu_berlin.inf.dpp.annotations.Component;
+import de.fu_berlin.inf.dpp.observables.SessionIDObservable;
+import de.fu_berlin.inf.dpp.project.ISharedProject;
+import de.fu_berlin.inf.dpp.project.SessionManager;
+import de.fu_berlin.inf.dpp.util.Util;
+
+/**
+ * Collects some general session data (session time, session ID, session count),
+ * platform informations and settings.
+ * 
+ * @author Lisa Dohrmann
+ */
+@Component(module = "feedback")
+public class SessionDataCollector extends AbstractStatisticCollector {
+
+    protected SessionIDObservable sessionID;
+    protected FeedbackManager feedbackManager;
+    protected Saros saros;
+
+    protected String currentSessionID;
+    protected long sessionStart;
+    protected long sessionTime;
+
+    public SessionDataCollector(StatisticManager statisticManager,
+        SessionManager sessionManager, SessionIDObservable sessionID,
+        Saros saros, FeedbackManager feedbackManager) {
+        super(statisticManager, sessionManager);
+        this.sessionID = sessionID;
+        this.saros = saros;
+        this.feedbackManager = feedbackManager;
+    }
+
+    @Override
+    protected void processGatheredData() {
+        data.setSessionID(currentSessionID);
+        data.setSessionTime(StatisticManager.getTimeInMinutes(sessionTime));
+        data.setSessionCount(statisticManager.getSessionCount());
+
+        storeGeneralInfos();
+    }
+
+    @Override
+    protected void doOnSessionEnd(ISharedProject project) {
+        sessionTime = System.currentTimeMillis() - sessionStart;
+    }
+
+    @Override
+    protected void doOnSessionStart(ISharedProject project) {
+        currentSessionID = sessionID.getValue();
+        sessionStart = System.currentTimeMillis();
+    }
+
+    /**
+     * Stores some general platform information and settings, e.g. Saros
+     * version, Java version, feedback settings, etc.
+     */
+    protected void storeGeneralInfos() {
+        data.setSarosVersion(saros.getVersion());
+        data.setJavaVersion(System.getProperty("java.version",
+            "Unknown Java Version"));
+        data.setOSName(System.getProperty("os.name", "Unknown OS"));
+        data.setEclipseVersion(Util.getEclipsePlatformInfo());
+
+        data.setFeedbackDisabled(feedbackManager.isFeedbackDisabled());
+        data.setFeedbackInterval(feedbackManager.getSurveyInterval());
+
+        data.setUserID(statisticManager.getUserID());
+    }
+}
