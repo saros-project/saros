b0d3bb4d61b04aff5d04d5047824fbb8ead1922d
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-09-15 11:34:37 +0000

    [NOP] Removed a newline

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/internal/DataTransferManager.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/internal/DataTransferManager.java
@@ -111,7 +111,6 @@ public class DataTransferManager implements ConnectionSessionListener {
                 }
             }
         }
-
     };
 
     protected DispatchingJingleFileTransferListener jingleListener;
