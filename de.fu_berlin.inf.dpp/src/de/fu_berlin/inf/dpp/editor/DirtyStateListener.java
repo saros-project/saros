b668f13e12f65269d48e036b03021ebf3a8c3409
Christopher Oezbek <coezbek@users.sourceforge.net>
2009-09-15 11:38:07 +0000

    [FIX] Caught a threading violation when deleting a dirty file

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/editor/DirtyStateListener.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/editor/DirtyStateListener.java
@@ -1,10 +1,13 @@
 package de.fu_berlin.inf.dpp.editor;
 
 import org.apache.commons.lang.ObjectUtils;
+import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.IElementStateListener;
+
+import de.fu_berlin.inf.dpp.util.Util;
 
 /**
  * Listener registered on Editors to be informed about their dirty state.
@@ -13,7 +16,10 @@ import org.eclipse.ui.texteditor.IElementStateListener;
  */
 public class DirtyStateListener implements IElementStateListener {
 
-    private final EditorManager editorManager;
+    private static final Logger log = Logger
+        .getLogger(DirtyStateListener.class);
+
+    protected final EditorManager editorManager;
 
     DirtyStateListener(EditorManager editorManager) {
         this.editorManager = editorManager;
@@ -35,22 +41,28 @@ public class DirtyStateListener implements IElementStateListener {
         if (!(element instanceof FileEditorInput))
             return;
 
-        IFile file = ((FileEditorInput) element).getFile();
+        final IFile file = ((FileEditorInput) element).getFile();
 
         if (!ObjectUtils.equals(file.getProject(), editorManager.sharedProject
             .getProject())) {
             return;
         }
 
-        // Only trigger save events for files managed in the editor pool
-        if (!editorManager.isConnected(file)) {
-            return;
-        }
+        Util.runSafeSWTSync(log, new Runnable() {
 
-        EditorManager.log.debug("Dirty state reset for: " + file.toString());
+            public void run() {
 
-        IPath path = file.getProjectRelativePath();
-        this.editorManager.sendEditorActivitySaved(path);
+                // Only trigger save events for files managed in the editor pool
+                if (!editorManager.isConnected(file)) {
+                    return;
+                }
+
+                EditorManager.log.debug("Dirty state reset for: "
+                    + file.toString());
+                IPath path = file.getProjectRelativePath();
+                editorManager.sendEditorActivitySaved(path);
+            }
+        });
     }
 
     public void elementContentAboutToBeReplaced(Object element) {
