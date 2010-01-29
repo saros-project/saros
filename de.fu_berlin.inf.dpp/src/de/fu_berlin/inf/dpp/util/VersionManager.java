35f38d8795364f6320d15c07abf5db66bae58382
Karl Beecher <k_beecher@users.sourceforge.net>
2010-01-29 15:17:02 +0000

    Changes necessary for Release 10.1.29.r1970

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/VersionManager.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/VersionManager.java
@@ -137,6 +137,12 @@ public class VersionManager {
          */
 
         /**
+         * Version 10.1.29.r1970
+         */
+        compatibilityChart.put(new Version("10.1.29.r1970"), Arrays
+            .asList(new Version("10.1.29.r1970")));
+
+        /**
          * Version 9.12.04.r1862
          */
         compatibilityChart.put(new Version("9.12.4.r1878"), Arrays
