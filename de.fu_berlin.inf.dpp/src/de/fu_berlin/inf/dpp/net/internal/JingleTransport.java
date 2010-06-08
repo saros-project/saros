39a9fa31cc2057fd2a2f62e53fae541245de171f
sbauch <sbauch@users.sourceforge.net>
2010-06-08 05:28:06 +0000

    Jingle

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/internal/JingleTransport.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/internal/JingleTransport.java
@@ -1,41 +1,109 @@
 package de.fu_berlin.inf.dpp.net.internal;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smackx.bytestreams.BytestreamSession;
+import org.jivesoftware.smackx.jingle.JingleManager;
 import org.jivesoftware.smackx.jingle.JingleSession;
 import org.jivesoftware.smackx.jingle.JingleSessionRequest;
+import org.jivesoftware.smackx.jingle.JingleSessionStatePending;
 import org.jivesoftware.smackx.jingle.listeners.JingleSessionRequestListener;
-import org.picocontainer.annotations.Inject;
+import org.jivesoftware.smackx.jingle.media.JingleMediaManager;
+import org.jivesoftware.smackx.jingle.media.JingleMediaSession;
+import org.jivesoftware.smackx.jingle.media.PayloadType;
+import org.jivesoftware.smackx.jingle.nat.ICETransportManager;
+import org.jivesoftware.smackx.jingle.nat.JingleTransportManager;
+import org.jivesoftware.smackx.jingle.nat.TransportCandidate;
 
 import de.fu_berlin.inf.dpp.Saros;
 import de.fu_berlin.inf.dpp.net.JID;
 import de.fu_berlin.inf.dpp.net.internal.DataTransferManager.IBytestreamConnection;
 import de.fu_berlin.inf.dpp.net.internal.DataTransferManager.ITransport;
-import de.fu_berlin.inf.dpp.net.internal.DataTransferManager.JingleConnectionState;
 import de.fu_berlin.inf.dpp.net.internal.DataTransferManager.NetTransferMode;
-import de.fu_berlin.inf.dpp.net.jingle.JingleFileTransferManager;
-import de.fu_berlin.inf.dpp.net.jingle.JingleFileTransferManager.FileTransferConnection;
-import de.fu_berlin.inf.dpp.preferences.PreferenceUtils;
+import de.fu_berlin.inf.dpp.net.jingle.JingleFileTransferSession;
+import de.fu_berlin.inf.dpp.preferences.PreferenceConstants;
+import de.fu_berlin.inf.dpp.util.CausedIOException;
 
 public class JingleTransport implements ITransport {
 
-    private static final Logger log = Logger.getLogger(JingleTransport.class);
+    private class FileMediaManager extends JingleMediaManager {
+        public FileMediaManager(JingleTransportManager transportManager) {
+            super(transportManager);
 
-    @Inject
-    protected DiscoveryManager discoveryManager;
+        }
 
-    @Inject
-    protected PreferenceUtils preferenceUtils;
+        @Override
+        public JingleMediaSession createMediaSession(PayloadType payload,
+            TransportCandidate remoteCandidate,
+            TransportCandidate localCandidate, JingleSession jingleSession) {
 
-    private Saros saros;
-    private JingleFileTransferManager manager;
-    private IBytestreamConnectionListener connectionListener;
-    private static JingleTransport instance = null;
+            final JID remoteJID = isInitiator(jingleSession) ? new JID(
+                jingleSession.getResponder()) : new JID(jingleSession
+                .getInitiator());
+
+            final JingleFileTransferSession newSession = new JingleFileTransferSession(
+                payload, remoteCandidate, localCandidate, null, jingleSession);
+
+            if (newSession.isConnected()) {
+                log.debug("Jingle [" + remoteJID.getName()
+                    + "] Media Session - Success using "
+                    + NetTransferMode.JINGLETCP);
+                if (!isInitiator(jingleSession)) {
+
+                    try {
+                        final BinaryChannel channel = new BinaryChannel(
+                            newSession, getDefaultNetTransferMode());
+                        listener.connectionChanged(remoteJID,
+                            new BinaryChannelConnection(remoteJID, channel,
+                                listener));
+                    } catch (IOException e) {
+                        new CausedIOException("Could not connect to "
+                            + remoteJID.getName(), e);
+                    }
+
+                } else {
+                    sessions.put(remoteJID.toString(), newSession);
+                }
+            } else {
+                log.debug("Jingle [" + remoteJID.getName()
+                    + "] Media Session - Failure");
+            }
+            return newSession;
+        }
+
+        protected List<PayloadType> payloads = Collections
+            .singletonList((PayloadType) new PayloadType.Audio(333, "fileshare"));
+
+        @Override
+        public List<PayloadType> getPayloads() {
+            return payloads;
+        }
+
+        public boolean isInitiator(JingleSession session) {
+            return session.getInitiator().equals(
+                session.getConnection().getUser());
+        }
+    }
+
+    private Logger log = Logger.getLogger(JingleTransport.class);
+
+    protected IBytestreamConnectionListener listener;
+    protected XMPPConnection connection;
+    protected Saros saros;
+    protected JingleManager manager;
+    protected static JingleTransport instance;
+    private static final int timeout = 20;
+
+    private Map<String, JingleFileTransferSession> sessions = Collections
+        .synchronizedMap(new HashMap<String, JingleFileTransferSession>());
 
     private JingleTransport(Saros saros) {
         this.saros = saros;
@@ -47,72 +115,135 @@ public class JingleTransport implements ITransport {
         return instance;
     }
 
-    public IBytestreamConnection connect(final JID peer, SubMonitor progress)
-        throws IOException, InterruptedException {
+    public synchronized IBytestreamConnection connect(JID peer,
+        SubMonitor progress) throws IOException, InterruptedException {
 
         progress.subTask("Try to initiate bytestream with " + toString());
-        BinaryChannel channel = establishBinaryChannel(peer.toString(),
-            progress);
-        return new BinaryChannelConnection(peer, channel, connectionListener);
+
+        JingleSession out = null;
+        try {
+            out = manager.createOutgoingJingleSession(peer.toString());
+            out.startOutgoing();
+            waitForConnection(out);
+        } catch (XMPPException e) {
+            log.error("Could not connect to " + peer.toString());
+            return null;
+        }
+
+        int i = 0;
+
+        while (!sessions.containsKey(peer.toString()) && i < timeout) {
+            Thread.sleep(500);
+            i++;
+        }
+
+        final JingleFileTransferSession session = sessions
+            .get(peer.toString());
+
+        if (session == null)
+            throw new IOException("Could not connect to " + peer.toString());
+
+        sessions.remove(peer.toString());
+
+        return new BinaryChannelConnection(peer, new BinaryChannel(session,
+            this.getDefaultNetTransferMode()), listener);
+
+    }
+
+    public void waitForConnection(JingleSession session) {
+
+        InterruptedException interrupted = null;
+        int i = 0;
+
+        while ((session.getSessionState() == JingleSessionStatePending
+            .getInstance())
+            && i < timeout) {
+
+            if (i % 2 == 0)
+                log.debug("Waiting for connection since " + (i * 500) / 1000
+                    + "s");
+
+            i++;
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                interrupted = e;
+            }
+        }
+        if (interrupted != null) {
+            log.error("Code not designed to be interruptable", interrupted);
+            Thread.currentThread().interrupt();
+        }
     }
 
     public void disposeXMPPConnection() {
+
         if (manager != null) {
-            manager.removeIncomingJingleSessionListener(streamListener);
-            manager = null;
+            manager.removeJingleSessionRequestListener(jingleListener);
+            sessions.clear();
         }
-    }
-
-    protected JingleSessionRequestListener streamListener = new JingleSessionRequestListener() {
-        public void sessionRequested(JingleSessionRequest request) {
-            try {
-                JID jid = new JID(request.getFrom());
-                FileTransferConnection incoming = manager
-                    .getFileConnection(jid);
-                log.debug("Receiving Jingle Session Request from " + jid);
-
-                incoming = manager.new FileTransferConnection(jid);
-                manager.putConnection(incoming, jid);
-                incoming.setState(incoming.getState());
-
-                try {
-                    // Accept the call
-                    JingleSession session = request.accept();
-                    incoming.putSession(session);
-                    manager.initJingleListener(incoming, jid);
-                    session.startIncoming();
-                } catch (XMPPException e) {
-                    log.error("Failed to start JingleSession", e);
-                    incoming.putSession(null);
-                    incoming.setState(JingleConnectionState.ERROR);
-                }
-            } catch (Exception e) {
-                log.error("Starting Session failed: ", e);
-            }
-        }
-    };
-
-    protected BinaryChannel establishBinaryChannel(String peer,
-        SubMonitor progress) throws IOException {
-        BytestreamSession session = null;
-        try {
-            session = manager.establishSession(peer.toString());
-        } catch (Exception e) {
-            log.debug("Could not establish a JingleSession");
-        }
-        return new BinaryChannel(session, getDefaultNetTransferMode());
+        manager = null;
     }
 
     public void prepareXMPPConnection(XMPPConnection connection,
         IBytestreamConnectionListener listener) {
-        this.connectionListener = listener;
-        manager = getManager(connection);
-        manager.addIncomingBytestreamListener(streamListener);
+
+        this.listener = listener;
+        this.connection = connection;
+        this.getManager();
+        this.manager.addJingleSessionRequestListener(jingleListener);
     }
 
-    public JingleFileTransferManager getManager(XMPPConnection connection) {
-        return JingleFileTransferManager.getManager(saros, connection,
-            connectionListener);
+    private JingleSessionRequestListener jingleListener = new JingleSessionRequestListener() {
+
+        public void sessionRequested(JingleSessionRequest request) {
+
+            log.info("Received request to establish a "
+                + getDefaultNetTransferMode() + " bytestream connection from "
+                + request.getFrom());
+
+            try {
+
+                JingleSession incoming = request.accept();
+                incoming.startIncoming();
+
+            } catch (XMPPException e) {
+
+                log.error("Could not connect to " + request.getFrom());
+            }
+
+        }
+
+    };
+
+    private void getManager() {
+
+        log.info("Starting to initialize jingle.");
+
+        JingleManager.setJingleServiceEnabled();
+        IPreferenceStore prefStore = saros.getPreferenceStore();
+
+        final String stunServer = prefStore.getString(PreferenceConstants.STUN);
+        final int stunServerPort = Integer.parseInt(prefStore
+            .getString(PreferenceConstants.STUN_PORT));
+
+        final ICETransportManager icetm0 = new ICETransportManager(connection,
+            stunServer, stunServerPort);
+
+        // final STUNTransportManager sm = new STUNTransportManager();
+
+        final FileMediaManager mediaManager = new FileMediaManager(icetm0);
+
+        if (!connection.isConnected()) {
+            throw new RuntimeException(
+                "Jingle Manager could not be started because connection was closed in the meantime");
+        }
+
+        manager = new JingleManager(connection, Collections
+            .singletonList((JingleMediaManager) mediaManager));
+
+        log.debug("Initialized jingle.");
     }
 
     @Override
@@ -121,7 +252,7 @@ public class JingleTransport implements ITransport {
     }
 
     public NetTransferMode getDefaultNetTransferMode() {
-        return NetTransferMode.JINGLEUDP;
+        return NetTransferMode.JINGLETCP;
     }
 
 }
--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/jingle/JingleFileTransferSession.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/jingle/JingleFileTransferSession.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
@@ -20,246 +19,86 @@ import org.jivesoftware.smackx.jingle.JingleSession;
 import org.jivesoftware.smackx.jingle.media.JingleMediaSession;
 import org.jivesoftware.smackx.jingle.media.PayloadType;
 import org.jivesoftware.smackx.jingle.nat.TransportCandidate;
-import org.limewire.nio.NIODispatcher;
-import org.limewire.rudp.DefaultRUDPContext;
-import org.limewire.rudp.DefaultRUDPSettings;
-import org.limewire.rudp.DefaultUDPService;
-import org.limewire.rudp.RudpMessageDispatcher;
-import org.limewire.rudp.UDPMultiplexor;
-import org.limewire.rudp.UDPSelectorProvider;
-import org.limewire.rudp.messages.RUDPMessageFactory;
-import org.limewire.rudp.messages.impl.DefaultMessageFactory;
 
-import de.fu_berlin.inf.dpp.net.JID;
-import de.fu_berlin.inf.dpp.net.internal.IBytestreamConnectionListener;
-import de.fu_berlin.inf.dpp.net.internal.DataTransferManager.NetTransferMode;
 import de.fu_berlin.inf.dpp.util.NamedThreadFactory;
 import de.fu_berlin.inf.dpp.util.Util;
 
-/**
- * This class implements a file transfer session with jingle.
- * 
- * Jingle is a XMPP-extension with id XEP-0166. Documentation can be found at
- * http://xmpp.org/extensions/xep-0166.html .
- * 
- * This implementation uses TCP as transport protocol which fall back to UDP
- * when a TCP connection failed. To ensure no data loss when transmitting with
- * UDP the RUDP implementation from the Limewire project are used.
- * 
- * Documentation for the RUDP component from limewire can be found at:
- * http://wiki.limewire.org/index.php?title=Javadocs .
- * 
- * The TCP connection uses a bidirectional, Saros specific protocol.
- * 
- * @author chjacob
- * @author oezbek
- * @author sszuecs
- */
 public class JingleFileTransferSession extends JingleMediaSession implements
     BytestreamSession {
 
-    private static final Logger log = Logger
-        .getLogger(JingleFileTransferSession.class);
+    private static final long TIMEOUTSECONDS = 30;
+    protected Logger log = Logger.getLogger(JingleFileTransferSession.class);
+    public Socket socket;
 
-    public static final int TIMEOUTSECONDS = 30;
+    protected String localIP;
+    protected int localPort;
+    protected String remoteIP;
+    protected int remotePort;
 
-    protected IBytestreamConnectionListener connectionListener;
-
-    private UDPSelectorProvider udpSelectorProvider;
-
-    private String remoteIp;
-    private String localIp;
-    private int localPort;
-    private int remotePort;
-    private Socket socket;
-    private JID connectTo;
-
-    /**
-     * Create AND initialize a new JingleFileTransferSession. This includes
-     * connecting to given remote side (if initiator).
-     * 
-     * @param payloadType
-     *            Our PayloadType, which identifies which packets we handle.
-     *            Smack takes care of this.
-     * @param remote
-     *            The remote IP and port suggested by Jingle to us.
-     * @param local
-     *            Our own IP and port suggested by Jingle to us.
-     * @param mediaLocator
-     *            MediaLocator to pass to super()
-     * @param jingleSession
-     *            may be null, the existing JingleSession which we are a part
-     *            of.
-     * @param connectionListener
-     *            We will notify this dtm if we receive files from the remote
-     *            side.
-     * @param connectTo
-     *            The JID of the user we are connecting to via this transfer
-     *            session.
-     */
     public JingleFileTransferSession(PayloadType payloadType,
         TransportCandidate remote, TransportCandidate local,
-        String mediaLocator, JingleSession jingleSession,
-        IBytestreamConnectionListener connectionListener, JID connectTo) {
+        String mediaLocator, JingleSession jingleSession) {
         super(payloadType, remote, local, mediaLocator, jingleSession);
 
-        this.connectTo = connectTo;
-        this.connectionListener = connectionListener;
         this.initialize();
+
     }
 
-    /**
-     * Initialization of the session. It tries to create sockets for both, TCP
-     * and UDP. The UDP Socket is a reliable implementation from the Limewire
-     * project. Documentation can be found at http://wiki.limewire.org.
-     */
     @Override
-    public synchronized void initialize() {
+    public void initialize() {
 
-        if (this.getLocal().getSymmetric() != null) {
+        this.localIP = this.getLocal().getIp();
+        this.localPort = this.getLocal().getPort();
 
-            // A Symmetric connection is one where a RTPBridge is used as a
-            // relay
-
-            localIp = this.getLocal().getLocalIp();
-            localPort = Util.getFreePort();
-
-            // Since we want to establish a TCP Connection, doing the relay
-            // would be impractical anyway
-            remoteIp = this.getLocal().getIp();
-            remotePort = this.getLocal().getSymmetric().getPort();
-
-            log.warn("Symmetric Connection using RTPBridge is not supported!!"
-                + " Attempting anyway: Jingle [" + connectTo.getName()
-                + "] Symmetric IPs - local: " + localIp + ":" + localPort
-                + " -> remote: " + remoteIp + ":" + remotePort);
-
-        } else {
-            localIp = this.getLocal().getLocalIp();
-            localPort = this.getLocal().getPort();
-
-            remoteIp = this.getRemote().getIp();
-            remotePort = this.getRemote().getPort();
-
-            log.info("Jingle [" + connectTo.getName()
-                + "] Not Symmetric IPs - local: " + localIp + ":" + localPort
-                + " <-> remote: " + remoteIp + ":" + remotePort);
-        }
-
-        createRUDP();
+        this.remoteIP = this.getRemote().getIp();
+        this.remotePort = this.getRemote().getPort();
 
         if (isInitiator()) {
             initializeAsServer();
         } else {
             initializeAsClient();
         }
+
     }
 
-    private boolean isInitiator() {
-        return getJingleSession().getInitiator().equals(
-            getJingleSession().getConnection().getUser());
-    }
+    private void initializeAsClient() {
 
-    private void createRUDP() {
-
-        RudpMessageDispatcher dispatcher = new RudpMessageDispatcher();
-        DefaultUDPService service = new DefaultUDPService(dispatcher);
-        RUDPMessageFactory factory = new DefaultMessageFactory();
-        udpSelectorProvider = new UDPSelectorProvider(new DefaultRUDPContext(
-            factory, NIODispatcher.instance().getTransportListener(), service,
-            new DefaultRUDPSettings()));
-        UDPMultiplexor udpMultiplexor = udpSelectorProvider.openSelector();
-        dispatcher.setUDPMultiplexor(udpMultiplexor);
-        NIODispatcher.instance().registerSelector(udpMultiplexor,
-            udpSelectorProvider.getUDPSocketChannelClass());
-        try {
-            service.start(localPort);
-        } catch (IOException e) {
-            log.error("Jingle [" + connectTo.getName()
-                + "] Failed to create RUDP service");
-        }
-    }
-
-    protected void initializeAsClient() {
-
-        SocketCreator creator = SocketCreator.getWrapped(
-            NetTransferMode.JINGLETCP, Util.retryEveryXms(
-                new Callable<Socket>() {
-                    public Socket call() throws Exception {
-                        log.debug("Jingle/TCP connection attempt to "
-                            + remoteIp + ":" + remotePort);
-                        try {
-                            return new Socket(remoteIp, remotePort);
-                        } catch (SocketException e) {
-                            log
-                                .debug("Jingle/TCP connection attempt FAILED to "
-                                    + remoteIp
-                                    + ":"
-                                    + remotePort
-                                    + " - "
-                                    + e.getMessage());
-                            throw e;
-                        }
-                    }
-                }, 1000));
-        connect(creator);
-    }
-
-    protected void initializeAsServer() {
-
-        SocketCreator creator = new SocketCreator(NetTransferMode.JINGLETCP) {
-
+        Callable<Socket> toExecute = Util.retryEveryXms(new Callable<Socket>() {
             public Socket call() throws Exception {
+                log.debug("Jingle/TCP connection attempt to " + remoteIP + ":"
+                    + remotePort);
+                return new Socket(remoteIP, remotePort);
+            }
+        }, 1000);
 
+        connect(toExecute);
+    }
+
+    private void initializeAsServer() {
+
+        Callable<Socket> toExecute = new Callable<Socket>() {
+            public Socket call() throws Exception {
                 log.debug("Starting Jingle/TCP Server Socket on port "
                     + localPort);
-                try {
-                    ServerSocket serverSocket = new ServerSocket(localPort);
-                    return serverSocket.accept();
-                } catch (SocketException e) {
-                    log.warn("Jingle/TCP Server Socket on port " + localPort
-                        + " did not receive a connection attempt within 30s: "
-                        + e.getMessage());
-                    throw e;
-                }
+                ServerSocket socket = new ServerSocket(localPort);
+                return socket.accept();
             }
+
         };
-        connect(creator);
-
+        connect(toExecute);
     }
 
-    protected abstract static class SocketCreator implements Callable<Socket> {
-
-        protected SocketCreator(NetTransferMode type) {
-            this.type = type;
-        }
-
-        protected NetTransferMode type;
-
-        public NetTransferMode getType() {
-            return this.type;
-        }
-
-        public static SocketCreator getWrapped(NetTransferMode type,
-            final Callable<Socket> callable) {
-            return new SocketCreator(type) {
-                public Socket call() throws Exception {
-                    return callable.call();
-                }
-            };
-        }
-    }
-
-    protected void connect(SocketCreator creator) {
+    private void connect(Callable<Socket> service) {
 
         ExecutorService thread = Executors
             .newSingleThreadExecutor(new NamedThreadFactory("Jingle-Connect-"
-                + connectTo.getName() + "-"));
+                + this.getJingleSession().getResponder() + "-"));
 
         try {
             ExecutorCompletionService<Socket> completionService = new ExecutorCompletionService<Socket>(
                 thread);
-            Future<Socket> future = completionService.submit(creator);
+
+            Future<Socket> future = completionService.submit(service);
             Future<Socket> socketFuture = null;
             try {
                 socketFuture = completionService.poll(TIMEOUTSECONDS,
@@ -271,7 +110,7 @@ public class JingleFileTransferSession extends JingleMediaSession implements
             }
 
             if (socketFuture == null) {
-                log.debug("Jingle [" + connectTo.getName()
+                log.debug("Jingle [" + getJID()
                     + "] Could not connect with TCP.");
 
                 return;
@@ -286,7 +125,7 @@ public class JingleFileTransferSession extends JingleMediaSession implements
                 Thread.currentThread().interrupt();
                 return;
             } catch (ExecutionException e) {
-                log.debug("Jingle [" + connectTo.getName()
+                log.debug("Jingle [" + getJID()
                     + "] Could not connect with TCP.");
 
             }
@@ -299,63 +138,68 @@ public class JingleFileTransferSession extends JingleMediaSession implements
         } finally {
             thread.shutdown();
         }
+
     }
 
-    /**
-     * This method is called from Jingle AFTER a jingle session is established.
-     * We thus could start sending here, but we want that others call us using
-     * send.
-     */
-    @Override
-    public void startTrasmit() {
-        // Do nothing -> Users should call send(...) directly
+    protected String getJID() {
+        if (isInitiator())
+            return this.getJingleSession().getInitiator();
+        return this.getJingleSession().getResponder();
     }
 
-    @Override
-    public void stopTrasmit() {
-        // Do nothing -> Users should call send(...) directly
+    private boolean isInitiator() {
+        return getJingleSession().getInitiator().equals(
+            getJingleSession().getConnection().getUser());
     }
 
     @Override
     public void setTrasmit(boolean active) {
         log.error("Unexpected call to setTrasmit(active ==" + active + ")");
+
     }
 
-    /**
-     * This method is called from Jingle AFTER a jingle session is established.
-     * Since we want others to call us, we need to be ready for transmitting
-     * before this
-     */
     @Override
     public void startReceive() {
-        // do nothing.
+        // Do nothing -> Users should call send(...) directly
+
+    }
+
+    @Override
+    public void startTrasmit() {
+        // TODO Auto-generated method stub
+
     }
 
     @Override
     public void stopReceive() {
-        close();
+        // Do nothing -> Users should call send(...) directly
     }
 
-    public void close() {
-        Util.close(this.socket);
+    @Override
+    public void stopTrasmit() {
+        // Do nothing
 
+    }
+
+    // Bytestream Methods
+    public void close() throws IOException {
+        Util.close(socket);
     }
 
     public InputStream getInputStream() throws IOException {
-        return this.socket.getInputStream();
+        return socket.getInputStream();
     }
 
     public OutputStream getOutputStream() throws IOException {
-        return this.socket.getOutputStream();
+        return socket.getOutputStream();
     }
 
     public int getReadTimeout() throws IOException {
-        return this.socket.getSoTimeout();
+        return socket.getSoTimeout();
     }
 
     public void setReadTimeout(int timeout) throws IOException {
-        this.socket.setSoTimeout(timeout);
-
+        socket.setSoTimeout(timeout);
     }
 
     public boolean isConnected() {
