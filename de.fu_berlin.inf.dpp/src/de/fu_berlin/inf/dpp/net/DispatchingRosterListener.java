409ca15ac0a647292d3fca4af7b2f8798b98bb82
A. Haferburg <ahaferburg@users.sourceforge.net>
2010-06-11 10:37:10 +0000

    Merged in changes to trunk up to revision 2227.

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/DispatchingRosterListener.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/DispatchingRosterListener.java
@@ -4,115 +4,96 @@
 package de.fu_berlin.inf.dpp.net;
 
 import java.util.Collection;
-import java.util.TreeSet;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.jivesoftware.smack.Roster;
 import org.jivesoftware.smack.packet.Presence;
 
+/**
+ * Dispatches {@link IRosterListener} events to registered listeners. Listeners
+ * get notified according to the order induced by
+ * {@link PrecedenceRosterListenerComparator}.<br>
+ * <br>
+ * Concurrency: If an event is being dispatched while a listener is added or
+ * removed concurrently, the listener may or may not get notified. Most notably,
+ * a listener should be prepared to get notified even after it was removed from
+ * a DispatchingRosterListener.
+ */
 public class DispatchingRosterListener implements IRosterListener {
-
-    protected TreeSet<IRosterListener> listeners = new TreeSet<IRosterListener>(
-        new PrecedenceRosterListenerComparator());
-
-    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-    private ReadLock readLock = lock.readLock();
-    private WriteLock writeLock = lock.writeLock();
+    protected List<IRosterListener> listeners = new CopyOnWriteArrayList<IRosterListener>();
+    protected Comparator<IRosterListener> listenerComparator = new PrecedenceRosterListenerComparator();
 
     public void entriesAdded(Collection<String> addresses) {
-        readLock.lock();
-        try {
-            for (IRosterListener listener : this.listeners) {
-                try {
-                    listener.entriesAdded(addresses);
-                } catch (RuntimeException e) {
-                    RosterTracker.log.error("Internal error:", e);
-                }
+        for (IRosterListener listener : this.listeners) {
+            try {
+                listener.entriesAdded(addresses);
+            } catch (RuntimeException e) {
+                RosterTracker.log.error("Internal error:", e);
             }
-        } finally {
-            readLock.unlock();
         }
     }
 
     public void entriesUpdated(Collection<String> addresses) {
-        readLock.lock();
-        try {
-            for (IRosterListener listener : this.listeners) {
-                try {
-                    listener.entriesUpdated(addresses);
-                } catch (RuntimeException e) {
-                    RosterTracker.log.error("Internal error:", e);
-                }
+        for (IRosterListener listener : this.listeners) {
+            try {
+                listener.entriesUpdated(addresses);
+            } catch (RuntimeException e) {
+                RosterTracker.log.error("Internal error:", e);
             }
-        } finally {
-            readLock.unlock();
         }
     }
 
     public void entriesDeleted(Collection<String> addresses) {
-        readLock.lock();
-        try {
-            for (IRosterListener listener : this.listeners) {
-                try {
-                    listener.entriesDeleted(addresses);
-                } catch (RuntimeException e) {
-                    RosterTracker.log.error("Internal error:", e);
-                }
+        for (IRosterListener listener : this.listeners) {
+            try {
+                listener.entriesDeleted(addresses);
+            } catch (RuntimeException e) {
+                RosterTracker.log.error("Internal error:", e);
             }
-        } finally {
-            readLock.unlock();
         }
     }
 
     public void presenceChanged(Presence presence) {
-        readLock.lock();
-        try {
-            for (IRosterListener listener : this.listeners) {
-                try {
-                    listener.presenceChanged(presence);
-                } catch (RuntimeException e) {
-                    RosterTracker.log.error("Internal error:", e);
-                }
+        for (IRosterListener listener : this.listeners) {
+            try {
+                listener.presenceChanged(presence);
+            } catch (RuntimeException e) {
+                RosterTracker.log.error("Internal error:", e);
             }
-        } finally {
-            readLock.unlock();
         }
     }
 
     public void rosterChanged(Roster roster) {
-        readLock.lock();
-        try {
-            for (IRosterListener listener : this.listeners) {
-                try {
-                    listener.rosterChanged(roster);
-                } catch (RuntimeException e) {
-                    RosterTracker.log.error("Internal error:", e);
-                }
+        for (IRosterListener listener : this.listeners) {
+            try {
+                listener.rosterChanged(roster);
+            } catch (RuntimeException e) {
+                RosterTracker.log.error("Internal error:", e);
             }
-        } finally {
-            readLock.unlock();
         }
     }
 
     public void add(IRosterListener rosterListener) {
-        writeLock.lock();
-        try {
+        // Synchronize to avoid two threads adding the same listener.
+        synchronized (this) {
             if (!listeners.contains(rosterListener)) {
-                listeners.add(rosterListener);
+                // A CopyOnWriteArrayList doesn't handle sorting, so we have to
+                // find the index ourselves.
+                int index = 0;
+                for (IRosterListener listener : this.listeners) {
+                    if (listenerComparator.compare(rosterListener, listener) < 0)
+                        index++;
+                    else
+                        break;
+                }
+                listeners.add(index, rosterListener);
             }
-        } finally {
-            writeLock.unlock();
         }
     }
 
     public void remove(IRosterListener rosterListener) {
-        writeLock.lock();
-        try {
-            listeners.remove(rosterListener);
-        } finally {
-            writeLock.unlock();
-        }
+        listeners.remove(rosterListener);
     }
 }
