60c165c1258914ba518d7f5f4aeb750770b0bb6a
Karl Beecher <k_beecher@users.sourceforge.net>
2010-06-17 15:38:20 +0000

    [INTERNAL] Optimisation of FolderActivities

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/activities/serializable/FolderActivityDataObject.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/activities/serializable/FolderActivityDataObject.java
@@ -37,6 +37,23 @@ public class FolderActivityDataObject extends AbstractActivityDataObject
         return null;
     }
 
+    /**
+     * Tests whether this FolderActivity operates on a folder that is the child
+     * of another in the same project.
+     * 
+     * @param other
+     *            The other folder to be tested against.
+     * @return <code>true</code> if <code>other</code> operates on a parent
+     *         folder in the same project as this activity's folder.
+     */
+    public boolean isChildOf(FolderActivityDataObject other) {
+        String thisProjectID = this.path.getProjectID();
+        String otherProjectID = other.path.getProjectID();
+
+        return thisProjectID.equals(otherProjectID)
+            && other.getPath().getIPath().isPrefixOf(this.path.getIPath());
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
