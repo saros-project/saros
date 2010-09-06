56d2ffd5f784754c10a70c0f500047d830d2c455
nudelfish <nudelfish@users.sourceforge.net>
2010-09-06 12:43:12 +0000

    STF_Junittests can be run locally on a computer, but now you need to prepare something before you start a junittest. First you need to manually modify the run-configurations of the saros-instances and then start them with testmode, which let the saros-inssancs to export RMI objects and start RMI Registry .The use of RMI requires parameter to define the codebasis, start SecurityManager and set the certain policy from the given policy file.
    Now i created three launch configurations, which is suitable for stf-testing and with which you don't need to configure the runconfiguration yourself.  They are:
    Saros_STF_Bob.launch
    Saros_STF_Alice.launch
    Saros_STF_Carl.launch
    
    The needed policy file is located in Directory [Saros-root]/STF
    
    The file "MyClass.java" located in Directory [Saros-root]/STF is used by STF.

---
+++ de.fu_berlin.inf.dpp/test/STF/MyClass.java
@@ -0,0 +1,11 @@
+package my.pkg;
+
+public class MyClass {
+
+        public static void main(String args[]) {
+        
+                System.out.println("This is my class.");
+        }
+        
+        
+}
