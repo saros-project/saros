72fb9b6e9006ebf7927977e371c5d6e9e81f2c2b
A. Haferburg <ahaferburg@users.sourceforge.net>
2010-09-13 15:35:55 +0000

    [INTERNAL] Added console listener to see how it works.

---
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/vcs/SubclipseConsoleListener.java
@@ -0,0 +1,52 @@
+package de.fu_berlin.inf.dpp.vcs;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.console.IPatternMatchListenerDelegate;
+import org.eclipse.ui.console.PatternMatchEvent;
+import org.eclipse.ui.console.TextConsole;
+
+public class SubclipseConsoleListener implements IPatternMatchListenerDelegate {
+    private static final Logger log = Logger
+        .getLogger(SubclipseConsoleListener.class);
+
+    TextConsole console;
+
+    public void connect(TextConsole console) {
+        log.trace("SVN console found");
+        this.console = console;
+    }
+
+    public void disconnect() {
+        log.trace("SVN console lost");
+        this.console = null;
+    }
+
+    List<String> operations;
+    {
+        String[] array = { "switch", "update", "checkout", };
+        operations = Arrays.asList(array);
+    }
+
+    public void matchFound(PatternMatchEvent event) {
+        log.trace("SVN console match");
+        IDocument document = console.getDocument();
+        int offset = event.getOffset();
+        int length = event.getLength();
+        try {
+            if (document.getChar(offset) == ' ')
+                return;
+            String match = document.get(offset, length);
+            String firstword = match.split(" ", 2)[0].trim();
+            if (operations.contains(firstword)) {
+                log.trace(match);
+            }
+        } catch (BadLocationException e) {
+            log.debug("", e);
+        }
+    }
+}
