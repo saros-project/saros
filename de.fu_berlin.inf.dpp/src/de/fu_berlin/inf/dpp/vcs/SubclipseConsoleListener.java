d2b82d3674879a54582b9b2e84bc1364c6cf78da
A. Haferburg <ahaferburg@users.sourceforge.net>
2010-10-11 09:12:58 +0000

    [DOCU] Updated SubclipseConsoleListener JavaDoc.

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/vcs/SubclipseConsoleListener.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/vcs/SubclipseConsoleListener.java
@@ -11,9 +11,9 @@ import org.eclipse.ui.console.PatternMatchEvent;
 import org.eclipse.ui.console.TextConsole;
 
 /**
- * SubclipseConsoleListener which doesn't do anything yet. Could be used to
- * parse SVN operations on drivers in order to send VCSActivities as soon as the
- * operation is started.
+ * A console listener for the SVN console, which doesn't do anything but logging
+ * yet. Could be used to parse SVN operations on drivers in order to send
+ * VCSActivities as soon as the operation is started.
  */
 public class SubclipseConsoleListener implements IPatternMatchListenerDelegate {
     private static final Logger log = Logger
@@ -38,9 +38,9 @@ public class SubclipseConsoleListener implements IPatternMatchListenerDelegate {
     }
 
     public void matchFound(PatternMatchEvent event) {
+        // TODO We could get old events here.
         if (!log.isTraceEnabled())
             return;
-        log.trace("SVN console match");
         IDocument document = console.getDocument();
         int offset = event.getOffset();
         int length = event.getLength();
