0b57aec586ceac4c191615dfef19b28affbf4578
Karl Beecher <k_beecher@users.sourceforge.net>
2011-01-07 12:22:53 +0000

    [BUILD] Changes necessary for Release 11.1.7.r2897

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/VersionManager.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/VersionManager.java
@@ -134,6 +134,14 @@ public class VersionManager {
     static {
 
         /**
+         * Version 11.1.7.r2897
+         */
+        compatibilityChart.put(new Version("11.1.7.r2897"), Arrays.asList(
+            new Version("11.1.7.r2897"), new Version("10.11.26.r2744"),
+            new Version("10.10.29.r2640"), new Version("10.10.01.r2552"),
+            new Version("10.8.27.r2333"), new Version("10.7.30.r2310")));
+
+        /**
          * Version 10.11.26.r2744
          */
         compatibilityChart.put(new Version("10.11.26.r2744"), Arrays.asList(
