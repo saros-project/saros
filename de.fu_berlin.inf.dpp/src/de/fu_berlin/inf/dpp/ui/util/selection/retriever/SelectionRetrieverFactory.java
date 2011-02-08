cf668fa4496ea2948cbe125de71d50dc74d3332f
Björn Kahlert <bkahlert@users.sourceforge.net>
2011-02-08 21:31:01 +0000

    [JAVADOC] Added examples for usage of SelectionRetrieverFactory [TRIVIAL] Added a flag so the caller can choose whether the returned SelectionRetriever is "smart" or not

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/util/selection/retriever/SelectionRetrieverFactory.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/util/selection/retriever/SelectionRetrieverFactory.java
@@ -13,15 +13,49 @@ import de.fu_berlin.inf.dpp.ui.util.selection.retriever.impl.SelectionRetriever;
  * capable of filtering the {@link IWorkbenchPart}'s {@link ISelection}s.<br/>
  * The filtered selection consists only of objects that can adapt to
  * AdapterType.
+ * <p>
+ * For example,
+ * 
+ * <pre>
+ * List&lt;JID&gt; buddies = SelectionRetrieverFactory.getSelectionRetriever(JID.class)
+ *     .getSelection();
+ * </pre>
+ * 
+ * or,
+ * 
+ * <pre>
+ * List&lt;IProject&gt; projects = SelectionRetrieverFactory.getSelectionRetriever(
+ *     IProject.class).getOverallSelection();
+ * </pre>
  * 
  * @author bkahlert
  */
 public class SelectionRetrieverFactory {
 
+    /**
+     * Returns a {@link SelectionRetriever} capable of retrieving selections of
+     * a specific type.
+     * <p>
+     * Generally the returned {@link SelectionRetriever} is straight forward. If
+     * smart is set to true an "intelligent" {@link SelectionRetriever} is
+     * returned.
+     * <p>
+     * In case of {@link IProject}.class the smart {@link SelectionRetriever}
+     * does not only return directly selected projects but also the projects,
+     * selected resources (like source folder and files) belong to.
+     * 
+     * @param <AdapterType>
+     *            type of the returned list
+     * @param adapter
+     *            type the selections should be adapted to
+     * @param smart
+     *            if true, returns an "intelligent" {@link SelectionRetriever}
+     * @return
+     */
     static public <AdapterType> ISelectionRetriever<AdapterType> getSelectionRetriever(
-        Class<? extends AdapterType> adapter) {
+        Class<? extends AdapterType> adapter, boolean smart) {
 
-        if (adapter.equals(IProject.class)) {
+        if (smart && adapter.equals(IProject.class)) {
             /*
              * If looking for projects we also want to find them in case a
              * project's resource was selected.
@@ -43,4 +77,24 @@ public class SelectionRetrieverFactory {
         }
     }
 
+    /**
+     * Returns a smart {@link SelectionRetriever} capable of retrieving
+     * selections of a specific type.
+     * <p>
+     * Generally the returned {@link SelectionRetriever} is straight forward.
+     * <p>
+     * In case of {@link IProject}.class the smart {@link SelectionRetriever}
+     * does not only return directly selected projects but also the projects,
+     * selected resources (like source folder and files) belong to.
+     * 
+     * @param <AdapterType>
+     *            type of the returned list
+     * @param adapter
+     *            type the selections should be adapted to
+     * @return
+     */
+    static public <AdapterType> ISelectionRetriever<AdapterType> getSelectionRetriever(
+        Class<? extends AdapterType> adapter) {
+        return getSelectionRetriever(adapter, true);
+    }
 }
