162673668836a7b1a2eca4ccd27894ddf795debd
Björn Kahlert <bkahlert@users.sourceforge.net>
2011-02-15 11:55:11 +0000

    [REFACTOR] Refactored the top-level Saros menu (incl. new icons) ~50 LOC

---
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/commandHandlers/CreateXMPPAccountHandler.java
@@ -0,0 +1,48 @@
+package de.fu_berlin.inf.dpp.ui.commandHandlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.jivesoftware.smack.Roster;
+import org.picocontainer.annotations.Inject;
+
+import de.fu_berlin.inf.dpp.Saros;
+import de.fu_berlin.inf.dpp.preferences.PreferenceUtils;
+import de.fu_berlin.inf.dpp.ui.SarosUI;
+import de.fu_berlin.inf.dpp.ui.wizards.CreateXMPPAccountWizard;
+
+public class CreateXMPPAccountHandler extends AbstractHandler {
+
+    @Inject
+    protected Saros saros;
+
+    @Inject
+    protected SarosUI sarosUI;
+
+    @Inject
+    protected PreferenceUtils preferenceUtils;
+
+    public CreateXMPPAccountHandler() {
+        Saros.injectDependenciesOnly(this);
+    }
+
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        WizardDialog wd = new WizardDialog(null, new CreateXMPPAccountWizard(
+            saros, preferenceUtils, true, true, false));
+        wd.setHelpAvailable(false);
+        // open() blocks until the user closed the dialog
+        wd.open();
+        /*
+         * activate Roster and bring it to front (if not done already) to let
+         * user start inviting buddies
+         */
+        sarosUI.activateRosterView();
+        Roster sarosRoster = saros.getRoster();
+        if (sarosRoster != null)
+            sarosRoster.reload();
+
+        return null;
+    }
+
+}
