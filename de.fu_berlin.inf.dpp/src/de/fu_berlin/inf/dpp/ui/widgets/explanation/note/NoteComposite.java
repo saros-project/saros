f6ce0d3171e3de87b35afdd0d47655bdf66d0d91
Björn Kahlert <bkahlert@users.sourceforge.net>
2011-02-19 03:56:23 +0000

    [JavaDoc] Added class description [Trivial] Added delegation methods

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/widgets/explanation/note/NoteComposite.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/widgets/explanation/note/NoteComposite.java
@@ -2,15 +2,36 @@ package de.fu_berlin.inf.dpp.ui.widgets.explanation.note;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Layout;
 
 import de.fu_berlin.inf.dpp.ui.util.LayoutUtils;
 import de.fu_berlin.inf.dpp.ui.widgets.RoundedComposite;
+import de.fu_berlin.inf.dpp.ui.widgets.explanation.ExplanationComposite;
 import de.fu_berlin.inf.dpp.ui.widgets.explanation.SimpleExplanationComposite;
 import de.fu_berlin.inf.dpp.ui.widgets.explanation.SimpleExplanationComposite.SimpleExplanation;
 
+/**
+ * A {@link RoundedComposite} that contains a {@link SimpleExplanationComposite}
+ * .
+ * <p>
+ * There are 2 possibilities to set the content:
+ * <ol>
+ * <li>For textual content use {@link #setExplanation(SimpleExplanation)}</li>
+ * <li>For complex content use
+ * <ol>
+ * <li>{@link #setExplanationImage(int)} or {@link #setExplanationImage(Image)}
+ * to set the image</li>
+ * <li>
+ * {@link #getClientComposite()} to get the {@link Composite} that can exactly
+ * hold one {@link Composite} containing your explanation.</li>
+ * </ol>
+ * </li>
+ * </ol>
+ */
 public class NoteComposite extends RoundedComposite {
 
     public static final int SPACING = 10;
@@ -37,6 +58,33 @@ public class NoteComposite extends RoundedComposite {
     public void setExplanation(SimpleExplanation simpleExplanation) {
         this.explanationComposite.setExplanation(simpleExplanation);
         this.layout();
+    }
+
+    /**
+     * Returns the {@link Composite} that is intended to <b>exactly one</b>
+     * child {@link Control}.
+     * <p>
+     * If you only wish to set textual content, please use
+     * {@link #setExplanation(SimpleExplanation)}.
+     * 
+     * @return
+     */
+    public Composite getClientComposite() {
+        return this.explanationComposite;
+    }
+
+    /**
+     * @see ExplanationComposite#setExplanationImage(int)
+     */
+    public void setExplanationImage(int systemImage) {
+        this.explanationComposite.setExplanationImage(systemImage);
+    }
+
+    /**
+     * @see ExplanationComposite#setExplanationImage(Image)
+     */
+    public void setExplanationImage(Image explanationImage) {
+        this.explanationComposite.setExplanationImage(explanationImage);
     }
 
     @Override
