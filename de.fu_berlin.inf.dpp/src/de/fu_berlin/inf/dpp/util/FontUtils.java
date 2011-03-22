8594e8436673889984a087ef72b464a22aea3c04
Björn Kahlert <bkahlert@users.sourceforge.net>
2011-03-22 02:35:35 +0000

    [TRIVIAL] getFont() is defined on Control (and not only on Label)

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/FontUtils.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/FontUtils.java
@@ -5,7 +5,7 @@ import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Control;
 
 public class FontUtils {
 
@@ -13,33 +13,33 @@ public class FontUtils {
         // no instantiation allowed
     }
 
-    public static void changeFontSizeBy(Label label, int fontSizeInc) {
-        FontData[] fontData = label.getFont().getFontData();
+    public static void changeFontSizeBy(Control control, int fontSizeInc) {
+        FontData[] fontData = control.getFont().getFontData();
         for (int i = 0; i < fontData.length; ++i)
             fontData[i].setHeight(fontData[i].getHeight() + fontSizeInc);
 
-        final Font newFont = new Font(label.getDisplay(), fontData);
-        label.setFont(newFont);
+        final Font newFont = new Font(control.getDisplay(), fontData);
+        control.setFont(newFont);
 
         // Since you created the font, you must dispose it
-        label.addDisposeListener(new DisposeListener() {
+        control.addDisposeListener(new DisposeListener() {
             public void widgetDisposed(DisposeEvent e) {
                 newFont.dispose();
             }
         });
     }
 
-    public static void makeBold(Label label) {
-        FontData[] fontData = label.getFont().getFontData();
+    public static void makeBold(Control control) {
+        FontData[] fontData = control.getFont().getFontData();
         for (FontData fontData_ : fontData) {
             fontData_.setStyle(SWT.BOLD);
         }
 
-        final Font newFont = new Font(label.getDisplay(), fontData);
-        label.setFont(newFont);
+        final Font newFont = new Font(control.getDisplay(), fontData);
+        control.setFont(newFont);
 
         // Since you created the font, you must dispose it
-        label.addDisposeListener(new DisposeListener() {
+        control.addDisposeListener(new DisposeListener() {
             public void widgetDisposed(DisposeEvent e) {
                 newFont.dispose();
             }
