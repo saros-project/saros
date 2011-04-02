9c46b93c13ff298931707f48d3c7b0efd2e7bdce
Björn Kahlert <bkahlert@users.sourceforge.net>
2011-04-02 04:22:33 +0000

    [TRIVIAL] Explicitly set IMBeepAction enabled

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/actions/IMBeepAction.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/actions/IMBeepAction.java
@@ -9,51 +9,52 @@ import de.fu_berlin.inf.dpp.preferences.PreferenceConstants;
 import de.fu_berlin.inf.dpp.ui.ImageManager;
 
 public class IMBeepAction extends Action {
-    public IMBeepAction() {
-        super("Toggle beep");
-        this.updateIcon();
-        this.updateTooltip();
-    }
+	public IMBeepAction() {
+		super("Toggle beep");
+		this.updateIcon();
+		this.updateTooltip();
+		this.setEnabled(true);
+	}
 
-    @Override
-    public void run() {
-        PlatformUI.getPreferenceStore();
+	@Override
+	public void run() {
+		PlatformUI.getPreferenceStore();
 
-        this.setOn(!this.isOn());
-        this.updateIcon();
-    }
+		this.setOn(!this.isOn());
+		this.updateIcon();
+	}
 
-    protected boolean isOn() {
-        return PlatformUI.getPreferenceStore().getBoolean(
-            PreferenceConstants.BEEP_UPON_IM);
-    }
+	protected boolean isOn() {
+		return PlatformUI.getPreferenceStore().getBoolean(
+				PreferenceConstants.BEEP_UPON_IM);
+	}
 
-    protected void setOn(boolean on) {
-        PlatformUI.getPreferenceStore().setValue(
-            PreferenceConstants.BEEP_UPON_IM, on);
+	protected void setOn(boolean on) {
+		PlatformUI.getPreferenceStore().setValue(
+				PreferenceConstants.BEEP_UPON_IM, on);
 
-        updateTooltip();
-    }
+		updateTooltip();
+	}
 
-    public void beep() {
-        if (this.isOn())
-            Toolkit.getDefaultToolkit().beep();
-    }
+	public void beep() {
+		if (this.isOn())
+			Toolkit.getDefaultToolkit().beep();
+	}
 
-    public void updateIcon() {
-        if (this.isOn()) {
-            this.setImageDescriptor(ImageManager
-                .getImageDescriptor("/icons/elcl16/speakeron.png"));
-        } else {
-            this.setImageDescriptor(ImageManager
-                .getImageDescriptor("/icons/elcl16/speakeroff.png"));
-        }
-    }
+	public void updateIcon() {
+		if (this.isOn()) {
+			this.setImageDescriptor(ImageManager
+					.getImageDescriptor("/icons/elcl16/speakeron.png"));
+		} else {
+			this.setImageDescriptor(ImageManager
+					.getImageDescriptor("/icons/elcl16/speakeroff.png"));
+		}
+	}
 
-    protected void updateTooltip() {
-        if (this.isOn())
-            setToolTipText("Turn beep notificaton off.");
-        else
-            setToolTipText("Turn beep notification on.");
-    }
+	protected void updateTooltip() {
+		if (this.isOn())
+			setToolTipText("Turn beep notificaton off.");
+		else
+			setToolTipText("Turn beep notification on.");
+	}
 }
