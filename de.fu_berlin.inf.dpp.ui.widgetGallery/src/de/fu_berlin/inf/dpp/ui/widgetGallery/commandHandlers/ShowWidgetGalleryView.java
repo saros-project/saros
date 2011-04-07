dbc2ccdc1cf250f216e33dd15a23976697aa6568
Björn Kahlert <bkahlert@users.sourceforge.net>
2011-04-07 11:35:30 +0000

    [FEATURE] SarosView provides a toolbar button to show the widget gallery - only visible if Saros has been startet with plugin "...dpp.ui.widgetGallery"

---
+++ de.fu_berlin.inf.dpp.ui.widgetGallery/src/de/fu_berlin/inf/dpp/ui/widgetGallery/commandHandlers/ShowWidgetGalleryView.java
@@ -0,0 +1,28 @@
+package de.fu_berlin.inf.dpp.ui.widgetGallery.commandHandlers;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
+import de.fu_berlin.inf.dpp.ui.widgetGallery.views.WidgetGalleryView;
+
+public class ShowWidgetGalleryView extends AbstractHandler {
+
+    protected final Logger log = Logger.getLogger(ShowWidgetGalleryView.class);
+
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+	try {
+	    PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+		    .getActivePage().showView(WidgetGalleryView.ID);
+	} catch (PartInitException e) {
+	    log.error(
+		    "Error showing " + WidgetGalleryView.class.getSimpleName(),
+		    e);
+	}
+	return null;
+    }
+
+}
--- de.fu_berlin.inf.dpp.ui.widgetGallery/src/de/fu_berlin/inf/dpp/ui/widgetGallery/demos/Demo.java
+++ de.fu_berlin.inf.dpp.ui.widgetGallery/src/de/fu_berlin/inf/dpp/ui/widgetGallery/demos/Demo.java
@@ -22,161 +22,163 @@ import de.fu_berlin.inf.dpp.ui.widgets.decoration.EmptyText;
 
 public abstract class Demo {
 
-	/**
-	 * {@link Control}Êthis {@link Demo} is based on.
+    /**
+     * {@link Control}Êthis {@link Demo} is based on.
+     */
+    protected Control control;
+
+    /**
+     * Contains all controls and the content.
+     */
+    protected Composite rootComposite;
+
+    /**
+     * Contains the console for debug information send by the {@link Demo}
+     * instance.
+     */
+    protected EmptyText console;
+    protected static final SimpleDateFormat consoleDateFormat = new SimpleDateFormat(
+	    "HH:mm:ss");
+
+    protected int consoleHeight = 150;
+
+    protected TabItem tabItem;
+    protected Composite content;
+
+    public Demo(Composite parent) {
+	Composite root = new Composite(parent, SWT.NONE);
+	this.control = root;
+	root.setLayout(new FillLayout());
+	createPartControls(root);
+    }
+
+    public Demo(DemoContainer parent, String title) {
+	TabFolder tabFolder = parent.register(this);
+	this.control = tabFolder;
+
+	this.tabItem = new TabItem(tabFolder, SWT.NULL);
+	this.tabItem.setText(title);
+
+	/*
+	 * tabItem's content
 	 */
-	protected Control control;
+	this.rootComposite = new Composite(tabFolder, SWT.NONE);
+	this.rootComposite.setLayout(new GridLayout(2, false));
+	this.tabItem.setControl(this.rootComposite);
 
-	/**
-	 * Contains all controls and the content.
+	/*
+	 * Content part
 	 */
-	protected Composite rootComposite;
+	this.content = new Composite(this.rootComposite, SWT.NONE);
+	this.content
+		.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
-	/**
-	 * Contains the console for debug information send by the {@link Demo}
-	 * instance.
+	/*
+	 * Control part
 	 */
-	protected EmptyText console;
-	protected static final SimpleDateFormat consoleDateFormat = new SimpleDateFormat(
-			"HH:mm:ss");
+	Composite controls = createControls(this.rootComposite);
+	controls.setLayoutData(new GridData(SWT.BEGINNING, SWT.FILL, false,
+		true, 1, 2));
 
-	protected int consoleHeight = 150;
+	/*
+	 * Console part
+	 */
+	Text console = new Text(this.rootComposite, SWT.V_SCROLL | SWT.BORDER
+		| SWT.MULTI);
+	console.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+	this.console = new EmptyText(console, "Debug Console");
+	this.hideConsole();
 
-	protected TabItem tabItem;
-	protected Composite content;
+	recreateDemo();
+    }
 
-	public Demo(Composite parent) {
-		Composite root = new Composite(parent, SWT.NONE);
-		this.control = root;
-		root.setLayout(new FillLayout());
-		createPartControls(root);
+    TabItem getTabItem() {
+	return this.tabItem;
+    }
+
+    /**
+     * Recreates the demo. Especially useful if debug mode is enabled.
+     */
+    public void recreateDemo() {
+	Control[] children = content.getChildren();
+	for (Control child : children) {
+	    if (!child.isDisposed()) {
+		child.dispose();
+	    }
 	}
+	this.content.setLayout(new FillLayout());
+	createPartControls(content);
+	content.layout();
+    }
 
-	public Demo(DemoContainer parent, String title) {
-		TabFolder tabFolder = parent.register(this);
-		this.control = tabFolder;
+    /**
+     * Shows the console
+     */
+    public void showConsole() {
+	((GridData) this.console.getControl().getLayoutData()).heightHint = consoleHeight;
+	this.console.getControl().setVisible(true);
+	this.rootComposite.layout();
+    }
 
-		this.tabItem = new TabItem(tabFolder, SWT.NULL);
-		this.tabItem.setText(title);
+    /**
+     * Hides the console
+     */
+    public void hideConsole() {
+	((GridData) this.console.getControl().getLayoutData()).heightHint = 0;
+	this.console.getControl().setVisible(false);
+	this.rootComposite.layout();
+    }
 
-		/*
-		 * tabItem's content
-		 */
-		this.rootComposite = new Composite(tabFolder, SWT.NONE);
-		this.rootComposite.setLayout(new GridLayout(2, false));
-		this.tabItem.setControl(this.rootComposite);
+    /**
+     * Adds a message to the console and shows it if hidden.
+     * 
+     * @param message
+     */
+    public void addConsoleMessage(String message) {
+	final String newLine = consoleDateFormat.format(new Date()) + " "
+		+ message + "\n";
+	String oldText = console.getText();
+	String newText = oldText + newLine;
+	console.setText(newText);
+	console.getControl().setSelection(newText.length());
+	this.showConsole();
+    }
 
-		/*
-		 * Content part
-		 */
-		this.content = new Composite(this.rootComposite, SWT.NONE);
-		this.content
-				.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+    /**
+     * Creates the controls for this demo
+     * 
+     * @param composite
+     * @return
+     */
+    public Composite createControls(Composite composite) {
+	Composite controls = new Composite(composite, SWT.NONE);
+	controls.setLayout(LayoutUtils.createGridLayout());
 
-		/*
-		 * Control part
-		 */
-		Composite controls = createControls(this.rootComposite);
-		controls.setLayoutData(new GridData(SWT.BEGINNING, SWT.FILL, false,
-				true, 1, 2));
-
-		/*
-		 * Console part
-		 */
-		Text console = new Text(this.rootComposite, SWT.V_SCROLL | SWT.BORDER
-				| SWT.MULTI);
-		console.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
-		this.console = new EmptyText(console, "Debug Console");
-		this.hideConsole();
-
+	Button recreateButton = new Button(controls, SWT.FLAT);
+	recreateButton
+		.setLayoutData(new GridData(SWT.END, SWT.END, true, true));
+	recreateButton.setImage(ImageManager.RELOAD);
+	recreateButton.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent e) {
 		recreateDemo();
-	}
+	    }
+	});
+	return controls;
+    }
 
-	TabItem getTabItem() {
-		return this.tabItem;
-	}
+    /**
+     * Creates the content for this demo
+     * 
+     * @param parent
+     */
+    public abstract void createPartControls(Composite parent);
 
-	/**
-	 * Recreates the demo. Especially useful if debug mode is enabled.
-	 */
-	public void recreateDemo() {
-		Control[] children = content.getChildren();
-		for (Control child : children) {
-			if (!child.isDisposed()) {
-				child.dispose();
-			}
-		}
-		this.content.setLayout(new FillLayout());
-		createPartControls(content);
-		content.layout();
-	}
-
-	/**
-	 * Shows the console
-	 */
-	public void showConsole() {
-		((GridData) this.console.getControl().getLayoutData()).heightHint = consoleHeight;
-		this.console.getControl().setVisible(true);
-		this.rootComposite.layout();
-	}
-
-	/**
-	 * Hides the console
-	 */
-	public void hideConsole() {
-		((GridData) this.console.getControl().getLayoutData()).heightHint = 0;
-		this.console.getControl().setVisible(false);
-		this.rootComposite.layout();
-	}
-
-	/**
-	 * Adds a message to the console and shows it if hidden.
-	 * 
-	 * @param message
-	 */
-	public void addConsoleMessage(String message) {
-		String newLine = consoleDateFormat.format(new Date()) + " " + message
-				+ "\n";
-		this.console.setText(this.console.getText() + newLine);
-		this.console.getControl().setSelection(this.console.getText().length());
-		this.showConsole();
-	}
-
-	/**
-	 * Creates the controls for this demo
-	 * 
-	 * @param composite
-	 * @return
-	 */
-	public Composite createControls(Composite composite) {
-		Composite controls = new Composite(composite, SWT.NONE);
-		controls.setLayout(LayoutUtils.createGridLayout());
-
-		Button recreateButton = new Button(controls, SWT.FLAT);
-		recreateButton
-				.setLayoutData(new GridData(SWT.END, SWT.END, true, true));
-		recreateButton.setImage(ImageManager.RELOAD);
-		recreateButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				recreateDemo();
-			}
-		});
-		return controls;
-	}
-
-	/**
-	 * Creates the content for this demo
-	 * 
-	 * @param parent
-	 */
-	public abstract void createPartControls(Composite parent);
-
-	/**
-	 * Is called when this demo is set visible
-	 */
-	public void setVisible() {
-		// do nothing
-	}
+    /**
+     * Is called when this demo is set visible
+     */
+    public void setVisible() {
+	// do nothing
+    }
 
 }
