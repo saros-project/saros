190f7a480f9a4603516924a0884f5db04e068133
Björn Kahlert <bkahlert@users.sourceforge.net>
2011-04-20 23:14:34 +0000

    [UI] Improved Banner

--- de.fu_berlin.inf.dpp.ui.widgetGallery/src/de/fu_berlin/inf/dpp/ui/widgetGallery/ImageManager.java
+++ de.fu_berlin.inf.dpp.ui.widgetGallery/src/de/fu_berlin/inf/dpp/ui/widgetGallery/ImageManager.java
@@ -12,31 +12,32 @@ import de.fu_berlin.inf.dpp.ui.widgetGallery.application.Activator;
  */
 public class ImageManager {
 
-	public static Image RELOAD = getImage("icons/reload.png");
+    public static Image RELOAD = getImage("icons/reload.png");
+    public static Image WIDGET_GALLERY_32 = getImage("icons/widget_gallery_misc32.png");
 
-	/**
-	 * Returns an image from the file at the given plug-in relative path.
-	 * 
-	 * @param path
-	 * @return image; the returned image <b>MUST be disposed after usage</b> to
-	 *         free up memory
-	 */
-	public static Image getImage(String path) {
-		return new Image(Display.getDefault(), getImageDescriptor(path)
-				.getImageData());
-	}
+    /**
+     * Returns an image from the file at the given plug-in relative path.
+     * 
+     * @param path
+     * @return image; the returned image <b>MUST be disposed after usage</b> to
+     *         free up memory
+     */
+    public static Image getImage(String path) {
+	return new Image(Display.getDefault(), getImageDescriptor(path)
+		.getImageData());
+    }
 
-	/**
-	 * Returns an image descriptor for the image file at the given plug-in
-	 * relative path.
-	 * 
-	 * @param path
-	 *            the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID,
-				path);
-	}
+    /**
+     * Returns an image descriptor for the image file at the given plug-in
+     * relative path.
+     * 
+     * @param path
+     *            the path
+     * @return the image descriptor
+     */
+    public static ImageDescriptor getImageDescriptor(String path) {
+	return AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID,
+		path);
+    }
 
 }
--- de.fu_berlin.inf.dpp.ui.widgetGallery/src/de/fu_berlin/inf/dpp/ui/widgetGallery/views/WidgetGalleryView.java
+++ de.fu_berlin.inf.dpp.ui.widgetGallery/src/de/fu_berlin/inf/dpp/ui/widgetGallery/views/WidgetGalleryView.java
@@ -1,35 +1,38 @@
 package de.fu_berlin.inf.dpp.ui.widgetGallery.views;
 
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
 import de.fu_berlin.inf.dpp.ui.util.LayoutUtils;
+import de.fu_berlin.inf.dpp.ui.widgetGallery.ImageManager;
 import de.fu_berlin.inf.dpp.ui.widgetGallery.suits.MainDemo;
-import de.fu_berlin.inf.dpp.ui.widgetGallery.widgets.SimpleBannerComposite;
+import de.fu_berlin.inf.dpp.ui.widgetGallery.widgets.BannerComposite;
+import de.fu_berlin.inf.dpp.ui.widgets.SimpleIllustratedComposite.IllustratedText;
 
 public class WidgetGalleryView extends ViewPart {
-	public static final String ID = "de.fu_berlin.inf.dpp.ui.widgetGallery.views.WidgetGalleryView";
-	public static SelectionProviderIntermediate selectionProviderIntermediate = new SelectionProviderIntermediate();
+    public static final String ID = "de.fu_berlin.inf.dpp.ui.widgetGallery.views.WidgetGalleryView";
+    public static SelectionProviderIntermediate selectionProviderIntermediate = new SelectionProviderIntermediate();
 
-	public void createPartControl(final Composite parent) {
-		parent.setLayout(LayoutUtils.createGridLayout());
+    public void createPartControl(final Composite parent) {
+	parent.setLayout(LayoutUtils.createGridLayout());
 
-		this.getSite().setSelectionProvider(selectionProviderIntermediate);
+	this.getSite().setSelectionProvider(selectionProviderIntermediate);
 
-		SimpleBannerComposite simpleBannerComposite = new SimpleBannerComposite(
-				parent, SWT.NONE);
-		simpleBannerComposite.setLayoutData(LayoutUtils
-				.createFillHGrabGridData());
-		simpleBannerComposite.setText("Saros Widget Gallery");
+	BannerComposite bannerComposite = new BannerComposite(parent, SWT.NONE);
+	bannerComposite.setLayoutData(GridDataFactory.fillDefaults()
+		.grab(true, false).create());
+	bannerComposite.setContent(new IllustratedText(
+		ImageManager.WIDGET_GALLERY_32, "Saros Widget Gallery"));
 
-		MainDemo mainDemo = new MainDemo(parent);
-		mainDemo.getControl().setLayoutData(LayoutUtils.createFillGridData());
-	}
+	MainDemo mainDemo = new MainDemo(parent);
+	mainDemo.getControl().setLayoutData(LayoutUtils.createFillGridData());
+    }
 
-	@Override
-	public void setFocus() {
-		// TODO Auto-generated method stub
+    @Override
+    public void setFocus() {
+	// TODO Auto-generated method stub
 
-	}
+    }
 }
