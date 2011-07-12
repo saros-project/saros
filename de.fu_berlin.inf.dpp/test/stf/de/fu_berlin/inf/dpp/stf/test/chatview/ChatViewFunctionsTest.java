3d28ec645900cfddd418c6a636b92262dd6f02fe
Stefan Rossbach <kargor@users.sourceforge.net>
2011-07-12 22:18:41 +0000

    [CI] was merged wrong and failed to compile

--- de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/chatview/ChatViewFunctionsTest.java
+++ de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/chatview/ChatViewFunctionsTest.java
@@ -5,7 +5,6 @@ import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.BOB;
 import static de.fu_berlin.inf.dpp.stf.shared.Constants.VIEW_SAROS;
 import static org.junit.Assert.assertEquals;
 
-import java.rmi.AccessException;
 import java.rmi.RemoteException;
 
 import org.junit.BeforeClass;
@@ -18,67 +17,15 @@ public class ChatViewFunctionsTest extends StfTestCase {
 
     String message = "Hello Bob";
 
-    /**
-     * Preconditions:
-     * <ol>
-     * <li>Alice (Host, Write Access)</li>
-     * <li>Bob (Read-Only Access)</li>
-     * </ol>
-     * 
-     * @throws AccessException
-     * @throws RemoteException
-     * @throws InterruptedException
-     */
-
     @BeforeClass
-    public static void runBeforeClass() throws RemoteException,
-        InterruptedException {
-        initTesters(ALICE, BOB);
-        setUpWorkbench();
-        setUpSaros();
-        Util.setUpSessionWithAJavaProjectAndAClass("foo", "bar", "test", ALICE,
-            BOB);
+    public static void selectTesters() throws Exception {
+        select(ALICE, BOB);
     }
-
-    /**
-     * Steps:
-     * <ol>
-     * <li>ALICE open the chat view</li>
-     * <li>BOB open the chat view</li>
-     * <li>ALICE sends IM to BOB (both users should have beep button active by
-     * default)</li>
-     * </ol>
-     * 
-     * Result:
-     * <ol>
-     * <li>The chat should have automatically connected. ALICE should see e.g.
-     * <p>
-     * "You&nbsp;&nbsp;&nbsp;31/10/10 17:19"
-     * <p>
-     * "... joined the chat"
-     * <p>
-     * "BOB1_fu@saros-con.imp.fu-berlin.de"
-     * <p>
-     * "... joined the chat"</li>
-     * <li>The chat should have automatically connected. BOB should see e.g.
-     * <p>
-     * "You&nbsp;&nbsp;&nbsp;31/10/10 17:19"
-     * <p>
-     * "... joined the chat"
-     * <p>
-     * "ALICE1_fu@saros-con.imp.fu-berlin.de"
-     * <p>
-     * "... joined the chat"</li>
-     * <li>BOB should receive the IM from ALICE.Only user B hears a beep when
-     * receiving IM.</li>
-     * </ol>
-     * 
-     * TODO: replace BOB.sleep() by a better condition and do we need the
-     * comperator.class for other tests?
-     */
 
     @Test
     public void testChat() throws RemoteException {
+        Util.setUpSessionWithJavaProjectAndClass("foo", "bar", "test", ALICE,
+            BOB);
 
         assertEquals("Roundtable", ALICE.remoteBot().view(VIEW_SAROS).bot()
             .cTabItem().getText());
