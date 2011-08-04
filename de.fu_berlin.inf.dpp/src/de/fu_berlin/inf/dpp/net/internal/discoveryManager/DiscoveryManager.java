7b91165b2c99a51c0a2b896ea9d2d5d437a0b473
Stefan Rossbach <kargor@users.sourceforge.net>
2011-08-04 20:53:57 +0000

    [FIX] concurrent modification exception was thrown  in rare cases

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/internal/discoveryManager/DiscoveryManager.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/internal/discoveryManager/DiscoveryManager.java
@@ -449,7 +449,7 @@ public class DiscoveryManager implements Disposable {
      * 
      * @param discoveryManagerListener
      */
-    public void addDiscoveryManagerListener(
+    public synchronized void addDiscoveryManagerListener(
         DiscoveryManagerListener discoveryManagerListener) {
         this.discoveryManagerListeners.add(discoveryManagerListener);
     }
@@ -459,7 +459,7 @@ public class DiscoveryManager implements Disposable {
      * 
      * @param discoveryManagerListener
      */
-    public void removeDiscoveryManagerListener(
+    public synchronized void removeDiscoveryManagerListener(
         DiscoveryManagerListener discoveryManagerListener) {
         this.discoveryManagerListeners.remove(discoveryManagerListener);
     }
@@ -468,8 +468,8 @@ public class DiscoveryManager implements Disposable {
      * Notify all {@link DiscoveryManagerListener}s about an updated feature
      * support.
      */
-    public void notifyFeatureSupportUpdated(JID jid, String feature,
-        boolean isSupported) {
+    public synchronized void notifyFeatureSupportUpdated(JID jid,
+        String feature, boolean isSupported) {
         for (DiscoveryManagerListener discoveryManagerListener : this.discoveryManagerListeners) {
             discoveryManagerListener.featureSupportUpdated(jid, feature,
                 isSupported);
