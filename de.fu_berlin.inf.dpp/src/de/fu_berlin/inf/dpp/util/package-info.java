4a9db81ad8c37432fd90cf21ca86a0116fb872cc
Karl Beecher <k_beecher@users.sourceforge.net>
2011-11-03 13:00:18 +0000

    [DOC] Package info files for util package

---
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Classes that provide general functionality which either fits nowhere 
+ * else or is used throughout the project.
+ * 
+ * Notable contents include:
+ * <ol>
+ * <li>Useful ADTs, like {@link MappedList}, {@link Pair},
+ * {@link UncloseableInputStream}, or {@link AutoHashMap}.
+ * <li>Some classes which include more specific static utility functions, 
+ * like {@link ActivityUtils}, {@link FileUtils}, {@ArrayUtils}.
+ * <li>Iterator classes: {@link ArrayIterator}, {@link MappingIterator}.
+ * <li>{@link Utils}, a class full of useful functions, including 
+ * ones for thread-management.
+ */
+package de.fu_berlin.inf.dpp.util;
+
+import de.fu_berlin.inf.dpp.util.*;
---
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/util/pico/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * These classes provide methods for managing dependencies as part of
+ * the PicoContainer framework.
+ */
+
+package de.fu_berlin.inf.dpp.util.pico;
