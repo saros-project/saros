5e4b0edafa27947551918017b67571c55319fef6
Karl Beecher <k_beecher@users.sourceforge.net>
2011-11-03 14:14:27 +0000

    [DOC] Added net.jingle package info

---
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/jingle/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for managing file transfers via Jingle.
+ * 
+ * NOTE: Jingle file transfer is currently disabled in Saros. We use
+ * only SOCKS or IBB.
+ */
+
+package de.fu_berlin.inf.dpp.net.jingle;
