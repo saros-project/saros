8b49e0276bb37931af5c00058d9189afdbdfe139
Karl Beecher <k_beecher@users.sourceforge.net>
2011-11-03 13:36:56 +0000

    [DOC] Added JavaDoc

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/UPnP/IUPnPAccess.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/UPnP/IUPnPAccess.java
@@ -20,12 +20,49 @@ public interface IUPnPAccess {
         UnknownHostException, IOException, SAXException,
         ParserConfigurationException;
 
+    /**
+     * Deletes the specified port mapping associated with the external port and
+     * the protocol.
+     * 
+     * @param gateway
+     *            A UPnP-compliant gateway device using the mapping.
+     * @param port
+     *            The external port
+     * @param protocol
+     *            The protocol
+     * @return Error code. 0 if successful.
+     * @throws IOException
+     * @throws SAXException
+     */
     int deletePortMapping(GatewayDevice gateway, int port, String protocol)
         throws IOException, SAXException;
 
     PortMappingEntry getSpecificPortMappingEntry(GatewayDevice gateway,
         int port, String protocol) throws IOException, SAXException;
 
+    /**
+     * Adds a new port mapping to a gateway device.
+     * 
+     * @param gateway
+     *            The gateway device receiving the new mapping.
+     * 
+     * @param externalPort
+     *            The external port associated with the new mapping
+     * @param internalPort
+     *            The internal port associated with the new mapping
+     * @param internalClient
+     *            The internal client associated with the new mapping
+     * @param protocol
+     *            The protocol associated with the new mapping
+     * @param description
+     *            A description to accompany the mapping
+     * @param leaseDuration
+     *            amount of seconds this mapping is valid. Use 0 for unlimited
+     *            duration.
+     * @return Error code. 0 if successful.
+     * @throws IOException
+     * @throws SAXException
+     */
     int addPortMapping(GatewayDevice gateway, int externalPort,
         int internalPort, String internalClient, String protocol,
         String description, int leaseDuration) throws IOException, SAXException;
