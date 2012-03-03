48033c6fdf7506f6fbbc4ad6162f0dae894ee9d8
Stefan Rossbach <kargor@users.sourceforge.net>
2012-03-03 17:33:58 +0000

    [REMOVED] XMPPConnectionTest.java was testing Smack stuff but not Saros

--- de.fu_berlin.inf.dpp/test/junit/de/fu_berlin/inf/dpp/net/NetTestSuite.java
+++ de.fu_berlin.inf.dpp/test/junit/de/fu_berlin/inf/dpp/net/NetTestSuite.java
@@ -8,9 +8,8 @@ import de.fu_berlin.inf.dpp.net.util.NetUtilTestSuite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ FileListTest.class, GZipTest.class, JIDTest.class,
-    XMPPConnectionTest.class, RosterTrackerTest.class, UPnPTest.class,
-    TimedActivityDataObjectTest.class, XMPPUtilTest.class,
-    InternalTestSuite.class, NetUtilTestSuite.class })
+    RosterTrackerTest.class, UPnPTest.class, TimedActivityDataObjectTest.class,
+    XMPPUtilTest.class, InternalTestSuite.class, NetUtilTestSuite.class })
 public class NetTestSuite {
     // the class remains completely empty,
     // being used only as a holder for the above annotations
