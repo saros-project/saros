9600441955e09583f24647deb8b008065e80964d
Stefan Rossbach <kargor@users.sourceforge.net>
2012-03-08 20:25:10 +0000

    [REFACORED] Removed unused variable(s). Changed dependency from Saros to IPreference in the ctor.

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/communication/muc/negotiation/MUCSessionPreferencesNegotiatingManager.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/communication/muc/negotiation/MUCSessionPreferencesNegotiatingManager.java
@@ -3,12 +3,16 @@ package de.fu_berlin.inf.dpp.communication.muc.negotiation;
 import java.util.Random;
 
 import org.apache.log4j.Logger;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.picocontainer.annotations.Inject;
 
-import de.fu_berlin.inf.dpp.Saros;
-import de.fu_berlin.inf.dpp.net.internal.XStreamExtensionProvider;
 import de.fu_berlin.inf.dpp.observables.SessionIDObservable;
 import de.fu_berlin.inf.dpp.preferences.PreferenceConstants;
+
+/*
+ * FIXME this class seems not to transmit anything anymore. This class is only
+ * queried for the current configuration. It should be renamed.
+ */
 
 /**
  * The CommunicationNegotiatingManager is responsible for transmitting the
@@ -17,65 +21,60 @@ import de.fu_berlin.inf.dpp.preferences.PreferenceConstants;
  * 
  * @author ologa
  * @author bkahlert
- * 
  */
 public class MUCSessionPreferencesNegotiatingManager {
 
-    private static final Logger log = Logger
-        .getLogger(MUCSessionPreferencesNegotiatingManager.class);
+	private static final Logger log = Logger
+		.getLogger(MUCSessionPreferencesNegotiatingManager.class);
 
-    @Inject
-    protected Saros saros;
+	@Inject
+	protected IPreferenceStore preferences;
 
-    protected SessionIDObservable sessionID;
+	protected SessionIDObservable sessionID;
 
-    protected MUCSessionPreferences localPreferences;
-    protected MUCSessionPreferences sessionPreferences;
+	protected MUCSessionPreferences localPreferences;
+	protected MUCSessionPreferences sessionPreferences;
 
-    protected String password;
+	protected String password;
 
-    private Random random = new Random();
+	private Random random = new Random();
 
-    XStreamExtensionProvider<MUCSessionPreferences> communicationProvider = new XStreamExtensionProvider<MUCSessionPreferences>(
-        "sarosComPrefs", MUCSessionPreferences.class);
+	public MUCSessionPreferencesNegotiatingManager(
+		SessionIDObservable sessionID, IPreferenceStore preferences) {
+		this.sessionID = sessionID;
+		this.password = Integer.toString(random.nextInt());
+	}
 
-    public MUCSessionPreferencesNegotiatingManager(SessionIDObservable sessionID) {
-        this.sessionID = sessionID;
-        this.password = Integer.toString(random.nextInt());
-    }
+	/**
+	 * Load communication settings from PreferenceStore and generate chat room
+	 * and chat room password.
+	 */
+	public MUCSessionPreferences getOwnPreferences() {
+		String service = preferences.getString(PreferenceConstants.CHATSERVER);
+		String roomName = "SAROS" + sessionID.getValue();
+		return new MUCSessionPreferences(service, roomName, this.password);
+	}
 
-    /**
-     * Load communication settings from PreferenceStore and generate chat room
-     * and chat room password.
-     */
-    public MUCSessionPreferences getOwnPreferences() {
-        String service = saros.getPreferenceStore().getString(
-            PreferenceConstants.CHATSERVER);
-        String roomName = "SAROS" + sessionID.getValue();
-        return new MUCSessionPreferences(service, roomName, this.password);
-    }
+	/**
+	 * @return temporarily session preferences
+	 */
+	public MUCSessionPreferences getSessionPreferences() {
+		return sessionPreferences;
+	}
 
-    /**
-     * 
-     * @return temporarily session preferences
-     */
-    public MUCSessionPreferences getSessionPreferences() {
-        return sessionPreferences;
-    }
+	/**
+	 * Set temporarily communication shared project settings
+	 * 
+	 * @param remotePreferences
+	 *            received communication settings
+	 */
+	public void setSessionPreferences(MUCSessionPreferences remotePreferences) {
+		log.debug("Got hosts Communication Config: server "
+				+ remotePreferences.getService() + " room "
+				+ remotePreferences.getRoomName() + " pw "
+				+ remotePreferences.getPassword());
 
-    /**
-     * Set temporarily communication shared project settings
-     * 
-     * @param remotePreferences
-     *            received communication settings
-     */
-    public void setSessionPreferences(MUCSessionPreferences remotePreferences) {
-        log.debug("Got hosts Communication Config: server "
-            + remotePreferences.getService() + " room "
-            + remotePreferences.getRoomName() + " pw "
-            + remotePreferences.getPassword());
-
-        sessionPreferences = remotePreferences;
-    }
+		sessionPreferences = remotePreferences;
+	}
 
 }
