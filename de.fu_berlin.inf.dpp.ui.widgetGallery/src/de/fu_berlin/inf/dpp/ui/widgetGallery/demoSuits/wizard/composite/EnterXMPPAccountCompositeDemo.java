71c114835052f403c41afca884885415ea0b8ba0
Stefan Rossbach <kargor@users.sourceforge.net>
2012-03-16 02:58:56 +0000

    [INTERNAL] changes needed for bug fix ID: 3469236

--- de.fu_berlin.inf.dpp.ui.widgetGallery/src/de/fu_berlin/inf/dpp/ui/widgetGallery/demoSuits/wizard/composite/EnterXMPPAccountCompositeDemo.java
+++ de.fu_berlin.inf.dpp.ui.widgetGallery/src/de/fu_berlin/inf/dpp/ui/widgetGallery/demoSuits/wizard/composite/EnterXMPPAccountCompositeDemo.java
@@ -7,39 +7,18 @@ import org.eclipse.swt.widgets.Composite;
 import de.fu_berlin.inf.dpp.ui.widgetGallery.annotations.Demo;
 import de.fu_berlin.inf.dpp.ui.widgetGallery.demoSuits.AbstractDemo;
 import de.fu_berlin.inf.dpp.ui.widgets.wizard.EnterXMPPAccountComposite;
-import de.fu_berlin.inf.dpp.ui.widgets.wizard.events.EnterXMPPAccountCompositeListener;
-import de.fu_berlin.inf.dpp.ui.widgets.wizard.events.IsSarosXMPPServerChangedEvent;
-import de.fu_berlin.inf.dpp.ui.widgets.wizard.events.XMPPServerChangedEvent;
 import de.fu_berlin.inf.nebula.utils.LayoutUtils;
 
 @Demo
 public class EnterXMPPAccountCompositeDemo extends AbstractDemo {
-    @Override
-    public void createDemo(Composite parent) {
-	parent.setLayout(LayoutUtils.createGridLayout());
-	showConsole();
+	@Override
+	public void createDemo(Composite parent) {
+		parent.setLayout(LayoutUtils.createGridLayout());
+		showConsole();
 
-	EnterXMPPAccountComposite enterXMPPAccountComposite = new EnterXMPPAccountComposite(
-		parent, SWT.BORDER);
-	enterXMPPAccountComposite.setLayoutData(new GridData(SWT.FILL,
-		SWT.CENTER, true, true));
-	enterXMPPAccountComposite
-		.addEnterXMPPAccountCompositeListener(new EnterXMPPAccountCompositeListener() {
-		    public void isSarosXMPPServerChanged(
-			    IsSarosXMPPServerChangedEvent event) {
-			addConsoleMessage("isSarosXMPPServerChanged changed to "
-				+ ((event.isSarosXMPPServer()) ? "true"
-					: "false"));
-		    }
-
-		    public void xmppServerValidityChanged(
-			    XMPPServerChangedEvent event) {
-			addConsoleMessage("XMPP server "
-				+ event.getXMPPServer()
-				+ " is "
-				+ ((event.isXMPPServerValid()) ? "valid"
-					: "invalid"));
-		    }
-		});
-    }
+		EnterXMPPAccountComposite enterXMPPAccountComposite = new EnterXMPPAccountComposite(
+				parent, SWT.BORDER);
+		enterXMPPAccountComposite.setLayoutData(new GridData(SWT.FILL,
+				SWT.CENTER, true, true));
+	}
 }
