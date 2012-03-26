a7c7633210af9f711a5c958d34cb6fecce18542b
Stefan Rossbach <kargor@users.sourceforge.net>
2012-03-26 15:52:43 +0000

    [REFACTOR] moved static method from XMPPUtil to RosterUtils [REMOVED] XMPPUtil as it is now no longer contains code [REMOVED] unused test cases

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/util/RosterUtils.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/util/RosterUtils.java
@@ -9,17 +9,23 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
 import org.jivesoftware.smack.AccountManager;
 import org.jivesoftware.smack.Connection;
+import org.jivesoftware.smack.PacketCollector;
 import org.jivesoftware.smack.Roster;
 import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.SmackConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.filter.AndFilter;
+import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.filter.PacketIDFilter;
+import org.jivesoftware.smack.filter.PacketTypeFilter;
+import org.jivesoftware.smack.packet.IQ;
 import org.jivesoftware.smack.packet.Registration;
 import org.jivesoftware.smack.packet.XMPPError;
 import org.jivesoftware.smackx.ServiceDiscoveryManager;
 
 import de.fu_berlin.inf.dpp.net.JID;
 import de.fu_berlin.inf.dpp.net.SarosNet;
-import de.fu_berlin.inf.dpp.net.XMPPUtil;
 import de.fu_berlin.inf.dpp.ui.util.DialogUtils;
 import de.fu_berlin.inf.dpp.util.Utils;
 
@@ -231,7 +237,7 @@ public class RosterUtils {
 
         Registration registration = null;
         try {
-            registration = XMPPUtil.getRegistrationInfo(username, connection);
+            registration = getRegistrationInfo(username, connection);
         } catch (XMPPException e) {
             log.error("Server " + connection.getHost()
                 + " does not support XEP-0077"
@@ -422,4 +428,40 @@ public class RosterUtils {
         }
     }
 
+    /**
+     * Retrieve XMPP Registration information from a server.
+     * 
+     * This implementation reuses code from Smack but also sets the from element
+     * of the IQ-Packet so that the server could reply with information that the
+     * account already exists as given by XEP-0077.
+     * 
+     * To see what additional information can be queried from the registration
+     * object, refer to the XEP directly:
+     * 
+     * http://xmpp.org/extensions/xep-0077.html
+     */
+    public static synchronized Registration getRegistrationInfo(
+        String toRegister, Connection connection) throws XMPPException {
+        Registration reg = new Registration();
+        reg.setTo(connection.getServiceName());
+        reg.setFrom(toRegister);
+        PacketFilter filter = new AndFilter(new PacketIDFilter(
+            reg.getPacketID()), new PacketTypeFilter(IQ.class));
+        PacketCollector collector = connection.createPacketCollector(filter);
+        connection.sendPacket(reg);
+        IQ result = (IQ) collector.nextResult(SmackConfiguration
+            .getPacketReplyTimeout());
+
+        // Stop queuing results
+        collector.cancel();
+
+        if (result == null) {
+            // TODO This exception is shown incorrectly to the user!!
+            throw new XMPPException("No response from server.");
+        } else if (result.getType() == IQ.Type.ERROR) {
+            throw new XMPPException(result.getError());
+        } else {
+            return (Registration) result;
+        }
+    }
 }
