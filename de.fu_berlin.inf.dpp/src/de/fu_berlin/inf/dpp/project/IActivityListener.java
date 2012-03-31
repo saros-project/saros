c931e2f951f12a27f7d78db0793c932e3991dd93
Holger Freyther <freyther@users.sourceforge.net>
2012-03-31 15:27:30 +0000

    [INTERNAL] Pedantic, indicate the right type in the parameter
    IActivityListener.activityCreated is working on IActivities and
    not on the IActivityDataObject, rename the parameter name to
    indicate the right type.
    
    Change-Id: Id63b9d93fcf7b4358871ac06b9fe64e3eaa3ab4d

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/IActivityListener.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/IActivityListener.java
@@ -5,7 +5,11 @@ import de.fu_berlin.inf.dpp.activities.business.IActivity;
 public interface IActivityListener {
 
     /**
+     * Called when an activity was created.
+     * 
      * @swt Must be called from the SWT Thread!
+     * @param activityData
+     *            The IActivity that was created.
      */
-    public void activityCreated(IActivity activityDataObject);
+    public void activityCreated(IActivity activityData);
 }
