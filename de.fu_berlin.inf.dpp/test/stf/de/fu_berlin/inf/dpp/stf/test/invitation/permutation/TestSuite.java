87652ce05590cbee255016083058cda3523946d4
Alexander Waldmann <netcorps@users.sourceforge.net>
2012-04-04 16:02:16 +0000

    [FIX] Accidentally facerolled my keyboard and deleted STF tests from the testsuite before commiting.... They are reactivated now

--- de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/invitation/permutation/TestSuite.java
+++ de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/invitation/permutation/TestSuite.java
@@ -4,7 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ ShareProjectWizardUITest.class })
+@Suite.SuiteClasses({ ShareProjectWizardUITest.class,
+    Share2UsersSequentiallyTest.class, Share3UsersSequentiallyTest.class,
+    Share3UsersConcurrentlyTest.class, Share3UsersLeavingSessionTest.class,
+    ShareProjectUsingExistingProjectTest.class })
 public class TestSuite {
     // the class remains completely empty,
     // being used only as a holder for the above annotations
