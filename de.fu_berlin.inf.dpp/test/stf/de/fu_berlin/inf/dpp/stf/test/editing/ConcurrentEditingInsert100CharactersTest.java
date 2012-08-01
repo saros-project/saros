66a7602db00b8cdd3df5609b3191279d2888a354
Stefan Rossbach <srossbach@arcor.de>
2012-08-02 01:55:20 +0200

    [STF] refactor ConcurrentEditingInsert100CharactersTest test case
    Using already built in STF functionality instead
    of reinventing the wheel. Cleaned up code.
    
    Change-Id: I43909262ffe42e81d82c3344f3fb041bb813207b
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/174
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/editing/ConcurrentEditingInsert100CharactersTest.java
+++ de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/editing/ConcurrentEditingInsert100CharactersTest.java
@@ -5,124 +5,104 @@ import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.BOB;
 import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.CARL;
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import de.fu_berlin.inf.dpp.stf.client.StfTestCase;
+import de.fu_berlin.inf.dpp.stf.client.tester.AbstractTester;
 import de.fu_berlin.inf.dpp.stf.client.util.Util;
 import de.fu_berlin.inf.dpp.stf.shared.Constants.TypeOfCreateProject;
 import de.fu_berlin.inf.dpp.test.util.TestThread;
 
-/**
- * - ALICE, BOB and CARL edit readme.txt
- * 
- * - Every Buddy has 100 key strokes.
- * 
- * - the test waits 3 seconds
- * 
- * - the texts of the buddies will be compared
- */
-
 public class ConcurrentEditingInsert100CharactersTest extends StfTestCase {
+
+    private static final long TIMEOUT = 5 * 60 * 1000;
+
+    private static final int KEYSTROKE_LIMIT = 100;
+
     private TestThread aliceEditTaskThread;
-    public volatile static int A_ALICE = 0;
 
     private TestThread bobEditTaskThread;
-    public volatile static int A_BOB = 0;
 
     private TestThread carlEditTaskThread;
-    public volatile static int A_CARL = 0;
 
-    /*
-     * Set the limit of keystrokes that every buddy has to make during this
-     * test.
-     */
-    private final static int LIMIT = 100;
+    private static class TypeTask implements TestThread.Runnable {
+
+        private AbstractTester tester;
+        private String editorName;
+        private char from;
+        private char to;
+
+        public TypeTask(AbstractTester tester, String editorName, char from,
+            char to) {
+            this.tester = tester;
+            this.editorName = editorName;
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public void run() throws Exception {
+            char character = from;
+            int typed = 0;
+
+            while ((!Thread.currentThread().isInterrupted())
+                && (typed++ < KEYSTROKE_LIMIT)) {
+
+                tester.remoteBot().editor(editorName).navigateTo(0, 0);
+
+                if (character <= to) {
+                    tester.remoteBot().editor(editorName)
+                        .typeText(String.valueOf(character));
+
+                    character++;
+                } else {
+                    tester.remoteBot().editor(editorName).typeText("\n");
+                    character = from;
+                }
+            }
+        }
+    }
 
     @BeforeClass
     public static void selectTesters() throws Exception {
-        resetDefaultAccount();
         select(ALICE, BOB, CARL);
     }
 
     @Before
     public void runBeforeEveryTest() throws Exception {
         clearWorkspaces();
-        resetDefaultAccount();
     }
 
     @After
     public void runAfterEveryTest() throws Exception {
-        if (aliceEditTaskThread != null && aliceEditTaskThread.isAlive()) {
-            aliceEditTaskThread.interrupt();
-            aliceEditTaskThread.join(10000);
-        }
-        if (bobEditTaskThread != null && bobEditTaskThread.isAlive()) {
-            bobEditTaskThread.interrupt();
-            bobEditTaskThread.join(10000);
-        }
-        if (carlEditTaskThread != null && carlEditTaskThread.isAlive()) {
-            carlEditTaskThread.interrupt();
-            carlEditTaskThread.join(10000);
-        }
-
+        killThread(aliceEditTaskThread);
+        killThread(bobEditTaskThread);
+        killThread(carlEditTaskThread);
         leaveSessionHostFirst(ALICE);
-        ALICE.remoteBot().sleep(1000);
-        BOB.remoteBot().sleep(1000);
-        CARL.remoteBot().sleep(1000);
     }
 
-    /**
-     * Three Buddies (each one in a dedicated thread) insert text in the same
-     * file on the same line (Line 1).
-     * 
-     * @throws Exception
-     */
     @Test
-    public void ThreeInsert() throws Exception {
+    public void testInsertCharactersOnTheSameLine() throws Exception {
 
         ALICE.superBot().internal().createProject("foo");
         ALICE
             .superBot()
             .internal()
-            .createFile("foo", "readme.txt",
-                "\nVerbesserung des algorithmischen Kerns, Gleichzeitiges Editieren\n");
+            .createFile(
+                "foo",
+                "readme.txt",
+                "package test;\n\npublic class paulaBean {\n\n\tprivate String paula = \"Brillant\";\n\n\tpublic String getPaula() {\n\t\treturn paula;\n\t}\n}");
 
-        ALICE.superBot().menuBar().saros()
-            .shareProjects("foo", Util.getJID(BOB, CARL));
+        Util.buildSessionConcurrently("foo", TypeOfCreateProject.NEW_PROJECT,
+            ALICE, BOB, CARL);
 
-        List<Callable<Void>> joinSessionTasks = new ArrayList<Callable<Void>>();
-
-        joinSessionTasks.add(new Callable<Void>() {
-            public Void call() throws Exception {
-                BOB.superBot().confirmShellSessionInvitationAndShellAddProject(
-                    "foo", TypeOfCreateProject.NEW_PROJECT);
-                return null;
-            }
-        });
-
-        joinSessionTasks.add(new Callable<Void>() {
-            public Void call() throws Exception {
-                CARL.remoteBot().sleep(1000);
-                CARL.superBot()
-                    .confirmShellSessionInvitationAndShellAddProject("foo",
-                        TypeOfCreateProject.NEW_PROJECT);
-                return null;
-            }
-        });
-
-        Util.workAll(joinSessionTasks);
-
-        A_ALICE = 0;
-        A_BOB = 0;
-        A_CARL = 0;
         BOB.superBot().views().packageExplorerView()
+            .waitUntilResourceIsShared("foo/readme.txt");
+
+        CARL.superBot().views().packageExplorerView()
             .waitUntilResourceIsShared("foo/readme.txt");
 
         ALICE.superBot().views().packageExplorerView()
@@ -133,112 +113,36 @@ public class ConcurrentEditingInsert100CharactersTest extends StfTestCase {
             .selectFile("foo", "readme.txt").open();
         BOB.remoteBot().editor("readme.txt").waitUntilIsActive();
 
-        /*
-         * The Alice-Thread insert the lower case characters
-         */
-        TestThread.Runnable aliceEditTask = new TestThread.Runnable() {
-            public void run() throws Exception {
-                int i = 97;
-                while ((!Thread.currentThread().isInterrupted())
-                    && (A_ALICE < LIMIT)) {
-                    if (i >= 123) {
-                        ALICE.remoteBot().editor("readme.txt").typeText("\n");
-                        i = 96;
-                    } else {
-                        ALICE.remoteBot().editor("readme.txt")
-                            .typeText("" + ((char) i) + "");
-                        ALICE.remoteBot().editor("readme.txt").navigateTo(1, 1);
-
-                    }
-                    ++A_ALICE;
-                    i++;
-                }
-            }
-        };
-
-        /*
-         * The Carl-Thread insert numbers
-         */
-        TestThread.Runnable carlEditTask = new TestThread.Runnable() {
-            public void run() throws Exception {
-                int i = 48;
-                while ((!Thread.currentThread().isInterrupted())
-                    && (A_CARL < LIMIT)) {
-                    if (i >= 58) {
-                        CARL.remoteBot().editor("readme.txt").typeText("\n");
-                        i = 47;
-                    } else {
-                        CARL.remoteBot().editor("readme.txt")
-                            .typeText("" + ((char) i) + "");
-                        CARL.remoteBot().editor("readme.txt").navigateTo(1, 1);
-
-                    }
-                    ++A_CARL;
-                    i++;
-                }
-            }
-        };
-
-        /*
-         * The Bob-Thread insert the upper case characters
-         */
-        TestThread.Runnable bobEditTask = new TestThread.Runnable() {
-            public void run() throws Exception {
-                int i = 65;
-                while ((!Thread.currentThread().isInterrupted())
-                    && (A_BOB < LIMIT)) {
-                    if (i >= 91) {
-                        BOB.remoteBot().editor("readme.txt").typeText("\n");
-                        i = 64;
-                    } else {
-                        BOB.remoteBot().editor("readme.txt")
-                            .typeText("" + ((char) i) + "");
-                        BOB.remoteBot().editor("readme.txt").navigateTo(1, 1);
-                    }
-                    ++A_BOB;
-                    i++;
-                }
-            }
-        };
-
-        aliceEditTaskThread = new TestThread(aliceEditTask);
-        aliceEditTaskThread.start();
-
-        bobEditTaskThread = new TestThread(bobEditTask);
-        bobEditTaskThread.start();
-
-        CARL.superBot().views().packageExplorerView()
-            .waitUntilResourceIsShared("foo/readme.txt");
-
         CARL.superBot().views().packageExplorerView()
             .selectFile("foo", "readme.txt").open();
         CARL.remoteBot().editor("readme.txt").waitUntilIsActive();
 
-        carlEditTaskThread = new TestThread(carlEditTask);
+        aliceEditTaskThread = new TestThread(new TypeTask(ALICE, "readme.txt",
+            'A', 'Z'));
+        aliceEditTaskThread.start();
+
+        bobEditTaskThread = new TestThread(new TypeTask(BOB, "readme.txt", 'a',
+            'z'));
+        bobEditTaskThread.start();
+
+        carlEditTaskThread = new TestThread(new TypeTask(CARL, "readme.txt",
+            '0', '9'));
         carlEditTaskThread.start();
 
         /*
-         * Here, it's important, that the test waits until every user makes the
-         * limit count of keystrokes.
+         * Wait up to 5 minutes, if the test threads are still running, the test
+         * will fail
          */
-        while ((A_ALICE < LIMIT) || (A_BOB < LIMIT) || (A_CARL < LIMIT))
-            aliceEditTaskThread.join(100);
 
-        aliceEditTaskThread.interrupt();
-        aliceEditTaskThread.join(10000);
+        Util.joinAll(TIMEOUT, aliceEditTaskThread, bobEditTaskThread,
+            carlEditTaskThread);
+
         aliceEditTaskThread.verify();
-
-        bobEditTaskThread.interrupt();
-        bobEditTaskThread.join(10000);
         bobEditTaskThread.verify();
-
-        carlEditTaskThread.interrupt();
-        carlEditTaskThread.join(10000);
         carlEditTaskThread.verify();
 
-        ALICE.remoteBot().sleep(1000);
-        BOB.remoteBot().sleep(1000);
-        CARL.remoteBot().sleep(1000);
+        // ensure that all queues on the client sides are flushed
+        Thread.sleep(10 * 1000);
 
         String aliceText = ALICE.remoteBot().editor("readme.txt").getText();
         String bobText = BOB.remoteBot().editor("readme.txt").getText();
@@ -253,9 +157,14 @@ public class ConcurrentEditingInsert100CharactersTest extends StfTestCase {
         CARL.remoteBot().editor("readme.txt").closeWithoutSave();
         CARL.remoteBot().waitUntilEditorClosed("readme.txt");
 
-        assertEquals(aliceText.length(), bobText.length());
-        assertEquals(carlText.length(), bobText.length());
         assertEquals(bobText, carlText);
         assertEquals(aliceText, bobText);
+    }
+
+    private void killThread(Thread thread) throws InterruptedException {
+        if (thread != null && thread.isAlive()) {
+            thread.interrupt();
+            thread.join(10000);
+        }
     }
 }
