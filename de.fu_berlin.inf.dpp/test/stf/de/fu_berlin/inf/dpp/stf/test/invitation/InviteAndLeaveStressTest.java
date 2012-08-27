218a262dea79214f95031f6a036b8fa46b756aae
Stefan Rossbach <srossbach@arcor.de>
2012-08-28 01:04:43 +0200

    [STF] fixed test case InviteAndLeaveStressTest
    Commit 834005831eb5b0d6ecaf0b45dddcec8d142ffff3
    introduced a bug fix for the "Add Projects to Session"
    wizard which will now override the destination project
    name.
    
    The test case relied on the fact, that it could
    add as many projects as it wants, because new auto
    generated names were used.
    
    This fix generate the projects first, and then shares
    them will fully qualified names.
    
    
    Change-Id: Idc8fae4ecd00303480264cc90e0b59f37c6ec52e
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/223
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/invitation/InviteAndLeaveStressTest.java
+++ de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/invitation/InviteAndLeaveStressTest.java
@@ -4,7 +4,6 @@ import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.ALICE;
 import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.BOB;
 import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.CARL;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -14,23 +13,23 @@ import de.fu_berlin.inf.dpp.stf.shared.Constants.TypeOfCreateProject;
 
 public class InviteAndLeaveStressTest extends StfTestCase {
 
+    private static final int MAX_PROJECTS = 10;
+
     @BeforeClass
     public static void selectTesters() throws Exception {
         select(ALICE, BOB, CARL);
     }
 
-    @Before
-    public void beforeEveryTest() throws Exception {
-        clearWorkspaces();
-        ALICE.superBot().views().packageExplorerView().tree().newC()
-            .javaProjectWithClasses("foo", "bar", "HelloWorld");
-    }
-
     @Test
     public void testInviteAndLeaveStress() throws Exception {
 
-        for (int i = 0; i < 10; i++) {
-            Util.buildSessionConcurrently("foo",
+        for (int i = 0; i < MAX_PROJECTS; i++) {
+            ALICE.superBot().internal().createProject("foo" + i);
+            ALICE.superBot().internal().createFile("foo" + i, "foo.txt", "foo");
+        }
+
+        for (int i = 0; i < MAX_PROJECTS; i++) {
+            Util.buildSessionConcurrently("foo" + i,
                 TypeOfCreateProject.NEW_PROJECT, ALICE, BOB, CARL);
 
             leaveSessionHostFirst(ALICE);
