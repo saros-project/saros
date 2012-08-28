7f9f789702f1c6f6ea5a73e81c0d12892ccf0830
Stefan Rossbach <srossbach@arcor.de>
2012-08-28 23:26:32 +0200

    [FIX] #3562501 adding buddy while in read-only mode
    Just removed the logic that prevented further execution.
    
    Change-Id: I2638e8800c10ad76182dfffbc3bf66d90425a9a4
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/225
    Tested-by: Jenkins CI
    Reviewed-by: Norman Warnatsch <norman@warnatsch.name>
    Reviewed-by: Sebastian Starroske <sebastian.starroske@googlemail.com>
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/internal/ProjectsAddedManager.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/internal/ProjectsAddedManager.java
@@ -1,20 +1,13 @@
 package de.fu_berlin.inf.dpp.project.internal;
 
-import java.text.MessageFormat;
-
-import org.apache.log4j.Logger;
 import org.picocontainer.Startable;
 
-import de.fu_berlin.inf.dpp.User;
 import de.fu_berlin.inf.dpp.activities.business.AbstractActivityReceiver;
 import de.fu_berlin.inf.dpp.activities.business.IActivity;
 import de.fu_berlin.inf.dpp.activities.business.ProjectsAddedActivity;
-import de.fu_berlin.inf.dpp.net.JID;
 import de.fu_berlin.inf.dpp.project.AbstractActivityProvider;
 import de.fu_berlin.inf.dpp.project.ISarosSession;
 import de.fu_berlin.inf.dpp.project.ISarosSessionManager;
-import de.fu_berlin.inf.dpp.project.Messages;
-import de.fu_berlin.inf.dpp.util.Utils;
 
 /**
  * This class processes incoming {@link ProjectsAddedActivity
@@ -22,9 +15,6 @@ import de.fu_berlin.inf.dpp.util.Utils;
  */
 public class ProjectsAddedManager extends AbstractActivityProvider implements
     Startable {
-
-    private static final Logger log = Logger
-        .getLogger(ProjectsAddedManager.class);
 
     protected final ISarosSessionManager sessionManager;
     protected final ISarosSession sarosSession;
@@ -43,22 +33,9 @@ public class ProjectsAddedManager extends AbstractActivityProvider implements
     };
 
     protected void handleFileListActivity(ProjectsAddedActivity fileListActivity) {
-
-        User user = fileListActivity.getSource();
-        if (!user.isInSarosSession()) {
-            throw new IllegalArgumentException(MessageFormat.format(
-                Messages.ProjectsAddedManager_user_no_participant_of_session,
-                user));
-        }
-        if (!user.hasWriteAccess()) {
-            log.warn(Utils.prefix(user.getJID())
-                + " send FileListActivity, but has no writing permission in session"); //$NON-NLS-1$
-            return;
-        }
-        JID from = fileListActivity.getSource().getJID();
-        sessionManager
-            .incomingProjectReceived(from, fileListActivity.getProjectInfos(),
-                fileListActivity.getProcessID());
+        sessionManager.incomingProjectReceived(fileListActivity.getSource()
+            .getJID(), fileListActivity.getProjectInfos(), fileListActivity
+            .getProcessID());
     }
 
     @Override
