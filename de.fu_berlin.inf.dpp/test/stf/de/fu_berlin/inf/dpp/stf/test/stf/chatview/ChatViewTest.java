895653193c859799ebea529b9321211d0a2802e1
Stefan Rossbach <srossbach@arcor.de>
2012-11-20 18:48:26 +0100

    [STF] fixed ChatViewTest test case
    It is possible that the test cases inside
    a test case class are not executed in the
    order they occurs in the code.
    
    If the testChatSend test was not executed first
    the testChatRegex test was failing.
    
    Change-Id: Ibb5007d7e315b418549c95eebb7b6e4b5ac15f80
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/406
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/stf/chatview/ChatViewTest.java
+++ de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/stf/chatview/ChatViewTest.java
@@ -2,17 +2,15 @@ package de.fu_berlin.inf.dpp.stf.test.stf.chatview;
 
 import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.ALICE;
 import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.BOB;
-import static de.fu_berlin.inf.dpp.stf.shared.Constants.VIEW_SAROS;
 import static org.junit.Assert.assertEquals;
 
-import java.rmi.RemoteException;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import de.fu_berlin.inf.dpp.stf.client.StfTestCase;
 import de.fu_berlin.inf.dpp.stf.client.util.Util;
-import de.fu_berlin.inf.dpp.stf.shared.Constants;
 
 public class ChatViewTest extends StfTestCase {
     String messageBob = "Hello Bob";
@@ -23,44 +21,66 @@ public class ChatViewTest extends StfTestCase {
         select(ALICE, BOB);
     }
 
-    @Test
-    public void testChatSend() throws Exception {
+    @Before
+    public void setUp() throws Exception {
+        closeAllShells();
+        closeAllEditors();
+        clearWorkspaces();
+
         Util.setUpSessionWithJavaProjectAndClass("foo", "bar", "test", ALICE,
             BOB);
 
         BOB.superBot().views().packageExplorerView()
             .waitUntilResourceIsShared("foo/src/bar/test.java");
+    }
 
-        assertEquals(Constants.CHATROOM_TAB_LABEL,
-            ALICE.remoteBot().view(VIEW_SAROS).bot().cTabItem().getText());
+    @After
+    public void tearDown() throws Exception {
+        leaveSessionPeersFirst(ALICE);
+    }
+
+    @Test
+    public void testChatSend() throws Exception {
 
         ALICE.superBot().views().sarosView().selectChatroom()
             .sendChatMessage(messageBob);
-        ALICE.remoteBot().sleep(5000);
+
+        /* Wait so that the message is received on the other side */
+        Thread.sleep(5000);
+
         assertEquals(messageBob, BOB.superBot().views().sarosView()
             .selectChatroom().getTextOfLastChatLine());
+
         BOB.superBot().views().sarosView().selectChatroom()
             .sendChatMessage(messageAlice);
-        ALICE.remoteBot().sleep(5000);
+
+        /* Wait so that the message is received on the other side */
+        Thread.sleep(5000);
+
         assertEquals(messageAlice, ALICE.superBot().views().sarosView()
             .selectChatroom().getTextOfLastChatLine());
     }
 
     @Test
-    public void testChatRegex() throws RemoteException {
+    public void testChatRegex() throws Exception {
 
         ALICE.superBot().views().sarosView().selectChatroom()
             .sendChatMessage(messageBob);
+
         ALICE.superBot().views().sarosView().selectChatroom()
             .sendChatMessage(messageBob);
+
         ALICE.superBot().views().sarosView().selectChatroom()
             .sendChatMessage("ababababab");
-        ALICE.superBot().views().sarosView().selectChatroom()
-            .sendChatMessage(messageBob);
+
         ALICE.superBot().views().sarosView().selectChatroom()
             .sendChatMessage(messageBob);
 
-        ALICE.remoteBot().sleep(5000);
+        ALICE.superBot().views().sarosView().selectChatroom()
+            .sendChatMessage(messageBob);
+
+        /* Wait so that the message is received on the other side */
+        Thread.sleep(5000);
 
         assertEquals("ababababab", ALICE.superBot().views().sarosView()
             .selectChatroom().getTextOfChatLine("(ab)++"));
