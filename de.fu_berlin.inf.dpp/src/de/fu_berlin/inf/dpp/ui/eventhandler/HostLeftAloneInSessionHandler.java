e1800f975505a86bf700f562513375afd6bcb091
Stefan Rossbach <srossbach@arcor.de>
2013-02-06 17:19:51 +0100

    [INTERNAL|GUI|STF] always show the exit session dialog if left alone in session
    The STF highly depends on the automatically close of a session that
    is executed by this handler. Stack traces shown that there are
    race conditions and bugs in the project negotiation so that
    a project negotiation returns with exit status OK even if the
    remote participant is no longer in the session and so this handler
    would fail to handle the leave correctly.
    
    The user is now always asked to leave the session if he is left alone
    regardless of the project negotiation status.
    
    Change-Id: I968f69df522af81f803e84ce835a67fd1b527529
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/585
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/eventhandler/HostLeftAloneInSessionHandler.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/eventhandler/HostLeftAloneInSessionHandler.java
@@ -51,9 +51,61 @@ public class HostLeftAloneInSessionHandler {
                      * IncomingProjectNegotiation, and the session will be
                      * closed anyway.
                      */
-                    if (processes.getProcesses().size() == 0) {
-                        handleHostLeftAlone();
-                    }
+
+                    /*
+                     * Stefan Rossbach: Welcome to global state programming,
+                     * threading and network latency.
+                     * 
+                     * This currently does NOT work. It is possible the the
+                     * project negotiation is about to finish (last cancellation
+                     * check passed). As we do not have a final synchronization
+                     * packet in this negotiation process it is possible that
+                     * the user left packet arrives but cancellation packet
+                     * arrives lately or never. See stack trace below were the
+                     * negotiation process passes although Bob leaves the
+                     * session during negotiation.
+                     */
+
+                    /**
+                     * Alice side:
+                     * 
+                     * <pre>
+                     * DEBUG 16:49:08,878 [main] (HostLeftAloneInSessionHandler.java:44) sessionManager.userLeft
+                     * INFO  16:49:08,888 [main] (SarosSession.java:612) Buddy [jenkins_bob_stf@saros-con.imp.fu-berlin.de/Saros]  left session
+                     * DEBUG 16:49:08,888 [Thread-217] (BinaryChannelConnection.java:51) SOCKS5 (direct) [jenkins_bob_stf@saros-con.imp.fu-berlin.de/Saros] Connection was closed by me. socket closed
+                     * TRACE 16:49:09,060 [main] (EditorManager.java:1027) .partActivated invoked
+                     * TRACE 16:49:09,060 [main] (SharedProjectFileDecorator.java:133) User: jenkins_alice_stf activated an editor -> SPath [editorType=txt, path=src/de/alice/HelloWorld.java, project=java]
+                     * TRACE 16:49:09,070 [Worker-10] (SharedProjectFileDecorator.java:301) No Deco: L/java/src/de/alice/HelloWorld.java
+                     * TRACE 16:49:09,083 [Worker-10] (SharedProjectFileDecorator.java:301) No Deco: P/java
+                     * TRACE 16:49:09,122 [RMI TCP Connection(5)-127.0.0.1] (RemoteWorkbenchBot.java:75) opening view with id: de.fu_berlin.inf.dpp.ui.views.SarosView
+                     * DEBUG 16:49:09,422 [Worker-9] (OutgoingProjectNegotiation.java:433) OPN [remote side: jenkins_bob_stf@saros-con.imp.fu-berlin.de/Saros] : archive send
+                     * DEBUG 16:49:09,422 [Worker-9] (CancelableProcess.java:280) process OPN [remote side: jenkins_bob_stf@saros-con.imp.fu-berlin.de/Saros] exit status: OK
+                     * </pre>
+                     * 
+                     * Bob side:
+                     * 
+                     * <pre>
+                     * 
+                     * DEBUG 16:49:09,402 [Worker-6] (FileUtils.java:205) File written to disk: .project
+                     * DEBUG 16:49:09,402 [Worker-6] (FileUtils.java:209) Unpacked archive in 0 s
+                     * TRACE 16:49:09,402 [Worker-6] (ChecksumCacheImpl.java:74) invalidating checksum for existing file: /java/.classpath [0x8F53373DCA77CBCA9383FE23E0132271]
+                     * TRACE 16:49:09,402 [Worker-6] (ChecksumCacheImpl.java:74) invalidating checksum for existing file: /java/.project [0x5A01062F55D09CDA404237A34113627E]
+                     * TRACE 16:49:09,402 [Worker-6] (ChecksumCacheImpl.java:74) invalidating checksum for existing file: /java/src/de/alice/HelloWorld.java [0xCC20B91A3A971472D6482719A2540DA5]
+                     * TRACE 16:49:09,453 [main] (EditorPool.java:278) EditorPool.getAllEditors invoked
+                     * DEBUG 16:49:09,455 [Worker-6] (SharedProjectDecorator.java:80) PROJECT ADDED: 1212967801
+                     * ERROR 16:49:09,455 [Worker-6] (SarosSessionManager.java:782) Internal error in notifying listener of an added project: java.lang.NullPointerException
+                     * at de.fu_berlin.inf.dpp.ui.decorators.SharedProjectDecorator$1.projectAdded(SharedProjectDecorator.java:81)
+                     * at de.fu_berlin.inf.dpp.project.SarosSessionManager.projectAdded(SarosSessionManager.java:780)
+                     * at de.fu_berlin.inf.dpp.invitation.IncomingProjectNegotiation.accept(IncomingProjectNegotiation.java:241)
+                     * at de.fu_berlin.inf.dpp.ui.wizards.AddProjectToSessionWizard$2.run(AddProjectToSessionWizard.java:267)
+                     * at org.eclipse.core.internal.jobs.Worker.run(Worker.java:54)
+                     * DEBUG 16:49:09,456 [Worker-6] (CancelableProcess.java:280) process IPN [remote side: jenkins_alice_stf@saros-con.imp.fu-berlin.de/Saros] exit status: OK
+                     * </pre>
+                     */
+
+                    // if (processes.getProcesses().size() == 0) {
+                    handleHostLeftAlone();
+                    // }
                 }
             }
         };
