7ac2526420d227f89e846428b19f9ef91b6071c3
Stefan Rossbach <srossbach@arcor.de>
2013-03-11 19:19:40 +0100

    [STF] added test case for bug #793
    See: #793 Partial Sharing overwrite non shared files 
    https://sourceforge.net/p/dpp/bugs/793/
    
    Change-Id: I65c1e2ecee55dd4f61edca4e2603b9f3b60207b3
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/634
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/partialsharing/TestSuite.java
+++ de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/partialsharing/TestSuite.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ NeedBasedSyncTest.class,
+@Suite.SuiteClasses({ ModifyNonSharedFilesTest.class, NeedBasedSyncTest.class,
     ShareFilesFromOneProjectToMultipleRemoteProjectsTest.class,
     ShareFilesToProjectsWithDifferentEncodingTest.class, })
 public class TestSuite {
