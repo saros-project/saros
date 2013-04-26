948c3c2679bea486e9701e5c4954e8050cd11200
srossbach <srossbach@arcor.de>
2013-04-26 22:36:00 +0200

    [INTERNAL] use the display provided by Eclipse
    Display.getDefault() may create a new display
    and so would execute code that should no longer
    be executed. This also prevents out of order
    execution if multiple displays may be present
    in the current Eclipse instance.
    
    
    Change-Id: Ia68b1eecce551e0618aebf7a0878d49970c19457
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/752
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/synchronize/internal/SWTSynchronizer.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/synchronize/internal/SWTSynchronizer.java
@@ -1,43 +1,70 @@
 package de.fu_berlin.inf.dpp.synchronize.internal;
 
+import org.apache.log4j.Logger;
+import org.eclipse.swt.SWTException;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
 
 import de.fu_berlin.inf.dpp.annotations.Component;
 import de.fu_berlin.inf.dpp.synchronize.UISynchronizer;
+import de.fu_berlin.inf.dpp.util.StackTrace;
 
 @Component(module = "eclipse")
+// TODO rename to EclipseSWTSynchronizer
 public class SWTSynchronizer implements UISynchronizer {
+
+    private static final Logger LOG = Logger.getLogger(SWTSynchronizer.class);
 
     @Override
     public void asyncExec(Runnable runnable) {
-        Display display = getDisplay();
-
-        if (display.isDisposed())
-            return;
-
-        display.asyncExec(runnable);
+        exec(runnable, true);
     }
 
     @Override
     public void syncExec(Runnable runnable) {
-        Display display = getDisplay();
-
-        if (display.isDisposed())
-            return;
-
-        display.syncExec(runnable);
+        exec(runnable, false);
     }
 
     @Override
     public boolean isUIThread() {
-        return Thread.currentThread() == getDisplay().getThread();
+        // On Win32 it is possible to have multiple displays
+        // return Thread.currentThread() == getDisplay().getThread();
+        return Display.getCurrent() != null;
     }
 
-    /*
-     * always return the default display as some OS like Windows have the
-     * capability to support multiple displays.
-     */
+    private void exec(Runnable runnable, boolean async) {
+        try {
+            Display display = getDisplay();
+
+            /*
+             * this will not work, although the chance is really small, it is
+             * possible that the device is disposed after this check and before
+             * the a(sync)Exec call
+             */
+            // if (display.isDisposed())
+            // return;
+
+            if (async)
+                display.asyncExec(runnable);
+            else
+                display.syncExec(runnable);
+
+        } catch (SWTException e) {
+
+            if (PlatformUI.getWorkbench().isClosing()) {
+                LOG.warn("could not execute runnable " + runnable
+                    + ", UI thread is not available", new StackTrace());
+            } else {
+                LOG.error(
+                    "could not execute runnable "
+                        + runnable
+                        + ", workbench display was disposed before workbench shutdown",
+                    e);
+            }
+        }
+    }
+
     private Display getDisplay() {
-        return Display.getDefault();
+        return PlatformUI.getWorkbench().getDisplay();
     }
 }
