33ff5ab719929e1df41928c616e57133e7a81bec
srossbach <srossbach@arcor.de>
2013-05-06 21:13:12 +0200

    [REFACTOR] moved static methods
    Moved the optimize methods from the
    ActivitySquencer to the ActivityUtils class.
    
    Change-Id: Ie177fe4877c8358aa5fa406ba7bfe75a60e00787
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/773
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/internal/ActivitySequencer.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/internal/ActivitySequencer.java
@@ -22,7 +22,6 @@ package de.fu_berlin.inf.dpp.net.internal;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -34,13 +33,7 @@ import org.jivesoftware.smack.packet.PacketExtension;
 import org.picocontainer.Startable;
 
 import de.fu_berlin.inf.dpp.User;
-import de.fu_berlin.inf.dpp.activities.SPathDataObject;
-import de.fu_berlin.inf.dpp.activities.business.FolderActivity.Type;
-import de.fu_berlin.inf.dpp.activities.serializable.FolderActivityDataObject;
 import de.fu_berlin.inf.dpp.activities.serializable.IActivityDataObject;
-import de.fu_berlin.inf.dpp.activities.serializable.TextEditActivityDataObject;
-import de.fu_berlin.inf.dpp.activities.serializable.TextSelectionActivityDataObject;
-import de.fu_berlin.inf.dpp.activities.serializable.ViewportActivityDataObject;
 import de.fu_berlin.inf.dpp.net.ITransmitter;
 import de.fu_berlin.inf.dpp.net.JID;
 import de.fu_berlin.inf.dpp.net.business.DispatchThreadContext;
@@ -201,7 +194,8 @@ public class ActivitySequencer implements Startable {
 
                     for (Entry<User, List<IActivityDataObject>> e : toSend
                         .entrySet()) {
-                        sendActivities(e.getKey(), optimize(e.getValue()));
+                        sendActivities(e.getKey(),
+                            ActivityUtils.optimize(e.getValue()));
                     }
 
                     return abort;
@@ -374,119 +368,6 @@ public class ActivitySequencer implements Startable {
      */
     public Map<JID, Integer> getExpectedSequenceNumbers() {
         return incomingQueues.getExpectedSequenceNumbers();
-    }
-
-    /**
-     * This method tries to reduce the number of activityDataObjects transmitted
-     * by removing activityDataObjects that would overwrite each other and
-     * joining activityDataObjects that can be send as a single
-     * activityDataObject.
-     */
-    private static List<IActivityDataObject> optimize(
-        List<IActivityDataObject> toOptimize) {
-
-        List<IActivityDataObject> result = new ArrayList<IActivityDataObject>(
-            toOptimize.size());
-
-        TextSelectionActivityDataObject selection = null;
-        LinkedHashMap<SPathDataObject, ViewportActivityDataObject> viewport = new LinkedHashMap<SPathDataObject, ViewportActivityDataObject>();
-
-        for (IActivityDataObject activityDataObject : toOptimize) {
-
-            if (activityDataObject instanceof TextEditActivityDataObject) {
-                TextEditActivityDataObject textEdit = (TextEditActivityDataObject) activityDataObject;
-                textEdit = joinTextEdits(result, textEdit);
-                result.add(textEdit);
-            } else if (activityDataObject instanceof TextSelectionActivityDataObject) {
-                selection = (TextSelectionActivityDataObject) activityDataObject;
-            } else if (activityDataObject instanceof ViewportActivityDataObject) {
-                ViewportActivityDataObject viewActivity = (ViewportActivityDataObject) activityDataObject;
-                viewport.remove(viewActivity.getPath());
-                viewport.put(viewActivity.getPath(), viewActivity);
-            } else if (activityDataObject instanceof FolderActivityDataObject) {
-                FolderActivityDataObject folderEdit = (FolderActivityDataObject) activityDataObject;
-                foldRecursiveDelete(result, folderEdit);
-            } else {
-                result.add(activityDataObject);
-            }
-        }
-
-        // only send one selection activityDataObject
-        if (selection != null)
-            result.add(selection);
-
-        // Add only one viewport per editor
-        for (Entry<SPathDataObject, ViewportActivityDataObject> entry : viewport
-            .entrySet()) {
-            result.add(entry.getValue());
-        }
-
-        assert !result.contains(null);
-
-        return result;
-    }
-
-    private static void foldRecursiveDelete(List<IActivityDataObject> result,
-        FolderActivityDataObject folderEdit) {
-
-        if (folderEdit.getType() != Type.Removed) {
-            result.add(folderEdit);
-            return;
-        }
-
-        int i = result.size() - 1;
-        boolean dropNew = false;
-
-        while (i >= 0 && !dropNew) {
-            FolderActivityDataObject curr = null;
-
-            if (result.get(i) instanceof FolderActivityDataObject)
-                curr = (FolderActivityDataObject) result.get(i);
-            else {
-                i--;
-                continue;
-            }
-
-            if (curr.isChildOf(folderEdit))
-                result.remove(i);
-
-            else if (curr.getPath().equals(folderEdit.getPath())) {
-                result.remove(i);
-                dropNew = true;
-            }
-
-            i--;
-        }
-
-        if (!dropNew)
-            result.add(folderEdit);
-    }
-
-    private static TextEditActivityDataObject joinTextEdits(
-        List<IActivityDataObject> result, TextEditActivityDataObject textEdit) {
-        if (result.size() == 0) {
-            return textEdit;
-        }
-
-        IActivityDataObject lastActivity = result.get(result.size() - 1);
-        if (lastActivity instanceof TextEditActivityDataObject) {
-            TextEditActivityDataObject lastTextEdit = (TextEditActivityDataObject) lastActivity;
-
-            if (((lastTextEdit.getSource() == null) || lastTextEdit.getSource()
-                .equals(textEdit.getSource()))
-                && (textEdit.getOffset() == lastTextEdit.getOffset()
-                    + lastTextEdit.getText().length())) {
-                result.remove(lastTextEdit);
-                textEdit = new TextEditActivityDataObject(
-                    lastTextEdit.getSource(),
-                    lastTextEdit.getOffset(),
-                    lastTextEdit.getText() + textEdit.getText(),
-                    lastTextEdit.getReplacedText() + textEdit.getReplacedText(),
-                    lastTextEdit.getPath());
-            }
-        }
-
-        return textEdit;
     }
 
     /**
