saros-project/saros

commit 609885ac8a6518b3c9c3771560989cf2d5ff2dcb

[INTERNAL] Changed moment when certain operations take place
Added the new methods userStartedQueuing() and
userFinishedProjectNegotiation to the ISharedProjectListener,
indicating when a user is able to process IResourceActivities. 

userStartedQueuing is called when the client started
queuing and indicates, that he can now queue IRessourceActivities.
userFinishedProjectNegotiation is transmitted to every participant,
signaling them that the new user is now able to process
IRessourceActivities.

Some operations where executed too early. These operations were moved
from userJoined() to the new methods.

1. The clients now send their awareness-informations (which editor is
open etc.) after the user finished the Project Negotiation(PN). As a
result the user can now
see these informations as soon as he finished his PN.

2. The host now adds a new user to the JupiterServer after that user has
started queuing. As a result inconsistent JupiterTimestamps, caused by 
JupiterActivities sent between Session Negotiation(SN) and PN, can be
prevented.

3. Host only sends IResourceActivities to users that can process them.
As a result obviously failing Activity-Transformations (resources
doesn't exist between SN and PN) at the joining user can be prevented,
resulting in less exceptions. 

Change-Id: Iba0dfc4154a8bc148d49358aab344a4f0671a3fa
Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/869
Tested-by: Jenkins CI
Reviewed-by: Stefan Rossbach <srossbach@arcor.de>
Reviewed-by: Patrick Schlott <pschlottk@gmail.com>

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/internal/SarosProjectMapper.java
@@ -16,6 +16,7 @@ import org.eclipse.core.resources.IResource;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 
+import de.fu_berlin.inf.dpp.User;
 import de.fu_berlin.inf.dpp.net.JID;
 import de.fu_berlin.inf.dpp.project.ISarosSession;
 import de.fu_berlin.inf.dpp.project.SharedProject;
@@ -62,10 +63,15 @@ class SarosProjectMapper {
     private HashMap<JID, List<IProject>> projectOwnershipMapping = new HashMap<JID, List<IProject>>();
 
     /**
+     * Map containing the projects of the clients. Used by the host to determine
+     * which Activities can be send.
+     */
+    private HashMap<User, List<String>> projectsOfUsers = new HashMap<User, List<String>>();
+
+    /**
      * Map containing the partially shared resources for each shared project.
      * The value is <code>null</code> for completely shared projects.
      */
-
     private HashMap<IProject, Set<IResource>> partiallySharedResourceMapping = new HashMap<IProject, Set<IResource>>();
 
     /** Mapping from project IDs to {@link SharedProject} instances */
@@ -518,5 +524,39 @@ class SarosProjectMapper {
      */
     public synchronized boolean isPartiallyShared(IProject project) {
         return partiallySharedProjects.contains(project);
+    }
+
+    /**
+     * Checks if the given user already has the given project.
+     * 
+     * @param user
+     *            The user to be checked
+     * @param project
+     *            The project to be checked
+     */
+    public synchronized boolean userHasProject(User user, IProject project) {
+        return projectsOfUsers.get(user).contains(getID(project));
+    }
+
+    /**
+     * Adds all missing projects to the projects of the given user. This should
+     * be called once the user started queuing.
+     * 
+     * @param user
+     */
+    public synchronized void addMissingProjectsToUser(User user) {
+        List<String> projects = new ArrayList<String>();
+        for (String project : sharedProjects.keySet()) {
+            projects.add(project);
+        }
+
+        this.projectsOfUsers.put(user, projects);
+    }
+
+    /**
+     * Removes the user-project mapping of the user that left the session.
+     */
+    public void userLeft(User user) {
+        projectsOfUsers.remove(user);
     }
 }

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/SarosContext.java
@@ -78,6 +78,7 @@ import de.fu_berlin.inf.dpp.net.internal.extensions.PingExtension;
 import de.fu_berlin.inf.dpp.net.internal.extensions.PongExtension;
 import de.fu_berlin.inf.dpp.net.internal.extensions.ProjectNegotiationOfferingExtension;
 import de.fu_berlin.inf.dpp.net.internal.extensions.SarosLeaveExtension;
+import de.fu_berlin.inf.dpp.net.internal.extensions.UserFinishedProjectNegotiationExtension;
 import de.fu_berlin.inf.dpp.net.internal.extensions.UserListExtension;
 import de.fu_berlin.inf.dpp.net.internal.extensions.UserListReceivedExtension;
 import de.fu_berlin.inf.dpp.net.internal.extensions.UserListRequestExtension;
@@ -354,6 +355,7 @@ public class SarosContext implements ISarosContext {
             Class.forName(PingExtension.class.getName());
             Class.forName(PongExtension.class.getName());
             Class.forName(ProjectNegotiationOfferingExtension.class.getName());
+            Class.forName(UserFinishedProjectNegotiationExtension.class.getName());
 
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/concurrent/management/ConcurrentDocumentServer.java
@@ -71,7 +71,7 @@ public class ConcurrentDocumentServer implements Startable {
     public class HostSideProjectListener extends AbstractSharedProjectListener {
 
         @Override
-        public void userJoined(User user) {
+        public void userStartedQueuing(User user) {
             server.addUser(user);
         }
 

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/editor/EditorManager.java
@@ -244,7 +244,7 @@ public class EditorManager extends AbstractActivityProvider {
         }
 
         @Override
-        public void userJoined(User user) {
+        public void userFinishedProjectNegotiation(User user) {
 
             // TODO The user should be able to ask us for this state
 

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/invitation/OutgoingProjectNegotiation.java
@@ -112,6 +112,10 @@ public class OutgoingProjectNegotiation extends ProjectNegotiation {
             getRemoteFileList(monitor);
             monitor.subTask("");
 
+            // inform all listeners that the peer has started queuing and can
+            // therefore process IResourceActivities now
+            sarosSession.userStartedQueuing(sarosSession.getUser(peer));
+
             editorManager.setAllLocalOpenedEditorsLocked(false);
             zipArchives = createProjectArchives(projectFilesToSend, monitor);
             monitor.subTask("");
@@ -149,6 +153,8 @@ public class OutgoingProjectNegotiation extends ProjectNegotiation {
             deleteCollectors();
             monitor.done();
         }
+
+        sarosSession.userFinishedProjectNegotiation(sarosSession.getUser(peer));
 
         return terminateProcess(exception);
     }

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/internal/StreamServiceManager.java
@@ -74,6 +74,7 @@ import de.fu_berlin.inf.dpp.net.internal.StreamSession.StreamSessionOutputStream
 import de.fu_berlin.inf.dpp.observables.SarosSessionObservable;
 import de.fu_berlin.inf.dpp.observables.SessionIDObservable;
 import de.fu_berlin.inf.dpp.project.AbstractSarosSessionListener;
+import de.fu_berlin.inf.dpp.project.AbstractSharedProjectListener;
 import de.fu_berlin.inf.dpp.project.ISarosSession;
 import de.fu_berlin.inf.dpp.project.ISarosSessionManager;
 import de.fu_berlin.inf.dpp.project.ISharedProjectListener;
@@ -1736,8 +1737,8 @@ public class StreamServiceManager implements Startable {
     /**
      * Removes a user's sessions when he leaves the shared project
      */
-    protected final class SharedProjectListener implements
-        ISharedProjectListener {
+    protected final class SharedProjectListener extends
+        AbstractSharedProjectListener {
         @Override
         public void userLeft(User user) {
 
@@ -1751,16 +1752,6 @@ public class StreamServiceManager implements Startable {
                     session.reportErrorAndDispose(new ReceiverGoneException());
                 }
             }
-        }
-
-        @Override
-        public void userJoined(User user) {
-            // NOP
-        }
-
-        @Override
-        public void permissionChanged(User user) {
-            // NOP
         }
     }
 

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/internal/extensions/UserFinishedProjectNegotiationExtension.java
@@ -1,0 +1,29 @@
+package de.fu_berlin.inf.dpp.net.internal.extensions;
+
+import de.fu_berlin.inf.dpp.net.JID;
+
+public class UserFinishedProjectNegotiationExtension extends
+    SarosSessionPacketExtension {
+
+    public static final Provider PROVIDER = new Provider();
+    private JID jid;
+
+    public UserFinishedProjectNegotiationExtension(String sessionID, JID jid) {
+        super(sessionID);
+        this.jid = jid;
+    }
+
+    public JID getJID() {
+        return jid;
+    }
+
+    public static class Provider
+        extends
+        SarosSessionPacketExtension.Provider<UserFinishedProjectNegotiationExtension> {
+
+        private Provider() {
+            super("userFinishedProjectNegotiation",
+                UserFinishedProjectNegotiationExtension.class);
+        }
+    }
+}

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/AbstractSarosSessionListener.java
@@ -36,7 +36,8 @@ public class AbstractSarosSessionListener implements ISarosSessionListener {
     }
 
     @Override
-    public void postOutgoingInvitationCompleted(IProgressMonitor monitor, User user) {
+    public void postOutgoingInvitationCompleted(IProgressMonitor monitor,
+        User user) {
         // do nothing
     }
 

commit f6b46f2d4497e37b50e3a5ad5d87d0bcd09ec2d0

[FIX] Prevent NPE in SarosProjectMapper
This Bug was introduced in commit
609885ac8a6518b3c9c3771560989cf2d5ff2dcb. Happened when Bob sends
IRessourceActivities while Carl is still in SessionNegotiation.

Change-Id: I834165fb26f3cbc70c1a651d6012ef2356bdff4a
Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/882
Reviewed-by: Franz Zieris <zieris@inf.fu-berlin.de>
Tested-by: Jenkins CI
Reviewed-by: Stefan Rossbach <srossbach@arcor.de>
Reviewed-by: Patrick Schlott <pschlottk@gmail.com>

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/project/internal/SarosProjectMapper.java
@@ -535,7 +535,10 @@ class SarosProjectMapper {
      *            The project to be checked
      */
     public synchronized boolean userHasProject(User user, IProject project) {
-        return projectsOfUsers.get(user).contains(getID(project));
+        if (projectsOfUsers.containsKey(user)) {
+            return projectsOfUsers.get(user).contains(getID(project));
+        }
+        return false;
     }
 
     /**