777548e26985f50d951843bb9ef15888ee89724e
Stefan Rossbach <srossbach@arcor.de>
2013-08-23 18:15:48 +0200

    [FEATURE] hyperlinks for chat communication
    This patch enables support for hyperlinks within
    chat communication. Valid URLs inside a chat message
    are colored and underlined and will be opened
    with the default system browser if a user clicks
    on such a hyperlink.
    
    
    Change-Id: Ia6a651cad486f40a26521f11d78de0650d061b59
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/879
    Tested-by: Jenkins CI
    Reviewed-by: Richard Möhn <richard.moehn@fu-berlin.de>
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/widgets/chatControl/items/ChatLine.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/widgets/chatControl/items/ChatLine.java
@@ -1,10 +1,22 @@
 package de.fu_berlin.inf.dpp.ui.widgets.chatControl.items;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+
+import de.fu_berlin.inf.dpp.ui.util.SWTUtils;
 
 /**
  * This composite is used to display a chat message.
@@ -14,6 +26,16 @@ import org.eclipse.swt.widgets.Composite;
 public class ChatLine extends Composite {
     protected StyledText text;
 
+    private static class WordLocation {
+        public int offset;
+        public int length;
+
+        public WordLocation(int offset, int length) {
+            this.offset = offset;
+            this.length = length;
+        }
+    }
+
     public ChatLine(Composite parent, String message) {
         super(parent, SWT.NONE);
         this.setLayout(new FillLayout());
@@ -21,6 +43,8 @@ public class ChatLine extends Composite {
         text = new StyledText(this, SWT.WRAP);
         text.setText(message);
         text.setEditable(false);
+        decorateHyperLinks(text);
+        addHyperLinkListener(text);
     }
 
     @Override
@@ -31,5 +55,120 @@ public class ChatLine extends Composite {
 
     public String getText() {
         return text.getText();
+    }
+
+    /**
+     * Returns a list of {@linkplain WordLocation word locations} for the given
+     * text. Each word location will contain the offset (0 based) indicating the
+     * start inside the text and its length. A word is considered to not contain
+     * any whitespace characters and its length is greater or equal to 1. This
+     * method <b>offers no support</b> for escaping whitespace characters.
+     * 
+     * <pre>
+     * Example: " foo bar" (without quotes) will return the word locations
+     * 1, 3
+     * 5, 3
+     * </pre>
+     * 
+     * 
+     * @param text
+     *            the text to be searched
+     * @return list of {@linkplain WordLocation word locations} containing the
+     *         offset and length of each word
+     */
+    private List<WordLocation> getWordLocations(String text) {
+
+        List<WordLocation> links = new ArrayList<WordLocation>();
+
+        int startIdx = 0, currentIdx = 0;
+
+        for (Character c : text.toCharArray()) {
+            if (Character.isWhitespace(c)) {
+
+                if (currentIdx > startIdx)
+                    links
+                        .add(new WordLocation(startIdx, currentIdx - startIdx));
+
+                startIdx = currentIdx + 1;
+            }
+
+            currentIdx++;
+        }
+
+        if (currentIdx > startIdx)
+            links.add(new WordLocation(startIdx, currentIdx - startIdx));
+
+        return links;
+    }
+
+    private void decorateHyperLinks(StyledText text) {
+
+        String content = text.getText();
+
+        List<WordLocation> wordLocations = getWordLocations(content);
+
+        for (Iterator<WordLocation> it = wordLocations.iterator(); it.hasNext();) {
+
+            WordLocation wordLocation = it.next();
+
+            String word = content.substring(wordLocation.offset,
+                wordLocation.offset + wordLocation.length);
+
+            if (!isValidURL(word))
+                it.remove();
+        }
+
+        if (wordLocations.isEmpty())
+            return;
+
+        StyleRange[] styles = new StyleRange[wordLocations.size()];
+
+        int[] ranges = new int[wordLocations.size() * 2];
+
+        int rangeIdx = 0, styleIdx = 0;
+
+        for (WordLocation wordLocation : wordLocations) {
+            ranges[rangeIdx++] = wordLocation.offset;
+            ranges[rangeIdx++] = wordLocation.length;
+
+            StyleRange style = new StyleRange();
+            style.underline = true;
+            style.underlineStyle = SWT.UNDERLINE_LINK;
+            style.data = content.substring(wordLocation.offset,
+                wordLocation.offset + wordLocation.length);
+            styles[styleIdx++] = style;
+        }
+
+        text.setStyleRanges(ranges, styles);
+    }
+
+    private void addHyperLinkListener(final StyledText text) {
+        text.addListener(SWT.MouseDown, new Listener() {
+            @Override
+            public void handleEvent(Event event) {
+                try {
+
+                    int offset = text.getOffsetAtLocation(new Point(event.x,
+                        event.y));
+
+                    StyleRange style = text.getStyleRangeAtOffset(offset);
+                    if (style != null && style.underline
+                        && style.underlineStyle == SWT.UNDERLINE_LINK) {
+                        String url = (String) style.data;
+                        SWTUtils.openInternalBrowser(url, url);
+                    }
+                } catch (IllegalArgumentException e) {
+                    // no character under event.x, event.y
+                }
+            }
+        });
+    }
+
+    private boolean isValidURL(String urlString) {
+        try {
+            return /* dummy = */new URL(urlString) != null;
+        } catch (MalformedURLException e) {
+            return false;
+        }
     }
 }
