19eb4203cc316ce4c81a4b6235ac6d18c1ebc004
Stefan Rossbach <srossbach@arcor.de>
2013-10-09 15:01:44 +0200

    [API] deprecated SessionIDObservable class
    Because it was and is misused in all places the class is accessed.
    
    Change-Id: I4963d66e248a6f5cb4144d0dbdcb797dc59120e1
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1140
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/observables/SessionIDObservable.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/observables/SessionIDObservable.java
@@ -1,6 +1,7 @@
 package de.fu_berlin.inf.dpp.observables;
 
 import de.fu_berlin.inf.dpp.annotations.Component;
+import de.fu_berlin.inf.dpp.project.ISarosSession;
 import de.fu_berlin.inf.dpp.util.ObservableValue;
 
 /**
@@ -12,7 +13,13 @@ import de.fu_berlin.inf.dpp.util.ObservableValue;
  * 
  * If in a shared project session the value of this Observable is the string
  * representation of a random integer.
+ * 
+ * @deprecated The common usage in Saros is to use this observable as a global
+ *             variable to perform global state programming which was not
+ *             intended. Use {@link ISarosSession#getID()} instead which will be
+ *             almost equivalent in most cases.
  */
+@Deprecated
 @Component(module = "observables")
 public class SessionIDObservable extends ObservableValue<String> {
 
