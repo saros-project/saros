ff3f7d5766d08ed5d97413387b780c0059f6acab
Stefan Rossbach <srossbach@arcor.de>
2013-10-20 16:03:02 +0200

    [INTERNAL] ensure thread safety
    SharedEditorListenerDispatch was not thread safe regarding adding /
    removing and iterating over the listeners. Just replaced the ArrayList
    with an CopyOnWriteArrayList.
    
    Change-Id: Iaacd73d06d47e7f6a611f1a2e4da91b9f40ed853
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1174
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/editor/SharedEditorListenerDispatch.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/editor/SharedEditorListenerDispatch.java
@@ -1,7 +1,6 @@
 package de.fu_berlin.inf.dpp.editor;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.eclipse.jface.text.source.ILineRange;
 import org.eclipse.ui.IEditorPart;
@@ -17,88 +16,77 @@ import de.fu_berlin.inf.dpp.activities.business.ViewportActivity;
  */
 public class SharedEditorListenerDispatch implements ISharedEditorListener {
 
-    protected List<ISharedEditorListener> editorListeners = new ArrayList<ISharedEditorListener>();
+    private CopyOnWriteArrayList<ISharedEditorListener> editorListeners = new CopyOnWriteArrayList<ISharedEditorListener>();
 
     public void add(ISharedEditorListener editorListener) {
-        if (!this.editorListeners.contains(editorListener)) {
-            this.editorListeners.add(editorListener);
-        }
+        editorListeners.addIfAbsent(editorListener);
     }
 
     public void remove(ISharedEditorListener editorListener) {
-        this.editorListeners.remove(editorListener);
+        editorListeners.remove(editorListener);
     }
 
     @Override
     public void activeEditorChanged(User user, SPath path) {
-        for (ISharedEditorListener listener : editorListeners) {
+        for (ISharedEditorListener listener : editorListeners)
             listener.activeEditorChanged(user, path);
-        }
     }
 
     @Override
     public void editorRemoved(User user, SPath path) {
-        for (ISharedEditorListener listener : editorListeners) {
+        for (ISharedEditorListener listener : editorListeners)
             listener.editorRemoved(user, path);
-        }
     }
 
     @Override
     public void userWithWriteAccessEditorSaved(SPath path, boolean replicated) {
-        for (ISharedEditorListener listener : editorListeners) {
+        for (ISharedEditorListener listener : editorListeners)
             listener.userWithWriteAccessEditorSaved(path, replicated);
-        }
     }
 
     @Override
     public void followModeChanged(User user, boolean isFollowed) {
-        for (ISharedEditorListener listener : editorListeners) {
+        for (ISharedEditorListener listener : editorListeners)
             listener.followModeChanged(user, isFollowed);
-        }
     }
 
     @Override
     public void textEditRecieved(User user, SPath editor, String text,
         String replacedText, int offset) {
-        for (ISharedEditorListener listener : editorListeners) {
+
+        for (ISharedEditorListener listener : editorListeners)
             listener.textEditRecieved(user, editor, text, replacedText, offset);
-        }
     }
 
     @Override
     public void textSelectionMade(TextSelectionActivity selection) {
-        for (ISharedEditorListener listener : editorListeners) {
+        for (ISharedEditorListener listener : editorListeners)
             listener.textSelectionMade(selection);
-        }
-
     }
 
     @Override
     public void viewportChanged(ViewportActivity viewport) {
-        for (ISharedEditorListener listener : editorListeners) {
+        for (ISharedEditorListener listener : editorListeners)
             listener.viewportChanged(viewport);
-        }
     }
 
     @Override
     public void jumpedToUser(User jumpedTo) {
-        for (ISharedEditorListener listener : editorListeners) {
+        for (ISharedEditorListener listener : editorListeners)
             listener.jumpedToUser(jumpedTo);
-        }
     }
 
     @Override
     public void viewportGenerated(IEditorPart part, ILineRange viewport,
         SPath path) {
-        for (ISharedEditorListener listener : editorListeners) {
+
+        for (ISharedEditorListener listener : editorListeners)
             listener.viewportGenerated(part, viewport, path);
-        }
     }
 
     @Override
     public void colorChanged() {
-        for (ISharedEditorListener listener : editorListeners) {
+        for (ISharedEditorListener listener : editorListeners)
             listener.colorChanged();
-        }
     }
 }
