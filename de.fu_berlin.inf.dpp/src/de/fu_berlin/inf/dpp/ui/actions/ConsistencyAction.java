80f739adbbba1a9250c81213c26a41fbc7e569dc
Stefan Rossbach <srossbach@arcor.de>
2013-11-02 18:23:19 +0100

    [GUI] inconsistency icon now "flashes" on detected inconsistency
    I am pretty sure this will violate the default Eclipse UX design guide
    as this change turns the Saros View much more into a GUI of a computer
    game but: if you want the user to pay attention of something you must
    catch its attention ... and making something flashing will mostly not be
    left unrecognized.
    
    If this is too disturbing we can still remove it.
    
    Change-Id: I8391aba743fd484dd3a74d0ab9b3b350387bf7c6
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1208
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/actions/ConsistencyAction.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/actions/ConsistencyAction.java
@@ -12,6 +12,9 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.picocontainer.annotations.Inject;
 
@@ -31,16 +34,49 @@ import de.fu_berlin.inf.dpp.ui.views.SarosView;
 import de.fu_berlin.inf.dpp.util.Utils;
 import de.fu_berlin.inf.dpp.util.ValueChangeListener;
 
+/*
+ * Please be aware of how we exploit the set***ImageDescriptor methods.
+ * 
+ * setImageDescriptor("foo") with setDisabledImageDescriptor(null)
+ * 
+ * will display a gray scaled "foo" if the action is disabled while
+ * 
+ * setImageDescriptor("foo") with setDisabledImageDescriptor("bar")
+ * 
+ * will display "bar" if the action is disabled
+ */
 @Component(module = "action")
 public class ConsistencyAction extends Action {
+
+    private static final int MIN_ALPHA_VALUE = 64;
+    private static final int MAX_ALPHA_VALUE = 255;
+
+    private static final int FADE_UPDATE_SPEED = 100; // ms
+    private static final int FADE_SPEED_DELTA = 16;
+    private static final int FADE_DOWN = -1;
+    private static final int FADE_UP = 1;
 
     private static final ImageDescriptor IN_SYNC = ImageManager
         .getImageDescriptor("icons/etool16/in_sync.png"); //$NON-NLS-1$;
 
-    private static final ImageDescriptor OUT_SYNC = ImageManager
-        .getImageDescriptor("icons/etool16/out_sync.png"); //$NON-NLS-1$;
+    private static final Image OUT_SYNC = ImageManager
+        .getImage("icons/etool16/out_sync.png"); //$NON-NLS-1$;
 
     private static final Logger log = Logger.getLogger(ConsistencyAction.class);
+
+    private static class MemoryImageDescriptor extends ImageDescriptor {
+
+        private final ImageData data;
+
+        public MemoryImageDescriptor(ImageData data) {
+            this.data = data;
+        }
+
+        @Override
+        public ImageData getImageData() {
+            return data;
+        }
+    }
 
     @Inject
     protected ISarosSessionManager sessionManager;
@@ -50,6 +86,8 @@ public class ConsistencyAction extends Action {
 
     @Inject
     protected IsInconsistentObservable inconsistentObservable;
+
+    private boolean isFading;
 
     public ConsistencyAction() {
 
@@ -96,6 +134,18 @@ public class ConsistencyAction extends Action {
             inconsistentObservable.addAndNotify(isConsistencyListener);
         } else {
             setEnabled(false);
+
+            /*
+             * make sure we reset the default "enabled image" otherwise the GUI
+             * will grayscale the alpha scaled image and so it is possible that
+             * nothing is displayed anymore
+             */
+            SWTUtils.runSafeSWTSync(log, new Runnable() {
+                @Override
+                public void run() {
+                    setImageDescriptor(IN_SYNC);
+                }
+            });
         }
     }
 
@@ -119,7 +169,17 @@ public class ConsistencyAction extends Action {
                 return;
             }
 
-            setImageDescriptor(OUT_SYNC);
+            SWTUtils.runSafeSWTSync(log, new Runnable() {
+
+                @Override
+                public void run() {
+                    if (isFading)
+                        return;
+
+                    startFading(MAX_ALPHA_VALUE, FADE_DOWN);
+                }
+
+            });
 
             final Set<SPath> paths = new HashSet<SPath>(
                 watchdogClient.getPathsWithWrongChecksums());
@@ -151,7 +211,6 @@ public class ConsistencyAction extends Action {
                 }
             });
         }
-
     };
 
     @Override
@@ -190,5 +249,74 @@ public class ConsistencyAction extends Action {
                 }
             }
         });
+    }
+
+    /**
+     * Must be called within the SWT context.
+     */
+    private void startFading(final int startValue, final int direction) {
+        Display display = SWTUtils.getDisplay();
+
+        isFading = false;
+
+        if (display.isDisposed() || !isEnabled())
+            return;
+
+        isFading = true;
+
+        display.timerExec(FADE_UPDATE_SPEED, new Runnable() {
+
+            @Override
+            public void run() {
+
+                if (!isEnabled()) {
+                    setImageDescriptor(IN_SYNC);
+                    isFading = false;
+                    return;
+                }
+
+                int newValue = startValue + FADE_SPEED_DELTA * direction;
+                int newDirection = direction;
+
+                if (newValue > MAX_ALPHA_VALUE) {
+                    newValue = MAX_ALPHA_VALUE;
+                    newDirection = FADE_DOWN;
+                } else if (newValue < MIN_ALPHA_VALUE) {
+                    newValue = MIN_ALPHA_VALUE;
+                    newDirection = FADE_UP;
+                }
+
+                setImageDescriptor(new MemoryImageDescriptor(
+                    modifyAlphaChannel(OUT_SYNC, newValue)));
+
+                startFading(newValue, newDirection);
+            }
+
+        });
+    }
+
+    private static ImageData modifyAlphaChannel(Image image, int alpha) {
+
+        if (alpha < 0)
+            alpha = 0;
+
+        if (alpha > 255)
+            alpha = 255;
+
+        ImageData data = image.getImageData();
+
+        for (int x = 0; x < data.width; x++) {
+            for (int y = 0; y < data.height; y++) {
+                int a = data.getAlpha(x, y);
+
+                // value depends on the image, must be determined empirically
+                if (a <= MIN_ALPHA_VALUE)
+                    continue;
+
+                data.setAlpha(x, y, alpha);
+            }
+        }
+
+        return data;
     }
 }
