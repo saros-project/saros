saros-project/saros

commit 36b49c5de05d51517f2c75225d439bdef2bc7bda

[INTERNAL|REFACTOR] removed preferences from UPNPService
They were mainly used to remove previous port mappings.

Problem: The right way to remove those mappings is to query
the IGD for all port forwardings and then delete those that match.

As it is very uncommon that.
1. Eclipse crashes
2. A user or SMACK will change ports after the crash
3. The port mapping is still overwritten if it was created by Saros

It is removed and will be replaced later with the recommendation above

REFACTOR: moved most parts from the UPNPService to SarosNet because the
UPNPService should handle the port mapping and should not start to
retrieve the port from the Socks5 server.

Change-Id: I103298ee8d0b2fb3ca6a858f5002dd1e2e91f469
Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1249
Tested-by: Jenkins CI
Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/SarosNet.java
@@ -2,12 +2,14 @@ package de.fu_berlin.inf.dpp.net;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
 
 import org.apache.log4j.Logger;
+import org.bitlet.weupnp.GatewayDevice;
 import org.jivesoftware.smack.Connection;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.ConnectionListener;
@@ -29,9 +31,13 @@ import de.fu_berlin.inf.dpp.util.Utils;
  * This class is responsible for establishing XMPP connections and notifying
  * registered listeners about the state of the current connection.
  */
+// FIXME synchronization of the whole connect / disconnect logic !!!
 @Component(module = "net")
 public class SarosNet {
     private static final Logger LOG = Logger.getLogger(SarosNet.class);
+
+    // DO NOT CHANGE THE CONTENT OF THIS STRING, NEVER NEVER NEVER !!!
+    private static final String PORT_MAPPING_DESCRIPTION = "Saros Socks5 TCP";
 
     private Connection connection;
 
@@ -39,9 +45,24 @@ public class SarosNet {
     private String resource;
     private int proxyPort;
     private boolean isProxyEnabled;
+    private String gatewayDeviceID;
+
+    /** The current IP address of the STUN server or <code>null</code>. */
     private String stunServer;
+
+    /** The current port of the STUN server or <code>null</code>. */
     private int stunPort;
-    private boolean isPortMappingEnabled;
+
+    /** The current gateway device to use for port mapping or <code>null</code>. */
+    private GatewayDevice device;
+
+    /**
+     * The listening port of the current Socks5Proxy. Is <b>-1</b> if the proxy
+     * is not running.
+     */
+    private int socks5ProxyPort;
+
+    private final Object portMappingLock = new Object();
 
     private JID localJID;
 
@@ -59,6 +80,42 @@ public class SarosNet {
     private final IUPnPService upnpService;
 
     private int packetReplyTimeout;
+
+    private final class XMPPConnectionListener implements ConnectionListener {
+
+        @Override
+        public void connectionClosed() {
+            /*
+             * see Smack Source: connectionClosed is called before
+             * connectionClosedOnError and so would violate the state transition
+             * described in the ConnectionState class
+             */
+        }
+
+        @Override
+        public void connectionClosedOnError(Exception e) {
+            LOG.error("XMPP connection error: ", e);
+            setConnectionState(ConnectionState.ERROR, e);
+            disconnectInternal();
+            setConnectionState(ConnectionState.NOT_CONNECTED, null);
+            localJID = null;
+        }
+
+        @Override
+        public void reconnectingIn(int seconds) {
+            // NOP
+        }
+
+        @Override
+        public void reconnectionFailed(Exception e) {
+            // NOP
+        }
+
+        @Override
+        public void reconnectionSuccessful() {
+            // NOP
+        }
+    }
 
     public SarosNet(@Nullable IUPnPService upnpService,
         @Nullable IStunService stunService) {
@@ -83,27 +140,57 @@ public class SarosNet {
      *            true to enable Socks5Proxy
      * @param proxyPort
      *            Socks5Proxy port
+     * @param gatewayDeviceID
+     *            the USN of a UPNP compatible gateways device to perform port
+     *            mapping on or <code>null</code>
      * @param stunServer
-     *            STUN server (address)
+     *            STUN server address or <code>null</code> if STUN discovery
+     *            should not be performed
      * @param stunPort
      *            STUN server port if 0 the default stun port will be used
-     * @param enablePortMapping
-     *            true to enable UPnP port mapping
      */
-    public void configure(String namespace, String resource,
-        boolean enableDebug, boolean proxyEnabled, int proxyPort,
-        String stunServer, int stunPort, boolean enablePortMapping) {
+    public synchronized void configure(final String namespace,
+        final String resource, final boolean enableDebug,
+        final boolean proxyEnabled, final int proxyPort,
+        final String gatewayDeviceID, final String stunServer,
+        final int stunPort, boolean enablePortMapping) {
+
+        if (isConnected())
+            throw new IllegalStateException(
+                "cannot configure the network while a connection is established");
+
         Connection.DEBUG_ENABLED = enableDebug;
         this.namespace = namespace;
         this.resource = resource;
         this.proxyPort = proxyPort;
         this.isProxyEnabled = proxyEnabled;
+        this.gatewayDeviceID = gatewayDeviceID;
         this.stunServer = stunServer;
         this.stunPort = stunPort;
-        this.isPortMappingEnabled = enablePortMapping;
 
-        if (this.stunServer.isEmpty())
+        this.device = null;
+
+        if (this.stunServer != null && this.stunServer.isEmpty())
             this.stunServer = null;
+
+        if (this.gatewayDeviceID != null && this.gatewayDeviceID.isEmpty())
+            this.gatewayDeviceID = null;
+
+        if (this.gatewayDeviceID == null)
+            return;
+
+        /*
+         * perform blocking tasks in the background meanwhile to speed up first
+         * connection attempt, currently it is only UPNP discovery
+         */
+
+        Utils.runSafeAsync("SearchUPNP", LOG, new Runnable() {
+            @Override
+            public void run() {
+                if (upnpService != null)
+                    upnpService.discoverGateways();
+            }
+        });
     }
 
     /**
@@ -151,7 +238,7 @@ public class SarosNet {
         if (isConnected())
             disconnect();
 
-        setBytestreamConnectionProperties();
+        initialzeNetworkComponents();
 
         Roster.setDefaultSubscriptionMode(Roster.SubscriptionMode.manual);
 
@@ -264,6 +351,7 @@ public class SarosNet {
     }
 
     private void disconnectInternal() {
+
         if (connection == null)
             return;
 
@@ -273,6 +361,8 @@ public class SarosNet {
         } catch (RuntimeException e) {
             LOG.warn("could not disconnect from the current XMPPConnection", e);
         } finally {
+
+            uninitialzeNetworkComponents();
             connection = null;
         }
     }
@@ -313,7 +403,7 @@ public class SarosNet {
      * Socks5Proxy configuration, look up streamhost address candiates for
      * Socks5Proxy, update UPnP settings on proxy setting change.
      */
-    private void setBytestreamConnectionProperties() {
+    private void initialzeNetworkComponents() {
 
         /*
          * Setting the Smack timeout for packet replies. The default of 5000 can
@@ -323,136 +413,149 @@ public class SarosNet {
          */
         SmackConfiguration.setPacketReplyTimeout(packetReplyTimeout);
 
-        // Socks5 Proxy Configuration
-        boolean settingsChanged = false;
-        boolean portChanged = false;
+        if (!isProxyEnabled)
+            return; // we are done, STUN and UPNP only affect Socks5
+
+        SmackConfiguration.setLocalSocks5ProxyEnabled(true);
+        SmackConfiguration.setLocalSocks5ProxyPort(proxyPort);
+
+        final Socks5Proxy proxy = Socks5Proxy.getSocks5Proxy();
+
+        proxy.start();
+        socks5ProxyPort = proxy.getPort();
+
+        LOG.debug("started Socks5 proxy on port: " + socks5ProxyPort
+            + " [listening on all interfaces]");
+
+        List<String> interfaceAddresses = new ArrayList<String>();
+
+        for (InetAddress interfaceAddress : NetworkingUtils
+            .getAllNonLoopbackLocalIPAdresses(true)) {
+            interfaceAddresses.add(interfaceAddress.getHostAddress());
+        }
+
+        LOG.debug("found interface addresses: " + interfaceAddresses);
+        proxy.replaceLocalAddresses(interfaceAddresses);
 
         /*
-         * TODO Fix in Smack: Either always start proxy when enabled in the
-         * smack configuration or never start it automatically. Currently it
-         * only starts after initiation the singleton on first access.
+         * The public IP address from the STUN result may be added to late but
+         * this is definitely better then blocking the connection establishment
+         * for several seconds. Also it is very uncommon the a connection
+         * attempt to another is done just several seconds after the connection
+         * to the XMPP server is established
          */
+        if (stunService != null && stunServer != null) {
+            Utils.runSafeAsync("StunAddProxys", LOG, new Runnable() {
+                @Override
+                public void run() {
+                    Collection<InetSocketAddress> addresses = stunService
+                        .discover(stunServer, stunPort, 10000);
 
-        Socks5Proxy proxy = NetworkingUtils.getSocks5ProxySafe();
-
-        if (isProxyEnabled != SmackConfiguration.isLocalSocks5ProxyEnabled()) {
-            settingsChanged = true;
-            SmackConfiguration.setLocalSocks5ProxyEnabled(isProxyEnabled);
+                    for (InetSocketAddress address : addresses)
+                        NetworkingUtils.addProxyAddress(address.getAddress()
+                            .getHostAddress(), true);
+                }
+            });
         }
 
-        // Note: The proxy gets restarted on port change, too.
-        if (proxyPort != SmackConfiguration.getLocalSocks5ProxyPort()) {
-            settingsChanged = true;
-            portChanged = true;
-            SmackConfiguration.setLocalSocks5ProxyPort(proxyPort);
-        }
+        final CountDownLatch mappingStart = new CountDownLatch(1);
 
-        // Get & set all (useful) internal and external IP addresses as
-        // potential connect addresses
-        if (isProxyEnabled) {
+        if (gatewayDeviceID != null && upnpService != null) {
+            final String gatewayDeviceIDToFind = gatewayDeviceID;
+            Utils.runSafeAsync("CreatePortMapping", LOG, new Runnable() {
+                @Override
+                public void run() {
+                    synchronized (portMappingLock) {
+                        mappingStart.countDown();
+
+                        List<GatewayDevice> devices = upnpService.getGateways();
+
+                        if (devices == null)
+                            devices = upnpService.getGateways();
+
+                        for (GatewayDevice currentDevice : devices) {
+                            if (gatewayDeviceIDToFind.equals(currentDevice
+                                .getUSN())) {
+                                device = currentDevice;
+                                break;
+                            }
+                        }
+
+                        if (device == null) {
+                            LOG.warn("could not find gateway device with id: + "
+                                + gatewayDeviceID
+                                + " in the current network environment");
+                            return;
+                        }
+
+                        upnpService.deletePortMapping(device, socks5ProxyPort,
+                            IUPnPService.TCP);
+
+                        LOG.debug("creating port mapping on device: "
+                            + device.getFriendlyName() + " [" + socks5ProxyPort
+                            + "|" + IUPnPService.TCP + "]");
+
+                        if (!upnpService.createPortMapping(device,
+                            socks5ProxyPort, IUPnPService.TCP,
+                            PORT_MAPPING_DESCRIPTION)) {
+
+                            LOG.warn("failed to create port mapping on device: "
+                                + device.getFriendlyName()
+                                + " ["
+                                + socks5ProxyPort
+                                + "|"
+                                + IUPnPService.TCP
+                                + "]");
+
+                            device = null;
+                            return;
+                        }
+
+                        InetAddress externalAddress = upnpService
+                            .getExternalAddress(device);
+
+                        if (externalAddress != null)
+                            NetworkingUtils.addProxyAddress(
+                                externalAddress.getHostAddress(), true);
+                    }
+                }
+            });
 
             try {
-                List<InetAddress> myAdresses = NetworkingUtils
-                    .getAllNonLoopbackLocalIPAdresses(true);
-
-                if (!myAdresses.isEmpty()) {
-                    // convert to List of Strings
-                    List<String> myAdressesStr = new LinkedList<String>();
-                    for (InetAddress ip : myAdresses)
-                        myAdressesStr.add(ip.getHostAddress());
-                    proxy.replaceLocalAddresses(myAdressesStr);
-                }
-
-                if (stunService != null && stunServer != null) {
-                    Utils.runSafeAsync("StunAddProxys", LOG, new Runnable() {
-                        @Override
-                        public void run() {
-                            Collection<InetSocketAddress> addresses = stunService
-                                .discover(stunServer, stunPort, 10000);
-
-                            for (InetSocketAddress address : addresses)
-                                NetworkingUtils.addProxyAddress(address
-                                    .getAddress().getHostAddress(), true);
-                        }
-                    });
-                }
-            } catch (Exception e) {
-                LOG.debug("Error while retrieving IP addresses", e);
-            }
-        }
-
-        if (settingsChanged || proxy.isRunning() != isProxyEnabled) {
-            StringBuilder sb = new StringBuilder();
-            if (settingsChanged)
-                sb.append("Socks5Proxy properties changed. ");
-
-            if (proxy.isRunning()) {
-                proxy.stop();
-                sb.append("Socks5Proxy stopped. ");
-            }
-            if (isProxyEnabled) {
-                proxy.start();
-                sb.append("Socks5Proxy started on port " + proxy.getPort()
-                    + "...");
-            }
-            LOG.debug(sb);
-
-            if (upnpService != null) {
-                // Update the mapped port on Socks5Proy port change
-                if (isProxyEnabled && portChanged && upnpService.isMapped())
-                    upnpService.createSarosPortMapping();
-
-                // create UPnP port mapping if not existing
-                if (isProxyEnabled && isPortMappingEnabled
-                    && !upnpService.isMapped())
-                    upnpService.createSarosPortMapping();
-
-                // remove UPnP port mapping if not required
-                if (!isProxyEnabled && upnpService.isMapped())
-                    upnpService.removeSarosPortMapping();
-
-                if (upnpService.isMapped()) {
-                    String gatewayPublicIP = upnpService.getPublicGatewayIP();
-                    if (gatewayPublicIP != null)
-                        NetworkingUtils.addProxyAddress(gatewayPublicIP, true);
-                }
+                mappingStart.await();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
         }
     }
 
-    private class XMPPConnectionListener implements ConnectionListener {
+    private void uninitialzeNetworkComponents() {
+        if (socks5ProxyPort == -1)
+            return;
 
-        @Override
-        public void connectionClosed() {
-            /*
-             * see Smack Source: connectionClosed is called before
-             * connectionClosedOnError and so would violate the state transition
-             * described in the ConnectionState class
-             */
+        Socks5Proxy.getSocks5Proxy().stop();
+
+        deleteMapping: synchronized (portMappingLock) {
+            if (device == null)
+                break deleteMapping;
+
+            if (!upnpService
+                .isMapped(device, socks5ProxyPort, IUPnPService.TCP))
+                break deleteMapping;
+
+            LOG.debug("deleting port mapping on device: "
+                + device.getFriendlyName() + " [" + socks5ProxyPort + "|"
+                + IUPnPService.TCP + "]");
+
+            if (!upnpService.deletePortMapping(device, socks5ProxyPort,
+                IUPnPService.TCP)) {
+                LOG.warn("failed to delete port mapping on device: "
+                    + device.getFriendlyName() + " [" + socks5ProxyPort + "|"
+                    + IUPnPService.TCP + "]");
+            }
         }
 
-        @Override
-        public void connectionClosedOnError(Exception e) {
-            LOG.error("XMPP connection error: ", e);
-            setConnectionState(ConnectionState.ERROR, e);
-            disconnectInternal();
-            setConnectionState(ConnectionState.NOT_CONNECTED, null);
-            localJID = null;
-        }
-
-        @Override
-        public void reconnectingIn(int seconds) {
-            // NOP
-        }
-
-        @Override
-        public void reconnectionFailed(Exception e) {
-            // NOP
-        }
-
-        @Override
-        public void reconnectionSuccessful() {
-            // NOP
-        }
+        socks5ProxyPort = -1;
+        device = null;
     }
 }

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/Saros.java
@@ -57,7 +57,6 @@ import de.fu_berlin.inf.dpp.editor.annotations.SarosAnnotation;
 import de.fu_berlin.inf.dpp.editor.colorstorage.UserColorID;
 import de.fu_berlin.inf.dpp.net.SarosNet;
 import de.fu_berlin.inf.dpp.net.upnp.IUPnPService;
-import de.fu_berlin.inf.dpp.net.upnp.internal.UPnPAccessImpl;
 import de.fu_berlin.inf.dpp.preferences.PreferenceConstants;
 import de.fu_berlin.inf.dpp.preferences.PreferenceUtils;
 import de.fu_berlin.inf.dpp.project.ISarosSessionManager;
@@ -277,8 +276,6 @@ public class Saros extends AbstractUIPlugin {
 
         isInitialized = true;
 
-        upnpService.init(new UPnPAccessImpl(), getPreferenceStore());
-
         /*
          * If other colors than the ones we support are set in the
          * PreferenceStore, overwrite them
@@ -331,21 +328,16 @@ public class Saros extends AbstractUIPlugin {
                     @Override
                     public void run() {
 
-                        /*
-                         * Always remove UPnP port mapping for Saros first
-                         * because some routers have no lease duration and so
-                         * this mapping will still be present if something goes
-                         * wrong in the shutdown process
-                         */
-                        IUPnPService upnpManager = sarosContext
-                            .getComponent(IUPnPService.class);
+                        try {
 
-                        if (upnpManager.isMapped())
-                            upnpManager.removeSarosPortMapping();
-
-                        sessionManager.stopSarosSession();
-
-                        getSarosNet().disconnect();
+                            sessionManager.stopSarosSession();
+                        } finally {
+                            /*
+                             * Always shutdown the network to ensure a proper
+                             * cleanup(currently only UPNP)
+                             */
+                            getSarosNet().disconnect();
+                        }
 
                         /**
                          * This will cause dispose() to be called on all
@@ -654,11 +646,17 @@ public class Saros extends AbstractUIPlugin {
         boolean useTLS = account.useTLS();
         boolean useSASL = account.useSASL();
 
-        sarosNet.configure(NAMESPACE, RESOURCE,
+        sarosNet.configure(
+            NAMESPACE,
+            RESOURCE,
             preferenceUtils.isDebugEnabled(),
             preferenceUtils.isLocalSOCKS5ProxyEnabled(),
-            preferenceUtils.getFileTransferPort(), preferenceUtils.getStunIP(),
-            preferenceUtils.getStunPort(),
+            preferenceUtils.getFileTransferPort(),
+
+            getPreferenceStore().getString(
+                PreferenceConstants.AUTO_PORTMAPPING_DEVICEID),
+
+            preferenceUtils.getStunIP(), preferenceUtils.getStunPort(),
             preferenceUtils.isAutoPortmappingEnabled());
 
         Exception connectionError = null;

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/SarosCoreContextFactory.java
@@ -36,7 +36,9 @@ import de.fu_berlin.inf.dpp.net.internal.XMPPTransmitter;
 import de.fu_berlin.inf.dpp.net.stun.IStunService;
 import de.fu_berlin.inf.dpp.net.stun.internal.StunServiceImpl;
 import de.fu_berlin.inf.dpp.net.subscriptionmanager.SubscriptionManager;
+import de.fu_berlin.inf.dpp.net.upnp.IUPnPAccess;
 import de.fu_berlin.inf.dpp.net.upnp.IUPnPService;
+import de.fu_berlin.inf.dpp.net.upnp.internal.UPnPAccessImpl;
 import de.fu_berlin.inf.dpp.net.upnp.internal.UPnPServiceImpl;
 import de.fu_berlin.inf.dpp.observables.FileReplacementInProgressObservable;
 import de.fu_berlin.inf.dpp.observables.InvitationProcessObservable;
@@ -118,6 +120,7 @@ public class SarosCoreContextFactory extends AbstractSarosContextFactory {
 
         // broken - uses SWT GUI stuff
         Component.create(IUPnPService.class, UPnPServiceImpl.class),
+        Component.create(IUPnPAccess.class, UPnPAccessImpl.class),
         Component.create(IReceiver.class, XMPPReceiver.class),
         Component.create(ITransmitter.class, XMPPTransmitter.class),
 

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/internal/DataTransferManager.java
@@ -31,7 +31,6 @@ import de.fu_berlin.inf.dpp.net.IncomingTransferObject;
 import de.fu_berlin.inf.dpp.net.JID;
 import de.fu_berlin.inf.dpp.net.NetTransferMode;
 import de.fu_berlin.inf.dpp.net.SarosNet;
-import de.fu_berlin.inf.dpp.net.upnp.IUPnPService;
 import de.fu_berlin.inf.dpp.preferences.PreferenceUtils;
 import de.fu_berlin.inf.dpp.util.Utils;
 
@@ -59,8 +58,6 @@ public class DataTransferManager implements IConnectionListener {
     private Connection connection;
 
     private final IReceiver receiver;
-
-    private final IUPnPService upnpService;
 
     private final ITransport mainTransport;
 
@@ -167,10 +164,6 @@ public class DataTransferManager implements IConnectionListener {
             }
 
             transferModeDispatch.connectionChanged(peer, connection);
-
-            if (connection.getMode() == NetTransferMode.IBB && incomingRequest
-                && upnpService != null)
-                upnpService.checkAndInformAboutUPnP();
         }
 
         @Override
@@ -189,13 +182,11 @@ public class DataTransferManager implements IConnectionListener {
     public DataTransferManager(SarosNet sarosNet, IReceiver receiver,
         @Nullable @Socks5Transport ITransport mainTransport,
         @Nullable @IBBTransport ITransport fallbackTransport,
-        @Nullable IUPnPService upnpService,
         @Nullable PreferenceUtils preferenceUtils) {
 
         this.receiver = receiver;
         this.fallbackTransport = fallbackTransport;
         this.mainTransport = mainTransport;
-        this.upnpService = upnpService;
         this.preferenceUtils = preferenceUtils;
         this.initTransports();
 

commit bd218c336f90b98a604b1971ab126caa46770d42

[FIX] Socks5 proxy is always enabled
Fixed a regression that was introduced with commit
36b49c5de05d51517f2c75225d439bdef2bc7bda 

The Socks5 proxy is enabled by default. As this has no impact on the
internal connection establishment the file transfer in the OPN/IPN uses
Smack directly and thus may use Socks5 although the user denied it.

Change-Id: Ica88e82aa52710e3bc0e9cc77df5a24cd5eecaca
Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1296
Tested-by: Jenkins CI
Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/net/SarosNet.java
@@ -409,10 +409,11 @@ public class SarosNet {
          */
         SmackConfiguration.setPacketReplyTimeout(packetReplyTimeout);
 
+        SmackConfiguration.setLocalSocks5ProxyEnabled(isProxyEnabled);
+
         if (!isProxyEnabled)
             return; // we are done, STUN and UPNP only affect Socks5
 
-        SmackConfiguration.setLocalSocks5ProxyEnabled(true);
         SmackConfiguration.setLocalSocks5ProxyPort(proxyPort);
 
         final Socks5Proxy proxy = Socks5Proxy.getSocks5Proxy();