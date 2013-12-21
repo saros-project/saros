61c57b26f6985ee362bd05c7683f5b1aae7337fb
Stefan Rossbach <srossbach@arcor.de>
2013-12-21 22:48:43 +0100

    [FIX] fixed OOM error in DataTransferCollector
    I do not know why the aggregation of the data was not done on the fly
    but the current implementation had one major flow.
    
    Assume ~= 50 byte per entry and so this would be a total of ~= 500 Kb
    after 10000 transfer events. If the session would be run as sever it
    should not take more than a month until JVM will throw an OOM.
    
    Also fixed concurrency issues as two different threads read and write
    the data.
    
    Change-Id: I9e9b6d2a7ff129f1f69a7b75fed6f611f2733184
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1313
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/feedback/DataTransferCollector.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/feedback/DataTransferCollector.java
@@ -1,7 +1,8 @@
 package de.fu_berlin.inf.dpp.feedback;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import de.fu_berlin.inf.dpp.annotations.Component;
 import de.fu_berlin.inf.dpp.net.ITransferModeListener;
@@ -10,8 +11,6 @@ import de.fu_berlin.inf.dpp.net.NetTransferMode;
 import de.fu_berlin.inf.dpp.net.internal.DataTransferManager;
 import de.fu_berlin.inf.dpp.net.internal.IByteStreamConnection;
 import de.fu_berlin.inf.dpp.project.ISarosSession;
-import de.fu_berlin.inf.dpp.util.Function;
-import de.fu_berlin.inf.dpp.util.Pair;
 
 /**
  * Collects information about the amount of data transfered with the different
@@ -22,65 +21,17 @@ import de.fu_berlin.inf.dpp.util.Pair;
 @Component(module = "feedback")
 public class DataTransferCollector extends AbstractStatisticCollector {
 
-    public static class TransferEvent {
-
-        protected NetTransferMode mode;
-
-        protected boolean incoming;
-
-        protected long sizeTransferred;
-
-        protected long sizeUncompressed;
-
-        protected long transmissionMillisecs;
-
-        public TransferEvent(NetTransferMode newMode, boolean incoming,
-            long sizeTransferred, long sizeUncompressed,
-            long transmissionMillisecs) {
-            this.mode = newMode;
-            this.incoming = incoming;
-            this.sizeTransferred = sizeTransferred;
-            this.sizeUncompressed = sizeUncompressed;
-            this.transmissionMillisecs = transmissionMillisecs;
-        }
-
-        public long getTransmissionMillisecs() {
-            return transmissionMillisecs;
-        }
-
-        /**
-         * Returns size of the transferred data for this {@link TransferEvent}
-         * 
-         * @return transferred data size in bytes
-         */
-        public long getTransferredSize() {
-            return sizeTransferred;
-        }
-
-        /**
-         * Transfered data may be uncompressed after reception. This function
-         * returns the data size of the data of this {@link TransferEvent} after
-         * decompression. If the data was not compressed, this value equals the
-         * {@link #getTransferredSize()} call.
-         * 
-         * @return uncompressed data size in bytes
-         */
-        public long getUncompressedSize() {
-            return sizeUncompressed;
-        }
-
-        public boolean isIncoming() {
-            return incoming;
-        }
-
-        public NetTransferMode getMode() {
-            return mode;
-        }
+    // we currently do not distinguish between sent and received data
+    private static class TransferStatisticHolder {
+        private long bytesTransferred;
+        private long transferTime; // ms
+        private int count;
     }
 
-    protected DataTransferManager dataTransferManager;
+    private final Map<NetTransferMode, TransferStatisticHolder> statistic = new EnumMap<NetTransferMode, TransferStatisticHolder>(
+        NetTransferMode.class);
 
-    protected List<TransferEvent> transferEvents = new ArrayList<TransferEvent>();
+    private final DataTransferManager dataTransferManager;
 
     private final ITransferModeListener dataTransferlistener = new ITransferModeListener() {
 
@@ -93,8 +44,22 @@ public class DataTransferCollector extends AbstractStatisticCollector {
         public void transferFinished(JID jid, NetTransferMode newMode,
             boolean incoming, long sizeTransferred, long sizeUncompressed,
             long transmissionMillisecs) {
-            transferEvents.add(new TransferEvent(newMode, incoming,
-                sizeTransferred, sizeUncompressed, transmissionMillisecs));
+
+            // see processGatheredData
+            synchronized (DataTransferCollector.this) {
+                TransferStatisticHolder holder = statistic.get(newMode);
+
+                if (holder == null) {
+                    holder = new TransferStatisticHolder();
+                    statistic.put(newMode, holder);
+                }
+
+                holder.bytesTransferred += sizeTransferred;
+                holder.transferTime += transmissionMillisecs;
+                holder.count++;
+
+                // TODO how to handle overflow ?
+            }
         }
 
         @Override
@@ -110,35 +75,22 @@ public class DataTransferCollector extends AbstractStatisticCollector {
     }
 
     @Override
-    protected void processGatheredData() {
+    protected synchronized void processGatheredData() {
 
-        // Group Events by NetTransferMode
-        List<Pair<NetTransferMode, List<TransferEvent>>> transferClasses = Pair
-            .partition(transferEvents,
-                new Function<TransferEvent, NetTransferMode>() {
-                    @Override
-                    public NetTransferMode apply(TransferEvent u) {
-                        return u.getMode();
-                    }
-                });
+        for (final Entry<NetTransferMode, TransferStatisticHolder> entry : statistic
+            .entrySet()) {
+            final NetTransferMode mode = entry.getKey();
+            final TransferStatisticHolder holder = entry.getValue();
 
-        // Compute summary statistics per class
-        for (Pair<NetTransferMode, List<TransferEvent>> transferClass : transferClasses) {
+            data.setTransferStatistic(
+                mode.toString(),
+                holder.count,
+                holder.bytesTransferred / 1024,
+                holder.transferTime,
+                holder.bytesTransferred * 1000.0 / 1024.0
+                    / Math.max(1.0, holder.transferTime));
 
-            long totalSize = 0;
-            long totalTransferTime = 0;
-            int nTransferEvents = 0;
-            for (TransferEvent event : transferClass.v) {
-                totalSize += event.getTransferredSize();
-                totalTransferTime += event.getTransmissionMillisecs();
-                nTransferEvents++;
-            }
-
-            data.setTransferStatistic(transferClass.p.toString(),
-                nTransferEvents, totalSize / 1024, totalTransferTime, totalSize
-                    * 1000.0 / 1024.0 / Math.max(1.0, totalTransferTime));
         }
-        transferEvents.clear();
     }
 
     @Override
