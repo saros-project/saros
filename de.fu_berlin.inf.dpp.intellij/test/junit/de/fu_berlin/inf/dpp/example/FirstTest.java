fb4666cc665c7fa43460b084ad9236c5b513186b
Graham Allan <graham.allan@timgroup.com>
2014-04-02 19:30:26 +0200

    [BUILD] Build script for the IntelliJ plugin
    The build.xml was originally generated by IntelliJ, and then
    restructured and beautified by Graham Allan.
    Further contributions by: Arndt Lasarzik and Franz Zieris.
    
    Change-Id: Iebd8a7eca6f9edfefaddf4765b5897b55b2f9a76
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1476
    Tested-by: Jenkins CI
    Reviewed-by: Bernd Bieber <brotlee@zedat.fu-berlin.de>
    Reviewed-by: Franz Zieris <zieris@inf.fu-berlin.de>

---
+++ de.fu_berlin.inf.dpp.intellij/test/junit/de/fu_berlin/inf/dpp/example/FirstTest.java
@@ -0,0 +1,19 @@
+package de.fu_berlin.inf.dpp.example;
+
+import com.intellij.codeHighlighting.HighlightingPass;
+import com.thoughtworks.xstream.XStream;
+import de.fu_berlin.inf.dpp.SarosCoreTestAction;
+import de.fu_berlin.inf.dpp.activities.business.TextEditActivity;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void canReferenceClassesAcrossDependencies() {
+        Assert.assertNotNull(SarosCoreTestAction.class); // from saros-i main classpath
+        Assert.assertNotNull(TextEditActivity.class); // from saros core classpath
+        Assert.assertNotNull(XStream.class); // from saros core libs
+        Assert.assertNotNull(HighlightingPass.class); // from IntelliJ dependencies
+    }
+}
