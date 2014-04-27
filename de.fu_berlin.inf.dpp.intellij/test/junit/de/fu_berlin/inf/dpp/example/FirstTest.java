fee0b317a1184c94ec01632b36e8eda75b3a3e3b
Graham Allan <graham.allan@timgroup.com>
2014-04-27 18:21:22 +0100

    [INTERNAL] Compile saros-i tests.

--- de.fu_berlin.inf.dpp.intellij/test/junit/de/fu_berlin/inf/dpp/example/FirstTest.java
+++ de.fu_berlin.inf.dpp.intellij/test/junit/de/fu_berlin/inf/dpp/example/FirstTest.java
@@ -1,5 +1,9 @@
 package de.fu_berlin.inf.dpp.example;
 
+import com.intellij.codeHighlighting.HighlightingPass;
+import com.thoughtworks.xstream.XStream;
+import de.fu_berlin.inf.dpp.SarosCoreTestAction;
+import de.fu_berlin.inf.dpp.activities.business.TextEditActivity;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,5 +12,13 @@ public class FirstTest {
     @Test
     public void failsDeliberately() {
         Assert.fail("Expected to fail, used to configure build scripts.");
+    }
+
+    @Test
+    public void canReferenceClassesAcrossDependencies() {
+        Assert.assertNotNull(SarosCoreTestAction.class); // from saros-i main classpath
+        Assert.assertNotNull(TextEditActivity.class); // from saros core classpath
+        Assert.assertNotNull(XStream.class); // from saros core libs
+        Assert.assertNotNull(HighlightingPass.class); // from IntelliJ dependencies
     }
 }
