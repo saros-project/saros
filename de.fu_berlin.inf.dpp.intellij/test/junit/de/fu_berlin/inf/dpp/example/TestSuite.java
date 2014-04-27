02d3939174ed54caa7fb1ac2d955dad08247d0a4
Graham Allan <graham.allan@timgroup.com>
2014-04-27 20:42:17 +0100

    [INTERNAL] Add a junit task. Deliberately fail a test.

--- de.fu_berlin.inf.dpp.intellij/test/junit/de/fu_berlin/inf/dpp/example/TestSuite.java
+++
@@ -1,9 +0,0 @@
-package de.fu_berlin.inf.dpp.example;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ FirstTest.class})
-public class TestSuite {
-}
