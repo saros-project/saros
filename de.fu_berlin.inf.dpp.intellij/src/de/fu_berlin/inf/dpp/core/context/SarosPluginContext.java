9073355806a6d7b9fd18c48bc90b04e85c493136
Raimondas Kvietkauskas <r.kvietkauskas@uniplicity.com>
2014-06-18 14:56:07 +0300

    [INTERNAL] add independent part of context package
    These classes have few dependencies and are added first
    The call to Saros.checkInitialized() in SarosPluginContext is disabled
    until the class Saros is added to this repository
    
    Change-Id: I091d1d464db43265cb6e2a087f10ca2dea0d8c44
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1698
    Tested-by: Jenkins CI
    Reviewed-by: Franz Zieris <zieris@inf.fu-berlin.de>

---
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/core/context/SarosPluginContext.java
@@ -0,0 +1,73 @@
+/*
+ *
+ *  DPP - Serious Distributed Pair Programming
+ *  (c) Freie Universität Berlin - Fachbereich Mathematik und Informatik - 2010
+ *  (c) NFQ (www.nfq.com) - 2014
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 1, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * /
+ */
+
+package de.fu_berlin.inf.dpp.core.context;
+
+/**
+ * Provides the possibility to initialize a component with the components hold
+ * in the given {@link de.fu_berlin.inf.dpp.SarosContext}. You can set the
+ * context via
+ * {@link de.fu_berlin.inf.dpp.SarosPluginContext#setSarosContext(SarosContext)}
+ * .
+ * <p/>
+ * Typically this is the context created by {@link de.fu_berlin.inf.dpp.Saros}
+ * while it's initialization.
+ *
+ * @author philipp.cordes
+ */
+public class SarosPluginContext {
+
+    private static SarosContext sarosContext;
+
+    public static void setSarosContext(SarosContext sarosContext) {
+        SarosPluginContext.sarosContext = sarosContext;
+    }
+
+    /**
+     * Use this only in eclipse-specific components like actions or views. If
+     * you want to initialize a saros-specific component which is located in our
+     * 'business-logic' you should use
+     * {@link de.fu_berlin.inf.dpp.SarosContext#initComponent(Object)} directly
+     * from context of the current Saros.
+     *
+     * @param toInjectInto component which should to be initialized.
+     */
+    public static void initComponent(Object toInjectInto) {
+        // Saros.checkInitialized();  //todo: enable it when Saros class is submitted
+        sarosContext.initComponent(toInjectInto);
+    }
+
+    /**
+     * Use this only in eclipse-specific components like actions or views. If
+     * you want to reinject a saros-specific component which is located in our
+     * 'business-logic' you should use
+     * {@link de.fu_berlin.inf.dpp.SarosContext#reinject(Object)} directly from
+     * context of the current Saros.
+     *
+     * @param toReinject component which should to be reinjected.
+     */
+    public static void reinject(Object toReinject) {
+        // Saros.checkInitialized();  //todo: enable it when Saros class is submitted
+        sarosContext.reinject(toReinject);
+    }
+}
+
