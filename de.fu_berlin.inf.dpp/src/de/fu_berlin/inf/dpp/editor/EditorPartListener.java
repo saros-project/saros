0fb3a19cc8b9aad256095efe9f1133b53c9fc743
Stefan Rossbach <srossbach@arcor.de>
2014-07-31 13:51:19 +0200

    [INTERNAL] cleaned up EditorPartListener
    Made fields private and final, removed no needed null checks and
    put the class Javadoc to the right place.
    
    Change-Id: Icfd7f42a4fead23cedb2bb3b23260606703250d2
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1848
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/editor/EditorPartListener.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/editor/EditorPartListener.java
@@ -1,10 +1,3 @@
-/**
- * The EditorPartListener listens to changes to the editors view, e.g. if the view is opened, activated,
- * closed, brought to the top or the source has changed. The Listener notifies the EditorManager about
- * the state of the view, so the Manager can react on it, e.g. to update followers.
- * 
- * @author awaldmann and nwarnatsch
- */
 package de.fu_berlin.inf.dpp.editor;
 
 import org.eclipse.ui.IEditorPart;
@@ -12,10 +5,21 @@ import org.eclipse.ui.IPartListener2;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchPartReference;
 
+/**
+ * The <code>EditorPartListener</code> listens to changes to the editors view,
+ * e.g. if the view is opened, activated, closed, brought to the top or the
+ * source has changed.
+ * <p>
+ * The Listener notifies the EditorManager about the state of the view, so the
+ * Manager can react on it, e.g. to update followers.
+ * 
+ * @see EditorManager#partInputChanged(IEditorPart)
+ * @author awaldmann
+ * @author nwarnatsch
+ */
+public final class EditorPartListener implements IPartListener2 {
 
-public class EditorPartListener implements IPartListener2 {
-
-    protected EditorManager editorManager;
+    private final EditorManager editorManager;
 
     public EditorPartListener(EditorManager editorManager) {
         this.editorManager = editorManager;
@@ -23,46 +27,38 @@ public class EditorPartListener implements IPartListener2 {
 
     @Override
     public void partActivated(IWorkbenchPartReference partRef) {
-        IWorkbenchPart part = partRef.getPart(false);
+        final IWorkbenchPart part = partRef.getPart(false);
 
-        if ((part != null) && (part instanceof IEditorPart)) {
-            IEditorPart editor = (IEditorPart) part;
-            editorManager.partActivated(editor);
-        }
+        if (part instanceof IEditorPart)
+            editorManager.partActivated((IEditorPart) part);
     }
 
     @Override
     public void partOpened(IWorkbenchPartReference partRef) {
-        IWorkbenchPart part = partRef.getPart(false);
+        final IWorkbenchPart part = partRef.getPart(false);
 
-        if ((part != null) && (part instanceof IEditorPart)) {
-            IEditorPart editor = (IEditorPart) part;
-            editorManager.partOpened(editor);
-        }
+        if (part instanceof IEditorPart)
+            editorManager.partOpened((IEditorPart) part);
     }
 
     @Override
     public void partClosed(IWorkbenchPartReference partRef) {
-        IWorkbenchPart part = partRef.getPart(false);
+        final IWorkbenchPart part = partRef.getPart(false);
 
-        if ((part != null) && (part instanceof IEditorPart)) {
-            IEditorPart editor = (IEditorPart) part;
-            editorManager.partClosed(editor);
-        }
+        if (part instanceof IEditorPart)
+            editorManager.partClosed((IEditorPart) part);
     }
 
-    /**
+    /*
      * We need to catch partBroughtToTop events because partActivate events are
      * missing if Editors are opened programmatically.
      */
     @Override
     public void partBroughtToTop(IWorkbenchPartReference partRef) {
-        IWorkbenchPart part = partRef.getPart(false);
+        final IWorkbenchPart part = partRef.getPart(false);
 
-        if ((part != null) && (part instanceof IEditorPart)) {
-            IEditorPart editor = (IEditorPart) part;
-            editorManager.partActivated(editor);
-        }
+        if (part instanceof IEditorPart)
+            editorManager.partActivated((IEditorPart) part);
     }
 
     @Override
@@ -80,17 +76,11 @@ public class EditorPartListener implements IPartListener2 {
         // do nothing
     }
 
-    /**
-     * Called for instance when a file was renamed. We just close and open the
-     * editor.
-     */
     @Override
     public void partInputChanged(IWorkbenchPartReference partRef) {
-        IWorkbenchPart part = partRef.getPart(false);
+        final IWorkbenchPart part = partRef.getPart(false);
 
-        if ((part != null) && (part instanceof IEditorPart)) {
-            IEditorPart editor = (IEditorPart) part;
-            editorManager.partInputChanged(editor);
-        }
+        if (part instanceof IEditorPart)
+            editorManager.partInputChanged((IEditorPart) part);
     }
 }
