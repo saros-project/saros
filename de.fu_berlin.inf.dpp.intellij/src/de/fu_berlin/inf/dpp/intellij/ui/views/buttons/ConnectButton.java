98fcfa8e2ed8d8ba6e6e9b126f2d03a59953d400
Holger Schmeisky <google@schmeisky.com>
2014-09-17 10:42:44 +0200

    [UI] Adds Toolbar buttons.
    Adds the views.buttons package containing the buttons for the toolbar.
    
    Change-Id: Ibfabd7a47baef0b4f2507f86b172f5424afb18ec
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1959
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>
    Reviewed-by: Holger Schmeisky <google@schmeisky.com>

---
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/ui/views/buttons/ConnectButton.java
@@ -0,0 +1,195 @@
+/*
+ *
+ *  DPP - Serious Distributed Pair Programming
+ *  (c) Freie Universität Berlin - Fachbereich Mathematik und Informatik - 2010
+ *  (c) NFQ (www.nfq.com) - 2014
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 1, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * /
+ */
+
+package de.fu_berlin.inf.dpp.intellij.ui.views.buttons;
+
+import de.fu_berlin.inf.dpp.account.XMPPAccount;
+import de.fu_berlin.inf.dpp.account.XMPPAccountStore;
+import de.fu_berlin.inf.dpp.core.context.SarosPluginContext;
+import de.fu_berlin.inf.dpp.intellij.ui.actions.AbstractSarosAction;
+import de.fu_berlin.inf.dpp.intellij.ui.actions.ConnectServerAction;
+import de.fu_berlin.inf.dpp.intellij.ui.actions.DisconnectServerAction;
+import de.fu_berlin.inf.dpp.intellij.ui.actions.NotImplementedAction;
+import de.fu_berlin.inf.dpp.intellij.ui.util.SafeDialogUtils;
+import org.apache.log4j.Logger;
+import org.picocontainer.annotations.Inject;
+
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Implementation of connect XMPP/jabber server button
+ */
+public class ConnectButton extends ToolbarButton {
+    public static final String CONNECT_ICON_PATH = "icons/elcl16/connect.png";
+    private static final Logger LOG = Logger.getLogger(ConnectButton.class);
+
+    private JPopupMenu popupMenu = new JPopupMenu();
+    private JMenuItem menuItemAdd;
+    private JMenuItem configure;
+    private JMenuItem disconnect;
+
+    private final AbstractSarosAction disconnectAction;
+    private final ConnectServerAction connectAction;
+    private final NotImplementedAction configureAccounts;
+
+    @Inject
+    private XMPPAccountStore accountStore;
+
+    public ConnectButton() {
+        super(ConnectServerAction.NAME, "Connect", CONNECT_ICON_PATH,
+            "Connect to XMPP/Jabber server");
+        SarosPluginContext.initComponent(this);
+        disconnectAction = new DisconnectServerAction();
+        connectAction = new ConnectServerAction();
+        connectAction.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent action) {
+                createMenuItems();
+            }
+        });
+        configureAccounts = new NotImplementedAction("configure accounts");
+
+        createDisconnectMenuItem();
+        createAddAccountMenuÍtem();
+        createConfigureAccountMenuItem();
+        createMenuItems();
+
+        addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                if (accountStore.isEmpty()) {
+                    XMPPAccount account = createNewAccount();
+                    connectAction.executeWithUser(account.getUsername());
+                } else {
+                    popupMenu.show(ConnectButton.this, 0,
+                        getBounds().y + getBounds().height);
+                }
+            }
+        });
+    }
+
+    private void createMenuItems() {
+        popupMenu.removeAll();
+        for (XMPPAccount account : accountStore.getAllAccounts()) {
+            createAccountMenuItem(account);
+        }
+
+        popupMenu.addSeparator();
+        popupMenu.add(menuItemAdd);
+        popupMenu.add(configure);
+        popupMenu.add(disconnect);
+    }
+
+    private void createAccountMenuItem(XMPPAccount account) {
+        final String userName =
+            account.getUsername() + "@" + account.getDomain();
+        JMenuItem accountItem = createMenuItemForUser(userName);
+        popupMenu.add(accountItem);
+    }
+
+    private JMenuItem createMenuItemForUser(final String userName) {
+        JMenuItem accountItem = new JMenuItem(userName);
+        accountItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                connectAction.executeWithUser(userName);
+            }
+        });
+        return accountItem;
+    }
+
+    private void createDisconnectMenuItem() {
+        disconnect = new JMenuItem("Disconnect server");
+        disconnect.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                disconnectAction.execute();
+            }
+        });
+    }
+
+    private void createConfigureAccountMenuItem() {
+        configure = new JMenuItem("Configure accounts...");
+        configure.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent actionEvent) {
+                configureAccounts.execute();
+            }
+        });
+    }
+
+    private void createAddAccountMenuÍtem() {
+        menuItemAdd = new JMenuItem("Add account...");
+        menuItemAdd.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                XMPPAccount account = createNewAccount();
+                if (account == null) {
+                    SafeDialogUtils
+                        .showError("Account was not created.", "Error");
+                }
+                createMenuItems();
+            }
+        });
+    }
+
+    /**
+     * Asks for Name, Password and domain and server for a new XMPP account.
+     */
+    protected XMPPAccount createNewAccount() {
+        final String username = SafeDialogUtils
+            .showInputDialog("Your User-ID", "", "Login");
+        if (username.isEmpty()) {
+            return null;
+        }
+        final String password = SafeDialogUtils
+            .showInputDialog("Password", "", "Login");
+        if (password.isEmpty()) {
+            return null;
+        }
+        final String domain = SafeDialogUtils.showInputDialog(
+            "XMPP domain " + "(e.g. 'saros-con.imp.fu-berlin.de')",
+            "saros-con.imp.fu-berlin.de", "Server");
+        if (domain.isEmpty()) {
+            return null;
+        }
+        String server = SafeDialogUtils.showInputDialog(
+            "XMPP server (optional, not necessary in most cases)", "",
+            "Server");
+
+        try {
+            return accountStore
+                .createAccount(username, password, domain, server, 0, false,
+                    false);
+        } catch (IllegalArgumentException e) {
+            LOG.error("Error creating account", e);
+            SafeDialogUtils.showError(
+                "There was an error creating the account.\n Details:\n\n" + e
+                    .getMessage(), "Error"
+            );
+        }
+        return null;
+    }
+
+    public void addActionListenerToActions(ActionListener listener) {
+        disconnectAction.addActionListener(listener);
+        connectAction.addActionListener(listener);
+    }
+}
---
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/ui/views/buttons/ConsistencyButton.java
@@ -0,0 +1,247 @@
+/*
+ *
+ *  DPP - Serious Distributed Pair Programming
+ *  (c) Freie Universität Berlin - Fachbereich Mathematik und Informatik - 2010
+ *  (c) NFQ (www.nfq.com) - 2014
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 1, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * /
+ */
+
+package de.fu_berlin.inf.dpp.intellij.ui.views.buttons;
+
+import com.intellij.openapi.application.ApplicationManager;
+import de.fu_berlin.inf.dpp.activities.SPath;
+import de.fu_berlin.inf.dpp.core.concurrent.ConsistencyWatchdogClient;
+import de.fu_berlin.inf.dpp.core.concurrent.IsInconsistentObservable;
+import de.fu_berlin.inf.dpp.core.context.SarosPluginContext;
+import de.fu_berlin.inf.dpp.core.project.ISarosSessionManager;
+import de.fu_berlin.inf.dpp.intellij.ui.Messages;
+import de.fu_berlin.inf.dpp.intellij.ui.actions.ConsistencyAction;
+import de.fu_berlin.inf.dpp.intellij.ui.util.DialogUtils;
+import de.fu_berlin.inf.dpp.intellij.ui.util.NotificationPanel;
+import de.fu_berlin.inf.dpp.observables.ValueChangeListener;
+import de.fu_berlin.inf.dpp.session.ISarosSession;
+import de.fu_berlin.inf.dpp.session.ISarosSessionListener;
+import de.fu_berlin.inf.dpp.session.NullSarosSessionListener;
+import org.apache.log4j.Logger;
+import org.picocontainer.annotations.Inject;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.MessageFormat;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Button for triggering a {@link ConsistencyAction}. Displays a different symbol
+ * when state is inconsistent or not.
+ */
+public class ConsistencyButton extends ToolbarButton {
+    private static final Logger LOG = Logger.getLogger(ConsistencyButton.class);
+
+    private static final String IN_SYNC_ICON_PATH = "icons/etool16/in_sync.png";
+    private static final String OUT_SYNC_ICON_PATH = "icons/etool16/out_sync.png";
+
+    private final ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (isEnabled() && isInconsistent) {
+                setEnabledFromUIThread(false);
+
+                final Set<SPath> paths = new HashSet<SPath>(
+                    watchdogClient.getPathsWithWrongChecksums());
+
+                String inconsistentFiles = createConfirmationMessage(paths);
+
+                if (!DialogUtils.showQuestion(null, inconsistentFiles,
+                    Messages.ConsistencyAction_confirm_dialog_title)) {
+                    setEnabledFromUIThread(true);
+                    return;
+                }
+
+                action.execute();
+            }
+        }
+    };
+
+    private final ActionListener consistencyActionListener = new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            setInconsistent(
+                !watchdogClient.getPathsWithWrongChecksums().isEmpty());
+        }
+    };
+
+    private boolean isInconsistent = false;
+
+    private ConsistencyAction action;
+
+    private final ISarosSessionListener sessionListener = new NullSarosSessionListener() {
+        @Override
+        public void sessionStarted(ISarosSession newSarosSession) {
+            setSarosSession(newSarosSession);
+            setEnabledFromUIThread(true);
+        }
+
+        @Override
+        public void sessionEnded(ISarosSession oldSarosSession) {
+            setSarosSession(null);
+            setEnabledFromUIThread(false);
+        }
+    };
+
+    private final ValueChangeListener<Boolean> isConsistencyListener = new ValueChangeListener<Boolean>() {
+
+        @Override
+        public void setValue(Boolean newValue) {
+            handleConsistencyChange(newValue);
+        }
+    };
+
+    @Inject
+    private ISarosSessionManager sessionManager;
+
+    @Inject
+    private ConsistencyWatchdogClient watchdogClient;
+
+    @Inject
+    private IsInconsistentObservable inconsistentObservable;
+
+    private ISarosSession sarosSession;
+
+    /**
+     * Creates a Consistency button, adds a sessionListener and disables the button.
+     */
+    public ConsistencyButton() {
+        super(ConsistencyAction.NAME, "Recover inconsistencies",
+            IN_SYNC_ICON_PATH, "Files are consistent");
+        SarosPluginContext.initComponent(this);
+        action = new ConsistencyAction();
+        action.addActionListener(consistencyActionListener);
+
+        setSarosSession(sessionManager.getSarosSession());
+        sessionManager.addSarosSessionListener(sessionListener);
+
+        addActionListener(actionListener);
+        setEnabled(false);
+    }
+
+    public void setInconsistent(boolean isInconsistent) {
+        this.isInconsistent = isInconsistent;
+
+        if (isInconsistent) {
+            setEnabledFromUIThread(true);
+            setIcon(OUT_SYNC_ICON_PATH, "Files are NOT consistent");
+        } else {
+            setEnabledFromUIThread(false);
+            setIcon(IN_SYNC_ICON_PATH, "Files are consistent");
+        }
+    }
+
+    private void setSarosSession(ISarosSession newSession) {
+        if (sarosSession != null) {
+            inconsistentObservable.remove(isConsistencyListener);
+        }
+
+        sarosSession = newSession;
+
+        if (sarosSession != null) {
+            inconsistentObservable.addAndNotify(isConsistencyListener);
+        }
+    }
+
+    /**
+     * This method activates the consistency recovery button, if an inconsistency
+     * was detected and displays a tooltip.
+     */
+    private void handleConsistencyChange(final Boolean isInconsistent) {
+
+        LOG.debug(
+            "Inconsistency indicator goes: " + (isInconsistent ? "on" : "off"));
+
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                setInconsistent(isInconsistent);
+            }
+        });
+
+        if (!isInconsistent) {
+            showNotification(
+                Messages.ConsistencyAction_tooltip_no_inconsistency);
+            return;
+        }
+
+        final Set<SPath> paths = new HashSet<SPath>(
+            watchdogClient.getPathsWithWrongChecksums());
+
+        final String files = createInconsistentPathsMessage(paths);
+
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // set tooltip
+                showNotification(MessageFormat.format(
+                    Messages.ConsistencyAction_tooltip_inconsistency_detected,
+                    files));
+
+                NotificationPanel.showNotification(
+                    Messages.ConsistencyAction_title_inconsistency_detected,
+                    MessageFormat.format(
+                        Messages.ConsistencyAction_message_inconsistency_detected,
+                        files)
+                );
+            }
+        });
+    }
+
+    private String createConfirmationMessage(Set<SPath> paths) {
+        StringBuilder sbInconsistentFiles = new StringBuilder();
+        for (SPath path : paths) {
+            sbInconsistentFiles.append("project: ");
+            sbInconsistentFiles.append(path.getProject().getName());
+            sbInconsistentFiles.append(", file: ");
+            sbInconsistentFiles
+                .append(path.getProjectRelativePath().toOSString());
+            sbInconsistentFiles.append("\n");
+
+        }
+
+        sbInconsistentFiles.append("Please confirm project modifications.\n\n"
+            + "                + The recovery process will perform changes to files and folders of the current shared project(s).\n\n"
+            + "                + The affected files and folders may be either modified, created, or deleted.");
+        return sbInconsistentFiles.toString();
+    }
+
+    private String createInconsistentPathsMessage(Set<SPath> paths) {
+        StringBuilder sb = new StringBuilder();
+
+        for (SPath path : paths) {
+            if (sb.length() > 0) {
+                sb.append(", ");
+            }
+
+            sb.append(path.getFullPath().toOSString());
+        }
+
+        return sb.toString();
+    }
+
+    private void showNotification(String text) {
+        NotificationPanel.showNotification(text, "Consistency warning");
+    }
+}
