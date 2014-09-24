740316ca4d7c56d79e4102d3925e9dc5d4865009
Holger Schmeisky <google@schmeisky.com>
2014-09-24 11:01:18 +0200

    [UI] Adds IDEA tree views.
    Change-Id: Iefda7e29b72d215b625e99febed34d1fc5262a22
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1968
    Tested-by: Jenkins CI
    Reviewed-by: Holger Schmeisky <google@schmeisky.com>

---
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/ui/tree/ContactTreeRootNode.java
@@ -0,0 +1,243 @@
+/*
+ *
+ *  DPP - Serious Distributed Pair Programming
+ *  (c) Freie Universität Berlin - Fachbereich Mathematik und Informatik - 2010
+ *  (c) NFQ (www.nfq.com) - 2014
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 1, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * /
+ */
+
+package de.fu_berlin.inf.dpp.intellij.ui.tree;
+
+import com.intellij.util.ui.UIUtil;
+import de.fu_berlin.inf.dpp.core.context.SarosPluginContext;
+import de.fu_berlin.inf.dpp.intellij.ui.util.IconManager;
+import de.fu_berlin.inf.dpp.net.xmpp.JID;
+import de.fu_berlin.inf.dpp.net.xmpp.XMPPConnectionService;
+import de.fu_berlin.inf.dpp.net.xmpp.roster.IRosterListener;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.packet.RosterPacket;
+import org.picocontainer.annotations.Inject;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Root node for the tree of contacts, that a user has in their contact list.
+ * <p/>
+ * It registers as {@link IRosterListener} and keeps the displayed
+ * contacts in sync with the server.
+ */
+public class ContactTreeRootNode extends DefaultMutableTreeNode
+    implements IRosterListener {
+    public static final String TREE_TITLE = "Contacts";
+
+    private final SarosTreeView treeView;
+    private final Map<String, ContactInfo> contactMap = new HashMap<String, ContactInfo>();
+    private final DefaultTreeModel treeModel;
+
+    @Inject
+    private XMPPConnectionService connectionService;
+
+    public ContactTreeRootNode(SarosTreeView treeView) {
+        super(treeView);
+        SarosPluginContext.initComponent(this);
+
+        this.treeView = treeView;
+        treeModel = (DefaultTreeModel) this.treeView.getModel();
+        setUserObject(TREE_TITLE);
+    }
+
+    /**
+     * Creates a tree root node and adds a {@link DefaultMutableTreeNode} containing
+     * a {@link de.fu_berlin.inf.dpp.intellij.ui.tree.ContactTreeRootNode.ContactInfo}
+     * as content.
+     */
+    public void createContactNodes() {
+        removeAllChildren();
+        DefaultMutableTreeNode node;
+
+        Roster roster = connectionService.getRoster();
+        //add contacts
+        for (RosterEntry contactEntry : roster.getEntries()) {
+            ContactInfo contactInfo = new ContactInfo(contactEntry);
+            Presence presence = roster.getPresence(contactEntry.getUser());
+
+            if (presence.getType() == Presence.Type.available) {
+                contactInfo.setOnline(true);
+            } else {
+                contactInfo.setOnline(false);
+            }
+
+            node = new DefaultMutableTreeNode(contactInfo);
+
+            add(node);
+
+            contactMap.put(contactInfo.getTitle(), contactInfo);
+        }
+    }
+
+    /**
+     * Adds the node of the given contact to the tree model.
+     */
+    public void showContact(final String name) {
+        ContactInfo contactInfo = contactMap.get(name);
+        if (contactInfo == null || !contactInfo.isHidden()) {
+            return;
+        }
+
+        contactInfo.setHidden(false);
+        MutableTreeNode node = new DefaultMutableTreeNode(contactInfo);
+
+        add(node);
+    }
+
+    /**
+     * Removes the node of the given contact from the tree.
+     */
+    public void hideContact(final String name) {
+        ContactInfo contactInfo = contactMap.get(name);
+        if (contactInfo == null || contactInfo.isHidden()) {
+            return;
+        }
+
+        contactInfo.setHidden(true);
+        for (int i = 0; i < getChildCount(); i++) {
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode) getChildAt(
+                i);
+            if (contactInfo.equals(node.getUserObject())) {
+                treeModel.removeNodeFromParent(node);
+                node.setUserObject(null);
+                break;
+            }
+        }
+
+    }
+
+    /**
+     * Removes all contacts.
+     */
+    public void removeContacts() {
+        removeAllChildren();
+        contactMap.clear();
+    }
+
+    @Override
+    public void rosterChanged(Roster roster) {
+        //NOP
+    }
+
+    @Override
+    public void entriesAdded(Collection<String> addresses) {
+        //NOP
+    }
+
+    @Override
+    public void entriesUpdated(Collection<String> addresses) {
+        //NOP
+    }
+
+    @Override
+    public void entriesDeleted(Collection<String> addresses) {
+        //NOP
+    }
+
+    /**
+     * Updates online / offline status of a contact and updates the tree view.
+     */
+    @Override
+    public void presenceChanged(Presence presence) {
+        String user = new JID(presence.getFrom()).getBareJID().toString();
+
+        ContactInfo info = contactMap.get(user);
+        if (info != null) {
+            if (presence.getType() == Presence.Type.available) {
+                info.setOnline(true);
+            } else {
+                info.setOnline(false);
+            }
+
+            Runnable action = new Runnable() {
+                @Override
+                public void run() {
+                    treeView.collapseRow(2);
+                    treeView.expandRow(2);
+                }
+            };
+
+            UIUtil.invokeAndWaitIfNeeded(action);
+        }
+    }
+
+    /**
+     * Class to keep contact info
+     */
+    protected class ContactInfo extends LeafInfo {
+
+        private final String status;
+        private final RosterEntry rosterEntry;
+        private boolean isOnline;
+        private boolean isHidden = false;
+
+        private ContactInfo(RosterEntry rosterEntry) {
+            super(rosterEntry.getUser());
+            this.rosterEntry = rosterEntry;
+            status = rosterEntry.getStatus() == null ?
+                null :
+                rosterEntry.getStatus().toString();
+        }
+
+        public RosterPacket.ItemStatus getStatus() {
+            return rosterEntry.getStatus();
+        }
+
+        public RosterEntry getRosterEntry() {
+            return rosterEntry;
+        }
+
+        public boolean isOnline() {
+            return isOnline;
+        }
+
+        public boolean isHidden() {
+            return isHidden;
+        }
+
+        public void setHidden(boolean isHidden) {
+            this.isHidden = isHidden;
+        }
+
+        public void setOnline(boolean isOnline) {
+            this.isOnline = isOnline;
+            if (isOnline) {
+                setIcon(IconManager.CONTACT_ONLINE_ICON);
+            } else {
+                setIcon(IconManager.CONTACT_OFFLINE_ICON);
+            }
+        }
+
+        public String toString() {
+            return status == null ? title : title + " (" + status + ")";
+        }
+    }
+
+}
---
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/ui/tree/SarosTreeView.java
@@ -0,0 +1,171 @@
+/*
+ *
+ *  DPP - Serious Distributed Pair Programming
+ *  (c) Freie Universität Berlin - Fachbereich Mathematik und Informatik - 2010
+ *  (c) NFQ (www.nfq.com) - 2014
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 1, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * /
+ */
+
+package de.fu_berlin.inf.dpp.intellij.ui.tree;
+
+import com.intellij.ui.treeStructure.Tree;
+import com.intellij.util.ui.UIUtil;
+import de.fu_berlin.inf.dpp.account.XMPPAccount;
+import de.fu_berlin.inf.dpp.account.XMPPAccountStore;
+import de.fu_berlin.inf.dpp.core.context.SarosPluginContext;
+import de.fu_berlin.inf.dpp.intellij.ui.util.IconManager;
+import de.fu_berlin.inf.dpp.net.xmpp.XMPPConnectionService;
+import org.picocontainer.annotations.Inject;
+
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeCellRenderer;
+import javax.swing.tree.TreePath;
+import javax.swing.tree.TreeSelectionModel;
+import java.awt.Component;
+
+/**
+ * Saros tree view for contacts and sessions.
+ */
+public class SarosTreeView extends Tree {
+
+    private final SessionTreeRootNode sessionTreeRootNode;
+    private final ContactTreeRootNode contactTreeRootNode;
+
+    @Inject
+    private XMPPAccountStore accountStore;
+
+    @Inject
+    private XMPPConnectionService connectionService;
+
+    /**
+     * A cell renderer that sets the node icon according to the node type
+     * (root, session, contact, or other).
+     */
+    private final TreeCellRenderer renderer = new DefaultTreeCellRenderer() {
+        @Override
+        public Component getTreeCellRendererComponent(JTree tree, Object value,
+            boolean selected, boolean expanded, boolean isLeaf, int row,
+            boolean focused) {
+            Component c = super
+                .getTreeCellRendererComponent(tree, value, selected, expanded,
+                    isLeaf, row, focused);
+
+            TreePath path = tree.getPathForRow(row);
+            if (path != null) {
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode) path
+                    .getLastPathComponent();
+
+                if (node != null) {
+                    if (node instanceof SarosTreeRootNode) {
+                        setIcon(null);
+                    } else if (node instanceof SessionTreeRootNode) {
+                        setIcon(IconManager.SESSIONS_ICON);
+                    } else if (node instanceof ContactTreeRootNode) {
+                        setIcon(IconManager.CONTACTS_ICON);
+                    } else {
+                        if (node.getUserObject() instanceof LeafInfo) {
+                            LeafInfo info = (LeafInfo) node.getUserObject();
+                            if (info.getIcon() != null) {
+                                setIcon(info.getIcon());
+                            }
+                        }
+                    }
+                }
+            }
+
+            return c;
+        }
+
+    };
+
+    public SarosTreeView() {
+        super(new SarosTreeRootNode());
+        SarosPluginContext.initComponent(this);
+
+        sessionTreeRootNode = new SessionTreeRootNode(this);
+        ((SarosTreeRootNode) getModel().getRoot()).add(sessionTreeRootNode);
+        contactTreeRootNode = new ContactTreeRootNode(this);
+        ((SarosTreeRootNode) getModel().getRoot()).add(contactTreeRootNode);
+
+        addMouseListener(new TreeClickListener(this));
+
+        getSelectionModel()
+            .setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+        setCellRenderer(renderer);
+    }
+
+    /**
+     * Displays the 'user@domain (connected)' string and populates the contact list.
+     *
+     * Called after a connection was made.
+     */
+    public void renderConnected() {
+        XMPPAccount activeAccount = accountStore.getActiveAccount();
+
+        String rootText =
+            activeAccount.getUsername() + "@" + activeAccount.getDomain()
+                + " (Connected)";
+        getSarosTreeRootNode().setTitle(rootText);
+
+        //add contacts
+        contactTreeRootNode.createContactNodes();
+
+        //add listener for on-line contacts
+        connectionService.getRoster().addRosterListener(contactTreeRootNode);
+
+        updateTree();
+    }
+
+    /**
+     * Clears the contact list and title.
+     *
+     * Called after a connection was disconnected.
+     */
+    public void renderDisconnected() {
+        getSarosTreeRootNode().setTitleDefault();
+
+        contactTreeRootNode.removeContacts();
+        sessionTreeRootNode.removeAllChildren();
+
+        updateTree();
+    }
+
+    public void updateTree() {
+        Runnable updateTreeModel = new Runnable() {
+            @Override
+            public void run() {
+                DefaultTreeModel model = (DefaultTreeModel) (getModel());
+                model.reload();
+
+                expandRow(2);
+            }
+        };
+
+        UIUtil.invokeAndWaitIfNeeded(updateTreeModel);
+    }
+
+    protected ContactTreeRootNode getContactTreeRootNode() {
+        return contactTreeRootNode;
+    }
+
+    private SarosTreeRootNode getSarosTreeRootNode() {
+        return (SarosTreeRootNode) getModel().getRoot();
+    }
+}
