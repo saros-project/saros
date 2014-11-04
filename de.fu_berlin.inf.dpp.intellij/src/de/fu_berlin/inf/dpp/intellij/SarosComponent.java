12b10122ee1eba56b49544b2bffb74561970921c
Holger Schmeisky <google@schmeisky.com>
2014-11-04 11:10:30 +0100

    [UI] Adds IntelliJ plugin Component
    Change-Id: Idc39c62739f248ef3bac99464b9c1dfdde927be2
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/1995
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>
    Tested-by: Jenkins CI
    Reviewed-by: Christian Cikryt <c.cikryt@googlemail.com>
    Reviewed-by: Holger Schmeisky <google@schmeisky.com>

---
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/SarosComponent.java
@@ -0,0 +1,83 @@
+/*
+ *
+ *  DPP - Serious Distributed Pair Programming
+ *  (c) Freie Universität Berlin - Fachbereich Mathematik und Informatik - 2010
+ *  (c) NFQ (www.nfq.com) - 2014
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 1, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * /
+ */
+
+package de.fu_berlin.inf.dpp.intellij;
+
+import com.intellij.openapi.actionSystem.KeyboardShortcut;
+import com.intellij.openapi.keymap.Keymap;
+import com.intellij.openapi.keymap.KeymapManager;
+import com.intellij.openapi.project.Project;
+import de.fu_berlin.inf.dpp.core.Saros;
+import org.apache.log4j.PropertyConfigurator;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.KeyStroke;
+import java.awt.event.KeyEvent;
+import java.net.URL;
+
+/**
+ * Component that is initalized when a project is loaded.
+ * It initializes the logging, shortcuts and the {@link Saros} object.
+ */
+public class SarosComponent
+    implements com.intellij.openapi.components.ProjectComponent {
+
+    public SarosComponent(final Project project) {
+        //Has to be loaded explicitly
+        URL log4jProperties = getClass().getResource("/log4j.properties");
+        PropertyConfigurator.configure(log4jProperties);
+
+        Keymap keymap = KeymapManager.getInstance().getActiveKeymap();
+        keymap.addShortcut("ActivateSarosToolWindow", new KeyboardShortcut(
+            KeyStroke.getKeyStroke(KeyEvent.VK_F11,
+                java.awt.event.InputEvent.ALT_DOWN_MASK), null
+        ));
+
+        Saros.create(project);
+    }
+
+    @Override
+    public void initComponent() {
+        //NOP
+    }
+
+    @Override
+    public void disposeComponent() {
+        //NOP
+    }
+
+    @NotNull
+    @Override
+    public String getComponentName() {
+        return "Saros";
+    }
+
+    @Override
+    public void projectOpened() {
+        //TODO: Update project
+    }
+
+    @Override
+    public void projectClosed() {
+        //TODO: Update project
+    }
+}
