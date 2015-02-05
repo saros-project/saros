9e94a5091da708423f912999928a3eb2b2f6e930
Holger Schmeisky <google@schmeisky.com>
2015-02-05 16:25:13 +0100

    [INTERNAL] Deprecates current IPreference solution
    And adds a TODO to make a preference store based solution. Will probably
    follow next week.
    
    Change-Id: Ibc3d9e32ce7a6cf5f32317380f69315880c11dc6
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2086
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>
    Reviewed-by: Matthias Bohnstedt <matthias.bohnstedt@gmail.com>
    Reviewed-by: Holger Schmeisky <google@schmeisky.com>

--- de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/preferences/IPreferences.java
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/preferences/IPreferences.java
@@ -7,12 +7,17 @@ import de.fu_berlin.inf.dpp.editor.colorstorage.UserColorID;
 /**
  * IPreferences provide methods to get and set preferences. These Preferences
  * will be stored in an IDE specific way.
+ * 
+ * FIXME: This has to be split into a PreferenceStore / PreferenceUtils
+ * implementation, otherwise every implementation for each IDE would need to
+ * implement preferences that might not even be present or make sense.
  */
+@Deprecated
 public interface IPreferences {
 
     /**
      * Returns whether debug is enabled or not.
-     *
+     * 
      * @return true if debug is enabled.
      */
     public boolean isDebugEnabled();
@@ -30,7 +35,7 @@ public interface IPreferences {
 
     /**
      * Returns whether auto-connect is enabled or not.
-     *
+     * 
      * @return true if auto-connect is enabled.
      */
     public boolean isAutoConnecting();
@@ -38,7 +43,7 @@ public interface IPreferences {
     /**
      * Returns whether port mapping is enabled or not by evaluating the stored
      * deviceID to be empty or not.
-     *
+     * 
      * @return true if port mapping is enabled, false otherwise
      */
     public boolean isAutoPortmappingEnabled();
@@ -52,7 +57,7 @@ public interface IPreferences {
     /**
      * Returns whether the external address of the gateway should be used as a
      * Socks5 candidate or not.
-     *
+     * 
      * @return true if external address of the gateway should be used as a
      *         Socks5 candidate, false otherwise
      */
@@ -60,7 +65,7 @@ public interface IPreferences {
 
     /**
      * Returns the device ID of the gateway to perform port mapping on.
-     *
+     * 
      * @return Device ID of the gateway or empty String if disabled.
      */
     public String getAutoPortmappingGatewayID();
@@ -72,7 +77,7 @@ public interface IPreferences {
 
     /**
      * Returns the Skype user name or an empty string if none was specified.
-     *
+     * 
      * @return the user name.for Skype or an empty string
      */
     public String getSkypeUserName();
@@ -82,7 +87,7 @@ public interface IPreferences {
      * {@link PreferenceConstants#USE_NEXT_PORTS_FOR_FILE_TRANSFER} is set, a
      * negative number is returned (smacks will try next free ports above this
      * number)
-     *
+     * 
      * @return port for smacks configuration (negative if to try out ports
      *         above)
      */
@@ -91,35 +96,35 @@ public interface IPreferences {
     /**
      * Returns whether force In-Band Bytestreams (IBB) transport is enabled or
      * not.
-     *
+     * 
      * @return true if force In-Band Bytestreams (IBB) transport is enabled.
      */
     public boolean forceIBBTransport();
 
     /**
      * Returns whether concurrent undo is enabled or not.
-     *
+     * 
      * @return true if concurrent undo is enabled.
      */
     public boolean isConcurrentUndoActivated();
 
     /**
      * Returns whether version control is enabled or not.
-     *
+     * 
      * @return true if version control is enabled.
      */
     public boolean useVersionControl();
 
     /**
      * Sets the value of useVersionControl
-     *
+     * 
      * @param value
      */
     public void setUseVersionControl(boolean value);
 
     /**
      * Returns whether local SOCKS5 proxy is enabled or not.
-     *
+     * 
      * @return true if local SOCKS5 proxy is enabled.
      */
     public boolean isLocalSOCKS5ProxyEnabled();
@@ -136,7 +141,7 @@ public interface IPreferences {
 
     /**
      * Returns the favorite color ID that should be used during a session.
-     *
+     * 
      * @return the favorite color ID or {@value UserColorID#UNKNOWN} if no
      *         favorite color ID is available
      */
@@ -144,7 +149,7 @@ public interface IPreferences {
 
     /**
      * Returns the nickname that should be used in a session.
-     *
+     * 
      * @return the nickname which may be empty if no nickname is available
      */
     public String getSessionNickname();
