6ebc1ecd3c7181a8817dc8ebc0acfb84e144d2b5
Stefan Rossbach <srossbach@arcor.de>
2015-02-24 16:14:37 +0100

    [API] UI resources are now extracted if necessary
    The required resource files are now extracted if needed so that
    the browser engine can find additional resource files like CSS, JS etc.
    
    Change-Id: Ie2af71dd569313c628ba2d644514013fbadbc334
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2103
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/browser/EclipseBrowserCreator.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/ui/browser/EclipseBrowserCreator.java
@@ -1,9 +1,15 @@
 package de.fu_berlin.inf.dpp.ui.browser;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Platform;
@@ -16,9 +22,8 @@ import de.fu_berlin.inf.ag_se.browser.swt.SWTJQueryBrowser;
 import de.fu_berlin.inf.dpp.ui.view_parts.BrowserPage;
 
 /**
- * This class creates the SWT {@link Browser browser} instance as well as the
- * enclosing {@link Composite composite}. It's also set the {@link BrowserPage
- * BrowserPage} which is to displayed.
+ * This class creates JQuery {@link IJQueryBrowser browser} instances. The
+ * displayed content depends on the used {@link BrowserPage} implementation.
  */
 public class EclipseBrowserCreator {
 
@@ -28,13 +33,17 @@ public class EclipseBrowserCreator {
     // TODO central place
     private static final String UI_BUNDLE_ID = "de.fu_berlin.inf.dpp.ui";
 
+    private static final String HTML_ROOT_PATH = "html";
+
+    private static Map<String, String> fileMapping;
+
     private EclipseBrowserCreator() {
         // This private constructor hides the implicit public one
     }
 
     /**
      * Creates a new browser instance.
-     *
+     * 
      * @param composite
      *            the composite enclosing the browser.
      * @param style
@@ -51,41 +60,28 @@ public class EclipseBrowserCreator {
 
         if (bundle == null)
             throw new IllegalStateException("bundle with id: " + UI_BUNDLE_ID
-                + " is not available");
+                + " is not installed");
+
+        synchronized (EclipseBrowserCreator.class) {
+            if (fileMapping == null)
+                fileMapping = extractBundleResources(UI_BUNDLE_ID,
+                    HTML_ROOT_PATH, null, true);
+        }
 
         final String resourceName = page.getWebpage();
 
         assert resourceName != null;
 
-        URL resourceLocation = bundle.getResource(resourceName);
+        final String resourceLocation = fileMapping.get(resourceName);
 
-        /*
-         * TODO this currently works only in development environment or if the
-         * bundle is deflated when installed because the missing CCS and JS
-         * script files have to be present as well in the file system
-         */
+        final IJQueryBrowser browser = SWTJQueryBrowser.createSWTBrowser(
+            composite, style);
 
-        Exception exception = null;
-
-        try {
-            resourceLocation = FileLocator.resolve(resourceLocation);
-        } catch (IOException e) {
-            LOG.error("failed to resolve resource: " + resourceName, e);
-            exception = e;
-        }
-
-        IJQueryBrowser browser = SWTJQueryBrowser.createSWTBrowser(composite,
-            style);
-
-        if (exception != null) {
-            final String escapedStackTrace = StringUtils
-                .escapeForXML(ExceptionUtils.getFullStackTrace(exception));
-
-            browser.setText("<html><body><pre>" + escapedStackTrace
-                + "</pre></body></html>");
-
+        if (resourceLocation == null) {
+            browser.setText("<html><body><pre>" + "Resource <b>"
+                + StringUtils.escapeForXML(resourceName)
+                + "</b> could not be found.</pre></body></html>");
             return browser;
-
         }
 
         page.createRenderer(browser);
@@ -93,13 +89,160 @@ public class EclipseBrowserCreator {
 
         /*
          * TODO check if all browser work correctly with invalid Windows URLs
-         * like file:/C/... instead of file:///C/...
-         *
+         * like file:/C:/... instead of file:///C:/...
+         * 
          * Stefan Rossbach: works with default Browser (whatever that is ...
-         * IE?) on Windows
+         * Internet Explorer ?) on Windows
          */
         browser.open(resourceLocation.toString(), 5000);
         return browser;
 
+    }
+
+    private static Map<String, String> extractBundleResources(
+        final String bundleID, final String path, final String filePattern,
+        final boolean recursive) {
+
+        final Map<String, String> mapping = new HashMap<String, String>();
+
+        final Bundle bundle = Platform.getBundle(UI_BUNDLE_ID);
+
+        if (bundle == null) {
+            LOG.warn("bundle with id: " + UI_BUNDLE_ID + " is not installed");
+            return mapping;
+        }
+
+        List<URL> resourceLocations;
+
+        resourceLocations = getResourceLocationsPlain(bundle, path,
+            filePattern, recursive);
+
+        if (resourceLocations.isEmpty()) {
+            LOG.warn("could not find any resources using default bundle find method with path '"
+                + path + "' and file pattern '" + filePattern + "'");
+
+            resourceLocations = getResourceLocationsWiring(bundle, path,
+                filePattern, recursive);
+
+        }
+
+        if (resourceLocations.isEmpty()) {
+            LOG.warn("could not find any resources using wiring bundle find method with path '"
+                + path + "' and file pattern '" + filePattern + "'");
+
+            return mapping;
+        }
+
+        for (final URL resourceLocation : resourceLocations) {
+
+            // directory
+            if (resourceLocation.getPath().endsWith("/"))
+                continue;
+
+            if (LOG.isTraceEnabled())
+                LOG.trace("extracting resource: " + resourceLocation);
+
+            try {
+                final URL fileLocation = FileLocator
+                    .toFileURL(resourceLocation);
+                String urlPath = resourceLocation.getPath();
+
+                /** see {@linkplain BrowserPage#getPage()  */
+                if (urlPath.startsWith("/"))
+                    urlPath = urlPath.substring(1);
+
+                mapping.put(urlPath, fileLocation.toString());
+            } catch (IOException e) {
+                LOG.error("failed to extract resource: " + resourceLocation, e);
+            }
+        }
+
+        return mapping;
+    }
+
+    private static List<URL> getResourceLocationsPlain(final Bundle bundle,
+        final String path, final String filePattern, final boolean recursive) {
+
+        final List<URL> result = new ArrayList<URL>();
+
+        final Enumeration<URL> entries = bundle.findEntries(path, filePattern,
+            recursive);
+
+        if (entries == null)
+            return result;
+
+        while (entries.hasMoreElements())
+            result.add(entries.nextElement());
+
+        return result;
+    }
+
+    private static List<URL> getResourceLocationsWiring(final Bundle bundle,
+        final String path, final String filePattern, final boolean recursive) {
+
+        final List<URL> result = new ArrayList<URL>();
+
+        /*
+         * we must use reflection as the required OSGi framework is not
+         * available in Eclipse 3.6
+         */
+
+        try {
+
+            final Class<?> wiringClass = Class.forName(
+                "org.osgi.framework.wiring.BundleWiring", true,
+                EclipseBrowserCreator.class.getClassLoader());
+
+            final Method adapt = Bundle.class.getMethod("adapt",
+                new Class[] { Class.class });
+
+            final Object wiring = adapt.invoke(bundle, wiringClass);
+
+            if (wiring == null)
+                return result;
+
+            // BundleWiring.LISTRESOURCES_RECURSE = 1
+            final int recurse = recursive ? 1 : 0;
+
+            // BundleWiring.LISTRESOURCES_LOCAL = 2
+            final int local = 2;
+
+            final Method listResources = wiringClass.getMethod("listResources",
+                new Class[] { String.class, String.class, int.class });
+
+            final Method getClassLoader = wiringClass
+                .getMethod("getClassLoader");
+
+            final ClassLoader classLoader = (ClassLoader) getClassLoader
+                .invoke(wiring);
+
+            final Collection<String> entries = (Collection) listResources
+                .invoke(wiring, path, filePattern, recurse | local);
+
+            if (entries == null || entries.isEmpty())
+                return result;
+
+            for (final String resourceName : entries) {
+
+                final URL resourceLocation = classLoader
+                    .getResource(resourceName);
+
+                if (resourceLocation == null) {
+                    LOG.error("could not find resource in the bundle classpath: "
+                        + resourceName);
+
+                    continue;
+                }
+
+                result.add(resourceLocation);
+
+            }
+        } catch (Exception e) {
+            LOG.error(
+                "internal error while finding resources via bundle wiring API",
+                e);
+        }
+
+        return result;
     }
 }
