d2611ef0c89587cfd2f69d6b758061772410aad7
Holger Schmeisky <google@schmeisky.com>
2015-03-10 15:21:16 +0100

    [INTERNAL] Adds Eclipse Preferences implementation
    Adds an implementation for eclipse of the new Preferences class and a
    an adapter for the Eclipse preference store to the core's IPreference
    Store.
    
    Change-Id: I96b4d0a6e7996a73a2f542631ab20980022e94e4
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2136
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/Saros.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/Saros.java
@@ -53,7 +53,6 @@ import de.fu_berlin.inf.dpp.editor.annotations.SarosAnnotation;
 import de.fu_berlin.inf.dpp.editor.colorstorage.UserColorID;
 import de.fu_berlin.inf.dpp.feedback.FeedbackPreferences;
 import de.fu_berlin.inf.dpp.misc.pico.DotGraphMonitor;
-import de.fu_berlin.inf.dpp.preferences.EclipsePreferences;
 import de.fu_berlin.inf.dpp.preferences.IPreferences;
 import de.fu_berlin.inf.dpp.preferences.PreferenceConstants;
 import de.fu_berlin.inf.dpp.session.ISarosSessionManager;
@@ -64,7 +63,7 @@ import de.fu_berlin.inf.dpp.versioning.VersionManager;
 
 /**
  * The main plug-in of Saros.
- *
+ * 
  * @author rdjemili
  * @author coezbek
  */
@@ -78,14 +77,14 @@ public class Saros extends AbstractUIPlugin {
 
     /**
      * @JTourBusStop 1, Some Basics:
-     *
+     * 
      *               This class manages the lifecycle of the Saros plug-in,
      *               contains some important supporting data members and
      *               provides methods for the integration of Saros into Eclipse.
-     *
+     * 
      *               Browse the data members. Some are quite obvious (version,
      *               feature etc.) some need closer examination.
-     *
+     * 
      */
 
     /**
@@ -114,18 +113,18 @@ public class Saros extends AbstractUIPlugin {
     /**
      * To print an architecture diagram at the end of the plug-in life-cycle
      * initialize the dotMonitor with a new instance:
-     *
+     * 
      * <code>dotMonitor= new DotGraphMonitor();</code>
      */
     protected DotGraphMonitor dotMonitor;
 
     /**
      * @JTourBusStop 2, Some Basics:
-     *
+     * 
      *               Preferences are managed by Eclipse-provided classes. Most
      *               are kept by Preferences, but some sensitive data (like user
      *               account data) is kept in a SecurePreference.
-     *
+     * 
      *               If you press Ctrl+Shift+R and type in "*preference*" you
      *               will see every class in Saros that deals with preferences.
      *               Classes named "*PreferencePage" implement individual pages
@@ -149,10 +148,10 @@ public class Saros extends AbstractUIPlugin {
 
     /**
      * @JTourBusStop 4, Invitation Process:
-     *
+     * 
      *               If you haven't already read about PicoContainer, stop and
      *               do so now (http://picocontainer.codehaus.org).
-     *
+     * 
      *               Saros uses PicoContainer to manage dependencies on our
      *               behalf. The SarosContext class encapsulates our usage of
      *               PicoContainer. It's a well documented class, so take a look
@@ -245,7 +244,7 @@ public class Saros extends AbstractUIPlugin {
 
         connectionHandler = sarosContext.getComponent(ConnectionHandler.class);
         sessionManager = sarosContext.getComponent(ISarosSessionManager.class);
-        preferences = sarosContext.getComponent(EclipsePreferences.class);
+        preferences = sarosContext.getComponent(IPreferences.class);
 
         // additional initialization
 
@@ -360,7 +359,7 @@ public class Saros extends AbstractUIPlugin {
      * of the same JVM without external synchronization. If they are used by
      * multiple JVMs no guarantees can be made concerning data consistency (see
      * {@link Preferences} for details).
-     *
+     * 
      * @return the preferences node for this plug-in containing global
      *         preferences that are visible for all workspaces of this eclipse
      *         installation
@@ -428,10 +427,10 @@ public class Saros extends AbstractUIPlugin {
     /**
      * Returns a string representing the Saros Version number for instance
      * "9.5.7.r1266"
-     *
+     * 
      * This method only returns a valid version string after the plugin has been
      * started.
-     *
+     * 
      * This is equivalent to the bundle version.
      */
     public String getVersion() {
@@ -595,7 +594,7 @@ public class Saros extends AbstractUIPlugin {
     /**
      * Feature toggle for displaying Saros in a web browser in an additional
      * view. Also checks if required bundle is present.
-     *
+     * 
      * @return true if this feature is enabled, false otherwise
      */
     private static boolean isSwtBrowserEnabled() {
--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/preferences/EclipsePreferences.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/preferences/EclipsePreferences.java
@@ -1,155 +1,16 @@
 package de.fu_berlin.inf.dpp.preferences;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.jface.preference.IPreferenceStore;
-
 /**
- * EclipsePreferences implements {@link IPreferences} using Eclipse
- * {@link IPreferenceStore} for storing all Information.
+ * Eclipse implementation of the {@link Preferences}, that wraps a
+ * {@link org.eclipse.jface.preference.IPreferenceStore} in an
+ * {@link EclipsePreferenceStoreAdapter}.
+ * 
+ * Preferences that are custom to Eclipse may be defined here.
  */
-public class EclipsePreferences implements IPreferences {
+public class EclipsePreferences extends Preferences implements IPreferences {
 
-    private IPreferenceStore preferenceStore;
-
-    /**
-     * Constructs a new EclipsePreferences
-     *
-     * @param preferenceStore
-     *            instance of {@link IPreferenceStore}
-     */
-    public EclipsePreferences(IPreferenceStore preferenceStore) {
-        this.preferenceStore = preferenceStore;
-    }
-
-    @Override
-    public boolean isDebugEnabled() {
-        return preferenceStore.getBoolean(PreferenceConstants.DEBUG);
-    }
-
-    @Override
-    public String getSarosXMPPServer() {
-        return "saros-con.imp.fu-berlin.de";
-    }
-
-    @Override
-    public String getDefaultServer() {
-        return getSarosXMPPServer();
-    }
-
-    @Override
-    public boolean isAutoConnecting() {
-        return preferenceStore.getBoolean(PreferenceConstants.AUTO_CONNECT);
-    }
-
-    @Override
-    public boolean isAutoPortmappingEnabled() {
-        return !preferenceStore.getString(
-            PreferenceConstants.AUTO_PORTMAPPING_DEVICEID).isEmpty();
-    }
-
-    @Override
-    public List<String> getSocks5Candidates() {
-        String addresses = preferenceStore
-            .getString(PreferenceConstants.LOCAL_SOCKS5_PROXY_CANDIDATES);
-
-        List<String> result = new ArrayList<String>();
-
-        for (String address : addresses.split(",")) {
-            address = address.trim();
-
-            if (address.isEmpty())
-                continue;
-
-            result.add(address);
-        }
-
-        return result;
-    }
-
-    @Override
-    public boolean useExternalGatewayAddress() {
-        return preferenceStore
-            .getBoolean(PreferenceConstants.LOCAL_SOCKS5_PROXY_USE_UPNP_EXTERNAL_ADDRESS);
-    }
-
-    @Override
-    public String getAutoPortmappingGatewayID() {
-        return preferenceStore
-            .getString(PreferenceConstants.AUTO_PORTMAPPING_DEVICEID);
-    }
-
-    @Override
-    public int getAutoPortmappingLastPort() {
-        return preferenceStore
-            .getInt(PreferenceConstants.AUTO_PORTMAPPING_LASTMAPPEDPORT);
-    }
-
-    @Override
-    public String getSkypeUserName() {
-        return preferenceStore.getString(PreferenceConstants.SKYPE_USERNAME);
-    }
-
-    @Override
-    public int getFileTransferPort() {
-        int port = preferenceStore
-            .getInt(PreferenceConstants.FILE_TRANSFER_PORT);
-
-        if (preferenceStore
-            .getBoolean(PreferenceConstants.USE_NEXT_PORTS_FOR_FILE_TRANSFER))
-            return -port;
-        else
-            return port;
-    }
-
-    @Override
-    public boolean forceIBBTransport() {
-        return preferenceStore
-            .getBoolean(PreferenceConstants.FORCE_FILETRANSFER_BY_CHAT);
-    }
-
-    @Override
-    public boolean isConcurrentUndoActivated() {
-        return preferenceStore.getBoolean(PreferenceConstants.CONCURRENT_UNDO);
-    }
-
-    @Override
-    public boolean useVersionControl() {
-        return !preferenceStore
-            .getBoolean(PreferenceConstants.DISABLE_VERSION_CONTROL);
-    }
-
-    @Override
-    public void setUseVersionControl(boolean value) {
-        preferenceStore.setValue(PreferenceConstants.DISABLE_VERSION_CONTROL,
-            !value);
-    }
-
-    @Override
-    public boolean isLocalSOCKS5ProxyEnabled() {
-        return !preferenceStore
-            .getBoolean(PreferenceConstants.LOCAL_SOCKS5_PROXY_DISABLED);
-    }
-
-    @Override
-    public String getStunIP() {
-        return preferenceStore.getString(PreferenceConstants.STUN);
-    }
-
-    @Override
-    public int getStunPort() {
-        return preferenceStore.getInt(PreferenceConstants.STUN_PORT);
-    }
-
-    @Override
-    public int getFavoriteColorID() {
-        return preferenceStore
-            .getInt(PreferenceConstants.FAVORITE_SESSION_COLOR_ID);
-    }
-
-    @Override
-    public String getSessionNickname() {
-        return preferenceStore.getString(PreferenceConstants.SESSION_NICKNAME);
+    public EclipsePreferences(
+        org.eclipse.jface.preference.IPreferenceStore store) {
+        super(new EclipsePreferenceStoreAdapter(store));
     }
 }
