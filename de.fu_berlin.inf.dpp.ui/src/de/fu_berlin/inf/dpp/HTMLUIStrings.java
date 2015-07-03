6ca0212796430ecbe842159eb43d17d2992256b9
Bastian Sieker <bsieker@mail.uni-paderborn.de>
2015-07-03 16:40:28 +0200

    [INTERNAL][HTML] Encapsulate UI strings in static class
    All labels, messages, titles, etc. which will be visible in the
    HTML UI are encapsulated as public static fields in the class
    HTMLUIStrings to avoid hard-coding them.
    
    This class will be progressively extended while the UI is
    developing.
    
    Change-Id: Ifeefd6109e7a258e7fca31e368d03af54259c625
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2754
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

---
+++ de.fu_berlin.inf.dpp.ui/src/de/fu_berlin/inf/dpp/HTMLUIStrings.java
@@ -0,0 +1,13 @@
+package de.fu_berlin.inf.dpp;
+
+/**
+ * This class encapsulates all labels, messages, titles, etc. which will be
+ * visible in the HTML UI.
+ *
+ * TODO: Maybe this feature can be implemented more sophisticated in the future.
+ */
+public class HTMLUIStrings {
+
+    public static final String MAIN_PAGE_TITLE = "Main page";
+    public static final String START_SESSION_WIZARD_TITLE = "Share Project";
+}
