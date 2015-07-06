7c6885b255eccf5c17619b71b31e5a464dc14804
Stefan Rossbach <srossbach@arcor.de>
2015-07-06 14:45:01 +0200

    [BUILD] add test target to ANT UI plugin build
    Change-Id: Ic4fe1e7415baa493a9c54673274a7a89e9a50b89
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2756
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

---
+++ de.fu_berlin.inf.dpp.ui/test/junit/de/fu_berlin/inf/dpp/ui/model/AccountTest.java
@@ -0,0 +1,18 @@
+package de.fu_berlin.inf.dpp.ui.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AccountTest {
+
+    @Test
+    public void testGetters() {
+
+        final Account account = new Account("alice@example.org/test");
+
+        assertEquals("alice", account.getUsername());
+        assertEquals("example.org", account.getDomain());
+        assertEquals("alice@example.org", account.getBareJid());
+    }
+}
