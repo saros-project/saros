c1e277036b095dbe7694668834bfaf1fc2e38a4e
Denis Washington <denis@denisw.de>
2015-07-28 11:00:03 +0200

    [INTERNAL] Implement internalWorked() in CoreToEclipseMonitorAdapter
    The former no-op implementation is replaced with one that accumulates
    progress until full work units are reached and forwards those to
    worked(). This makes the adapter work well with clients that call
    internalWorked() directly instead of worked() (which seems to be the
    case with some parts of Subclipse, for instance).
    
    Change-Id: Ib42cddab9fbf77fcad2f213b1b47827ae0515f65
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2785
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/monitoring/CoreToEclipseMonitorAdapter.java
+++ de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/monitoring/CoreToEclipseMonitorAdapter.java
@@ -1,11 +1,24 @@
 package de.fu_berlin.inf.dpp.monitoring;
 
-public class CoreToEclipseMonitorAdapter implements
-    org.eclipse.core.runtime.IProgressMonitor,
+/**
+ * Adapter class which turns a Saros core progress monitor into an Eclipse one.
+ *
+ * @see de.fu_berlin.inf.dpp.monitoring.IProgressMonitor
+ * @see org.eclipse.core.runtime.IProgressMonitor
+ */
+public class CoreToEclipseMonitorAdapter
+    implements org.eclipse.core.runtime.IProgressMonitor,
     de.fu_berlin.inf.dpp.monitoring.IProgressMonitor {
 
     private final de.fu_berlin.inf.dpp.monitoring.IProgressMonitor monitor;
+    private double collectedWork;
 
+    /**
+     * Creates an Eclipse wrapper around a Saros core {@link IProgressMonitor}.
+     *
+     * @param monitor
+     *            Saros core progress monitor to wrap
+     */
     public CoreToEclipseMonitorAdapter(
         de.fu_berlin.inf.dpp.monitoring.IProgressMonitor monitor) {
         this.monitor = monitor;
@@ -21,10 +34,24 @@ public class CoreToEclipseMonitorAdapter implements
         monitor.done();
     }
 
+    /**
+     * Implementation of
+     * {@link org.eclipse.core.runtime.IProgressMonitor#internalWorked(double)}
+     * which delegates to {@link #worked(int)}, keeping track of passed
+     * fractions until whole full units of work are reached. This compensates
+     * for the lack of an <code>internalWorked()</code> method in the Saros core
+     * version of {@link IProgressMonitor}.
+     */
     @Override
     public void internalWorked(double work) {
-        // TODO this might not work for specific monitors
-        // NOP
+        collectedWork += work;
+
+        int floorOfCollectedWork = (int) Math.floor(collectedWork);
+        if (floorOfCollectedWork > 0) {
+            worked(floorOfCollectedWork);
+        }
+
+        collectedWork -= floorOfCollectedWork;
     }
 
     @Override
