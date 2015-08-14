c25371a29694a148c93ed8da048cc390c875c7bc
Bastian Sieker <bsieker@mail.uni-paderborn.de>
2015-08-14 15:18:25 +0200

    [HTML] Make contacts container collapsible
    With this commit the container for the list of
    contacts is collapsible. For convenience, I used
    icons provided by Bootstrap:
    http://getbootstrap.com/components/#glyphicons
    
    There may be lincense issues?!
    
    Change-Id: Iedc0fe6e30b0d321abb239656e73ba2e50ac6a86
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2819
    Tested-by: Jenkins CI
    Reviewed-by: Denis Washington <denis@denisw.de>
    Reviewed-by: Bastian Sieker <bsieker@mail.uni-paderborn.de>

--- de.fu_berlin.inf.dpp.ui.frontend/html/js/views/contacts.js
+++ de.fu_berlin.inf.dpp.ui.frontend/html/js/views/contacts.js
@@ -8,11 +8,41 @@ module.exports = AmpersandView.extend({
     // Add the dictionary to the context so that the template
     // engine can use it.
     d: dictionary,
+    props: {
+        collapsed: {
+            type: 'boolean',
+            default: false
+        }
+    },
+    events: {
+        'click h4.collapsible': 'toggleCollapse'
+    },
+    bindings: {
+        'collapsed': [
+            // toggle arrow icon down/up
+            {
+                type: 'booleanClass',
+                yes: 'glyphicon-chevron-down',
+                no: 'glyphicon-chevron-up',
+                hook: 'collapse-trigger'
+            },
+            // toggle list visibility
+            {
+                type: 'booleanClass',
+                hook: 'contact-list',
+                name: 'hide'
+            }
+        ]
+    },
     render: function() {
 
         this.renderWithTemplate(this);
         this.renderCollection(this.collection, ContactView,
             this.queryByHook('contact-list'));
         return this;
+    },
+    toggleCollapse: function() {
+
+        this.collapsed = !this.collapsed;
     }
 });
