d798c817143f5496dc471fe0857f1ddbcce61c09
Stefan Rossbach <srossbach@arcor.de>
2015-08-25 14:21:02 +0200

    [INTERNAL] move SharedProjectListenerDispatch to core
    This patch moves the still wrongly named SharedProjectListenerDispatch
    class to the core.
    
    This patch also cleans up the class by removing unnecessary this
    references and replaced the contains&add logic with an addIfAbsent
    call.
    
    Change-Id: I442fa3c1218e42221a3bab30043f63ca0f732cc1
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2841
    Tested-by: Jenkins CI
    Reviewed-by: Franz Zieris <zieris@inf.fu-berlin.de>
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/session/internal/SharedProjectListenerDispatch.java
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/session/internal/SharedProjectListenerDispatch.java
@@ -1,56 +1,50 @@
-package de.fu_berlin.inf.dpp.project.internal;
+package de.fu_berlin.inf.dpp.session.internal;
 
-import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import de.fu_berlin.inf.dpp.session.ISharedProjectListener;
 import de.fu_berlin.inf.dpp.session.User;
 
+// TODO rename this class along with the ISharedProjectListener interface
 public class SharedProjectListenerDispatch implements ISharedProjectListener {
-    protected List<ISharedProjectListener> listeners = new CopyOnWriteArrayList<ISharedProjectListener>();
+
+    private final CopyOnWriteArrayList<ISharedProjectListener> listeners = new CopyOnWriteArrayList<ISharedProjectListener>();
 
     @Override
     public void permissionChanged(User user) {
-        for (ISharedProjectListener listener : this.listeners) {
+        for (ISharedProjectListener listener : listeners)
             listener.permissionChanged(user);
-        }
     }
 
     @Override
     public void userJoined(User user) {
-        for (ISharedProjectListener listener : this.listeners) {
+        for (ISharedProjectListener listener : listeners)
             listener.userJoined(user);
-        }
     }
 
     @Override
     public void userStartedQueuing(User user) {
-        for (ISharedProjectListener listener : this.listeners) {
+        for (ISharedProjectListener listener : listeners)
             listener.userStartedQueuing(user);
-        }
     }
 
     @Override
     public void userFinishedProjectNegotiation(User user) {
-        for (ISharedProjectListener listener : this.listeners) {
+        for (ISharedProjectListener listener : listeners)
             listener.userFinishedProjectNegotiation(user);
-        }
     }
 
     @Override
     public void userLeft(User user) {
-        for (ISharedProjectListener listener : this.listeners) {
+        for (ISharedProjectListener listener : listeners)
             listener.userLeft(user);
-        }
     }
 
     public void add(ISharedProjectListener listener) {
-        if (!this.listeners.contains(listener)) {
-            this.listeners.add(listener);
-        }
+        listeners.addIfAbsent(listener);
     }
 
     public void remove(ISharedProjectListener listener) {
-        this.listeners.remove(listener);
+        listeners.remove(listener);
     }
 }
