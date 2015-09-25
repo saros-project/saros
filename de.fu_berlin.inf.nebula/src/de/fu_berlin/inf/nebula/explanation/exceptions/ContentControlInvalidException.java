be93b1b1c302e67831708a41ddbc237921faa3fe
Franz Zieris <zieris@inf.fu-berlin.de>
2015-09-25 21:35:36 +0200

    [BUILD] Delete Nebula from Saros repository
    Nebula is not used by Saros since 3b360ff (from 2013-12).
    
    Change-Id: Ie663131fbe65be0c19323198e6cf784c7363101d
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2909
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.nebula/src/de/fu_berlin/inf/nebula/explanation/exceptions/ContentControlInvalidException.java
+++
@@ -1,9 +0,0 @@
-package de.fu_berlin.inf.nebula.explanation.exceptions;
-
-public class ContentControlInvalidException extends RuntimeException {
-	private static final long serialVersionUID = 4000855428432271516L;
-
-	public ContentControlInvalidException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
