ade68952a33259d6aa9f2e8614f1a77152272b5a
Holger Schmeisky <google@schmeisky.com>
2015-11-02 15:40:53 +0100

    [DOC][I] Removed copyright headers in source file
    This patch removes the copyright header at the beginning of each
    source file and places it in a separate NOTICE file at the root
    of the project. It also adds the full GPL 2.0 license in the
    LICENSE file at the root of the project.
    
    Change-Id: Idb3c26266e8088c8deedefae9d40211de0821bd3
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2938
    Tested-by: Jenkins CI
    Reviewed-by: Holger Schmeisky <holger.schmeisky@gmail.com>
    Reviewed-by: Franz Zieris <zieris@inf.fu-berlin.de>

--- de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/runtime/IntelliJSynchronizer.java
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/runtime/IntelliJSynchronizer.java
@@ -1,25 +1,3 @@
-/*
- *
- *  DPP - Serious Distributed Pair Programming
- *  (c) Freie Universität Berlin - Fachbereich Mathematik und Informatik - 2010
- *  (c) NFQ (www.nfq.com) - 2014
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 1, or (at your option)
- *  any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * /
- */
-
 package de.fu_berlin.inf.dpp.intellij.runtime;
 
 import com.intellij.openapi.application.Application;
