saros-project/saros

commit ac51bd902b0bc4de8e62a72cfd05b9aab37c4e17

[API] Move SarosCoreContextFactory to core
SarosCoreContextFactory exists in both Eclipse and IntelliJ and was
meant to be moved to the core. However there are some minor problems
with that.

I took the Saros/E variant and moved it to the core. The following
components were removed:
   * MUCNegotiationManager (Eclipse only)
   * SkypeManager (Eclipse only)
   * IsInconsistentObservable (not part of the core yet)
   * AwarenessInformationCollector (not part of the UI yet)

The above components are now part of SarosEclipseContextFactory and/or
SarosIntelliJContextFactory.

Change-Id: I88df3a17cb06cb21b8ff5f3a67b0081186fb28c4
Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2971
Tested-by: Jenkins CI
Reviewed-by: Franz Zieris <zieris@inf.fu-berlin.de>
Reviewed-by: Denis Washington <denis@denisw.de>

de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/context/SarosIntellijContextFactory.java
@@ -5,7 +5,9 @@ import de.fu_berlin.inf.dpp.ISarosContextBindings;
 import de.fu_berlin.inf.dpp.communication.connection.IProxyResolver;
 import de.fu_berlin.inf.dpp.connection.NullProxyResolver;
 import de.fu_berlin.inf.dpp.core.Saros;
+import de.fu_berlin.inf.dpp.core.awareness.AwarenessInformationCollector;
 import de.fu_berlin.inf.dpp.core.concurrent.ConsistencyWatchdogClient;
+import de.fu_berlin.inf.dpp.core.concurrent.IsInconsistentObservable;
 import de.fu_berlin.inf.dpp.core.monitoring.remote.IntelliJRemoteProgressIndicatorFactoryImpl;
 import de.fu_berlin.inf.dpp.core.project.internal.SarosIntellijSessionContextFactory;
 import de.fu_berlin.inf.dpp.core.ui.eventhandler.NegotiationHandler;
@@ -17,6 +19,7 @@ import de.fu_berlin.inf.dpp.editor.IEditorManager;
 import de.fu_berlin.inf.dpp.filesystem.IChecksumCache;
 import de.fu_berlin.inf.dpp.filesystem.IPathFactory;
 import de.fu_berlin.inf.dpp.filesystem.IWorkspace;
+import de.fu_berlin.inf.dpp.filesystem.NullChecksumCacheImpl;
 import de.fu_berlin.inf.dpp.intellij.editor.EditorAPI;
 import de.fu_berlin.inf.dpp.intellij.editor.EditorManager;
 import de.fu_berlin.inf.dpp.intellij.editor.LocalEditorHandler;
@@ -24,7 +27,6 @@ import de.fu_berlin.inf.dpp.intellij.editor.LocalEditorManipulator;
 import de.fu_berlin.inf.dpp.intellij.editor.ProjectAPI;
 import de.fu_berlin.inf.dpp.intellij.preferences.IntelliJPreferences;
 import de.fu_berlin.inf.dpp.intellij.preferences.PropertiesComponentAdapter;
-import de.fu_berlin.inf.dpp.filesystem.NullChecksumCacheImpl;
 import de.fu_berlin.inf.dpp.intellij.project.filesystem.PathFactory;
 import de.fu_berlin.inf.dpp.intellij.runtime.IntelliJSynchronizer;
 import de.fu_berlin.inf.dpp.intellij.ui.actions.FollowModeAction;
@@ -60,6 +62,7 @@ public class SarosIntellijContextFactory extends AbstractSarosContextFactory {
         Component.create(ConsistencyWatchdogClient.class),
 
         Component.create(EditorAPI.class),
+
         Component.create(ProjectAPI.class),
 
         Component.create(IEditorManager.class, EditorManager.class),
@@ -78,8 +81,8 @@ public class SarosIntellijContextFactory extends AbstractSarosContextFactory {
 
         Component.create(UISynchronizer.class, IntelliJSynchronizer.class),
 
-        Component.create(IPreferenceStore.class,
-            PropertiesComponentAdapter.class),
+        Component
+            .create(IPreferenceStore.class, PropertiesComponentAdapter.class),
 
         Component.create(Preferences.class, IntelliJPreferences.class),
 
@@ -93,11 +96,17 @@ public class SarosIntellijContextFactory extends AbstractSarosContextFactory {
         Component.create(DialogManager.class, IntelliJDialogManager.class),
         Component.create(IWebResourceLocator.class,
             IntelliJWebResourceLocator.class),
+
         Component.create(ICollaborationUtils.class,
             IntelliJCollaborationUtilsImpl.class),
 
         // Proxy Support for the XMPP server connection
-        Component.create(IProxyResolver.class, NullProxyResolver.class) };
+        Component.create(IProxyResolver.class, NullProxyResolver.class),
+
+        Component.create(AwarenessInformationCollector.class),
+        Component.create(IsInconsistentObservable.class)
+
+    };
 
     public SarosIntellijContextFactory(Saros saros) {
         this.saros = saros;
@@ -122,10 +131,11 @@ public class SarosIntellijContextFactory extends AbstractSarosContextFactory {
         container.addComponent(saros);
 
         container.addComponent(BindKey.bindKey(String.class,
-            ISarosContextBindings.SarosVersion.class), "14.1.31.DEVEL"); // todo
+                ISarosContextBindings.SarosVersion.class),
+            "14.1.31.DEVEL"); // todo
 
         container.addComponent(BindKey.bindKey(String.class,
-            ISarosContextBindings.PlatformVersion.class), "4.3.2"); // todo
+                ISarosContextBindings.PlatformVersion.class), "4.3.2"); // todo
 
     }
 }

de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/SarosCoreContextFactory.java
@@ -1,40 +1,19 @@
-/*
- *
- *  DPP - Serious Distributed Pair Programming
- *  (c) Freie Universität Berlin - Fachbereich Mathematik und Informatik - 2010
- *  (c) NFQ (www.nfq.com) - 2014
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 1, or (at your option)
- *  any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * /
- */
+package de.fu_berlin.inf.dpp;
 
-package de.fu_berlin.inf.dpp.core.context;
+import java.util.Arrays;
 
-import de.fu_berlin.inf.dpp.AbstractSarosContextFactory;
-import de.fu_berlin.inf.dpp.ISarosContextBindings;
+import org.picocontainer.BindKey;
+import org.picocontainer.MutablePicoContainer;
+
 import de.fu_berlin.inf.dpp.account.XMPPAccountStore;
 import de.fu_berlin.inf.dpp.communication.chat.muc.MultiUserChatService;
 import de.fu_berlin.inf.dpp.communication.chat.single.SingleUserChatService;
 import de.fu_berlin.inf.dpp.communication.connection.ConnectionHandler;
-import de.fu_berlin.inf.dpp.core.awareness.AwarenessInformationCollector;
-import de.fu_berlin.inf.dpp.core.concurrent.IsInconsistentObservable;
-import de.fu_berlin.inf.dpp.core.vcs.NullVCSProviderFactoryImpl;
 import de.fu_berlin.inf.dpp.editor.colorstorage.ColorIDSetStorage;
 import de.fu_berlin.inf.dpp.monitoring.remote.RemoteProgressManager;
 import de.fu_berlin.inf.dpp.negotiation.hooks.SessionNegotiationHookManager;
 import de.fu_berlin.inf.dpp.net.DispatchThreadContext;
+import de.fu_berlin.inf.dpp.net.IConnectionManager;
 import de.fu_berlin.inf.dpp.net.IReceiver;
 import de.fu_berlin.inf.dpp.net.ITransmitter;
 import de.fu_berlin.inf.dpp.net.internal.DataTransferManager;
@@ -60,21 +39,21 @@ import de.fu_berlin.inf.dpp.observables.ProjectNegotiationObservable;
 import de.fu_berlin.inf.dpp.observables.SarosSessionObservable;
 import de.fu_berlin.inf.dpp.observables.SessionIDObservable;
 import de.fu_berlin.inf.dpp.observables.SessionNegotiationObservable;
-import de.fu_berlin.inf.dpp.vcs.VCSProviderFactory;
+import de.fu_berlin.inf.dpp.session.ColorNegotiationHook;
+import de.fu_berlin.inf.dpp.session.SarosSessionManager;
 import de.fu_berlin.inf.dpp.versioning.VersionManager;
-import org.picocontainer.BindKey;
-import org.picocontainer.MutablePicoContainer;
-
-import java.util.Arrays;
 
 /**
  * This is the basic core factory for Saros. All components that are created by
  * this factory <b>must</b> be working on any platform the application is
  * running on.
- *
+ * 
  * @author srossbach
  */
 public class SarosCoreContextFactory extends AbstractSarosContextFactory {
+
+    // TODO we must abstract the IPrefenceStore stuff otherwise anything here is
+    // broken
 
     private final Component[] components = new Component[] {
 
@@ -87,35 +66,34 @@ public class SarosCoreContextFactory extends AbstractSarosContextFactory {
         Component.create(MultiUserChatService.class),
         Component.create(SingleUserChatService.class),
 
+        Component.create(SarosSessionManager.class),
+
         Component.create(XMPPAccountStore.class),
         Component.create(ColorIDSetStorage.class),
 
         // Invitation hooks
         Component.create(SessionNegotiationHookManager.class),
-
-        // VCS (only dummy to satisfy dependencies)
-        Component
-            .create(VCSProviderFactory.class, NullVCSProviderFactoryImpl.class),
+        Component.create(ColorNegotiationHook.class),
 
         // Network
         Component.create(DispatchThreadContext.class),
 
-        Component.create(DataTransferManager.class),
+        Component.create(IConnectionManager.class, DataTransferManager.class),
 
         Component.create(DiscoveryManager.class),
 
         Component.create(BindKey.bindKey(ITransport.class,
             ISarosContextBindings.IBBTransport.class), IBBTransport.class),
 
-        Component.create(BindKey.bindKey(ITransport.class,
+        Component
+            .create(BindKey.bindKey(ITransport.class,
                 ISarosContextBindings.Socks5Transport.class),
-            Socks5Transport.class
-        ),
+                Socks5Transport.class),
 
         Component.create(RosterTracker.class),
         Component.create(XMPPConnectionService.class),
-        Component.create(MDNSService.class),
-        Component.create(TCPServer.class),
+        Component.create(MDNSService.class), Component.create(TCPServer.class),
+
         Component.create(IStunService.class, StunServiceImpl.class),
 
         Component.create(SubscriptionHandler.class),
@@ -125,26 +103,19 @@ public class SarosCoreContextFactory extends AbstractSarosContextFactory {
         Component.create(IReceiver.class, XMPPReceiver.class),
         Component.create(ITransmitter.class, XMPPTransmitter.class),
 
+        Component.create(RemoteProgressManager.class),
+
         // Observables
         Component.create(FileReplacementInProgressObservable.class),
         Component.create(SessionNegotiationObservable.class),
         Component.create(ProjectNegotiationObservable.class),
-        Component.create(IsInconsistentObservable.class),
         Component.create(SessionIDObservable.class),
-        Component.create(SarosSessionObservable.class),
-        Component.create(AwarenessInformationCollector.class),
-
-        // Handlers
-        Component.create(RemoteProgressManager.class),
-
-    };
+        Component.create(SarosSessionObservable.class) };
 
     @Override
     public void createComponents(MutablePicoContainer container) {
-        for (Component component : Arrays.asList(components)) {
-
+        for (Component component : Arrays.asList(components))
             container.addComponent(component.getBindKey(),
                 component.getImplementation());
-        }
     }
 }

de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/core/Saros.java
@@ -9,8 +9,8 @@ import com.intellij.openapi.project.Project;
 import de.fu_berlin.inf.dpp.ISarosContextFactory;
 import de.fu_berlin.inf.dpp.SarosContext;
 import de.fu_berlin.inf.dpp.HTMLUIContextFactory;
+import de.fu_berlin.inf.dpp.SarosCoreContextFactory;
 import de.fu_berlin.inf.dpp.SarosPluginContext;
-import de.fu_berlin.inf.dpp.core.context.SarosCoreContextFactory;
 import de.fu_berlin.inf.dpp.filesystem.IWorkspace;
 import de.fu_berlin.inf.dpp.intellij.context.SarosIntellijContextFactory;
 import de.fu_berlin.inf.dpp.intellij.project.filesystem.IntelliJWorkspaceImpl;

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/SarosCoreContextFactory.java
@@ -1,130 +1,0 @@
-package de.fu_berlin.inf.dpp;
-
-import java.util.Arrays;
-
-import org.picocontainer.BindKey;
-import org.picocontainer.MutablePicoContainer;
-
-import de.fu_berlin.inf.dpp.account.XMPPAccountStore;
-import de.fu_berlin.inf.dpp.awareness.AwarenessInformationCollector;
-import de.fu_berlin.inf.dpp.communication.SkypeManager;
-import de.fu_berlin.inf.dpp.communication.chat.muc.MultiUserChatService;
-import de.fu_berlin.inf.dpp.communication.chat.muc.negotiation.MUCNegotiationManager;
-import de.fu_berlin.inf.dpp.communication.chat.single.SingleUserChatService;
-import de.fu_berlin.inf.dpp.communication.connection.ConnectionHandler;
-import de.fu_berlin.inf.dpp.concurrent.watchdog.IsInconsistentObservable;
-import de.fu_berlin.inf.dpp.editor.colorstorage.ColorIDSetStorage;
-import de.fu_berlin.inf.dpp.monitoring.remote.RemoteProgressManager;
-import de.fu_berlin.inf.dpp.negotiation.hooks.SessionNegotiationHookManager;
-import de.fu_berlin.inf.dpp.net.DispatchThreadContext;
-import de.fu_berlin.inf.dpp.net.IConnectionManager;
-import de.fu_berlin.inf.dpp.net.IReceiver;
-import de.fu_berlin.inf.dpp.net.ITransmitter;
-import de.fu_berlin.inf.dpp.net.internal.DataTransferManager;
-import de.fu_berlin.inf.dpp.net.internal.IBBTransport;
-import de.fu_berlin.inf.dpp.net.internal.ITransport;
-import de.fu_berlin.inf.dpp.net.internal.Socks5Transport;
-import de.fu_berlin.inf.dpp.net.internal.TCPServer;
-import de.fu_berlin.inf.dpp.net.internal.XMPPReceiver;
-import de.fu_berlin.inf.dpp.net.internal.XMPPTransmitter;
-import de.fu_berlin.inf.dpp.net.mdns.MDNSService;
-import de.fu_berlin.inf.dpp.net.stun.IStunService;
-import de.fu_berlin.inf.dpp.net.stun.internal.StunServiceImpl;
-import de.fu_berlin.inf.dpp.net.upnp.IUPnPAccess;
-import de.fu_berlin.inf.dpp.net.upnp.IUPnPService;
-import de.fu_berlin.inf.dpp.net.upnp.internal.UPnPAccessImpl;
-import de.fu_berlin.inf.dpp.net.upnp.internal.UPnPServiceImpl;
-import de.fu_berlin.inf.dpp.net.xmpp.XMPPConnectionService;
-import de.fu_berlin.inf.dpp.net.xmpp.discovery.DiscoveryManager;
-import de.fu_berlin.inf.dpp.net.xmpp.roster.RosterTracker;
-import de.fu_berlin.inf.dpp.net.xmpp.subscription.SubscriptionHandler;
-import de.fu_berlin.inf.dpp.observables.FileReplacementInProgressObservable;
-import de.fu_berlin.inf.dpp.observables.ProjectNegotiationObservable;
-import de.fu_berlin.inf.dpp.observables.SarosSessionObservable;
-import de.fu_berlin.inf.dpp.observables.SessionIDObservable;
-import de.fu_berlin.inf.dpp.observables.SessionNegotiationObservable;
-import de.fu_berlin.inf.dpp.session.ColorNegotiationHook;
-import de.fu_berlin.inf.dpp.session.SarosSessionManager;
-import de.fu_berlin.inf.dpp.versioning.VersionManager;
-
-/**
- * This is the basic core factory for Saros. All components that are created by
- * this factory <b>must</b> be working on any platform the application is
- * running on.
- * 
- * @author srossbach
- */
-public class SarosCoreContextFactory extends AbstractSarosContextFactory {
-
-    // TODO we must abstract the IPrefenceStore stuff otherwise anything here is
-    // broken
-
-    private final Component[] components = new Component[] {
-
-        // Facades
-        Component.create(ConnectionHandler.class),
-
-        // Version support
-        Component.create(VersionManager.class),
-
-        Component.create(MultiUserChatService.class),
-        Component.create(SingleUserChatService.class),
-
-        Component.create(SarosSessionManager.class),
-
-        Component.create(XMPPAccountStore.class),
-        Component.create(ColorIDSetStorage.class),
-
-        // Invitation hooks
-        Component.create(SessionNegotiationHookManager.class),
-        Component.create(ColorNegotiationHook.class),
-        Component.create(MUCNegotiationManager.class),
-
-        // Network
-        Component.create(DispatchThreadContext.class),
-
-        Component.create(IConnectionManager.class, DataTransferManager.class),
-
-        Component.create(DiscoveryManager.class),
-
-        Component.create(BindKey.bindKey(ITransport.class,
-            ISarosContextBindings.IBBTransport.class), IBBTransport.class),
-
-        Component
-            .create(BindKey.bindKey(ITransport.class,
-                ISarosContextBindings.Socks5Transport.class),
-                Socks5Transport.class),
-
-        Component.create(RosterTracker.class),
-        Component.create(XMPPConnectionService.class),
-        Component.create(MDNSService.class),
-        Component.create(TCPServer.class),
-        Component.create(SkypeManager.class),
-
-        Component.create(IStunService.class, StunServiceImpl.class),
-
-        Component.create(SubscriptionHandler.class),
-
-        Component.create(IUPnPService.class, UPnPServiceImpl.class),
-        Component.create(IUPnPAccess.class, UPnPAccessImpl.class),
-        Component.create(IReceiver.class, XMPPReceiver.class),
-        Component.create(ITransmitter.class, XMPPTransmitter.class),
-
-        Component.create(RemoteProgressManager.class),
-
-        // Observables
-        Component.create(FileReplacementInProgressObservable.class),
-        Component.create(SessionNegotiationObservable.class),
-        Component.create(ProjectNegotiationObservable.class),
-        Component.create(IsInconsistentObservable.class),
-        Component.create(SessionIDObservable.class),
-        Component.create(SarosSessionObservable.class),
-        Component.create(AwarenessInformationCollector.class) };
-
-    @Override
-    public void createComponents(MutablePicoContainer container) {
-        for (Component component : Arrays.asList(components))
-            container.addComponent(component.getBindKey(),
-                component.getImplementation());
-    }
-}

de.fu_berlin.inf.dpp/src/de/fu_berlin/inf/dpp/SarosEclipseContextFactory.java
@@ -8,9 +8,13 @@ import org.osgi.service.prefs.Preferences;
 import org.picocontainer.BindKey;
 import org.picocontainer.MutablePicoContainer;
 
+import de.fu_berlin.inf.dpp.awareness.AwarenessInformationCollector;
+import de.fu_berlin.inf.dpp.communication.SkypeManager;
+import de.fu_berlin.inf.dpp.communication.chat.muc.negotiation.MUCNegotiationManager;
 import de.fu_berlin.inf.dpp.communication.connection.IProxyResolver;
 import de.fu_berlin.inf.dpp.communication.connection.Socks5ProxyResolver;
 import de.fu_berlin.inf.dpp.concurrent.undo.UndoManager;
+import de.fu_berlin.inf.dpp.concurrent.watchdog.IsInconsistentObservable;
 import de.fu_berlin.inf.dpp.editor.EditorManager;
 import de.fu_berlin.inf.dpp.editor.IEditorManager;
 import de.fu_berlin.inf.dpp.editor.internal.EditorAPI;
@@ -112,7 +116,13 @@ public class SarosEclipseContextFactory extends AbstractSarosContextFactory {
 
         // Remote progress indication
         Component.create(IRemoteProgressIndicatorFactory.class,
-            EclipseRemoteProgressIndicatorFactoryImpl.class) };
+            EclipseRemoteProgressIndicatorFactoryImpl.class),
+
+        Component.create(MUCNegotiationManager.class),
+        Component.create(SkypeManager.class),
+
+        Component.create(IsInconsistentObservable.class),
+        Component.create(AwarenessInformationCollector.class) };
 
     public SarosEclipseContextFactory(Saros saros) {
         this.saros = saros;

de.fu_berlin.inf.dpp/test/junit/de/fu_berlin/inf/dpp/session/internal/SarosSessionTest.java
@@ -40,6 +40,7 @@ import de.fu_berlin.inf.dpp.ISarosContext;
 import de.fu_berlin.inf.dpp.ISarosContextBindings;
 import de.fu_berlin.inf.dpp.Saros;
 import de.fu_berlin.inf.dpp.SarosCoreContextFactory;
+import de.fu_berlin.inf.dpp.awareness.AwarenessInformationCollector;
 import de.fu_berlin.inf.dpp.editor.EditorManager;
 import de.fu_berlin.inf.dpp.editor.internal.IEditorAPI;
 import de.fu_berlin.inf.dpp.feedback.FeedbackManager;
@@ -286,6 +287,7 @@ public class SarosSessionTest {
 
         container.addComponent(NonUISynchronizer.class);
         container.addComponent(FeedbackManager.class);
+        container.addComponent(AwarenessInformationCollector.class);
 
         // Init Feedback
         FeedbackPreferences.setPreferences(preferences);

commit 82def6d38bfe520afa88da9c1062e308622472fe

[FIX][I] Fix ContextFactory after introducing CoreContextFactory
The SarosSessionManager was created both in the
SarosCoreContextFactory and the IntellijContextFactory
which led to some errors. Also the VCSProviderFactory was
not instantiated.

These defects were introduced with commit ac51bd9.

Change-Id: I9d577bca059b3136b6115749a4f74790ff303d2b
Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/2991
Tested-by: Jenkins CI
Reviewed-by: David Sungaila <david.sungaila@arcor.de>
Reviewed-by: Holger Schmeisky <holger.schmeisky@gmail.com>

de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/context/SarosIntellijContextFactory.java
@@ -15,6 +15,7 @@ import de.fu_berlin.inf.dpp.core.ui.eventhandler.UserStatusChangeHandler;
 import de.fu_berlin.inf.dpp.core.ui.eventhandler.XMPPAuthorizationHandler;
 import de.fu_berlin.inf.dpp.core.util.FileUtils;
 import de.fu_berlin.inf.dpp.core.util.IntelliJCollaborationUtilsImpl;
+import de.fu_berlin.inf.dpp.core.vcs.NullVCSProviderFactoryImpl;
 import de.fu_berlin.inf.dpp.editor.IEditorManager;
 import de.fu_berlin.inf.dpp.filesystem.IChecksumCache;
 import de.fu_berlin.inf.dpp.filesystem.IPathFactory;
@@ -37,12 +38,11 @@ import de.fu_berlin.inf.dpp.monitoring.remote.IRemoteProgressIndicatorFactory;
 import de.fu_berlin.inf.dpp.preferences.IPreferenceStore;
 import de.fu_berlin.inf.dpp.preferences.Preferences;
 import de.fu_berlin.inf.dpp.session.ISarosSessionContextFactory;
-import de.fu_berlin.inf.dpp.session.ISarosSessionManager;
-import de.fu_berlin.inf.dpp.session.SarosSessionManager;
 import de.fu_berlin.inf.dpp.synchronize.UISynchronizer;
 import de.fu_berlin.inf.dpp.ui.ide_embedding.DialogManager;
 import de.fu_berlin.inf.dpp.ui.ide_embedding.IWebResourceLocator;
 import de.fu_berlin.inf.dpp.ui.util.ICollaborationUtils;
+import de.fu_berlin.inf.dpp.vcs.VCSProviderFactory;
 import org.picocontainer.BindKey;
 import org.picocontainer.MutablePicoContainer;
 
@@ -63,7 +63,6 @@ public class SarosIntellijContextFactory extends AbstractSarosContextFactory {
      */
     private final Component[] components = new Component[] {
 
-        Component.create(ISarosSessionManager.class, SarosSessionManager.class),
         // Core Managers
         Component.create(ConsistencyWatchdogClient.class),
 
@@ -77,6 +76,10 @@ public class SarosIntellijContextFactory extends AbstractSarosContextFactory {
 
         Component.create(ISarosSessionContextFactory.class,
             SarosIntellijSessionContextFactory.class),
+
+        // VCS (only dummy to satisfy dependencies)
+        Component
+            .create(VCSProviderFactory.class, NullVCSProviderFactoryImpl.class),
 
         // UI handlers
         Component.create(NegotiationHandler.class),
@@ -92,6 +95,7 @@ public class SarosIntellijContextFactory extends AbstractSarosContextFactory {
 
         Component.create(Preferences.class, IntelliJPreferences.class),
 
+        // UI actions
         Component.create(FollowModeAction.class),
         Component.create(LeaveSessionAction.class),
 