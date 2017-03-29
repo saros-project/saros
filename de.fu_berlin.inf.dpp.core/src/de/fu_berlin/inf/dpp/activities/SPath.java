fad2cf8a7571819e452a6a6e429e777b77ba38f7
Stefan Rossbach <srossbach@arcor.de>
2017-03-29 18:43:51 +0200

    [API][CORE] changes SPath#getResource behavior
    This method no longer performs IO / VFS structure queries but always
    returns a IResource instance regardless if it exists.
    
    See also bug: https://sourceforge.net/p/dpp/bugs/448/
    
    It adds no benefit if this method would return null because it hides
    issues as long as #getFile and #getFolder will return non null while
    #getResource would return null.
    
    In addition #getFile and #getFolder will not throw an exception if the
    resource is not of the given type.
    
    Change-Id: I849b28be652b94b7a6a8e0361fc4fdccf8273ba6
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/3255
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

--- de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/activities/SPath.java
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/activities/SPath.java
@@ -9,23 +9,25 @@ import de.fu_berlin.inf.dpp.filesystem.IFolder;
 import de.fu_berlin.inf.dpp.filesystem.IPath;
 import de.fu_berlin.inf.dpp.filesystem.IProject;
 import de.fu_berlin.inf.dpp.filesystem.IResource;
+import de.fu_berlin.inf.dpp.misc.xstream.SPathConverter;
 
 /**
- * Objects of this class point to a document in the *workspace*, possibly with
- * information about a specific open editor for that document.
- * 
- * An SPath consists of an IProject reference, IPath reference and editorType
- * identifier.
- * 
- * @immutable SPaths are value objects and thus immutable.
+ * A <i>SPath</i> points to a {@link IResource resource} in a {@link IProject
+ * project}. The specific resource does not need to exist, neither during the
+ * marshaling nor during unmarshaling.
+ * <p>
+ * <i>SPath</i> objects can be marshaled and unmarshaled.
+ *
+ * @see SPathConverter
  */
+
 @XStreamAlias("SPath")
 public class SPath {
 
     /**
      * @JTourBusStop 4, Some Basics:
-     * 
-     *               Individual Eclipse projects use IPaths to identify their
+     *
+     *               Individual IProjects use IPaths to identify their
      *               resources. However, because Saros needs to keep track of
      *               resources across multiple projects, it encapsulates IPaths
      *               in an SPath that includes additional identifying
@@ -33,31 +35,32 @@ public class SPath {
      */
 
     /**
-     * The project relative path of the resource or editor this SPath
-     * represents.
-     */
-    protected IPath projectRelativePath;
-
-    /**
-     * The local IProject in which the document is contained which this SPath
+     * The local IProject in which the resource is contained which this SPath
      * represents
      */
-    protected IProject project;
+    private final IProject project;
+
+    /**
+     * The project relative path of the resource this SPath represents.
+     */
+    private final IPath projectRelativePath;
 
     /**
      * Default constructor, initializing this SPath as a reference to the
-     * resource or editor identified by the given path in the given project.
-     * 
+     * resource identified by the given path in the given project.
+     *
      * @param path
-     *            May be <code>null</code> to represent "no editor".<br>
-     *            <em>FIXME <code>path == null</code> causes a NPE</em>
      * @throws IllegalArgumentException
-     *             if the path is not relative
+     *             if project is <code>null</code><br>
+     * @throws IllegalArgumentException
+     *             if the path is <code>null</code> or is not relative
      */
     public SPath(IProject project, IPath path) {
         if (project == null)
-            throw new IllegalArgumentException(
-                "SPath must be initialized with an IProject");
+            throw new IllegalArgumentException("project is null");
+
+        if (path == null)
+            throw new IllegalArgumentException("path is null");
 
         if (path.isAbsolute())
             throw new IllegalArgumentException("path is absolute: " + path);
@@ -75,21 +78,82 @@ public class SPath {
     }
 
     /**
-     * Returns the project relative path of the resource or editor represented
-     * by this SPath.
-     * 
-     * @return May return null if this SPath is used to represent no editor.
+     * Returns the project relative path of the resource represented by this
+     * SPath.
+     *
+     * @return project relative path of the resource
      */
     public IPath getProjectRelativePath() {
         return projectRelativePath;
+    }
+
+    /**
+     * Returns the IFile represented by this SPath.
+     *
+     * @return the IFile contained in the associated IProject for the given
+     *         project relative path
+     *
+     * @throws IllegalStateException
+     *             if the resource referenced by this SPath is not a file
+     *
+     */
+    public IFile getFile() {
+        if (projectRelativePath.hasTrailingSeparator())
+            throw new IllegalStateException("not a file: "
+                + projectRelativePath);
+
+        return project.getFile(projectRelativePath);
+    }
+
+    /**
+     * Returns the IResource represented by this SPath.
+     *
+     * @return the resource represented by this SPath
+     */
+    public IResource getResource() {
+        return projectRelativePath.hasTrailingSeparator() ? getFolder()
+            : getFile();
+    }
+
+    /**
+     * Returns the IFolder represented by this SPath.
+     *
+     * @return the IFolder contained in the associated IProject for the given
+     *         project relative path
+     *
+     * @throws IllegalStateException
+     *             if the resource referenced by this SPath is not a folder
+     */
+    public IFolder getFolder() {
+        if (!projectRelativePath.hasTrailingSeparator())
+            throw new IllegalStateException("not a folder: "
+                + projectRelativePath);
+
+        return project.getFolder(projectRelativePath);
+    }
+
+    /**
+     * Returns the project in which the referenced resource is located.
+     */
+    public IProject getProject() {
+        return project;
+    }
+
+    /**
+     * Convenience method for getting the full path of the file identified by
+     * this SPath.
+     */
+    public IPath getFullPath() {
+        final IPath fullProjectPath = project.getFullPath();
+        return fullProjectPath.append(projectRelativePath);
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ObjectUtils.hashCode(projectRelativePath);
         result = prime * result + ObjectUtils.hashCode(project);
+        result = prime * result + ObjectUtils.hashCode(projectRelativePath);
         return result;
     }
 
@@ -106,67 +170,14 @@ public class SPath {
 
         SPath other = (SPath) obj;
 
-        if (!ObjectUtils.equals(this.projectRelativePath,
-            other.projectRelativePath))
-            return false;
-
-        if (!ObjectUtils.equals(this.project, other.project))
-            return false;
-
-        return true;
+        return ObjectUtils.equals(project, other.project)
+            && ObjectUtils.equals(projectRelativePath,
+                other.projectRelativePath);
     }
 
     @Override
     public String toString() {
-        return "SPath [path="
-            + (projectRelativePath != null ? projectRelativePath
-                .toPortableString() : "<no path>") + ", project="
-            + (project != null ? project.getName() : "<no project>") + "]";
-    }
-
-    /**
-     * Returns the IFile represented by this SPath.
-     * 
-     * @return the IFile contained in the associated IProject for the given
-     *         project relative path
-     * 
-     * @convenience This method is using a straight forward implementation
-     */
-    public IFile getFile() {
-        return project.getFile(projectRelativePath);
-    }
-
-    /**
-     * Returns the {@linkplain IResource resource} represented by this SPath.
-     * 
-     * @return the resource represented by this SPath or <code>null</code> if
-     *         the resource does not exist locally
-     */
-    public IResource getResource() {
-        return project.findMember(projectRelativePath);
-    }
-
-    /**
-     * Returns the IFolder represented by this SPath.
-     */
-    public IFolder getFolder() {
-        return project.getFolder(projectRelativePath);
-    }
-
-    /**
-     * Returns the project in which the referenced resource or editor is
-     * located.
-     */
-    public IProject getProject() {
-        return project;
-    }
-
-    /**
-     * Convenience method for getting the full (workspace-relative) path of the
-     * file/editor identified by this SPath.
-     */
-    public IPath getFullPath() {
-        final IPath fullProjectPath = project.getFullPath();
-        return fullProjectPath.append(projectRelativePath);
+        return "SPath [project=" + project + ", projectRelativePath="
+            + projectRelativePath + "]";
     }
 }
