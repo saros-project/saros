6d3f6f11ca3d2449d46f2895a464f55e3757166e
Michael Krummrei <mikru.srs@gmail.com>
2017-03-30 15:16:01 +0200

    [INTERNAL][S] Implement ISarosSessionContextFactory for the Server
    Change-Id: Ia011ef9a8dc17364ce6a02d1506cfa5b74c0ff8d
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/3260
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

---
+++ de.fu_berlin.inf.dpp.server/src/de/fu_berlin/inf/dpp/server/session/ServerSessionContextFactory.java
@@ -0,0 +1,19 @@
+package de.fu_berlin.inf.dpp.server.session;
+
+import org.picocontainer.MutablePicoContainer;
+
+import de.fu_berlin.inf.dpp.session.ISarosSession;
+import de.fu_berlin.inf.dpp.session.ISarosSessionContextFactory;
+import de.fu_berlin.inf.dpp.session.SarosCoreSessionContextFactory;
+
+/**
+ * Server implementation of the {@link ISarosSessionContextFactory} interface.
+ */
+public class ServerSessionContextFactory extends SarosCoreSessionContextFactory {
+
+    @Override
+    public final void createNonCoreComponents(ISarosSession session,
+        MutablePicoContainer container) {
+
+    }
+}
