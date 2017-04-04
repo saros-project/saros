ed4bafde9b192a89a19196545676cd925a9571ee
Michael Krummrei <mikru.srs@gmail.com>
2017-04-04 17:07:36 +0200

    [INTERNAL][S] Add ServerFeatureAdvertiser
    This component instructs the network layer to advertise this Saros
    instance as a server.
    
    It is part of the Huge Server Patch originally published by 
    Dennis Washington.
    
    Change-Id: I26e4d32ae744f3ff616f568d3a1408a97e838f22
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/3275
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

---
+++ de.fu_berlin.inf.dpp.server/src/de/fu_berlin/inf/dpp/server/net/ServerFeatureAdvertiser.java
@@ -0,0 +1,55 @@
+package de.fu_berlin.inf.dpp.server.net;
+
+import org.apache.log4j.Logger;
+import org.jivesoftware.smack.Connection;
+import org.jivesoftware.smackx.ServiceDiscoveryManager;
+
+import de.fu_berlin.inf.dpp.SarosConstants;
+import de.fu_berlin.inf.dpp.annotations.Component;
+import de.fu_berlin.inf.dpp.net.ConnectionState;
+import de.fu_berlin.inf.dpp.net.xmpp.IConnectionListener;
+import de.fu_berlin.inf.dpp.net.xmpp.XMPPConnectionService;
+
+/**
+ * Instructs the network layer to advertise this Saros instance as a server.
+ */
+@Component(module = "server")
+public class ServerFeatureAdvertiser {
+
+    private static final Logger LOG = Logger
+        .getLogger(ServerFeatureAdvertiser.class);
+
+    private IConnectionListener connectionListener = new IConnectionListener() {
+        /**
+         * Configures server feature advertising for newly established
+         * connections.
+         */
+        @Override
+        public void connectionStateChanged(Connection connection,
+            ConnectionState newState) {
+
+            if (newState == ConnectionState.CONNECTING) {
+                advertiseServerFeature(connection);
+            }
+        }
+    };
+
+    /**
+     * Initializes the ServerFeatureAdvertiser.
+     * 
+     * @param connectionService
+     *            service to listen for new connections with
+     */
+    public ServerFeatureAdvertiser(XMPPConnectionService connectionService) {
+        connectionService.addListener(connectionListener);
+    }
+
+    private static void advertiseServerFeature(Connection connection) {
+        LOG.info("Starting to advertise ourselves as server");
+
+        ServiceDiscoveryManager discoveryManager = ServiceDiscoveryManager
+            .getInstanceFor(connection);
+
+        discoveryManager.addFeature(SarosConstants.NAMESPACE_SERVER);
+    }
+}
