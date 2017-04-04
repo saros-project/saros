1e8d2a72016bbc9568b5dfc7011c97fad262f250
Michael Krummrei <mikru.srs@gmail.com>
2017-04-04 17:05:42 +0200

    [INTERNAL][S] Add SubscriptionAuthorizer
    This Change adds a component which automatically authorizes all imcoming
    presence subscription requests from contacts
    
    Change-Id: I55af081797b28240bb677b6cba1267ffe0d9699f
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/3274
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

---
+++ de.fu_berlin.inf.dpp.server/src/de/fu_berlin/inf/dpp/server/net/SubscriptionAuthorizer.java
@@ -0,0 +1,38 @@
+package de.fu_berlin.inf.dpp.server.net;
+
+import org.apache.log4j.Logger;
+
+import de.fu_berlin.inf.dpp.annotations.Component;
+import de.fu_berlin.inf.dpp.net.xmpp.JID;
+import de.fu_berlin.inf.dpp.net.xmpp.subscription.SubscriptionHandler;
+import de.fu_berlin.inf.dpp.net.xmpp.subscription.SubscriptionListener;
+
+/**
+ * A component which automatically authorizes all incoming presence subscription
+ * requests from contacts.
+ */
+@Component(module = "server")
+public class SubscriptionAuthorizer implements SubscriptionListener {
+
+    private static final Logger LOG = Logger
+        .getLogger(SubscriptionAuthorizer.class);
+
+    private SubscriptionHandler subscriptionHandler;
+
+    /**
+     * Initializes the SubscriptionAuthorizer.
+     * 
+     * @param subscriptionHandler
+     *            the subscription handler to use
+     */
+    public SubscriptionAuthorizer(SubscriptionHandler subscriptionHandler) {
+        this.subscriptionHandler = subscriptionHandler;
+        subscriptionHandler.addSubscriptionListener(this);
+    }
+
+    @Override
+    public void subscriptionRequestReceived(JID jid) {
+        LOG.info("Accepting presence subscription request from " + jid);
+        subscriptionHandler.addSubscription(jid, true);
+    }
+}
