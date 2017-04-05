d819b4a37d704198f5c5eb6aa181ec1fe87fa428
Michael Krummrei <mikru.srs@gmail.com>
2017-04-05 11:38:15 +0200

    [INTERNAL][S] Implement IRemoteProgressIndicator for the Server
    Adds dummy implementations of IRemoteProgressIndicator and
    IRemoteProgressIndicatorFactory for the server, so the methods of the
    NullRemoteProgressIndicator-class "don't do anything" (except
    setting/returning parameters defined by the interface). This is
    sufficient, since indicating remote progress is not necessary when 
    running Saros as a server.
    
    Change-Id: I35320bd0ece97b0ae58a153f70a8cd7cf6767e15
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/3278
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

---
+++ de.fu_berlin.inf.dpp.server/src/de/fu_berlin/inf/dpp/server/dummies/NullRemoteProgressIndicatorFactory.java
@@ -0,0 +1,23 @@
+package de.fu_berlin.inf.dpp.server.dummies;
+
+import de.fu_berlin.inf.dpp.monitoring.remote.IRemoteProgressIndicator;
+import de.fu_berlin.inf.dpp.monitoring.remote.IRemoteProgressIndicatorFactory;
+import de.fu_berlin.inf.dpp.monitoring.remote.RemoteProgressManager;
+import de.fu_berlin.inf.dpp.session.User;
+
+/**
+ * An implementation of {@link IRemoteProgressIndicatorFactory} which returns
+ * {@link NullRemoteProgressIndicator} instances.
+ */
+public class NullRemoteProgressIndicatorFactory implements
+    IRemoteProgressIndicatorFactory {
+
+    @Override
+    public IRemoteProgressIndicator create(
+        RemoteProgressManager remoteProgressManager, String remoteProgressID,
+        User remoteUser) {
+
+        return new NullRemoteProgressIndicator(remoteProgressID, remoteUser);
+    }
+
+}
