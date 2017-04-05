5f9ef86bbbe690c9c56709f75ecefbfdccee0f68
Stefan Rossbach <srossbach@arcor.de>
2017-04-05 21:32:24 +0200

    [INTERNAL][S] adds Server configuration
    Adds an Utility class for obtaining values for the server runtime that
    must be set using Java Properties.
    
    Change-Id: I3a568ff3fdf876812641b8e19f7c88f805decc86
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/3280
    Tested-by: Jenkins CI
    Reviewed-by: Stefan Rossbach <srossbach@arcor.de>

---
+++ de.fu_berlin.inf.dpp.server/src/de/fu_berlin/inf/dpp/server/ServerConfig.java
@@ -0,0 +1,44 @@
+package de.fu_berlin.inf.dpp.server;
+
+/**
+ * A facade for accessing the configuration properties that were passed to the
+ * Saros server on startup.
+ */
+class ServerConfig {
+
+    private static final String JID_KEY = "de.fu_berlin.inf.dpp.server.jid";
+    private static final String PASSWORD_KEY = "de.fu_berlin.inf.dpp.server.password";
+    private static final String WORKSPACE_PATH_KEY = "de.fu_berlin.inf.dpp.server.workspace";
+
+    /**
+     * Returns the JID that the Saros server should use to connect to the XMPP
+     * network.
+     * 
+     * @return JID to use, or <code>null</code> if not specified
+     */
+    public static final String getJID() {
+        return System.getProperty(JID_KEY);
+    }
+
+    /**
+     * Returns the password that the Saros server should use to authenticate
+     * with the XMPP server.
+     * 
+     * @return password to use, or <code>null</code> if not specified
+     */
+    public static final String getPassword() {
+        return System.getProperty(PASSWORD_KEY);
+    }
+
+    /**
+     * Returns the path of the directory to use as the server's workspace. May
+     * be <code>null</code>, which means the server should create a temporary
+     * workspace directory by itself and delete it on exit.
+     * 
+     * @return the path of the workspace directory to use, or <code>null</code>
+     *         if a temporary directory should be used
+     */
+    public static String getWorkspacePath() {
+        return System.getProperty(WORKSPACE_PATH_KEY);
+    }
+}
