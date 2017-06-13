f35f1335863a0121d978e75778bb6785925550f1
Marius Schidlack <marius.schidlack@fu-berlin.de>
2017-06-13 23:07:44 +0200

    [BUILD][HTML] optimize build process, cleanup
    improve build file folder structure
    remove package-lock.json
    
    Change-Id: I0ea542e5d2a633771d33662a7a30b9ca0d2fee55
    Reviewed-on: http://saros-build.imp.fu-berlin.de/gerrit/3347
    Tested-by: Jenkins CI
    Reviewed-by: Franz Zieris <zieris@inf.fu-berlin.de>

--- de.fu_berlin.inf.dpp.ui.frontend/html/webpack.config.js
+++ de.fu_berlin.inf.dpp.ui.frontend/html/webpack.config.js
@@ -1,25 +1,43 @@
 const { resolve } = require('path')
 
+// This plugin is used for bundling all css files being imported into one css bundle
+// (Only in production mode)
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+// This plugin is used for generating the .html files, based on the template.ejs file
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 
+// see https://github.com/jantimon/html-webpack-plugin
+// EJS template engine is used by default
+const createPage = (name) => {
+  return new HtmlWebpackPlugin({
+    filename: `${name}.html`,
+    title: name,
+    template: 'template.ejs'
+  })
+}
+
 module.exports = (env = {}) => {
+  // In production mode we do some extra code optimizations
   const isProd = !!env.production
 
   const extractCss = new ExtractTextPlugin({
-    filename: 'css/[name].css',
+    filename: 'css/[name].[hash].css',
     disable: !isProd
   })
 
   return {
     context: resolve(__dirname, 'src'),
     entry: [
+      // We need this polyfill so that native functions defined in JavaScript ES6 can be used
+      'babel-polyfill/dist/polyfill.js',
+      // The entry point of our application
       './index.jsx'
     ],
     output: {
       path: resolve(__dirname, 'dist'),
-      filename: 'js/bundle.js'
+      // We need the [hash] for cache breaking
+      filename: 'js/bundle.[hash].js'
     },
     devtool: 'source-map',
     module: {
@@ -27,11 +45,8 @@ module.exports = (env = {}) => {
         {
           test: /\.jsx?$/,
           exclude: /node_modules/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['react', 'es2015', 'stage-0'],
-            plugins: ['transform-decorators-legacy']
-          }
+          // We use babel for transpiling our scripts into old ES5 javascript
+          use: 'babel-loader'
         },
         // CSS loader as described in https://github.com/webpack-contrib/sass-loader
         // Modified to only work with plain css
@@ -45,25 +60,26 @@ module.exports = (env = {}) => {
             fallback: 'style-loader'
           })
         },
-        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
-        { test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000' },
-        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
-        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }
+        // Those loaders are for using the bootstrap node package
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader?name=assets/[name].[ext]' },
+        { test: /\.(woff|woff2)$/, use: 'url-loader?prefix=font/&limit=5000&name=assets/[name].[ext]' },
+        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream&name=assets/[name].[ext]' },
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml&name=assets/[name].[ext]' }
       ]
     },
     plugins: [
       extractCss,
-      // see https://github.com/jantimon/html-webpack-plugin
-      // EJS template engine is used by default
-      new HtmlWebpackPlugin({
-        filename: 'main-page.html',
-        template: 'template.ejs'
-      })
-      // Optimizing for Production
-    ].concat(isProd ? [
+      // TODO there should be a more elegant way of telling the application which view to show
+      createPage('main-page'),
+      createPage('session-wizard')
+    ]
+    .concat(isProd ? [
+      // In Production Mode the global variable process.env.NODE_ENV is set to production
+      // Causing some code to be optimized away
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production')
       }),
+      // This optimizes and minifies the javascript code
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false,
@@ -74,9 +90,10 @@ module.exports = (env = {}) => {
     ] : []),
     resolve: {
       extensions: ['.js', '.jsx'],
-      // This is why we can just import 'Utils'
+      // This is why we can just import 'Utils' and 'Constants'
       alias: {
-        Utils: resolve(__dirname, 'src/utils/')
+        Utils: resolve(__dirname, 'src/utils/'),
+        Constants: resolve(__dirname, 'src/constants.jsx')
       }
     }
   }
