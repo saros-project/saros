129bdbc3567ae346bc36a10d2a92ead297b99567
Tobias Bouschen <tobias.bouschen@googlemail.com>
2018-04-26 15:43:29 +0200

    [INTERNAL][I] Add and read saros.properties file
    The saros.properties file contains all entries from the matching Saros/E
    file that are also used in the intellij module or the core.
    
    Change-Id: I46914b3cac84877706bad2dfa97b70ed83c04c51
    Reviewed-on: https://saros-build.imp.fu-berlin.de/gerrit/3499
    Tested-by: Jenkins CI
    Reviewed-by: Tobias Bouschen <tobias.bouschen@googlemail.com>

--- de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/SarosComponent.java
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/SarosComponent.java
@@ -10,6 +10,7 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.KeyStroke;
 import java.awt.event.KeyEvent;
+import java.io.InputStream;
 
 /**
  * Component that is initalized when a project is loaded.
@@ -31,6 +32,24 @@ public class SarosComponent
         keymap.addShortcut("ActivateSarosToolWindow", new KeyboardShortcut(
             KeyStroke.getKeyStroke(KeyEvent.VK_F11,
                 java.awt.event.InputEvent.ALT_DOWN_MASK), null));
+
+        try {
+            InputStream sarosProperties = SarosComponent.class.getClassLoader()
+                .getResourceAsStream("saros.properties");
+
+            if (sarosProperties == null) {
+                LogLog.warn("could not initialize Saros properties because "
+                    + "the 'saros.properties' file could not be found on the "
+                    + "current JAVA class path");
+            } else {
+                System.getProperties().load(sarosProperties);
+                sarosProperties.close();
+            }
+        } catch (Exception e) {
+            LogLog
+                .error("could not load saros property file 'saros.properties'",
+                    e);
+        }
 
         IntellijProjectLifecycle.getInstance(project).start();
     }
