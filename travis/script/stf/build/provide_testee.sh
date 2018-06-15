4cd787ddcd84052c58cb47944d668ea9db26adeb
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2018-06-15 12:20:44 +0200

    Add stf self tests to Travis CI
    * Increased timeouts
     * All failures are caused by exceeding timeouts
    
    Change-Id: I360d17027ec0c70c44ea1f312de8d231e08fcaf9
    Reviewed-on: https://saros-build.imp.fu-berlin.de/gerrit/3563
    Tested-by: Jenkins CI
    Reviewed-by: Kelvin Glaß <kelvin.glass@fu-berlin.de>

--- travis/script/stf/build/provide_testee.sh
+++ travis/script/stf/build/provide_testee.sh
@@ -3,5 +3,11 @@
 [ ! -z "$DEBUG_BASH" ] && set -x
 $SCRIPT_DIR/build/build_eclipse.sh
 
+# Provide build artifacts
 cd $WORKSPACE
 cp **/build/plugins/*.jar $STF_WS/
+
+# Provide plugins of eclipse (required for stf self test)
+mkdir -p $STF_WS/eclipse_plugins
+cd $ECLIPSE_HOME
+cp plugins/*.jar $STF_WS/eclipse_plugins/
