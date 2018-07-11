82d7c6bf7b22ab2b676522838619c7169fe93fb8
Jannis Fey <jannisfey@gmail.com>
2018-07-11 10:00:58 +0200

    [STF] Add RemoteHTMLInputField and use it in AddContactTest
    In addition I added a method to get the contactlist from the HTML-GUI.
    To reduce code duplication I build a method to get list items by any
    selector. I build it in a new BotUtils class.
    
    Change-Id: Ia9e158a32d772593cbd74eb7a215252d7af2889d
    Reviewed-on: https://saros-build.imp.fu-berlin.de/gerrit/3493
    Tested-by: Jenkins CI
    Reviewed-by: Jannis Fey <jannisfey@gmail.com>

--- de.fu_berlin.inf.dpp.ui.frontend/html/src/components/AddContactView/index.jsx
+++ de.fu_berlin.inf.dpp.ui.frontend/html/src/components/AddContactView/index.jsx
@@ -1,6 +1,6 @@
+import { action, observable } from 'mobx'
+import { inject, observer } from 'mobx-react'
 import React from 'react'
-import { observable, action } from 'mobx'
-import { observer, inject } from 'mobx-react'
 import { Text } from 'react-localize'
 
 @inject('mainUI')
@@ -12,11 +12,20 @@ export default class AddContactView extends React.Component {
   }
 
   @action onChangeField = (e) => {
-    this.fields[e.target.name] = e.target.value
+    this.setFieldValue(e.target.name, e.target.value)
   }
 
   @action onClickSubmit = () => {
     this.props.mainUI.doSubmitAddContact(this.fields.jid, this.fields.displayName)
+  }
+
+  @action setFieldValue = (field, value) => {
+    this.fields[field] = value
+  }
+
+  componentDidMount () {
+    // Expose this view globally to be accessible for Java
+    window.view = this
   }
 
   render () {
@@ -46,10 +55,10 @@ export default class AddContactView extends React.Component {
         </div>
         <div className='form-group'>
           <div className='col-sm-offset-2 col-sm-10'>
-            <button onClick={this.props.mainUI.doCancelAddContact} className='cancel-add-contact-btn btn btn-default'>
+            <button id='cancel-add-contact' onClick={this.props.mainUI.doCancelAddContact} className='cancel-add-contact-btn btn btn-default'>
               <Text message='action.cancel' />
             </button>
-            <button onClick={this.onClickSubmit} className='submit-add-contact btn btn-primary'>
+            <button id='add-contact' onClick={this.onClickSubmit} className='submit-add-contact btn btn-primary'>
               <Text message='action.addContact' />
             </button>
           </div>
--- de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/html/AddContactTest.java
+++ de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/html/AddContactTest.java
@@ -1,26 +1,63 @@
 package de.fu_berlin.inf.dpp.stf.test.html;
 
 import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.ALICE;
+import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.BOB;
 import static de.fu_berlin.inf.dpp.stf.server.rmi.remotebot.widget.IRemoteHTMLView.View.ADD_CONTACT;
 import static de.fu_berlin.inf.dpp.stf.server.rmi.remotebot.widget.IRemoteHTMLView.View.MAIN_VIEW;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import de.fu_berlin.inf.dpp.stf.client.StfHtmlTestCase;
+import de.fu_berlin.inf.dpp.stf.client.util.Util;
 
 public class AddContactTest extends StfHtmlTestCase {
     @BeforeClass
     public static void selectTesters() throws Exception {
-        select(ALICE);
+        select(ALICE, BOB);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        Util.removeTestersFromContactList(ALICE, BOB);
     }
 
     @Test
-    public void openAddContactForm() throws Exception {
-        ALICE.htmlBot().view(MAIN_VIEW).button("add-contact").click();
+    public void addBobAsContact() throws Exception {
+        // Precondition
+        List<String> aliceContactList = ALICE.htmlBot().getContactList();
+        List<String> bobContactList = BOB.htmlBot().getContactList();
+        assertFalse("Alice still has Bob as contact",
+            aliceContactList.contains(BOB.getBaseJid()));
+        assertFalse("Bob still has Alice as contact",
+            bobContactList.contains(ALICE.getBaseJid()));
 
-        assertTrue("Form for adding a contact did not open", ALICE.htmlBot()
-            .view(ADD_CONTACT).isOpen());
+        // open contact form
+        ALICE.htmlBot().view(MAIN_VIEW).button("add-contact").click();
+        assertTrue(ALICE.htmlBot().view(ADD_CONTACT).isOpen());
+
+        // add bob as contact
+        ALICE.htmlBot().view(ADD_CONTACT).inputField("jid")
+            .enter(BOB.getBaseJid());
+        ALICE.htmlBot().view(ADD_CONTACT).button("add-contact").click();
+        assertTrue("Main View did not open", ALICE.htmlBot().view(MAIN_VIEW)
+            .isOpen());
+
+        // confirm
+        BOB.superBot().confirmShellRequestOfSubscriptionReceived();
+        ALICE.superBot().confirmShellRequestOfSubscriptionReceived();
+
+        // check new contact
+        aliceContactList = ALICE.htmlBot().getContactList();
+        bobContactList = BOB.htmlBot().getContactList();
+        assertTrue("Alice has Bob not as contact",
+            aliceContactList.contains(BOB.getBaseJid()));
+        assertTrue("Bob has Alice not as contact",
+            bobContactList.contains(ALICE.getBaseJid()));
     }
 }
