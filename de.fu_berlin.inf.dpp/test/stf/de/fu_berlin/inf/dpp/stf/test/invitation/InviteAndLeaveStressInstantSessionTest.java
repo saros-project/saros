00e64177c7d2b1e2c03c0befd5d7f76104d55389
Stefan Moll <stefan@stefaus.de>
2018-07-12 11:57:05 +0200

    Add 2 Basic Invitation Tests for Instant Session Start.

---
+++ de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/invitation/InviteAndLeaveStressInstantSessionTest.java
@@ -0,0 +1,34 @@
+package de.fu_berlin.inf.dpp.stf.test.invitation;
+
+import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.ALICE;
+import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.BOB;
+import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.CARL;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class InviteAndLeaveStressInstantSessionTest extends
+    InviteAndLeaveStressTest {
+
+    @BeforeClass
+    public static void selectTesters() throws Exception {
+        select(ALICE, BOB, CARL);
+
+        ALICE.superBot().menuBar().saros().preferences()
+            .preferInstantSessionStart();
+        BOB.superBot().menuBar().saros().preferences()
+            .preferInstantSessionStart();
+        CARL.superBot().menuBar().saros().preferences()
+            .preferInstantSessionStart();
+    }
+
+    @AfterClass
+    public static void reset() throws Exception {
+        ALICE.superBot().menuBar().saros().preferences()
+            .preferArchiveSessionStart();
+        BOB.superBot().menuBar().saros().preferences()
+            .preferArchiveSessionStart();
+        CARL.superBot().menuBar().saros().preferences()
+            .preferArchiveSessionStart();
+    }
+}
---
+++ de.fu_berlin.inf.dpp/test/stf/de/fu_berlin/inf/dpp/stf/test/invitation/Share2UsersSequentiallyInstantSessionTest.java
@@ -0,0 +1,30 @@
+package de.fu_berlin.inf.dpp.stf.test.invitation;
+
+import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.ALICE;
+import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.BOB;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class Share2UsersSequentiallyInstantSessionTest extends
+    Share2UsersSequentiallyTest {
+
+    @BeforeClass
+    public static void selectTesters() throws Exception {
+        select(ALICE, BOB);
+
+        ALICE.superBot().menuBar().saros().preferences()
+            .preferInstantSessionStart();
+        BOB.superBot().menuBar().saros().preferences()
+            .preferInstantSessionStart();
+    }
+
+    @AfterClass
+    public static void reset() throws Exception {
+        ALICE.superBot().menuBar().saros().preferences()
+            .preferArchiveSessionStart();
+        BOB.superBot().menuBar().saros().preferences()
+            .preferArchiveSessionStart();
+    }
+
+}
