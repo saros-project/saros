32a94fb228ea0fdf929233cb712581426c1bce8f
Jannis Fey <jannisfey@gmail.com>
2018-07-18 14:42:09 +0200

    [REFACTOR] Move HTML-STF-Part to STF-Project
    I moved the html STF classes from ui project to the new stf project. The
    Saros Eclipse project (where the test are located) now depends on the
    stf project.

--- de.fu_berlin.inf.dpp.ui/test/framework/stf/src/de/fu_berlin/inf/dpp/stf/server/bot/BotPreferences.java
+++
@@ -1,9 +0,0 @@
-package de.fu_berlin.inf.dpp.stf.server.bot;
-
-public class BotPreferences {
-    public static final long DEFAULT_TIMEOUT = 30 * 1000;
-
-    public static final long LONG_TIMEOUT = 60 * 1000;
-
-    public static final long SHORT_TIMEOUT = 10 * 1000;
-}
