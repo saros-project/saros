33d50fe2faa5254c3208014f8b2a5b92d5efe8a2
Stefan <stefan@stefaus.de>
2018-07-23 17:51:58 +0200

    [FEATURE][CORE] Introduce Instant Session Start
    This patch implements the core functionality of Instant Session Start,
    which is optional selectable for Project Negotiation. The main scope of
    this feature is to decrease the waiting time for users, till the first
    files are provided and they can participate in a session.
    
    Deviant to previous approaches, this one uses a file stream based
    solution. Compared to using Activities, this provides increased
    performance, because of small overheads and better compresion
    possibilities, big files support and is for now, not interfering with
    existing activity handling code.
    
    This first Version implements a basic prioritisation, by first sending
    important eclipse project files, afterwards allways opened files first
    and remaining sorted by path hierarchy.
    
    Following features are excluded, to limit this rather big patch or are
    planned for the future:
    
     - Proper handling of Activities, to remove editor lock
    
     - Compression (using a libary like zstd)
    
     - Configurable File Prioritisations
    
     - More progress infos (bandwith, remaining file size, time...)
    
     - Performance optimizations, refactoring of ProjectNegotiation
    
     - User interface enhancements
    
     - Priority file sending, while active file transmission

---
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/negotiation/stream/IncomingStreamProtocol.java
@@ -0,0 +1,88 @@
+package de.fu_berlin.inf.dpp.negotiation.stream;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.BoundedInputStream;
+import org.apache.log4j.Logger;
+
+import de.fu_berlin.inf.dpp.exceptions.LocalCancellationException;
+import de.fu_berlin.inf.dpp.filesystem.FileSystem;
+import de.fu_berlin.inf.dpp.filesystem.IFile;
+import de.fu_berlin.inf.dpp.monitoring.IProgressMonitor;
+import de.fu_berlin.inf.dpp.negotiation.NegotiationTools.CancelOption;
+import de.fu_berlin.inf.dpp.session.ISarosSession;
+
+/**
+ * Implements Stream processing in {@link AbstractStreamProtocol} format.
+ */
+public class IncomingStreamProtocol extends AbstractStreamProtocol {
+
+    private static final Logger log = Logger
+        .getLogger(IncomingStreamProtocol.class);
+
+    private DataInputStream in;
+
+    public IncomingStreamProtocol(InputStream in, ISarosSession session,
+        IProgressMonitor monitor) {
+        super(session, monitor);
+        this.in = new DataInputStream(in);
+    }
+
+    /**
+     * Receive Files from {@code InputStream in} via in
+     * {@link AbstractStreamProtocol} defined protocol.
+     * 
+     * @throws IOException
+     *             if any file or stream operation fails
+     * @throws LocalCancellationException
+     *             on local user cancellation
+     */
+    public void receiveStream() throws IOException, LocalCancellationException {
+        BoundedInputStream fileIn = null;
+        try {
+            while (true) {
+                String projectID = in.readUTF();
+
+                /* check stream end */
+                if (projectID.isEmpty())
+                    break;
+
+                String fileName = in.readUTF();
+                IFile file = session.getProject(projectID).getFile(fileName);
+
+                String message = "receiving " + displayName(file);
+                log.debug(message);
+                monitor.subTask(message);
+
+                /*
+                 * folder creation is already done after file exchange, but in
+                 * case of future changes
+                 */
+                FileSystem.createFolder(file);
+
+                long fileSize = in.readLong();
+                fileIn = new BoundedInputStream(in, fileSize);
+                fileIn.setPropagateClose(false);
+
+                if (file.exists())
+                    file.setContents(fileIn, false, true);
+                else
+                    file.create(fileIn, false);
+
+                if (monitor.isCanceled()) {
+                    throw new LocalCancellationException(
+                        "User canceled transmission", CancelOption.NOTIFY_PEER);
+                }
+
+                monitor.worked(1);
+            }
+        } finally {
+            IOUtils.closeQuietly(fileIn);
+            IOUtils.closeQuietly(in);
+        }
+    }
+
+}
