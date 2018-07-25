a5c1510991a4a70191a7f425754ee5d6ea14d334
Jannis Fey <jannisfey@gmail.com>
2018-07-25 13:41:18 +0200

    [BUILD] Build new stf.test project
    I moved all STF-Framework parts and STF-Self-Test in the stf project.
    All STF-Test are now in the stf.test project.

---
+++ de.fu_berlin.inf.dpp.stf.test/test/eclipse/de/fu_berlin/inf/dpp/stf/test/consistency/ModifyDocumentBeforeProjectNegotiationTest.java
@@ -0,0 +1,105 @@
+package de.fu_berlin.inf.dpp.stf.test.consistency;
+
+import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.ALICE;
+import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.BOB;
+import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.CARL;
+import static de.fu_berlin.inf.dpp.stf.shared.Constants.ACCEPT;
+import static de.fu_berlin.inf.dpp.stf.shared.Constants.SHELL_ADD_PROJECTS;
+import static de.fu_berlin.inf.dpp.stf.shared.Constants.SHELL_SESSION_INVITATION;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.fu_berlin.inf.dpp.stf.client.StfTestCase;
+import de.fu_berlin.inf.dpp.stf.client.util.Util;
+import de.fu_berlin.inf.dpp.stf.test.stf.Constants;
+
+public class ModifyDocumentBeforeProjectNegotiationTest extends StfTestCase {
+
+    @BeforeClass
+    public static void selectTesters() throws Exception {
+        select(ALICE, BOB, CARL);
+    }
+
+    @Test
+    public void testModifyDocumentBeforeProjectNegotiation() throws Exception {
+
+        Util.setUpSessionWithJavaProjectAndClass(Constants.PROJECT1,
+            Constants.PKG1, Constants.CLS1, ALICE, BOB);
+
+        BOB.superBot()
+            .views()
+            .packageExplorerView()
+            .waitUntilClassExists(Constants.PROJECT1, Constants.PKG1,
+                Constants.CLS1);
+
+        ALICE.superBot().views().sarosView().selectContact(CARL.getJID())
+            .addToSarosSession();
+
+        CARL.remoteBot().shell(SHELL_SESSION_INVITATION).confirm(ACCEPT);
+
+        CARL.remoteBot().waitLongUntilShellIsOpen(SHELL_ADD_PROJECTS);
+
+        // this test will fail if a jupiter proxy is added when bob is typing
+        // text now
+        BOB.superBot().views().packageExplorerView()
+            .selectClass(Constants.PROJECT1, Constants.PKG1, Constants.CLS1)
+            .open();
+
+        BOB.remoteBot().editor(Constants.CLS1_SUFFIX)
+            .typeText("The mighty Foobar");
+
+        BOB.controlBot().getNetworkManipulator()
+            .synchronizeOnActivityQueue(ALICE.getJID(), 10000);
+
+        CARL.superBot()
+            .confirmShellAddProjectWithNewProject(Constants.PROJECT1);
+
+        CARL.superBot()
+            .views()
+            .packageExplorerView()
+            .waitUntilClassExists(Constants.PROJECT1, Constants.PKG1,
+                Constants.CLS1);
+
+        CARL.superBot().views().packageExplorerView()
+            .selectClass(Constants.PROJECT1, Constants.PKG1, Constants.CLS1)
+            .open();
+
+        BOB.remoteBot().editor(Constants.CLS1_SUFFIX)
+            .typeText(" bars everyfoo");
+
+        BOB.controlBot().getNetworkManipulator()
+            .synchronizeOnActivityQueue(CARL.getJID(), 10000);
+
+        CARL.remoteBot().editor(Constants.CLS1_SUFFIX)
+            .typeText("\n\n\nFoo yourself ");
+
+        CARL.controlBot().getNetworkManipulator()
+            .synchronizeOnActivityQueue(ALICE.getJID(), 10000);
+
+        ALICE.superBot().views().packageExplorerView()
+            .selectClass(Constants.PROJECT1, Constants.PKG1, Constants.CLS1)
+            .open();
+
+        ALICE.remoteBot().editor(Constants.CLS1_SUFFIX).navigateTo(0, 0);
+        ALICE.remoteBot().editor(Constants.CLS1_SUFFIX)
+            .typeText("blablablublub");
+
+        ALICE.controlBot().getNetworkManipulator()
+            .synchronizeOnActivityQueue(BOB.getJID(), 10000);
+
+        ALICE.controlBot().getNetworkManipulator()
+            .synchronizeOnActivityQueue(CARL.getJID(), 10000);
+
+        String aliceText = ALICE.remoteBot().editor(Constants.CLS1_SUFFIX)
+            .getText();
+        String bobText = BOB.remoteBot().editor(Constants.CLS1_SUFFIX)
+            .getText();
+        String carlText = CARL.remoteBot().editor(Constants.CLS1_SUFFIX)
+            .getText();
+
+        assertEquals(aliceText, bobText);
+        assertEquals(aliceText, carlText);
+    }
+}
---
+++ de.fu_berlin.inf.dpp.stf.test/test/eclipse/de/fu_berlin/inf/dpp/stf/test/invitation/HostInvitesBelatedlyTest.java
@@ -0,0 +1,121 @@
+package de.fu_berlin.inf.dpp.stf.test.invitation;
+
+import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.ALICE;
+import static de.fu_berlin.inf.dpp.stf.client.tester.SarosTester.BOB;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.eclipse.core.runtime.CoreException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.fu_berlin.inf.dpp.stf.client.StfTestCase;
+import de.fu_berlin.inf.dpp.stf.client.util.Util;
+import de.fu_berlin.inf.dpp.stf.shared.Constants.TypeOfCreateProject;
+import de.fu_berlin.inf.dpp.stf.test.stf.Constants;
+
+public class HostInvitesBelatedlyTest extends StfTestCase {
+
+    @BeforeClass
+    public static void selectTesters() throws Exception {
+        select(ALICE, BOB);
+    }
+
+    /**
+     * Steps:
+     * 
+     * 1. ALICE edits the file CLS but don't saves it.
+     * 
+     * 2. BOB edits the file CLS in the project currently used and saves it.
+     * 
+     * 3. ALICE edits the file CLS2 but don't saves it.
+     * 
+     * 4. BOB edits the file CLS2 in the project currently used and don't saves
+     * it.
+     * 
+     * 5. ALICE invites BOB.
+     * 
+     * 6. The question about the changed files at BOB is answered with YES.
+     * 
+     * 
+     * Expected Results:
+     * 
+     * 7. BOB has the same project like host.
+     * 
+     * FIXME: There are some bugs, if BOB's editors are not closed, BOB has the
+     * different project like host.
+     * 
+     * @throws CoreException
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Test
+    public void testSynchronizeWithOpenDirtyEditorsAtInviteesSide()
+        throws Exception {
+        ALICE
+            .superBot()
+            .views()
+            .packageExplorerView()
+            .tree()
+            .newC()
+            .javaProjectWithClasses(Constants.PROJECT1, Constants.PKG1,
+                Constants.CLS1, Constants.CLS2);
+
+        BOB.superBot()
+            .views()
+            .packageExplorerView()
+            .tree()
+            .newC()
+            .javaProjectWithClasses(Constants.PROJECT1, Constants.PKG1,
+                Constants.CLS1, Constants.CLS2);
+
+        ALICE.superBot().views().packageExplorerView()
+            .selectClass(Constants.PROJECT1, Constants.PKG1, Constants.CLS1)
+            .open();
+
+        ALICE.remoteBot().editor(Constants.CLS1_SUFFIX)
+            .setTextFromFile(Constants.CP1);
+
+        BOB.superBot().views().packageExplorerView()
+            .selectClass(Constants.PROJECT1, Constants.PKG1, Constants.CLS1)
+            .open();
+
+        BOB.remoteBot().editor(Constants.CLS1_SUFFIX)
+            .setTextFromFile(Constants.CP1_CHANGE);
+
+        ALICE.superBot().views().packageExplorerView()
+            .selectClass(Constants.PROJECT1, Constants.PKG1, Constants.CLS2)
+            .open();
+
+        ALICE.remoteBot().editor(Constants.CLS2_SUFFIX)
+            .setTextFromFile(Constants.CP2);
+
+        BOB.superBot().views().packageExplorerView()
+            .selectClass(Constants.PROJECT1, Constants.PKG1, Constants.CLS2)
+            .open();
+
+        BOB.remoteBot().editor(Constants.CLS2_SUFFIX)
+            .setTextFromFile(Constants.CP2_CHANGE);
+
+        Util.buildSessionConcurrently(Constants.PROJECT1,
+            TypeOfCreateProject.EXIST_PROJECT, ALICE, BOB);
+
+        Thread.sleep(5000);
+
+        String CLSContentOfAlice = ALICE.remoteBot()
+            .editor(Constants.CLS1_SUFFIX).getText();
+
+        String CLS2ContentOfAlice = ALICE.remoteBot()
+            .editor(Constants.CLS2_SUFFIX).getText();
+
+        String CLSContentOfBob = BOB.remoteBot().editor(Constants.CLS1_SUFFIX)
+            .getText();
+
+        String CLS2ContentOfBob = BOB.remoteBot().editor(Constants.CLS2_SUFFIX)
+            .getText();
+
+        assertEquals(CLSContentOfAlice, CLSContentOfBob);
+        assertEquals(CLS2ContentOfAlice, CLS2ContentOfBob);
+    }
+}
