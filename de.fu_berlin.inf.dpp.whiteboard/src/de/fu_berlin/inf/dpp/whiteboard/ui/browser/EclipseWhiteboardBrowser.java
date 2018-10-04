9ac8e41c0f4d6ccf645fe9493f4e419011c364c6
Abdullah Barhoum <ka70911@gmail.com>
2018-10-04 14:43:51 +0200

    [INTERNAL][WB] Added IWhiteboardBrowser and its Eclipse implementation (#165)

---
+++ de.fu_berlin.inf.dpp.whiteboard/src/de/fu_berlin/inf/dpp/whiteboard/ui/browser/EclipseWhiteboardBrowser.java
@@ -0,0 +1,52 @@
+package de.fu_berlin.inf.dpp.whiteboard.ui.browser;
+
+import org.apache.log4j.Logger;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.BrowserFunction;
+
+import de.fu_berlin.inf.dpp.ui.util.SWTUtils;
+import de.fu_berlin.inf.dpp.whiteboard.ui.HTMLWhiteboardView;
+
+/**
+ * the eclipse implementation of the IWhiteboardBrowser, this implementation
+ * uses the SWT browser to transfer events and commands between java and
+ * javascript
+ */
+public class EclipseWhiteboardBrowser implements IWhiteboardBrowser {
+
+    private static final Logger LOG = Logger
+        .getLogger(EclipseWhiteboardBrowser.class);
+
+    private final Browser browser;
+
+    /**
+     * this browser will be initialised in the {@link HTMLWhiteboardView}
+     * 
+     * @param browser
+     *            a SWT browser instance which displays the whiteboard
+     */
+    public EclipseWhiteboardBrowser(Browser browser) {
+        this.browser = browser;
+    }
+
+    @Override
+    public void asyncRun(final String script) {
+        SWTUtils.runSafeSWTAsync(LOG, new Runnable() {
+            @Override
+            public void run() {
+                browser.evaluate(script);
+            }
+        });
+    }
+
+    @Override
+    public void createBrowserFunction(String functionName,
+        final BrowserRunnable runnable) {
+        new BrowserFunction(browser, functionName) {
+            @Override
+            public Object function(Object[] arguments) {
+                return runnable.run(arguments);
+            }
+        };
+    }
+}
