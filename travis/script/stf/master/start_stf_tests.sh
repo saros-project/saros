c8e784198a37a3517bc9c4a22aec049f5e24b017
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2018-10-18 15:16:18 +0200

    [BUILD] Build saros with gradle

--- travis/script/stf/master/start_stf_tests.sh
+++ travis/script/stf/master/start_stf_tests.sh
@@ -4,16 +4,6 @@
 [ ! -z "$DEBUG_BASH" ] && set -x
 
 echo "STARTING REGRESSION: TIMEOUT IS 60 MINUTES"
+cd /home/ci/saros_src
 
-timeout 60m ant -Dsrc.dir=$STF_WS/src \
-    -Dlib.dir=$STF_WS/lib \
-    -Declipse.dir=$STF_WS \
-    -Djunit.dir=$STF_WS/junit \
-    -Dsaros.plugin.dir=$STF_WS/plugins \
-    -Dstf.client.config.files=$CONFIG_DIR/stf_config \
-    -lib $JUNIT_HOME -lib $COBERTURA_HOME -f $CONFIG_DIR/saros_stf_test.xml
-
-return_code=$?
-
-[ $return_code = 124 ] && echo "TIMEOUT EXCEEDED!"
-exit "$return_code"
+timeout 60m ./gradlew --stacktrace -Dstf.client.configuration.files=/home/ci/saros_src/travis/config/stf_config :de.fu_berlin.inf.dpp:stfTest
