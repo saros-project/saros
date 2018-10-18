07e8fb684a0a924e91ca4cfad5ea04a8f6ce1dd9
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2018-10-18 15:18:37 +0200

    [BUILD] Call gradle for CI build

--- travis/script/build/build_all.sh
+++ travis/script/build/build_all.sh
@@ -2,9 +2,5 @@
 
 [ ! -z "$DEBUG_BASH" ] && set -x
 
-$SCRIPT_DIR/build/build_eclipse.sh
-$SCRIPT_DIR/build/build_intellij.sh
-# following build steps are conditional
-$SCRIPT_DIR/build/build_server.sh
-$SCRIPT_DIR/build/build_netbeans.sh
-$SCRIPT_DIR/build/build_frontend.sh
+/home/ci/saros_src/travis/script/build/build_java.sh
+/home/ci/saros_src/travis/script/build/build_js.sh
