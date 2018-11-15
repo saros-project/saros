d5cf9d51c85e205c872492e171ed92a72daf728f
Tobias Bouschen <tobias.bouschen@googlemail.com>
2018-11-15 15:52:40 +0100

    [INTERNAL][I] Remove old filesystem implementation
    Subsequently removes the test cases for the old filesystem
    implementation.

--- de.fu_berlin.inf.dpp.intellij/test/junit/de/fu_berlin/inf/dpp/intellij/project/filesystem/TestSuite.java
+++ de.fu_berlin.inf.dpp.intellij/test/junit/de/fu_berlin/inf/dpp/intellij/project/filesystem/TestSuite.java
@@ -4,8 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ IntelliJFileImplTest.class, IntelliJFolderImplTest.class,
-    IntelliJPathImplTest.class, IntelliJResourceImplTest.class })
+@Suite.SuiteClasses({ IntelliJPathImplTest.class })
 public class TestSuite {
     // the class remains completely empty,
     // being used only as a holder for the above annotations
