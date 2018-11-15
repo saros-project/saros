9c5e1881a6bbf99eb8b40f7604a949bbcb2ad1c7
Tobias Bouschen <tobias.bouschen@googlemail.com>
2018-11-15 20:59:03 +0100

    [FIX][I] Adjust PathFactory to meet interface specification

--- de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/project/filesystem/PathFactory.java
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/project/filesystem/PathFactory.java
@@ -6,12 +6,32 @@ import de.fu_berlin.inf.dpp.filesystem.IPathFactory;
 public class PathFactory implements IPathFactory {
     @Override
     public String fromPath(IPath path) {
+        if (path == null) {
+            throw new NullPointerException("Given path must not be null.");
+        }
+
+        if (path.isAbsolute()) {
+            throw new IllegalArgumentException(
+                "The given path must not be absolute. path: " + path);
+        }
+
         return path.toPortableString();
     }
 
     @Override
     public IPath fromString(String name) {
-        return IntelliJPathImpl.fromString(name);
-    }
+        if (name == null) {
+            throw new NullPointerException("Given string must not be null.");
+        }
 
+        IPath path = IntelliJPathImpl.fromString(name);
+
+        if (path.isAbsolute()) {
+            throw new IllegalArgumentException(
+                "The given string must not be represent an absolute path. "
+                    + "path: " + path);
+        }
+
+        return path;
+    }
 }
