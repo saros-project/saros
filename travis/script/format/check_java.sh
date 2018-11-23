cf1050dc7f022d537f4580c1bcd96f4509546da8
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2018-11-23 09:41:50 +0100

    [BUILD] Add google format check to Travis CI
    * Added a script that downloads the google java formatter
    * Introduced a new Travis stage "Formatting"

---
+++ travis/script/format/check_java.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+shopt -s globstar
+
+project_root_dir=$1
+formatter_jar_name=formatter.jar
+
+function download_formatter() {
+  local jar_name=$1
+  local url="https://repo.maven.apache.org/maven2/com/google/googlejavaformat/google-java-format/1.6/google-java-format-1.6-all-deps.jar"
+  echo "Downloading formatter from $url"
+  curl -Lo formatter.jar "$url"
+}
+
+function check_formatting() {
+  local jar_name=$1
+
+  echo 'Checking format'
+  java -jar "$jar_name" --dry-run --set-exit-if-changed **/*.java
+
+  local rc=$?
+  [ "$rc" == "0" ] &&
+    echo 'All files are well formatted' ||
+    printf "\nERROR: Some files are not well formatted. The file list above shows all files containing a wrong formatting.\n"
+
+  return "$rc"
+}
+
+cd $project_root_dir
+
+download_formatter $formatter_jar_name
+check_formatting $formatter_jar_name
+
+exit $?
