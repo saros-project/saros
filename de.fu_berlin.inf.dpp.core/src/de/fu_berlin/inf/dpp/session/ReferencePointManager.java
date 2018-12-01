ed3c621c597ea8bb19ac24fac0f3f2034fea29ab
Oliver Hanßen <oliver.hanssen@gmail.com>
2018-12-01 15:13:00 +0100

    [API][CORE] SharedProjectMapper: Using IReferencePoint
    SharedProjectMapper/-test:
    This class now with reference points with resources now.
    
    IReferencePointManager/ReferencePointManager:
    The IReferencePointManager maps an IReferencePoint to an core.IProject.
    Because not all classes will not adapted at the same time, the IReferencePointManager
    is needed for classes, who still works core.IProjects. The ReferencePointManager
    is an implementation of IReferencePointManager.
    
    SarosSession:
    After the SharedProjectManager works with reference points, the SarosSession needs
    the ÍReferencePointManager for getting IProjects given by IReferencePoints.
    The IReferencePointManager will instantiated in SarosSession and kept in
    the core session container. The IReferencePointManager will needed only if a session is
    running, so that the reason, that the IReferencePointManager is in the core session
    container. Currently the SarosSession is responsible for putting the pairs IReferencePoints
    and core.IProjects, which should to be shared.

---
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/session/ReferencePointManager.java
@@ -0,0 +1,41 @@
+package de.fu_berlin.inf.dpp.session;
+
+import de.fu_berlin.inf.dpp.filesystem.IProject;
+import de.fu_berlin.inf.dpp.filesystem.IReferencePoint;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/** The implementation of {@link IReferencePointManager} */
+public class ReferencePointManager implements IReferencePointManager {
+
+  private final HashMap<IReferencePoint, IProject> referencePointToProjectMapper;
+
+  public ReferencePointManager() {
+    referencePointToProjectMapper = new HashMap<IReferencePoint, IProject>();
+  }
+
+  @Override
+  public synchronized void put(IReferencePoint referencePoint, IProject project) {
+    if (!referencePointToProjectMapper.containsKey(referencePoint)) {
+      referencePointToProjectMapper.put(referencePoint, project);
+    }
+  }
+
+  @Override
+  public synchronized IProject get(IReferencePoint referencePoint) {
+    return referencePointToProjectMapper.get(referencePoint);
+  }
+
+  @Override
+  public synchronized Set<IProject> getProjects(Set<IReferencePoint> referencePoints) {
+    Set<IProject> projectSet = new HashSet<IProject>();
+
+    for (IReferencePoint referencePoint : referencePoints) {
+      if (referencePointToProjectMapper.containsKey(referencePoint))
+        projectSet.add(get(referencePoint));
+    }
+
+    return projectSet;
+  }
+}
