bed474293ea74d4f8bb68ef445b4c33084d50a3b
Abdullah Barhoum <ka70911@gmail.com>
2018-12-03 16:54:11 +0100

    updated travis build config

--- travis/script/build/build_js.sh
+++ travis/script/build/build_js.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -e
 
 [ ! -z "$DEBUG_BASH" ] && set -x
-cd /home/ci/saros_src
 
-ui_package_json="de.fu_berlin.inf.dpp.ui.frontend/html/package.json"
-if [ -f "$ui_package_json" ]; then
-  cd de.fu_berlin.inf.dpp.ui.frontend/html
-  npm cache verify
-  npm install
-  npm run lint
-  npm run build
-  npm run test:log
-fi
+#Setup
+npm cache verify
+
+echo "Building the HTML GUI"
+cd /home/ci/saros_src/de.fu_berlin.inf.dpp.ui.frontend/html
+npm install
+npm run lint
+npm run build
+npm run test:log
+
+
+echo "Building the HTML Whiteboard"
+cd /home/ci/saros_src/de.fu_berlin.inf.dpp.whiteboard/frontend
+npm install
+npm run build
+
