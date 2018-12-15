967bf8df09f754cb963793768a8cabb532d538dc
Victor Brekenfeld <github@drakulix.de>
2018-12-15 20:03:24 +0100

    [FEATURE][SERVER] Add InviteCommand (#282)
    * [S][FIX] Fixup Server Lifecycle/Console/Context initialization
    * [S][FEATURE] Add InviteCommand

--- de.fu_berlin.inf.dpp.server/src/de/fu_berlin/inf/dpp/server/console/ConsoleCommand.java
+++ de.fu_berlin.inf.dpp.server/src/de/fu_berlin/inf/dpp/server/console/ConsoleCommand.java
@@ -1,6 +1,7 @@
 package de.fu_berlin.inf.dpp.server.console;
 
 import java.io.PrintStream;
+import java.util.List;
 
 /** A command that can be registered to the {@link ServerConsole} and then called by the user. */
 public abstract class ConsoleCommand {
@@ -8,9 +9,5 @@ public abstract class ConsoleCommand {
 
   public abstract String help();
 
-  public boolean matches(String command) {
-    return command.split(" ")[0].equals(identifier());
-  }
-
-  public abstract void execute(String command, PrintStream out);
+  public abstract void execute(List<String> args, PrintStream out);
 }
