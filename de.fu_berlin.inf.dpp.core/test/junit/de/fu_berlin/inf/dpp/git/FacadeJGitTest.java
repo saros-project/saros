3465692786a670bb8f96363f22f0c52e7c52a021
Chris Jeschke <chrisjberlin@googlemail.com>
2018-12-26 17:42:59 +0100

    Unbundle a valid Bundle-File

--- de.fu_berlin.inf.dpp.core/test/junit/de/fu_berlin/inf/dpp/git/FacadeJGitTest.java
+++ de.fu_berlin.inf.dpp.core/test/junit/de/fu_berlin/inf/dpp/git/FacadeJGitTest.java
@@ -1,10 +1,15 @@
 package de.fu_berlin.inf.dpp.git;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,6 +21,8 @@ public class FacadeJGitTest {
 
   // the local user is creating the bundle
   private File localWorkDir;
+  // the remote user is receiving the bundle
+  private File remoteWorkDir;
 
   @Before
   public void SetUp() {
@@ -23,8 +30,18 @@ public class FacadeJGitTest {
       localWorkDir = tempFolder.newFolder("TempDir1");
       JGitFacade.initNewRepo(localWorkDir);
       JGitFacade.writeCommitToRepo(localWorkDir, 2);
+      remoteWorkDir = tempFolder.newFolder("TempDir2");
+      JGitFacade.clone(localWorkDir, remoteWorkDir);
+      JGitFacade.writeCommitToRepo(localWorkDir, 3);
+
     } catch (IOException e) {
       fail("IO");
+    } catch (InvalidRemoteException e) {
+      fail("GitRepo1");
+    } catch (TransportException e) {
+      fail("Transport");
+    } catch (GitAPIException e) {
+      fail("Other");
     }
   }
 
@@ -42,7 +59,24 @@ public class FacadeJGitTest {
   }
 
   @Test
-  public void testValidUnbundle() {}
+  public void testValidUnbundle() {
+    try {
+      File bundle = JGitFacade.createBundleByTag(localWorkDir, "CheckoutAt2");
+      JGitFacade.unbundle(bundle, remoteWorkDir);
+
+      assertEquals(
+          Git.open(localWorkDir).getRepository().resolve("HEAD"),
+          Git.open(remoteWorkDir).getRepository().resolve("FETCH_HEAD"));
+    } catch (IOException e) {
+      fail("IO");
+    } catch (InvalidRemoteException e) {
+      fail("GitRepo1");
+    } catch (TransportException e) {
+      fail("Transport");
+    } catch (GitAPIException e) {
+      fail("Other");
+    }
+  }
 
   @Test
   public void testEmptyfileUnbundle() {}
