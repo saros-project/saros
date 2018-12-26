92388f26f2d3102f7a8fe7bfecae30298290b9e4
Chris Jeschke <chrisjberlin@googlemail.com>
2018-12-26 14:07:17 +0100

    Add files from old System because new dev env. Add metadata dir to gitignore

---
+++ de.fu_berlin.inf.dpp.core/test/junit/de/fu_berlin/inf/dpp/git/JGitFacadeTest.java
@@ -0,0 +1,56 @@
+package de.fu_berlin.inf.dpp.git;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class JGitFacadeTest {
+
+  @Rule public TemporaryFolder tempFolder = new TemporaryFolder();
+
+  /**
+   * Method to create file,folder and .git Directory with a given number of commits. Commits can be
+   * accessed by Tag (The first is named CheckoutAtInit, the following
+   * CheckoutAtCommit2,CheckoutAtCommit3,...)
+   *
+   * @param iD Test should never use the same iD twice
+   * @param amountCommits At least 1 will be created. If >= 2 more than that.
+   * @return The file that will be used to identify the folder with the Git Directory
+   */
+  File createTestFile(int amountCommits, int iD) {
+    try {
+      File tempSubfolder = tempFolder.newFolder("tempFolder" + iD);
+      File tempFile = tempSubfolder.createTempFile("tempFile", ".txt");
+      JGitFacade.initNewRepo(tempFile);
+      for (int i = 2; i <= amountCommits; i++) {
+        JGitFacade.writeCommitToRepo(tempFile, i);
+      }
+      return tempFile;
+    } catch (IOException e) {
+
+    }
+    return null;
+  }
+
+  @Test
+  public void testCreateBundle() throws IOException {
+
+    File firstRepoTempFile = createTestFile(2, 1);
+    File bundle = JGitFacade.createBundleByTag(firstRepoTempFile, "CheckoutAtInit");
+    assertNotNull(bundle);
+  }
+
+  @Test
+  public void testCreateBundleRaisesNullPointerExeption() throws IOException {
+    try {
+      File bundle2 = JGitFacade.createBundleByTag(null, "CheckoutAtInit");
+      fail("created Bundle with null File");
+    } catch (NullPointerException e) {
+    }
+  }
+}
---
+++ de.fu_berlin.inf.dpp.core/test/junit/de/fu_berlin/inf/dpp/git/jgittest.java
@@ -0,0 +1,87 @@
+package de.fu_berlin.inf.dpp.git;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class jgittest {
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+    File GitDir1;
+    File GitDir2;
+
+    @Before
+    public void setUP() {
+        try {
+            tempFolder.delete();
+            File remote = tempFolder.newFolder("Gitdir1");
+            File local = tempFolder.newFolder("Gitdir2");
+            JGitFacade.initNewRepo(remote);
+            CloneCommand cloneCommand = Git.cloneRepository();
+            cloneCommand.setDirectory(local);
+            cloneCommand.setURI(JGitFacade.getUrlByGitRepo(remote));
+            cloneCommand.call();
+            JGitFacade.writeCommitToRepo(remote, 2);
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            fail("IO");
+        } catch (InvalidRemoteException e) {
+            fail("GitRepo1");
+        } catch (TransportException e) {
+            fail("Transport");
+        } catch (GitAPIException e) {
+            fail("Other");
+        }
+
+    }
+
+    @Test
+    public void testFileNoBundle() {
+        try {
+            File emptyFile = new File("");
+            emptyFile.createTempFile("emptyFile", "");
+            JGitFacade.unbundle(emptyFile, GitDir2);
+            fail("Wrong File accepted");
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    @Test
+    public void testFilebundle() {
+
+        File bundle;
+        try {
+            bundle = JGitFacade.createBundleByTag(GitDir1, "CheckoutAtInit");
+            ObjectId oldId = Git.open(GitDir2).getRepository().resolve("HEAD");
+            assertEquals(oldId,
+                Git.open(GitDir2).getRepository().resolve("HEAD"));
+            JGitFacade.unbundle(bundle, GitDir2);
+            assertNotEquals(oldId,
+                Git.open(GitDir2).getRepository().resolve("HEAD"));
+        } catch (IOException e) {
+            fail("IO");
+        } catch (Exception e) {
+            fail("Not IO");
+        }
+
+    }
+
+}
