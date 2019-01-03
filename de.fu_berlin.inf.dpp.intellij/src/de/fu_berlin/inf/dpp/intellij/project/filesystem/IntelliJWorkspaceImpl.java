e31f0d74dfe52801728ed94724f4601328e61ce2
Oliver Hanßen <oliver.hanssen@gmail.com>
2019-01-03 22:58:31 +0100

    [CORE][API] IWorkspace: Extended by reference points
    The IWorkspace interface was extended by two methods:
    
    IReferencePoint getReferencePoint(string projectName):
    Equivalent to getProject(string projectName) but returns the reference
    point
    
    run(IWorkspaceRunnable, IReferencePoint[], IReferencePointManager):
    is also equivalent to run(IWorkspaceRunnable, Resources[]). The
    IReferencePointManager is only needed in the eclipse implementation
    of IWorkspace

--- de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/project/filesystem/IntelliJWorkspaceImpl.java
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/project/filesystem/IntelliJWorkspaceImpl.java
@@ -7,12 +7,14 @@ import com.intellij.openapi.util.Computable;
 import de.fu_berlin.inf.dpp.exceptions.OperationCanceledException;
 import de.fu_berlin.inf.dpp.filesystem.IPath;
 import de.fu_berlin.inf.dpp.filesystem.IProject;
+import de.fu_berlin.inf.dpp.filesystem.IReferencePoint;
 import de.fu_berlin.inf.dpp.filesystem.IResource;
 import de.fu_berlin.inf.dpp.filesystem.IWorkspace;
 import de.fu_berlin.inf.dpp.filesystem.IWorkspaceRunnable;
 import de.fu_berlin.inf.dpp.intellij.filesystem.Filesystem;
 import de.fu_berlin.inf.dpp.intellij.filesystem.IntelliJProjectImpl;
 import de.fu_berlin.inf.dpp.monitoring.NullProgressMonitor;
+import de.fu_berlin.inf.dpp.session.IReferencePointManager;
 import java.io.IOException;
 import org.apache.log4j.Logger;
 
@@ -37,6 +39,15 @@ public class IntelliJWorkspaceImpl implements IWorkspace {
   }
 
   @Override
+  public void run(
+      IWorkspaceRunnable runnable,
+      IReferencePoint[] referencePoints,
+      IReferencePointManager referencePointManager)
+      throws IOException, OperationCanceledException {
+    run(runnable);
+  }
+
+  @Override
   public IProject getProject(final String moduleName) {
     Module module =
         Filesystem.runReadAction(
@@ -53,5 +64,12 @@ public class IntelliJWorkspaceImpl implements IWorkspace {
   @Override
   public IPath getLocation() {
     return IntelliJPathImpl.fromString(project.getBasePath());
+  }
+
+  @Override
+  public IReferencePoint getReferencePoint(String moduleName) {
+    IProject project = getProject(moduleName);
+
+    return project.getReferencePoint();
   }
 }
