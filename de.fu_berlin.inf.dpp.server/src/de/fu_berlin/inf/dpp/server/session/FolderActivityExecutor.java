17ca75220bbc045fd4c4972ff1b217adaf6fa376
Oliver Hanßen <oliver.hanssen@gmail.com>
2019-01-06 14:17:46 +0100

    [Server] SPath: Does not use getFolder() anymore

--- de.fu_berlin.inf.dpp.server/src/de/fu_berlin/inf/dpp/server/session/FolderActivityExecutor.java
+++ de.fu_berlin.inf.dpp.server/src/de/fu_berlin/inf/dpp/server/session/FolderActivityExecutor.java
@@ -4,10 +4,17 @@ import de.fu_berlin.inf.dpp.activities.FolderCreatedActivity;
 import de.fu_berlin.inf.dpp.activities.FolderDeletedActivity;
 import de.fu_berlin.inf.dpp.activities.SPath;
 import de.fu_berlin.inf.dpp.filesystem.IFolder;
+import de.fu_berlin.inf.dpp.filesystem.IPath;
+import de.fu_berlin.inf.dpp.filesystem.IReferencePoint;
 import de.fu_berlin.inf.dpp.filesystem.IResource;
 import de.fu_berlin.inf.dpp.server.editor.ServerEditorManager;
+import de.fu_berlin.inf.dpp.server.filesystem.ServerFileImpl;
+import de.fu_berlin.inf.dpp.server.filesystem.ServerFolderImpl;
+import de.fu_berlin.inf.dpp.server.filesystem.ServerReferencePointManager;
+import de.fu_berlin.inf.dpp.server.filesystem.ServerWorkspaceImpl;
 import de.fu_berlin.inf.dpp.session.AbstractActivityConsumer;
 import de.fu_berlin.inf.dpp.session.ISarosSession;
+import java.io.File;
 import java.io.IOException;
 import org.apache.log4j.Logger;
 import org.picocontainer.Startable;
@@ -19,6 +26,7 @@ public class FolderActivityExecutor extends AbstractActivityConsumer implements 
 
   private final ISarosSession session;
   private final ServerEditorManager editorManager;
+  private final ServerWorkspaceImpl workspace;
 
   /**
    * Creates a FolderActivityExecutor.
@@ -26,10 +34,12 @@ public class FolderActivityExecutor extends AbstractActivityConsumer implements 
    * @param session the current session
    * @param editorManager the editor manager
    */
-  public FolderActivityExecutor(ISarosSession session, ServerEditorManager editorManager) {
+  public FolderActivityExecutor(
+      ISarosSession session, ServerEditorManager editorManager, ServerWorkspaceImpl workspace) {
 
     this.session = session;
     this.editorManager = editorManager;
+    this.workspace = workspace;
   }
 
   @Override
@@ -62,14 +72,21 @@ public class FolderActivityExecutor extends AbstractActivityConsumer implements 
 
   private void executeFolderCreation(FolderCreatedActivity activity) throws IOException {
 
-    IFolder folder = activity.getPath().getFolder();
+    SPath path = activity.getPath();
+    IPath referencePointRelativePath = path.getProjectRelativePath();
+
+    IFolder folder = new ServerFolderImpl(workspace, referencePointRelativePath);
+
     folder.create(IResource.NONE, true);
   }
 
   private void executeFolderRemoval(FolderDeletedActivity activity) throws IOException {
 
     SPath path = activity.getPath();
-    IFolder folder = path.getFolder();
+    IPath referencePointRelativePath = path.getProjectRelativePath();
+
+    IFolder folder = new ServerFolderImpl(workspace, referencePointRelativePath);
+
     folder.delete(IResource.NONE);
     editorManager.closeEditorsInFolder(path);
   }
