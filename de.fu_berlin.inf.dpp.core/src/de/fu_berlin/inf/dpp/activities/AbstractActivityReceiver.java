a7157d502c41021c47a3865177631a0000054c71
Chris Jeschke <chrisjberlin@googlemail.com>
2019-01-08 14:47:48 +0100

    create activity send bundle

--- de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/activities/AbstractActivityReceiver.java
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/activities/AbstractActivityReceiver.java
@@ -7,106 +7,117 @@ package de.fu_berlin.inf.dpp.activities;
  */
 public abstract class AbstractActivityReceiver implements IActivityReceiver {
 
-  /**
-   * @JTourBusStop 4, Creating a new Activity type, Triple dispatch abstract class:
-   *
-   * <p>Instead of creating stubs for all receive() variants for IActivity types you're not even
-   * interested in, you can extend this abstract class and just override the one (or few) method(s)
-   * you actually care about.
-   *
-   * <p>So once you added a new receive() variant to IActivityReceiver, make sure to add a
-   * null-implementation for your new activity type here, to unburden all *other* IActivityReceiver
-   * implementations from having to implement the receive() method for *your* new activity.
-   */
+    /**
+     * @JTourBusStop 4, Creating a new Activity type, Triple dispatch abstract
+     *               class:
+     * 
+     *               Instead of creating stubs for all receive() variants for
+     *               IActivity types you're not even interested in, you can
+     *               extend this abstract class and just override the one (or
+     *               few) method(s) you actually care about.
+     * 
+     *               So once you added a new receive() variant to
+     *               IActivityReceiver, make sure to add a null-implementation
+     *               for your new activity type here, to unburden all *other*
+     *               IActivityReceiver implementations from having to implement
+     *               the receive() method for *your* new activity.
+     */
 
-  /** */
-  @Override
-  public void receive(ChangeColorActivity changeColorActivity) {
-    // do nothing
-  }
+    /***/
 
-  @Override
-  public void receive(ChecksumActivity checksumActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(ChangeColorActivity changeColorActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(ChecksumErrorActivity checksumErrorActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(ChecksumActivity checksumActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(EditorActivity editorActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(ChecksumErrorActivity checksumErrorActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(FileActivity fileActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(EditorActivity editorActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(FolderCreatedActivity folderCreatedActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(FileActivity fileActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(FolderDeletedActivity folderDeletedActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(FolderCreatedActivity folderCreatedActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(FolderMovedActivity folderMovedActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(FolderDeletedActivity folderDeletedActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(JupiterActivity jupiterActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(FolderMovedActivity folderMovedActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(NOPActivity nopActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(JupiterActivity jupiterActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(PermissionActivity permissionActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(NOPActivity nopActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(ProgressActivity progressActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(PermissionActivity permissionActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(StartFollowingActivity startFollowingActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(ProgressActivity progressActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(StopActivity stopActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(StartFollowingActivity startFollowingActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(StopFollowingActivity stopFollowingActivity) {
-    // do nothing
-  }
+    public void receive(SendBundleActivity sendBundleActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(TextEditActivity textEditActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(StopActivity stopActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(TextSelectionActivity textSelectionActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(StopFollowingActivity stopFollowingActivity) {
+        // do nothing
+    }
 
-  @Override
-  public void receive(ViewportActivity viewportActivity) {
-    // do nothing
-  }
+    @Override
+    public void receive(TextEditActivity textEditActivity) {
+        // do nothing
+    }
+
+    @Override
+    public void receive(TextSelectionActivity textSelectionActivity) {
+        // do nothing
+    }
+
+    @Override
+    public void receive(ViewportActivity viewportActivity) {
+        // do nothing
+    }
+
+
 }
--- de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/communication/extensions/ActivitiesExtension.java
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/communication/extensions/ActivitiesExtension.java
@@ -42,6 +42,7 @@ import de.fu_berlin.inf.dpp.activities.StopFollowingActivity;
 import de.fu_berlin.inf.dpp.activities.TargetedFileActivity;
 import de.fu_berlin.inf.dpp.activities.TextSelectionActivity;
 import de.fu_berlin.inf.dpp.activities.ViewportActivity;
+import de.fu_berlin.inf.dpp.activities.SendBundleActivity;
 import de.fu_berlin.inf.dpp.concurrent.jupiter.internal.JupiterVectorTime;
 import de.fu_berlin.inf.dpp.concurrent.jupiter.internal.text.DeleteOperation;
 import de.fu_berlin.inf.dpp.concurrent.jupiter.internal.text.InsertOperation;
@@ -56,108 +57,150 @@ import org.jivesoftware.smack.packet.PacketExtension;
 @XStreamAlias("ADOS")
 public class ActivitiesExtension extends SarosSessionPacketExtension {
 
-  public static final Provider PROVIDER = new Provider();
+    public static final Provider PROVIDER = new Provider();
 
-  @XStreamImplicit private final List<IActivity> activities;
+    @XStreamImplicit
+    private final List<IActivity> activities;
 
-  @XStreamAlias("seq")
-  @XStreamAsAttribute
-  private final int sequenceNumber;
+    @XStreamAlias("seq")
+    @XStreamAsAttribute
+    private final int sequenceNumber;
 
-  /**
-   * Creates an object that can be transformed into a {@linkplain PacketExtension} using the
-   * provider of this extension. All object parameters <b>must not be <code>null</code></b>. @Note
-   * This constructor does not check for correctness of the input parameters.
-   *
-   * @param sessionID the session id the {@linkplain IActivity activities} belong to
-   * @param activities the {@linkplain IActivity activities} that should be included in this
-   *     extension
-   * @param sequenceNumber the sequence number of the <b>first</b> {@linkplain IActivity activity}
-   */
-  public ActivitiesExtension(String sessionID, List<IActivity> activities, int sequenceNumber) {
-    super(sessionID);
-    this.activities = activities;
-    this.sequenceNumber = sequenceNumber;
-  }
-
-  /**
-   * Returns the {@linkplain IActivity activities} included in this extension.
-   *
-   * @return
-   */
-  public List<IActivity> getActivities() {
-    return activities;
-  }
-
-  /**
-   * Returns the sequence number of the first {@linkplain IActivity activities} in the list returned
-   * from {@link #getActivities()}.
-   *
-   * <p>All other sequence numbers can be recalculated where the sequence number of the activity
-   * from <code>getActivities().get(x)</code> is <code>getSequenceNumber() + x</code>.
-   *
-   * @return
-   */
-  public int getSequenceNumber() {
-    return sequenceNumber;
-  }
-
-  /**
-   * @JTourBusStop 5, Creating a new Activity type, XStream registration:
-   *
-   * <p>We use the XStream library to convert handy Java objects to easy-to-send XML string and vice
-   * versa. To beautify this XML output, we make use of annotations (such as XStreamAlias or
-   * XStreamAsAttribute).
-   *
-   * <p>To make a long story short: Just add your new activity type to the list below, so any
-   * annotations you might want to use will be honored. And again, please remember the alphabet.
-   *
-   * <p>Since you now know about XStream annotations, you might want to go back to your new class
-   * and add some of these? You can take a look at other activity classes for inspiration.
-   */
-
-  /** */
-  public static class Provider extends SarosSessionPacketExtension.Provider<ActivitiesExtension> {
-    private Provider() {
-      super(
-          "ados",
-          ActivitiesExtension.class,
-
-          // Misc
-          JID.class,
-          User.class,
-
-          // Jupiter classes
-
-          DeleteOperation.class,
-          InsertOperation.class,
-          JupiterVectorTime.class,
-          NoOperation.class,
-          SplitOperation.class,
-          TimestampOperation.class,
-
-          // SPATH
-          SPath.class,
-
-          // Activities
-          ChangeColorActivity.class,
-          ChecksumActivity.class,
-          ChecksumErrorActivity.class,
-          EditorActivity.class,
-          FileActivity.class,
-          FolderCreatedActivity.class,
-          FolderDeletedActivity.class,
-          FolderMovedActivity.class,
-          JupiterActivity.class,
-          NOPActivity.class,
-          PermissionActivity.class,
-          ProgressActivity.class,
-          TargetedFileActivity.class,
-          StartFollowingActivity.class,
-          StopActivity.class,
-          StopFollowingActivity.class,
-          TextSelectionActivity.class,
-          ViewportActivity.class);
+    /**
+     * Creates an object that can be transformed into a
+     * {@linkplain PacketExtension} using the provider of this extension. All
+     * object parameters <b>must not be <code>null</code></b>.
+     * 
+     * @Note This constructor does not check for correctness of the input
+     *       parameters.
+     * 
+     * @param sessionID
+     *            the session id the {@linkplain IActivity activities} belong to
+     * @param activities
+     *            the {@linkplain IActivity activities} that should be included
+     *            in this extension
+     * @param sequenceNumber
+     *            the sequence number of the <b>first</b> {@linkplain IActivity
+     *            activity}
+     */
+    public ActivitiesExtension(String sessionID, List<IActivity> activities,
+        int sequenceNumber) {
+        super(sessionID);
+        this.activities = activities;
+        this.sequenceNumber = sequenceNumber;
     }
-  }
+
+    /**
+     * Returns the {@linkplain IActivity activities} included in this extension.
+     * 
+     * @return
+     */
+    public List<IActivity> getActivities() {
+        return activities;
+    }
+
+    /**
+     * Returns the sequence number of the first {@linkplain IActivity
+     * activities} in the list returned from {@link #getActivities()}.
+     * 
+     * <p>
+     * All other sequence numbers can be recalculated where the sequence number
+     * of the activity from <code>getActivities().get(x)</code> is
+     * <code>getSequenceNumber() + x</code>.
+     * </p>
+     * 
+     * @return
+     */
+    public int getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    /**
+     * @JTourBusStop 5, Creating a new Activity type, XStream registration:
+     * 
+     *               We use the XStream library to convert handy Java objects to
+     *               easy-to-send XML string and vice versa. To beautify this
+     *               XML output, we make use of annotations (such as
+     *               XStreamAlias or XStreamAsAttribute).
+     * 
+     *               To make a long story short: Just add your new activity type
+     *               to the list below, so any annotations you might want to use
+     *               will be honored. And again, please remember the alphabet.
+     * 
+     *               Since you now know about XStream annotations, you might
+     *               want to go back to your new class and add some of these?
+     *               You can take a look at other activity classes for
+     *               inspiration.
+     */
+
+    /***/
+
+    public static class Provider extends
+        SarosSessionPacketExtension.Provider<ActivitiesExtension> {
+        private Provider() {
+            super("ados", ActivitiesExtension.class,
+
+            // Misc
+                JID.class,
+
+                User.class,
+
+                // Jupiter classes
+
+                DeleteOperation.class,
+
+                InsertOperation.class,
+
+                JupiterVectorTime.class,
+
+                NoOperation.class,
+
+                SplitOperation.class,
+
+                TimestampOperation.class,
+
+                // SPATH
+                SPath.class,
+
+                // Activities
+                ChangeColorActivity.class,
+
+                ChecksumActivity.class,
+
+                ChecksumErrorActivity.class,
+
+                EditorActivity.class,
+
+                FileActivity.class,
+
+                FolderCreatedActivity.class,
+
+                FolderDeletedActivity.class,
+
+                FolderMovedActivity.class,
+
+                JupiterActivity.class,
+
+                NOPActivity.class,
+
+                PermissionActivity.class,
+
+                ProgressActivity.class,
+
+                TargetedFileActivity.class,
+
+                SendBundleActivity.class,
+
+                StartFollowingActivity.class,
+
+                StopActivity.class,
+
+                StopFollowingActivity.class,
+
+                TextSelectionActivity.class,
+
+                ViewportActivity.class);
+        }
+    }
+  
 }
