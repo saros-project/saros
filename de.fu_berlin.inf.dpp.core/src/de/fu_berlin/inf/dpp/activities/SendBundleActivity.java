d8811351e09ff2901474e41367e3aa6a0b725433
Chris Jeschke <chrisjberlin@googlemail.com>
2019-01-10 14:28:37 +0100

    fix formatter issue and take out JGitFacade dependencie

--- de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/activities/SendBundleActivity.java
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/activities/SendBundleActivity.java
@@ -1,7 +1,7 @@
 package de.fu_berlin.inf.dpp.activities;
 
 import java.io.File;
-
+import org.apache.commons.lang3.NotImplementedException;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 
@@ -11,21 +11,23 @@ import de.fu_berlin.inf.dpp.session.User;
 
 @XStreamAlias("SendBundleActivity")
 public class SendBundleActivity extends AbstractActivity {
-    @XStreamAsAttribute
-    File bundleFile;
+  @XStreamAsAttribute File bundleFile;
 
-    public sendBundle(User source, File workDir, String actual, String basis) {
-        super(source);
-        this.bundleFile = JGitFacade.createBundle(workDir,actual,basis);
-    }
+  public sendBundle(User source, File workDir, String actual, String basis) {
+    super(source);
+    // Replace with
+    // this.bundleFile = JGitFacade.createBundle(workDir,actual,basis);
+    // as soon as the Facade is implemented
+    throw new NotImplementedException(
+        "after implementing JGitFacade replace throw Exception with createBundle");
+  }
 
-    @Override
-    public void dispatch(IActivityReceiver receiver) {
-        receiver.receive(this);
-    }
+  @Override
+  public void dispatch(IActivityReceiver receiver) {
+    receiver.receive(this);
+  }
 
-    public String getBundleFile() {
-        return bundleFile;
-    }
-
+  public String getBundleFile() {
+    return bundleFile;
+  }
 }
