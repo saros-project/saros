3b5827eb8c7c194190e1eaa67ad02a9bac41941b
Chris Jeschke <chrisjberlin@googlemail.com>
2019-01-10 14:59:43 +0100

    create SendBundleActivity

--- de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/activities/IActivityReceiver.java
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/activities/IActivityReceiver.java
@@ -3,61 +3,57 @@ package de.fu_berlin.inf.dpp.activities;
 /**
  * This interface is for implementing multiple dispatch in Java (see <a
  * href="http://en.wikipedia.org/wiki/Multiple_dispatch">Wikipedia entry</a>).
- * <p>
- * In our case we want to call one of the specialized {@code receive()} methods
- * in {@link IActivityReceiver} for a given {@link IActivity}. For instance, if
- * an activity is a {@link TextSelectionActivity}, we want the method
- * {@link #receive(TextSelectionActivity)} to be called.
+ *
+ * <p>In our case we want to call one of the specialized {@code receive()} methods in {@link
+ * IActivityReceiver} for a given {@link IActivity}. For instance, if an activity is a {@link
+ * TextSelectionActivity}, we want the method {@link #receive(TextSelectionActivity)} to be called.
  */
 public interface IActivityReceiver {
-    /**
-     * @JTourBusStop 3, Creating a new Activity type, Triple dispatch interface:
-     * 
-     *               This interface provides a receive() method for each
-     *               activity implementation. This is part of the so-called
-     *               "triple dispatch" (see "Activity sending" tour). For now,
-     *               it is enough to know that you need to declare a new
-     *               receive() method with your new Activity type in this
-     *               interface (follow the alphabet, please), and then go to the
-     *               next stop in this tour.
-     */
+  /**
+   * @JTourBusStop 3, Creating a new Activity type, Triple dispatch interface:
+   *
+   * <p>This interface provides a receive() method for each activity implementation. This is part of
+   * the so-called "triple dispatch" (see "Activity sending" tour). For now, it is enough to know
+   * that you need to declare a new receive() method with your new Activity type in this interface
+   * (follow the alphabet, please), and then go to the next stop in this tour.
+   */
 
-    /***/
+  /** */
+  void receive(ChangeColorActivity changeColorActivity);
 
-    void receive(ChangeColorActivity changeColorActivity);
+  void receive(ChecksumActivity checksumActivity);
 
-    void receive(ChecksumActivity checksumActivity);
+  void receive(ChecksumErrorActivity checksumErrorActivity);
 
-    void receive(ChecksumErrorActivity checksumErrorActivity);
+  void receive(EditorActivity editorActivity);
 
-    void receive(EditorActivity editorActivity);
+  void receive(FileActivity fileActivity);
 
-    void receive(FileActivity fileActivity);
+  void receive(FolderCreatedActivity folderCreatedActivity);
 
-    void receive(FolderCreatedActivity folderCreatedActivity);
+  void receive(FolderDeletedActivity folderDeletedActivity);
 
-    void receive(FolderDeletedActivity folderDeletedActivity);
+  void receive(FolderMovedActivity folderMovedActivity);
 
-    void receive(FolderMovedActivity folderMovedActivity);
+  void receive(JupiterActivity jupiterActivity);
 
-    void receive(JupiterActivity jupiterActivity);
+  void receive(NOPActivity nopActivity);
 
-    void receive(NOPActivity nopActivity);
+  void receive(PermissionActivity permissionActivity);
 
-    void receive(PermissionActivity permissionActivity);
+  void receive(ProgressActivity progressActivity);
 
-    void receive(ProgressActivity progressActivity);
+  void receive(SendBundleActivity sendBundleActivity);
 
-    void receive(StartFollowingActivity startFollowingActivity);
+  void receive(StartFollowingActivity startFollowingActivity);
 
-    void receive(StopActivity stopActivity);
+  void receive(StopActivity stopActivity);
 
-    void receive(StopFollowingActivity stopFollowingActivity);
+  void receive(StopFollowingActivity stopFollowingActivity);
 
-    void receive(TextEditActivity textEditActivity);
+  void receive(TextEditActivity textEditActivity);
 
-    void receive(TextSelectionActivity textSelectionActivity);
+  void receive(TextSelectionActivity textSelectionActivity);
 
-    void receive(ViewportActivity viewportActivity);
-
+  void receive(ViewportActivity viewportActivity);
 }
