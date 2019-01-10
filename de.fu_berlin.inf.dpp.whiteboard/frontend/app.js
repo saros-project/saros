077b27fcb3fdc8fd14746f18299aabb2cbc8b59c
Abdullah Barhoum <ka70911@gmail.com>
2019-01-10 20:31:14 +0100

    [INTERNAL] [HTML WB] Add view manager
    the view manager is responsible for managing everything related to the shapes on the whiteboard, it contains all the created shapes as well as allowing the creation of new ones, applying changes that were triggered by other users and communicates local changes to java.

--- de.fu_berlin.inf.dpp.whiteboard/frontend/app.js
+++ de.fu_berlin.inf.dpp.whiteboard/frontend/app.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import Menu from './src/components/menu.jsx';
 import ResizableCanvas from './src/components/resizablecanvas';
 import CanvasResizer from './src/services/canvasresizer';
+import ToolManager from './src/services/toolmanager';
+import ViewManager from './src/services/viewmanager';
 
 /**
  * Main Whiteboard app
@@ -14,6 +16,8 @@ class Whiteboard {
   constructor() {
     this.canvas = new ResizableCanvas('board');
     this.canvasResizer = new CanvasResizer(this.canvas);
+    this.viewManager = new ViewManager(this.canvas);
+    this.toolManager = new ToolManager(this.viewManager);
     ReactDOM.render(
       <Menu toolManager={this.toolManager}
       canvasResizer={this.canvasResizer}/> ,
