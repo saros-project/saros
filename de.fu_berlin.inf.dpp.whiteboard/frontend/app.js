745a1cbabe771bc29802ce8fe0b9d9f59c539eb7
Abdullah Barhoum <ka70911@gmail.com>
2019-01-10 19:07:05 +0100

    add components to app.js

--- de.fu_berlin.inf.dpp.whiteboard/frontend/app.js
+++ de.fu_berlin.inf.dpp.whiteboard/frontend/app.js
@@ -1,5 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Menu from './src/components/menu.jsx';
 import ResizableCanvas from './src/components/resizablecanvas';
 import CanvasResizer from './src/services/canvasresizer';
+
 /**
  * Main Whiteboard app
  */
@@ -10,6 +14,10 @@ class Whiteboard {
   constructor() {
     this.canvas = new ResizableCanvas('board');
     this.canvasResizer = new CanvasResizer(this.canvas);
+    ReactDOM.render(
+      <Menu toolManager={this.toolManager}
+      canvasResizer={this.canvasResizer}/> ,
+      document.getElementById('menuContainer'));
   }
 }
 
