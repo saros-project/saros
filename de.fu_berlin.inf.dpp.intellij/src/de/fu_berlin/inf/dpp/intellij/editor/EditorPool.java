a300e3a2986bfbf70aee860203a13a4fb22671cc
Tobias Bouschen <tobias.bouschen@googlemail.com>
2019-01-15 14:36:49 +0100

    [REFACTOR][I] Make EditorPool package-private
    Reduces the visibility of the class EditorPool and all its methods to
    package-private as it is never accessed outside the package.

--- de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/editor/EditorPool.java
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/intellij/editor/EditorPool.java
@@ -15,10 +15,10 @@ import org.jetbrains.annotations.Nullable;
  * The Intellij editor pool. It is used to store a mapping of <code>SPath</code>s onto <code>Editor
  * </code>s for all shared files that are open locally.
  */
-public class EditorPool {
+class EditorPool {
   private final Map<SPath, Editor> editors;
 
-  public EditorPool() {
+  EditorPool() {
     this.editors = new HashMap<>();
   }
 
@@ -28,7 +28,7 @@ public class EditorPool {
    * @param file the path for the file
    * @param editor the editor for the file
    */
-  public void add(@NotNull SPath file, @NotNull Editor editor) {
+  void add(@NotNull SPath file, @NotNull Editor editor) {
 
     editors.put(file, editor);
   }
@@ -38,7 +38,7 @@ public class EditorPool {
    *
    * @param file the path that is removed from the editor pool
    */
-  public void removeEditor(@NotNull SPath file) {
+  void removeEditor(@NotNull SPath file) {
 
     editors.remove(file);
   }
@@ -50,7 +50,7 @@ public class EditorPool {
    * @param oldPath the old path
    * @param newPath the new path
    */
-  public void replacePath(@NotNull SPath oldPath, @NotNull SPath newPath) {
+  void replacePath(@NotNull SPath oldPath, @NotNull SPath newPath) {
 
     Editor editor = editors.remove(oldPath);
 
@@ -60,14 +60,14 @@ public class EditorPool {
   }
 
   /** Sets all editors in the editor pool to read/write. */
-  public void unlockAllDocuments() {
+  void unlockAllDocuments() {
     for (Editor editor : editors.values()) {
       editor.getDocument().setReadOnly(false);
     }
   }
 
   /** Sets all editors in the editor pool to read only. */
-  public void lockAllDocuments() {
+  void lockAllDocuments() {
     for (Editor editor : editors.values()) {
       editor.getDocument().setReadOnly(true);
     }
@@ -81,7 +81,7 @@ public class EditorPool {
    *     not contained in the editor pool
    */
   @Nullable
-  public Document getDocument(@NotNull SPath file) {
+  Document getDocument(@NotNull SPath file) {
 
     Editor editor = editors.get(file);
 
@@ -96,7 +96,7 @@ public class EditorPool {
    *     not contained in the editor pool
    */
   @Nullable
-  public Editor getEditor(@NotNull SPath file) {
+  Editor getEditor(@NotNull SPath file) {
 
     return editors.get(file);
   }
@@ -109,7 +109,7 @@ public class EditorPool {
    *     could be found in the editor pool
    */
   @Nullable
-  public SPath getFile(@NotNull Document doc) {
+  SPath getFile(@NotNull Document doc) {
 
     for (Map.Entry<SPath, Editor> entry : editors.entrySet()) {
       if (entry.getValue().getDocument().equals(doc)) {
@@ -126,7 +126,7 @@ public class EditorPool {
    * @return all editors contained in the editor pool
    */
   @NotNull
-  public Collection<Editor> getEditors() {
+  Collection<Editor> getEditors() {
     return editors.values();
   }
 
@@ -136,7 +136,7 @@ public class EditorPool {
    * @return all paths contained in the editor pool
    */
   @NotNull
-  public Set<SPath> getFiles() {
+  Set<SPath> getFiles() {
     return editors.keySet();
   }
 
@@ -152,7 +152,7 @@ public class EditorPool {
   }
 
   /** Removes all mappings from the editor pool. */
-  public void clear() {
+  void clear() {
     editors.clear();
   }
 }
