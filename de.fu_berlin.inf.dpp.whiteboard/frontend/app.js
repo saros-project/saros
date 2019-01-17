d0bbb102047b301b795214d3e19b755ceb8fa704
Abdullah Barhoum <ka70911@gmail.com>
2019-01-17 10:04:31 +0100

    [INTERNAL][HTML WB] Setup development environment (#329)
    bundling the whiteboard is done through webpack, using the babel plugin to support older browsers.
    
    the package.json file contains the dependencies of the whiteboard, as well as the scripts to setup and build it.
    
    .babelrc is used to configure the babel plug in
    
    .eslint.json configuration file for the linter
    
    webpack.config.js the whiteboard bundler configuration.
    
    setting up the development environment before starting the development is done with the command npm run setup
    while developing the whiteboard the developer should use npm run dev
    when building for deployment npm run build
    
    * [BUILD] use new docker images version 0.3

---
+++ de.fu_berlin.inf.dpp.whiteboard/frontend/app.js
@@ -0,0 +1,13 @@
+/**
+ * Main Whiteboard app
+ */
+class Whiteboard {
+  /**
+   * initiates the Whiteboard
+   */
+  constructor() {
+    // add app components here
+  }
+}
+
+window.whiteboard = new Whiteboard();
