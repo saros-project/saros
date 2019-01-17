0433b0b5bffbe806612b548c1c94062c363fc02c
Oliver Hanßen <oliver.hanssen@gmail.com>
2019-01-17 20:56:54 +0100

    [CORE][API] Using isShared(IReferencePoint, IResource)

--- de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/session/internal/SarosSession.java
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/session/internal/SarosSession.java
@@ -258,7 +258,8 @@ public final class SarosSession implements ISarosSession {
 
     if (resources != null) {
       allResources = new HashSet<IResource>();
-      for (IResource resource : resources) allResources.addAll(getAllNonSharedChildren(resource));
+      for (IResource resource : resources)
+        allResources.addAll(getAllNonSharedChildren(referencePoint, resource));
     }
 
     if (!sharedReferencePointMapper.isShared(referencePoint)) {
@@ -291,12 +292,14 @@ public final class SarosSession implements ISarosSession {
   /**
    * Recursively get non-shared resources
    *
+   * @param referencePoint of {@link IResource resource}
    * @param resource of type {@link IResource#FOLDER} or {@link IResource#FILE}
    */
-  private List<IResource> getAllNonSharedChildren(IResource resource) {
+  private List<IResource> getAllNonSharedChildren(
+      IReferencePoint referencePoint, IResource resource) {
     List<IResource> list = new ArrayList<IResource>();
 
-    if (isShared(resource)) return list;
+    if (isShared(referencePoint, resource)) return list;
 
     list.add(resource);
 
@@ -304,7 +307,8 @@ public final class SarosSession implements ISarosSession {
       try {
         IResource[] members = ((IFolder) resource.getAdapter(IFolder.class)).members();
 
-        for (int i = 0; i < members.length; i++) list.addAll(getAllNonSharedChildren(members[i]));
+        for (int i = 0; i < members.length; i++)
+          list.addAll(getAllNonSharedChildren(referencePoint, members[i]));
       } catch (IOException e) {
         log.error("Can't get children of folder " + resource, e);
       }
@@ -817,7 +821,8 @@ public final class SarosSession implements ISarosSession {
 
       switch (fileActivity.getType()) {
         case CREATED:
-          if (!isShared(file.getParent())) {
+          if (file.getReferenceFolder() != file.getParent()
+              && !isShared(referencePoint, file.getParent())) {
             log.error(
                 "PSFIC -"
                     + " unable to update partial sharing state"
@@ -841,7 +846,7 @@ public final class SarosSession implements ISarosSession {
           break;
 
         case REMOVED:
-          if (!isShared(file)) {
+          if (!isShared(referencePoint, file)) {
             log.error("PSFIR -" + " file removal detected for a non shared file: " + file);
             return false;
           }
@@ -867,7 +872,7 @@ public final class SarosSession implements ISarosSession {
           IFile oldFile =
               referencePointManager.get(oldReferencePoint).getFile(oldReferencePointRelativePath);
 
-          if (!isShared(oldFile)) {
+          if (!isShared(referencePoint, oldFile)) {
             log.error(
                 "PSFIM -"
                     + " file move detected for a non shared file, source file is not shared, src: "
@@ -886,7 +891,7 @@ public final class SarosSession implements ISarosSession {
             return false;
           }
 
-          if (isShared(file)) {
+          if (isShared(referencePoint, file)) {
             log.error(
                 "PSFIM -"
                     + " file move detected for shared file, destination file already shared, src: "
@@ -913,7 +918,8 @@ public final class SarosSession implements ISarosSession {
     } else if (activity instanceof FolderCreatedActivity) {
       IFolder folder = project.getFolder(activity.getPath().getReferencePointRelativePath());
 
-      if (!isShared(folder.getParent())) {
+      if (folder.getParent() != folder.getReferenceFolder()
+          && !isShared(referencePoint, folder.getParent())) {
         log.error("PSFOC -" + " folder creation detected for a non shared parent: " + folder);
         return false;
       }
@@ -931,7 +937,7 @@ public final class SarosSession implements ISarosSession {
     } else if (activity instanceof FolderDeletedActivity) {
       IFolder folder = project.getFolder(activity.getPath().getReferencePointRelativePath());
 
-      if (!isShared(folder)) {
+      if (!isShared(referencePoint, folder)) {
         log.error("PSFOR -" + " folder removal detected for a non shared folder: " + folder);
         return false;
       }
