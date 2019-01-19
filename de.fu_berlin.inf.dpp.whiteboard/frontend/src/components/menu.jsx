56d3862bc5a08fac4a7ea764ac4f509db3dcb5b0
Abdullah Barhoum <ka70911@gmail.com>
2019-01-19 21:51:32 +0100

    add context menu mode

--- de.fu_berlin.inf.dpp.whiteboard/frontend/src/components/menu.jsx
+++ de.fu_berlin.inf.dpp.whiteboard/frontend/src/components/menu.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Menus from './menus';
 import LOGIC from '../constants/logic';
+import Menus from './menus';
 
 export default class Menu extends React.Component {
   /**
@@ -13,17 +13,16 @@ export default class Menu extends React.Component {
     this.canvasResizer = props.canvasResizer;
     this.state = {};
     this.miniMode = false;
+    this.contextMenuMode = false;
     this.wrapper = document.getElementById("menuContainer");
-    //start in miniMode because the windows in eclipse are small
-    //this.switchMiniMode();
   }
 
   /**
    * changes a logic to a given value,
    * the switch can be expanded to add additional functionality.
-   * @param {String} logicName
+   * @param {string} logicName
    * the name of the logic that will be changed
-   * @param {String} logicValue
+   * @param {string} logicValue
    * the value of the target logic
    */
   change(logicName, logicValue) {
@@ -31,7 +30,7 @@ export default class Menu extends React.Component {
     switch (logicName) {
       case LOGIC.TOOL:
         return this.toolManager.changeTool(logicValue);
-      case (LOGIC.CANVAS):
+      case LOGIC.CANVAS:
         return this.canvasResizer.execute(logicValue);
       default:
         throw "Unknown logic: " + logicName + " with value: " + logicValue;
@@ -39,15 +38,15 @@ export default class Menu extends React.Component {
   }
 
   /**
-   * the actual component, adding items to the menus.js file will automaticly show them
+   * the actual component, adding items to the menus.js file will automatically show them
    */
   render() {
     return <div>
       {Menus.map((menu, i) =>
         <div className="dropDownMenu" key={i} onDoubleClick={() => this.switchMiniMode()}
-        title="Double click to swtich mini mode">
+          title="Double click to switch mini mode">
           <div className="dropDownMenuContainer">
-            <img draggable="false" src={menu.imageSrc} className="dropDownMenuImage" alt={menu.name}/>
+            <img draggable="false" src={menu.imageSrc} className="dropDownMenuImage" alt={menu.name} />
             <span className="dropDownMenuText">{menu.name}</span>
           </div>
           <div className="dropDownMenuItemsContainer">
@@ -55,37 +54,42 @@ export default class Menu extends React.Component {
               <div onClick={() => this.change(tool.toolLogic, tool.toolName)} key={j}
                 title={tool.description} className={"dropDownMenuItem " +
                   (menu.onlyOneActive && this.state[LOGIC.TOOL] == tool.toolName ? "dropDownMenuItemActive" : "")}>
-                <img draggable="false" src={tool.imageSrc} className="dropDownMenuItemImage" alt={tool.name}/>
+                <img draggable="false" src={tool.imageSrc} className="dropDownMenuItemImage" alt={tool.name} />
               </div>)}
           </div>
         </div>)}
     </div>;
   }
 
+  /**
+   * enables or disables mini mode, making the menu smaller and using less screen space
+   */
   switchMiniMode() {
     this.miniMode = !this.miniMode;
-    if (this.miniMode)
-      this.wrapper.className = "miniMode";
-    else
-      this.wrapper.className = "";
+    this.updateWrapperCSS();
   }
+
   /**
-   * sets mini tool for the menu,
-   * is used  when right mouse button is clicked to force mini mode, but not permenantly
-   * @param {Boolean} state
+   * sets the css classes on the menu according to the internal state of the menu
    */
-  setMiniMode(state) {
-    if (this.miniMode)
-      return;
-    if (state)
-      this.wrapper.className = "miniMode";
-    else
-      this.wrapper.className = "";
+  updateWrapperCSS() {
+    let isMini = this.miniMode || this.contextMenuMode;
+    this.wrapper.className = isMini ? "miniMode" : "";
+  }
+
+  /**
+   * sets the context menu mode for the menu, it is used when right mouse button
+   * is clicked to force mini mode, but not permanently.
+   * @param {boolean} state
+   */
+  setContextMenuMode(state) {
+    this.contextMenuMode = state;
+    this.updateWrapperCSS();
   }
   /**
    * sets the position of the menu
-   * @param {Number} x
-   * @param {Number} y
+   * @param {number} x
+   * @param {number} y
    */
   setPosition(x = 0, y = 0) {
     this.wrapper.style.left = x + "px";
