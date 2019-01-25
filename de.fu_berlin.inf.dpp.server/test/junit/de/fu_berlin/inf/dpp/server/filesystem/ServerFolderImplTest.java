b01f3c05a65ba89e1a15ed910e1e3fbae9cf632e
Oliver Hanßen <oliver.hanssen@gmail.com>
2019-01-25 14:35:35 +0100

    [Server][JUNIT] Added tests from ServerProjectImplTest

--- de.fu_berlin.inf.dpp.server/test/junit/de/fu_berlin/inf/dpp/server/filesystem/ServerFolderImplTest.java
+++ de.fu_berlin.inf.dpp.server/test/junit/de/fu_berlin/inf/dpp/server/filesystem/ServerFolderImplTest.java
@@ -8,7 +8,11 @@ import static de.fu_berlin.inf.dpp.server.filesystem.FileSystemTestUtils.createF
 import static de.fu_berlin.inf.dpp.server.filesystem.FileSystemTestUtils.createWorkspaceFolder;
 import static de.fu_berlin.inf.dpp.server.filesystem.FileSystemTestUtils.path;
 import static org.easymock.EasyMock.expect;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 
+import de.fu_berlin.inf.dpp.filesystem.IFile;
 import de.fu_berlin.inf.dpp.filesystem.IFolder;
 import de.fu_berlin.inf.dpp.filesystem.IResource;
 import de.fu_berlin.inf.dpp.filesystem.IWorkspace;
@@ -26,6 +30,7 @@ public class ServerFolderImplTest extends EasyMockSupport {
   private static final String FOLDER_PARENT_PATH = "project";
 
   private IFolder folder;
+  private IFolder project;
   private IWorkspace workspace;
 
   @Before
@@ -36,6 +41,10 @@ public class ServerFolderImplTest extends EasyMockSupport {
     replayAll();
 
     folder = new ServerFolderImplV2(workspace.getLocation(), path(FOLDER_PATH));
+    project =
+        new ServerFolderImplV2(
+            workspace.getLocation().append(FOLDER_PARENT_PATH),
+            ServerPathImpl.fromString(new String()));
   }
 
   @After
@@ -67,5 +76,54 @@ public class ServerFolderImplTest extends EasyMockSupport {
   public void createAtNonExistentParent() throws Exception {
     assertResourceNotExists(workspace, FOLDER_PARENT_PATH);
     folder.create(IResource.NONE, true);
+  }
+
+  @Test
+  public void defaultCharsetUTF8() throws Exception {
+    assertEquals("UTF-8", project.getDefaultCharset());
+  }
+
+  @Test
+  public void findMember() throws Exception {
+    createFolder(workspace, "project");
+    createFolder(workspace, "project/folder");
+    createFile(workspace, "project/folder/file");
+
+    IResource folder = project.findMember(path("folder"));
+    IResource file = project.findMember(path("folder/file"));
+    IResource nonExistent = project.findMember(path("non/existent"));
+
+    assertEquals(IResource.FOLDER, folder.getType());
+    assertEquals(IResource.FILE, file.getType());
+    assertNull(nonExistent);
+  }
+
+  @Test
+  public void findMemberAtEmptyPath() throws Exception {
+    assertSame(project, project.findMember(path("")));
+  }
+
+  @Test
+  public void getFileByPath() {
+    IFile file = project.getFile(path("folder/file"));
+    assertEquals(path("folder/file"), file.getFullPath());
+  }
+
+  @Test
+  public void getFileByPathString() {
+    IFile file = project.getFile("folder/file");
+    assertEquals(path("folder/file"), file.getFullPath());
+  }
+
+  @Test
+  public void getFolderByPath() {
+    IFolder folder = project.getFolder(path("folder/subfolder"));
+    assertEquals(path("folder/subfolder"), folder.getFullPath());
+  }
+
+  @Test
+  public void getFolderByPathString() {
+    IFolder folder = project.getFolder("folder/subfolder");
+    assertEquals(path("folder/subfolder"), folder.getFullPath());
   }
 }
