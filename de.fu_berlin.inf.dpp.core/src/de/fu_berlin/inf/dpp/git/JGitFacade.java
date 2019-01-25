1439c14fd34cfb63ba4d2da95019696407972d00
Christian <chrisjberlin@googlemail.com>
2019-01-25 10:26:29 +0100

    fix formatter issues

--- de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/git/JGitFacade.java
+++ de.fu_berlin.inf.dpp.core/src/de/fu_berlin/inf/dpp/git/JGitFacade.java
@@ -17,159 +17,140 @@ import org.eclipse.jgit.transport.BundleWriter;
 import org.eclipse.jgit.transport.URIish;
 
 public class JGitFacade {
-    private static final Logger log = Logger.getLogger(JGitFacade.class);
+  private static final Logger log = Logger.getLogger(JGitFacade.class);
 
-    /**
-     * Create a bundle file with all commits from basis to actual of the Git
-     * repo.
-     *
-     * @param workDir
-     *            The directory that contains the .git directory
-     * @param actual
-     *            The name of the ref to lookup. For example: "HEAD" or
-     *            "refs/heads/master". Must not be a short-handform; e.g.,
-     *            "master" is not automatically expanded to"refs/heads/master".
-     *            Include the object the ref is point at in the bundle and (if
-     *            the basis is an empty string) everything reachable from it.
-     * @param basis
-     *            Assume that the recipient have at least the commit the basis
-     *            is pointing to. In order to fetch from a bundle the recipient
-     *            must have the commit the basis is pointing to.
-     * @throws IllegalArgumentException
-     *             workDir, actual or basis can't be resolved
-     * @throws IOException
-     *             Error while writing bundle, after resolving parameters
-     */
-    public static File createBundle(File workDir, String actual, String basis)
-        throws IllegalArgumentException, IOException {
+  /**
+   * Create a bundle file with all commits from basis to actual of the Git repo.
+   *
+   * @param workDir The directory that contains the .git directory
+   * @param actual The name of the ref to lookup. For example: "HEAD" or "refs/heads/master". Must
+   *     not be a short-handform; e.g., "master" is not automatically expanded
+   *     to"refs/heads/master". Include the object the ref is point at in the bundle and (if the
+   *     basis is an empty string) everything reachable from it.
+   * @param basis Assume that the recipient have at least the commit the basis is pointing to. In
+   *     order to fetch from a bundle the recipient must have the commit the basis is pointing to.
+   * @throws IllegalArgumentException workDir, actual or basis can't be resolved
+   * @throws IOException Error while writing bundle, after resolving parameters
+   */
+  public static File createBundle(File workDir, String actual, String basis)
+      throws IllegalArgumentException, IOException {
 
-        // The method can be divided into 4 Steps. After the first, second and
-        // third step the
-        // bundlewriter object is expanded with the parameters. The last step
-        // exists to write the
-        // bundle file.
+    // The method can be divided into 4 Steps. After the first, second and third step the
+    // bundlewriter object is expanded with the parameters. The last step exists to write the bundle
+    // file.
 
-        // Step 1
-        Git git;
-        try {
-            git = Git.open(workDir);
-        } catch (IOException e) {
-            throw new IllegalArgumentException("workDir can't be resolved", e);
-        }
-        Repository repo = git.getRepository();
-        BundleWriter bundlewriter = new BundleWriter(repo);
+    // Step 1
+    Git git;
+    try {
+      git = Git.open(workDir);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("workDir can't be resolved", e);
+    }
+    Repository repo = git.getRepository();
+    BundleWriter bundlewriter = new BundleWriter(repo);
 
-        // Step 2
-        Ref actualRef;
-        try {
-            actualRef = repo.exactRef(actual);
-        } catch (IOException e) {
-            throw new IllegalArgumentException("actual can't be resolved", e);
-        }
-        if (actualRef == null)
-            throw new IllegalArgumentException("actual can't be resolved");
-        bundlewriter.include("refs/heads/bundle", actualRef.getObjectId());
+    // Step 2
+    Ref actualRef;
+    try {
+      actualRef = repo.exactRef(actual);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("actual can't be resolved", e);
+    }
+    if (actualRef == null) throw new IllegalArgumentException("actual can't be resolved");
+    bundlewriter.include("refs/heads/bundle", actualRef.getObjectId());
 
-        // Step 3
-        if (basis != "") {
-            try {
-                RevWalk walk = new RevWalk(repo);
-                RevCommit basisCommit = walk.parseCommit(repo.resolve(basis));
-                bundlewriter.assume(basisCommit);
-            } catch (Exception e) {
-                throw new IllegalArgumentException("basis can't be resolved",
-                    e);
-            }
-        }
-
-        // Step 4
-        File bundle;
-        try {
-            bundle = File.createTempFile("file", ".bundle");
-            OutputStream fos = new FileOutputStream(bundle);
-
-            ProgressMonitor monitor = NullProgressMonitor.INSTANCE;
-
-            bundlewriter.writeBundle(monitor, fos);
-        } catch (Exception e) {
-            throw new IOException("failed to write bundle", e);
-        }
-        return bundle;
+    // Step 3
+    if (basis != "") {
+      try {
+        RevWalk walk = new RevWalk(repo);
+        RevCommit basisCommit = walk.parseCommit(repo.resolve(basis));
+        bundlewriter.assume(basisCommit);
+      } catch (Exception e) {
+        throw new IllegalArgumentException("basis can't be resolved", e);
+      }
     }
 
-    /**
-     * Fetching from a bundle file to an Git repo
-     *
-     * @param workDir
-     *            The directory that contains the .git directory
-     * @param bundleFile
-     * @throws IllegalArgumentException
-     *             workDir or bundleFile can't be resolved
-     * @throws Exception
-     *             Error while adding the bundle as a remote or during fetch
-     */
-    public static void fetchFromBundle(File workDir, File bundleFile)
-        throws IllegalArgumentException, Exception {
+    // Step 4
+    File bundle;
+    try {
+      bundle = File.createTempFile("file", ".bundle");
+      OutputStream fos = new FileOutputStream(bundle);
 
-        Git git;
-        try {
-            git = Git.open(workDir);
-        } catch (IOException e) {
-            throw new IllegalArgumentException("workDir can't be resolved", e);
-        }
+      ProgressMonitor monitor = NullProgressMonitor.INSTANCE;
 
-        try {
-            RemoteRemoveCommand rrc = git.remoteRemove();
-            rrc.setName("bundle");
-            rrc.call();
-        } catch (Exception e) {
-            throw new Exception("failed to remove old bundle path", e);
-        }
+      bundlewriter.writeBundle(monitor, fos);
+    } catch (Exception e) {
+      throw new IOException("failed to write bundle", e);
+    }
+    return bundle;
+  }
 
-        try {
-            git.remoteAdd()
-                .setUri(new URIish().setPath(bundleFile.getCanonicalPath()))
-                .setName("bundle").call();
-        } catch (IOException e) {
-            throw new IllegalArgumentException("bundleFile can't be resolved",
-                e);
-        } catch (Exception e) {
-            throw new Exception("failed to add bundle as remote", e);
-        }
+  /**
+   * Fetching from a bundle file to an Git repo
+   *
+   * @param workDir The directory that contains the .git directory
+   * @param bundleFile
+   * @throws IllegalArgumentException workDir or bundleFile can't be resolved
+   * @throws Exception Error while adding the bundle as a remote or during fetch
+   */
+  public static void fetchFromBundle(File workDir, File bundleFile)
+      throws IllegalArgumentException, Exception {
 
-        try {
-            git.fetch().setRemote("bundle").call();
-        } catch (Exception e) {
-            throw new Exception("failed to fetch from bundle", e);
-        }
+    Git git;
+    try {
+      git = Git.open(workDir);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("workDir can't be resolved", e);
     }
 
-    public static void cloneFromRepo(File from, File to)
-        throws IllegalArgumentException, Exception {
-        try {
-            Git.cloneRepository().setURI(getUrlByWorkDir(from)).setDirectory(to)
-                .call();
-        } catch (IOException e) {
-            throw new IllegalArgumentException(
-                "first parameter can't be resolved", e);
-        } catch (Exception e) {
-            throw new Exception("failed to clone", e);
-        }
+    try {
+      RemoteRemoveCommand rrc = git.remoteRemove();
+      rrc.setName("bundle");
+      rrc.call();
+    } catch (Exception e) {
+      throw new Exception("failed to remove old bundle path", e);
     }
 
-    /**
-     * @param workDir
-     *            The directory that contains the .git directory
-     * @return The URL to access/address directly the .git directory
-     * @throws IOException
-     */
-    static String getUrlByWorkDir(File workDir) throws IOException {
-        Git git = Git.open(workDir);
-        return git.getRepository().getDirectory().getCanonicalPath();
+    try {
+      git.remoteAdd()
+          .setUri(new URIish().setPath(bundleFile.getCanonicalPath()))
+          .setName("bundle")
+          .call();
+    } catch (IOException e) {
+      throw new IllegalArgumentException("bundleFile can't be resolved", e);
+    } catch (Exception e) {
+      throw new Exception("failed to add bundle as remote", e);
     }
 
-    public static String getSHA1HashByRevisionString(File workDir,
-        String revString) throws IOException {
-        return Git.open(workDir).getRepository().resolve(revString).name();
+    try {
+      git.fetch().setRemote("bundle").call();
+    } catch (Exception e) {
+      throw new Exception("failed to fetch from bundle", e);
     }
+  }
+
+  public static void cloneFromRepo(File from, File to) throws IllegalArgumentException, Exception {
+    try {
+      Git.cloneRepository().setURI(getUrlByWorkDir(from)).setDirectory(to).call();
+    } catch (IOException e) {
+      throw new IllegalArgumentException("first parameter can't be resolved", e);
+    } catch (Exception e) {
+      throw new Exception("failed to clone", e);
+    }
+  }
+
+  /**
+   * @param workDir The directory that contains the .git directory
+   * @return The URL to access/address directly the .git directory
+   * @throws IOException
+   */
+  static String getUrlByWorkDir(File workDir) throws IOException {
+    Git git = Git.open(workDir);
+    return git.getRepository().getDirectory().getCanonicalPath();
+  }
+
+  public static String getSHA1HashByRevisionString(File workDir, String revString)
+      throws IOException {
+    return Git.open(workDir).getRepository().resolve(revString).name();
+  }
 }
--- de.fu_berlin.inf.dpp.core/test/junit/de/fu_berlin/inf/dpp/git/JGitFacadeTest.java
+++ de.fu_berlin.inf.dpp.core/test/junit/de/fu_berlin/inf/dpp/git/JGitFacadeTest.java
@@ -20,166 +20,150 @@ import org.junit.rules.TemporaryFolder;
 
 public class JGitFacadeTest {
 
-    @Rule
-    public TemporaryFolder tempFolder = new TemporaryFolder();
+  @Rule public TemporaryFolder tempFolder = new TemporaryFolder();
 
-    // the local user is creating the bundle
-    private File localWorkDir;
+  // the local user is creating the bundle
+  private File localWorkDir;
 
-    @Before
-    public void setUp()
-        throws IOException, IllegalStateException, GitAPIException {
-        localWorkDir = tempFolder.newFolder("TempDir1");
+  @Before
+  public void setUp() throws IOException, IllegalStateException, GitAPIException {
+    localWorkDir = tempFolder.newFolder("TempDir1");
 
-        initNewRepo(localWorkDir);
-        writeCommitToRepo(localWorkDir, 2);
-    }
+    initNewRepo(localWorkDir);
+    writeCommitToRepo(localWorkDir, 2);
+  }
 
-    // lucky paths
-    @Test
-    public void testCreateBundleHEAD()
-        throws IllegalArgumentException, IOException {
-        File bundle = JGitFacade.createBundle(localWorkDir, "HEAD", "");
-        assertNotNull(bundle);
-    }
+  // lucky paths
+  @Test
+  public void testCreateBundleHEAD() throws IllegalArgumentException, IOException {
+    File bundle = JGitFacade.createBundle(localWorkDir, "HEAD", "");
+    assertNotNull(bundle);
+  }
 
-    @Test
-    public void testCreateBundleRef()
-        throws IllegalArgumentException, IOException {
-        File bundle = JGitFacade.createBundle(localWorkDir, "refs/heads/master",
-            "");
-        assertNotNull(bundle);
-    }
+  @Test
+  public void testCreateBundleRef() throws IllegalArgumentException, IOException {
+    File bundle = JGitFacade.createBundle(localWorkDir, "refs/heads/master", "");
+    assertNotNull(bundle);
+  }
 
-    @Test
-    public void testFetchFromBundle()
-        throws IllegalArgumentException, Exception {
-        File remoteWorkDir = tempFolder.newFolder("TempDir2");
+  @Test
+  public void testFetchFromBundle() throws IllegalArgumentException, Exception {
+    File remoteWorkDir = tempFolder.newFolder("TempDir2");
 
-        JGitFacade.cloneFromRepo(localWorkDir, remoteWorkDir);
+    JGitFacade.cloneFromRepo(localWorkDir, remoteWorkDir);
 
-        writeCommitToRepo(localWorkDir, 3);
+    writeCommitToRepo(localWorkDir, 3);
 
-        String basis = JGitFacade.getSHA1HashByRevisionString(remoteWorkDir,
-            "HEAD");
+    String basis = JGitFacade.getSHA1HashByRevisionString(remoteWorkDir, "HEAD");
 
-        File bundle = JGitFacade.createBundle(localWorkDir, "HEAD", basis);
+    File bundle = JGitFacade.createBundle(localWorkDir, "HEAD", basis);
 
-        assertNotEquals(getObjectIdByRevisionString(localWorkDir, "HEAD"),
-            getObjectIdByRevisionString(remoteWorkDir, "FETCH_HEAD"));
+    assertNotEquals(
+        getObjectIdByRevisionString(localWorkDir, "HEAD"),
+        getObjectIdByRevisionString(remoteWorkDir, "FETCH_HEAD"));
 
-        JGitFacade.fetchFromBundle(remoteWorkDir, bundle);
+    JGitFacade.fetchFromBundle(remoteWorkDir, bundle);
 
-        assertEquals(getObjectIdByRevisionString(localWorkDir, "HEAD"),
-            getObjectIdByRevisionString(remoteWorkDir, "FETCH_HEAD"));
-    }
+    assertEquals(
+        getObjectIdByRevisionString(localWorkDir, "HEAD"),
+        getObjectIdByRevisionString(remoteWorkDir, "FETCH_HEAD"));
+  }
 
-    // handling exceptions
-    @Test(expected = IllegalArgumentException.class)
-    public void testCreateBundleEmptyWorkDir()
-        throws IllegalArgumentException, IOException {
-        File emptyDir = tempFolder.newFolder("TempDir3");
+  // handling exceptions
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateBundleEmptyWorkDir() throws IllegalArgumentException, IOException {
+    File emptyDir = tempFolder.newFolder("TempDir3");
 
-        JGitFacade.createBundle(emptyDir, "HEAD", "CheckoutAtInit");
-    }
+    JGitFacade.createBundle(emptyDir, "HEAD", "CheckoutAtInit");
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testCreateBundleWrongRef()
-        throws IllegalArgumentException, IOException {
-        JGitFacade.createBundle(localWorkDir, "refs/heads/wrongRef",
-            "CheckoutAtInit");
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateBundleWrongRef() throws IllegalArgumentException, IOException {
+    JGitFacade.createBundle(localWorkDir, "refs/heads/wrongRef", "CheckoutAtInit");
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testCreateBundleWrongBasis()
-        throws IllegalArgumentException, IOException {
-        JGitFacade.createBundle(localWorkDir, "HEAD", "WrongBasis");
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateBundleWrongBasis() throws IllegalArgumentException, IOException {
+    JGitFacade.createBundle(localWorkDir, "HEAD", "WrongBasis");
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testFetchFromBundleEmptyDir()
-        throws IllegalArgumentException, Exception {
-        File emptyDir = tempFolder.newFolder("TempDir3");
+  @Test(expected = IllegalArgumentException.class)
+  public void testFetchFromBundleEmptyDir() throws IllegalArgumentException, Exception {
+    File emptyDir = tempFolder.newFolder("TempDir3");
 
-        writeCommitToRepo(localWorkDir, 3);
+    writeCommitToRepo(localWorkDir, 3);
 
-        File bundle = JGitFacade.createBundle(localWorkDir, "HEAD",
-            "CheckoutAtCommit2");
+    File bundle = JGitFacade.createBundle(localWorkDir, "HEAD", "CheckoutAtCommit2");
 
-        JGitFacade.fetchFromBundle(emptyDir, bundle);
-    }
+    JGitFacade.fetchFromBundle(emptyDir, bundle);
+  }
 
-    @Test(expected = Exception.class)
-    public void testFetchFromBundleBasisMissing()
-        throws IllegalArgumentException, Exception {
-        File remoteWorkDir = tempFolder.newFolder("TempDir2");
+  @Test(expected = Exception.class)
+  public void testFetchFromBundleBasisMissing() throws IllegalArgumentException, Exception {
+    File remoteWorkDir = tempFolder.newFolder("TempDir2");
 
-        JGitFacade.cloneFromRepo(localWorkDir, remoteWorkDir);
+    JGitFacade.cloneFromRepo(localWorkDir, remoteWorkDir);
 
-        writeCommitToRepo(localWorkDir, 3);
-        writeCommitToRepo(localWorkDir, 4);
+    writeCommitToRepo(localWorkDir, 3);
+    writeCommitToRepo(localWorkDir, 4);
 
-        File bundle = JGitFacade.createBundle(localWorkDir, "HEAD",
-            "CheckoutAtCommit3");
+    File bundle = JGitFacade.createBundle(localWorkDir, "HEAD", "CheckoutAtCommit3");
 
-        JGitFacade.fetchFromBundle(remoteWorkDir, bundle);
-    }
+    JGitFacade.fetchFromBundle(remoteWorkDir, bundle);
+  }
 
-    /**
-     * Creating a new Git directory,create a file and git add the file,create
-     * the first commit and create the Tag "CheckoutAtInit" that is pointing to
-     * the commit
-     *
-     * @param workDir
-     *            The directory that will contain the .git directory
-     * @throws GitAPIException
-     * @throws IllegalStateException
-     * @throws IOException
-     */
-    private static void initNewRepo(File workDir)
-        throws IllegalStateException, GitAPIException, IOException {
-        Git git = Git.init().setDirectory(workDir).call();
+  /**
+   * Creating a new Git directory,create a file and git add the file,create the first commit and
+   * create the Tag "CheckoutAtInit" that is pointing to the commit
+   *
+   * @param workDir The directory that will contain the .git directory
+   * @throws GitAPIException
+   * @throws IllegalStateException
+   * @throws IOException
+   */
+  private static void initNewRepo(File workDir)
+      throws IllegalStateException, GitAPIException, IOException {
+    Git git = Git.init().setDirectory(workDir).call();
 
-        File testFile1 = new File(workDir, "testFile1");
-        testFile1.createNewFile();
+    File testFile1 = new File(workDir, "testFile1");
+    testFile1.createNewFile();
 
-        git.add().addFilepattern(workDir.getAbsolutePath()).call();
+    git.add().addFilepattern(workDir.getAbsolutePath()).call();
 
-        git.commit().setMessage("Initial commit").call();
+    git.commit().setMessage("Initial commit").call();
 
-        git.tag().setName("CheckoutAtInit").setAnnotated(false)
-            .setForceUpdate(true).call();
-    }
+    git.tag().setName("CheckoutAtInit").setAnnotated(false).setForceUpdate(true).call();
+  }
 
-    /**
-     * Creating a new File, git add, git commit and create a Tag
-     * "CheckoutAtCommit(numberOfCommit)"
-     *
-     * @param workDir
-     *            The directory that contains the .git directory
-     * @param numberOfCommit
-     *            The first used number should be 2 and than incremented by 1
-     * @throws IOException
-     * @throws GitAPIException
-     * @throws NoFilepatternException
-     */
-    private static void writeCommitToRepo(File workDir, int numberOfCommit)
-        throws IOException, NoFilepatternException, GitAPIException {
-        Git git = Git.open(workDir);
+  /**
+   * Creating a new File, git add, git commit and create a Tag "CheckoutAtCommit(numberOfCommit)"
+   *
+   * @param workDir The directory that contains the .git directory
+   * @param numberOfCommit The first used number should be 2 and than incremented by 1
+   * @throws IOException
+   * @throws GitAPIException
+   * @throws NoFilepatternException
+   */
+  private static void writeCommitToRepo(File workDir, int numberOfCommit)
+      throws IOException, NoFilepatternException, GitAPIException {
+    Git git = Git.open(workDir);
 
-        File testfile = new File(workDir, "testfile" + numberOfCommit);
+    File testfile = new File(workDir, "testfile" + numberOfCommit);
 
-        git.add().addFilepattern(workDir.getAbsolutePath()).call(); // git add .
+    git.add().addFilepattern(workDir.getAbsolutePath()).call(); // git add .
 
-        git.commit().setMessage("new file Commit Nr." + numberOfCommit).call();
+    git.commit().setMessage("new file Commit Nr." + numberOfCommit).call();
 
-        git.tag().setName("CheckoutAtCommit" + numberOfCommit)
-            .setAnnotated(false).setForceUpdate(true).call();
-    }
+    git.tag()
+        .setName("CheckoutAtCommit" + numberOfCommit)
+        .setAnnotated(false)
+        .setForceUpdate(true)
+        .call();
+  }
 
-    private static ObjectId getObjectIdByRevisionString(File workDir,
-        String rev) throws RevisionSyntaxException, AmbiguousObjectException,
-        IncorrectObjectTypeException, IOException {
-        return Git.open(workDir).getRepository().resolve(rev);
-    }
+  private static ObjectId getObjectIdByRevisionString(File workDir, String rev)
+      throws RevisionSyntaxException, AmbiguousObjectException, IncorrectObjectTypeException,
+          IOException {
+    return Git.open(workDir).getRepository().resolve(rev);
+  }
 }
