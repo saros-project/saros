204f2665b762eb5b255c1a758ff5631c59cf868e
Christian <chrisjberlin@googlemail.com>
2019-03-01 11:38:29 +0100

    Add TestSuite for JGitFacadeTest

--- core/test/junit/saros/SarosCoreTestSuite.java
+++ core/test/junit/saros/SarosCoreTestSuite.java
@@ -13,6 +13,7 @@ import org.junit.runners.Suite;
   saros.editor.colorstorage.TestSuite.class,
   saros.editor.remote.TestSuite.class,
   saros.filesystem.TestSuite.class,
+  saros.git.TestSuite.class,
   saros.misc.xstream.TestSuite.class,
   saros.monitoring.TestSuite.class,
   saros.negotiation.TestSuite.class,
