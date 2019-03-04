e4f38ef1dc2b824ed6912d7ca6d918eeea29327e
Oliver Hanßen <oliver.hanssen@gmail.com>
2019-03-04 19:06:09 +0100

    [Core|E|I][API] FileList: Using ReferencePoints
    The FileList now contains resources (files and folders), that belongs
    to a reference points. So the FileListFactory had to be adjusted: It
    gets a reference point instead of a project and additionally the
    IReferencePointManager. The IReferencePointManager is needed to
    get resources, that belongs to a reference point.

--- intellij/src/saros/intellij/ui/wizards/AddProjectToSessionWizard.java
+++ intellij/src/saros/intellij/ui/wizards/AddProjectToSessionWizard.java
@@ -676,6 +676,8 @@ public class AddProjectToSessionWizard extends Wizard {
 
     fillReferencePointManager(session, new HashSet<>(projectMapping.values()));
 
+    IReferencePointManager referencePointManager = session.getComponent(IReferencePointManager.class);
+
     for (Map.Entry<String, IProject> entry : projectMapping.entrySet()) {
 
       String projectID = entry.getKey();
@@ -688,8 +690,8 @@ public class AddProjectToSessionWizard extends Wizard {
         if (data.isPartial()) throw new IllegalStateException("partial sharing is not supported");
 
         FileList localFileList =
-            FileListFactory.createFileList(
-                project,
+            FileListFactory.createFileList(referencePointManager,
+                project.getReferencePoint(),
                 null,
                 checksumCache,
                 new SubProgressMonitor(monitor, 1, SubProgressMonitor.SUPPRESS_SETTASKNAME));
