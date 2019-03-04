a0fe80d72b3fa07c73528cc8bb6ff91f9e6b1318
Oliver Hanßen <oliver.hanssen@gmail.com>
2019-03-04 20:56:06 +0100

    [CORE] Adjust ActivityQueuer to reference points
    The changes don't contain any magic. The references of core.IProject
    was replaced by IReferencePoint. So the SaroSession had to be adjusted
    to the changes and put IReferencePoints to the ActivityQueuer for
    enabling the queuing. The Unit tests for the ActivtiyQueuer were
    adjusted additionally

--- core/src/saros/session/internal/ActivityQueuer.java
+++ core/src/saros/session/internal/ActivityQueuer.java
@@ -10,19 +10,19 @@ import saros.activities.IActivity;
 import saros.activities.IResourceActivity;
 import saros.activities.JupiterActivity;
 import saros.activities.SPath;
-import saros.filesystem.IProject;
+import saros.filesystem.IReferencePoint;
 import saros.session.User;
 
 /** This class enables the queuing of {@linkplain IActivity activities} for given projects. */
 public class ActivityQueuer {
 
   private static class ProjectQueue {
-    private final IProject project;
+    private final IReferencePoint referencePoint;
     private final List<IResourceActivity> buffer;
     private int readyToFlush;
 
-    private ProjectQueue(IProject project) {
-      this.project = project;
+    private ProjectQueue(IReferencePoint referencePoint) {
+      this.referencePoint = referencePoint;
       buffer = new ArrayList<IResourceActivity>();
       readyToFlush = 1;
     }
@@ -65,19 +65,19 @@ public class ActivityQueuer {
    * project, increasing or decreasing the internal counter. Activities can be flushed when the
    * counter reaches zero.
    *
-   * @param project
+   * @param referencePoint the local representation of the reference point
    */
-  public synchronized void enableQueuing(final IProject project) {
+  public synchronized void enableQueuing(final IReferencePoint referencePoint) {
     for (final ProjectQueue projectQueue : projectQueues) {
 
-      if (projectQueue.project.equals(project)) {
+      if (projectQueue.referencePoint.equals(referencePoint)) {
 
         projectQueue.readyToFlush++;
         return;
       }
     }
 
-    projectQueues.add(new ProjectQueue(project));
+    projectQueues.add(new ProjectQueue(referencePoint));
   }
 
   /**
@@ -92,12 +92,12 @@ public class ActivityQueuer {
    * it stops the queuing for the given project which at least releases the queued activities to
    * prevent memory leaks.
    *
-   * @param project
+   * @param referencePoint the local representation of the reference point
    */
-  public synchronized void disableQueuing(final IProject project) {
+  public synchronized void disableQueuing(final IReferencePoint referencePoint) {
     for (final ProjectQueue projectQueue : projectQueues) {
 
-      if (projectQueue.project.equals(project)) {
+      if (projectQueue.referencePoint.equals(referencePoint)) {
 
         if (projectQueue.readyToFlush > 0) projectQueue.readyToFlush--;
 
@@ -142,8 +142,9 @@ public class ActivityQueuer {
         if (path != null) {
 
           // try to reuse the queue as lookup is O(n)
-          if (projectQueue == null || !projectQueue.project.equals(path.getProject())) {
-            projectQueue = getProjectQueue(path.getProject());
+          if (projectQueue == null
+              || !projectQueue.referencePoint.equals(path.getProject().getReferencePoint())) {
+            projectQueue = getProjectQueue(path.getProject().getReferencePoint());
           }
 
           if (projectQueue != null) {
@@ -210,10 +211,10 @@ public class ActivityQueuer {
       projectQueues.remove(projectQueue);
   }
 
-  private ProjectQueue getProjectQueue(final IProject project) {
+  private ProjectQueue getProjectQueue(final IReferencePoint referencePoint) {
 
     for (final ProjectQueue projectQueue : projectQueues) {
-      if (projectQueue.project.equals(project)) return projectQueue;
+      if (projectQueue.referencePoint.equals(referencePoint)) return projectQueue;
     }
 
     return null;
