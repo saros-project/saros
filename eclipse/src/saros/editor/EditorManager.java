3fa20c906b3416ef77ef0fbe0b7a948ed53bb605
Oliver Hanßen <oliver.hanssen@gmail.com>
2019-03-04 20:59:29 +0100

    [CORE][API] Adjust IEditorManager to reference points
    For saving the local editors of all shared files, the method
    takes an IReferencePoint instead of a core.IProject in IEditorManager.
    The implementationa of IEditorManager in the IDE parts were adjusted, too.

--- eclipse/src/saros/editor/EditorManager.java
+++ eclipse/src/saros/editor/EditorManager.java
@@ -54,7 +54,6 @@ import saros.editor.remote.UserEditorStateManager;
 import saros.editor.text.LineRange;
 import saros.editor.text.TextSelection;
 import saros.filesystem.EclipseFileImpl;
-import saros.filesystem.IProject;
 import saros.filesystem.IReferencePoint;
 import saros.filesystem.ResourceAdapterFactory;
 import saros.observables.FileReplacementInProgressObservable;
@@ -1410,7 +1409,7 @@ public class EditorManager implements IEditorManager {
   }
 
   @Override
-  public void saveEditors(final IProject project) {
+  public void saveEditors(final IReferencePoint referencePoint) {
     SWTUtils.runSafeSWTSync(
         LOG,
         new Runnable() {
@@ -1429,7 +1428,8 @@ public class EditorManager implements IEditorManager {
             editorPaths.addAll(openEditorPaths);
 
             for (final SPath path : editorPaths) {
-              if (project == null || project.equals(path.getProject())) saveLazy(path);
+              if (referencePoint == null
+                  || referencePoint.equals(path.getProject().getReferencePoint())) saveLazy(path);
             }
           }
         });
