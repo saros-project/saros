c7df144e3773b1fc6c285731f764e4631a0dd238
Oliver Hanßen <oliver.hanssen@gmail.com>
2019-03-04 20:43:50 +0100

    run(IWorkspaceRunnable, IReferencePoint[], IReferencePointManager): is also equivalent to run(IWorkspaceRunnable, Resources[]). The IReferencePointManager is only needed in the eclipse implementation of IWorkspace

--- core/src/saros/filesystem/IWorkspace.java
+++ core/src/saros/filesystem/IWorkspace.java
@@ -23,6 +23,24 @@ public interface IWorkspace {
   /**
    * Executes the given runnable at the next opportunity. If supported parts of the workspace will
    * be locked while the operation takes place. The parts to be lock are determined by the <code>
+   * resources</code> argument, i.e the locked regions will start with the given reference point and
+   * cascade down to all children of each resource. If the <code>resources</code> argument is <code>
+   * null</code> this call is equivalent to {@linkplain #run(IWorkspaceRunnable)}.
+   *
+   * @param runnable
+   * @param referencePoints
+   * @throws IOException
+   * @throws OperationCanceledException
+   */
+  public void run(
+      IWorkspaceRunnable runnable,
+      IReferencePoint[] referencePoints,
+      IReferencePointManager referencePointManager)
+      throws IOException, OperationCanceledException;
+
+  /**
+   * Executes the given runnable at the next opportunity. If supported parts of the workspace will
+   * be locked while the operation takes place. The parts to be lock are determined by the <code>
    * resources</code> argument, i.e the locked regions will start with the given resources and
    * cascade down to all children of each resource. If the <code>resources</code> argument is <code>
    * null</code> this call is equivalent to {@linkplain #run(IWorkspaceRunnable)}.
