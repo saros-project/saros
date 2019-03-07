05ccb0c1c12bd96585f312d1a4d738971106dae5
Abdullah Barhoum <ka70911@gmail.com>
2019-03-07 23:22:52 +0100

    Move eslint format check into the travis format checking stage (#458)

---
+++ travis/script/lint/lint_js.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+project_root_dir=$1
+linting_script=$project_root_dir/travis/script/lint/lint.js
+
+echo "Linting the HTML GUI"
+node $linting_script $project_root_dir/ui.frontend/html
+
+echo "Linting the HTML Whiteboard"
+node $linting_script $project_root_dir/whiteboard/frontend
