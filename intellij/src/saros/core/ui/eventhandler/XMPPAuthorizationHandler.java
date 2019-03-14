c95170f8829687e3fa8a551c5c74041564891c61
Tobias Bouschen <tobias.bouschen@googlemail.com>
2019-03-15 00:39:42 +0100

    [INTERNAL][I] Use UIProjectUtils in XMPPAuthorizationHandler
    Replaces the usage of the project object contained in the session
    context by using UIProjectUtils.runWithProject(...) to obtain a valid
    project object.
    
    Subsequently removes DialogUtils.oldShowConfirm(...) as it is no longer
    used.

--- intellij/src/saros/core/ui/eventhandler/XMPPAuthorizationHandler.java
+++ intellij/src/saros/core/ui/eventhandler/XMPPAuthorizationHandler.java
@@ -1,10 +1,12 @@
 package saros.core.ui.eventhandler;
 
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
 import java.text.MessageFormat;
 import org.apache.log4j.Logger;
 import saros.intellij.ui.Messages;
 import saros.intellij.ui.util.DialogUtils;
+import saros.intellij.ui.util.UIProjectUtils;
 import saros.net.xmpp.JID;
 import saros.net.xmpp.subscription.SubscriptionHandler;
 import saros.net.xmpp.subscription.SubscriptionListener;
@@ -13,34 +15,42 @@ import saros.net.xmpp.subscription.SubscriptionListener;
 public class XMPPAuthorizationHandler {
 
   private static final Logger LOG = Logger.getLogger(XMPPAuthorizationHandler.class);
+
+  private final UIProjectUtils projectUtils;
+
   private final SubscriptionListener subscriptionListener =
       new SubscriptionListener() {
 
         @Override
         public void subscriptionRequestReceived(final JID jid) {
 
-          ApplicationManager.getApplication()
-              .invokeLater(
-                  new Runnable() {
-                    @Override
-                    public void run() {
-                      handleAuthorizationRequest(jid);
-                    }
-                  });
+          projectUtils.runWithProject(
+              project ->
+                  ApplicationManager.getApplication()
+                      .invokeLater(
+                          new Runnable() {
+                            @Override
+                            public void run() {
+                              handleAuthorizationRequest(project, jid);
+                            }
+                          }));
         }
       };
   private final SubscriptionHandler subscriptionHandler;
 
-  public XMPPAuthorizationHandler(final SubscriptionHandler subscriptionHandler) {
+  public XMPPAuthorizationHandler(
+      final SubscriptionHandler subscriptionHandler, UIProjectUtils projectUtils) {
     this.subscriptionHandler = subscriptionHandler;
     this.subscriptionHandler.addSubscriptionListener(subscriptionListener);
+
+    this.projectUtils = projectUtils;
   }
 
-  private void handleAuthorizationRequest(final JID jid) {
+  private void handleAuthorizationRequest(Project project, final JID jid) {
 
     boolean accept =
-        DialogUtils.oldShowConfirm(
-            null,
+        DialogUtils.showConfirm(
+            project,
             Messages.SubscriptionManager_incoming_subscription_request_title,
             MessageFormat.format(
                 Messages.SubscriptionManager_incoming_subscription_request_message,
