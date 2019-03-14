ce737f2fc10cdf73f0bf4e06e3bc3ff85f030a37
Tobias Bouschen <tobias.bouschen@googlemail.com>
2019-03-15 00:53:14 +0100

    [INTERNAL][I] Use UIProjectUtils in IntelliJDialogManager
    Uses UIProjectUtils in IntelliJDialogManager to set the correct position
    of the created dialog without using the project object contained in the
    plugin context.

--- intellij/src/saros/intellij/ui/swt_browser/IntelliJDialogManager.java
+++ intellij/src/saros/intellij/ui/swt_browser/IntelliJDialogManager.java
@@ -1,11 +1,10 @@
 package saros.intellij.ui.swt_browser;
 
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.WindowManager;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
+import saros.intellij.ui.util.UIProjectUtils;
 import saros.synchronize.UISynchronizer;
 import saros.ui.ide_embedding.DialogManager;
 import saros.ui.ide_embedding.IBrowserDialog;
@@ -14,18 +13,21 @@ import saros.ui.pages.IBrowserPage;
 /** Implements the dialog manager for the IntelliJ platform. */
 public class IntelliJDialogManager extends DialogManager {
 
-  private Project project;
+  private UIProjectUtils projectUtils;
 
-  public IntelliJDialogManager(UISynchronizer uiSynchronizer, Project project) {
+  public IntelliJDialogManager(UISynchronizer uiSynchronizer, UIProjectUtils projectUtils) {
     super(uiSynchronizer);
 
-    this.project = project;
+    this.projectUtils = projectUtils;
   }
 
   @Override
   protected IBrowserDialog createDialog(final IBrowserPage startPage) {
-    JFrame parent = WindowManager.getInstance().getFrame(project);
-    JDialog jDialog = new JDialog(parent);
+    JDialog jDialog = new JDialog();
+
+    projectUtils.runWithProject(
+        project -> jDialog.setLocationRelativeTo(WindowManager.getInstance().getFrame(project)));
+
     jDialog.addWindowListener(
         new WindowAdapter() {
           @Override
