443a1dda5b5bb8a67c97ebb723331533d3ae631e
Abdullah Barhoum <ka70911@gmail.com>
2019-03-19 18:57:13 +0100

    add context menu

--- ui.frontend/html/src/components/MainView/ActiveAccount.jsx
+++ ui.frontend/html/src/components/MainView/ActiveAccount.jsx
@@ -5,14 +5,14 @@ export default class ActiveAccount extends React.Component {
   render () {
     const { activeAccount } = this.props
     let accountText
-    let className
+    let className = 'activeAccount '
 
     if (activeAccount && activeAccount.username && activeAccount.domain) {
       accountText = <span>{ activeAccount.username + '@' + activeAccount.domain }</span>
-      className = 'bg-primary text-white'
+      className += 'bg-primary text-white'
     } else {
       accountText = <Text message='message.noAccount' />
-      className = 'bg-info'
+      className += 'bg-info'
     }
 
     return (
