464fd694b3ff55176b67c248f50403798d1e0a22
Abdullah Barhoum <ka70911@gmail.com>
2019-03-26 16:41:46 +0100

    [INTERNAL][HTML] Use React-Bootstrap in AddAccountView

--- ui.frontend/html/src/components/AddContactView/index.jsx
+++ ui.frontend/html/src/components/AddContactView/index.jsx
@@ -2,7 +2,8 @@ import { Text } from 'react-localize'
 import { action, observable } from 'mobx'
 import { inject, observer } from 'mobx-react'
 import React from 'react'
-
+import { Form, Row, Col, Button, ButtonGroup } from 'react-bootstrap';
+import './style.css'
 
 export default
 @inject('mainUI')
@@ -25,47 +26,41 @@ class AddContactView extends React.Component {
     this.fields[field] = value
   }
 
-  componentDidMount () {
+  componentDidMount() {
     // Expose this view globally to be accessible for Java
     window.view = this
   }
 
-  render () {
+  render() {
     return (
-      <div className='form-horizontal' id='add-contact-form'>
-        <div className='form-group'>
-          <label className='col-sm-2 control-label'>
-            <Text message='label.jid' />
-          </label>
-          <div className='col-sm-10'>
-            <input
-              autoFocus
-              type='text'
-              value={this.fields.jid}
-              onChange={this.onChangeField}
-              name='jid'
-            />
-          </div>
-        </div>
-        <div className='form-group'>
-          <label className='col-sm-2 control-label'>
-            <Text message='label.nickname' />
-          </label>
-          <div className='col-sm-10'>
-            <input type='text' value={this.fields.displayName} onChange={this.onChangeField} name='displayName' />
-          </div>
-        </div>
-        <div className='form-group'>
-          <div className='col-sm-offset-2 col-sm-10'>
-            <button id='cancel-add-contact' onClick={this.props.mainUI.doCancelAddContact} className='cancel-add-contact-btn btn btn-default'>
-              <Text message='action.cancel' />
-            </button>
-            <button id='add-contact' onClick={this.onClickSubmit} className='submit-add-contact btn btn-primary'>
-              <Text message='action.addContact' />
-            </button>
-          </div>
-        </div>
-      </div>
+      <Row id='add-contact-form'>
+        <Col lg={{ offset: 4, span: 4 }}>
+          <Form>
+            <Form.Group>
+              <Form.Label><Text message='label.jid' /></Form.Label>
+              <Form.Control autoFocus type='text' placeholder='Contact JID'
+                value={this.fields.jid} onChange={this.onChangeField} name='jid' />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label><Text message='label.nickname' /></Form.Label>
+              <Form.Control type='text' placeholder='Nickname'
+                value={this.fields.displayName} onChange={this.onChangeField} name='displayName' />
+            </Form.Group>
+
+            <Form.Group className="d-flex flex-column">
+              <ButtonGroup >
+                <Button variant='light' id='cancel-add-contact' onClick={this.props.mainUI.doCancelAddContact}>
+                  <Text message='action.cancel' />
+                </Button>
+                <Button variant='primary' id='add-contact' onClick={this.onClickSubmit}>
+                  <Text message='action.addContact' />
+                </Button>
+              </ButtonGroup>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
     )
   }
 }
