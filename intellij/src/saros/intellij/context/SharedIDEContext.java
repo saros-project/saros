237fdf41bb8a223ca643d38cdceaf32420cc2e7e
Tobias Bouschen <tobias.bouschen@googlemail.com>
2019-06-08 01:20:05 +0200

    [INTERNAL][I] Add IDE context workaround for the project initialization
    Adjusts SharedIDEContext to use the project object contained in the
    plugin context. This is needed while the correct project object is not
    initialized by the components starting the session.

--- intellij/src/saros/intellij/context/SharedIDEContext.java
+++ intellij/src/saros/intellij/context/SharedIDEContext.java
@@ -36,13 +36,17 @@ public class SharedIDEContext implements Disposable {
 
   public SharedIDEContext(
       ApplicationEventHandlersFactory applicationEventHandlersFactory,
-      ProjectEventHandlersFactory projectEventHandlersFactory) {
+      ProjectEventHandlersFactory projectEventHandlersFactory,
+      Project project) {
 
     this.applicationEventHandlersFactory = applicationEventHandlersFactory;
     this.projectEventHandlersFactory = projectEventHandlersFactory;
 
     if (preregisteredProject != null) {
       startProjectComponents(preregisteredProject);
+    } else {
+      // TODO remove this workaround once the shared project object is correctly initialized
+      startProjectComponents(project);
     }
   }
 
