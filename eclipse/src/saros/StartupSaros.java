bb8729d5818c98698faf3f4a4179e25f281eccd7
Stefan Rossbach <srossbach@arcor.de>
2019-08-06 22:41:04 +0200

    [UI][E] Show configuration wizard on startup
    The URL was already dead. Also this web page gives no hint on how to
    open this wizard.

--- eclipse/src/saros/StartupSaros.java
+++ eclipse/src/saros/StartupSaros.java
@@ -1,23 +1,18 @@
 package saros;
 
 import org.apache.log4j.Logger;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.intro.IIntroManager;
 import org.eclipse.ui.intro.IIntroPart;
 import saros.account.XMPPAccountStore;
-import saros.annotations.Component;
-import saros.communication.connection.ConnectionHandler;
-import saros.context.IContainerContext;
 import saros.feedback.FeedbackPreferences;
 import saros.preferences.Preferences;
 import saros.repackaged.picocontainer.annotations.Inject;
-import saros.ui.commandHandlers.GettingStartedHandler;
 import saros.ui.util.SWTUtils;
 import saros.ui.util.ViewUtils;
+import saros.ui.util.WizardUtils;
 import saros.ui.util.XMPPConnectionSupport;
 
 /**
@@ -25,19 +20,12 @@ import saros.ui.util.XMPPConnectionSupport;
  * started.
  *
  * <p>{@link #earlyStartup()} is called after the workbench is initialized.
- *
- * @author Lisa Dohrmann, Sandor Szücs, Stefan Rossbach
  */
-@Component(module = "integration")
 public class StartupSaros implements IStartup {
 
-  private static final Logger LOG = Logger.getLogger(StartupSaros.class);
-
-  @Inject private IContainerContext context;
+  private static final Logger log = Logger.getLogger(StartupSaros.class);
 
   @Inject private IPreferenceStore preferenceStore;
-
-  @Inject private ConnectionHandler connectionHandler;
 
   @Inject private Preferences preferences;
 
@@ -63,65 +51,50 @@ public class StartupSaros implements IStartup {
 
     FeedbackPreferences.applyDefaults(preferenceStore);
 
-    if (xmppAccountStore.isEmpty()) showSarosView();
+    if (xmppAccountStore.isEmpty()) SWTUtils.runSafeSWTAsync(log, StartupSaros::openSarosView);
 
     Integer testmode = Integer.getInteger("saros.testmode");
 
-    if (testmode == null) {
-      /*
-       * Only show configuration wizard if no accounts are configured. If
-       * Saros is already configured, do not show the tutorial because the
-       * user is probably already experienced.
-       */
+    if (testmode != null) return;
 
-      if (xmppAccountStore.isEmpty()) showTutorial();
-      else {
-        /*
-         * HACK workaround for http://sourceforge.net/p/dpp/bugs/782/
-         * Perform connecting after the view is created so that the
-         * necessary GUI elements for the chat have already installed
-         * their listeners.
-         *
-         * FIXME This will not work if the view is not created on
-         * startup !
-         */
+    /*
+     * Only show the Configuration Wizard if no accounts are configured. If
+     * Saros is already configured, do not show the tutorial because the
+     * user is probably already experienced.
+     */
 
-        if (!preferences.isAutoConnecting() || xmppAccountStore.isEmpty()) return;
+    /*
+     *  TODO first display a dialog if the user wants to get some help. Afterwards open this wizard
+     *  and maybe also open a web site with getting started?
+     */
 
-        SWTUtils.runSafeSWTAsync(LOG, () -> XMPPConnectionSupport.getInstance().connect(true));
-      }
+    if (xmppAccountStore.isEmpty()) {
+      SWTUtils.runSafeSWTAsync(log, WizardUtils::openSarosConfigurationWizard);
+      return;
     }
+
+    /*
+     * HACK workaround for http://sourceforge.net/p/dpp/bugs/782/
+     * Perform connecting after the view is created so that the
+     * necessary GUI elements for the chat have already installed
+     * their listeners.
+     *
+     * FIXME This will not work if the view is not created on
+     * startup !
+     */
+
+    if (preferences.isAutoConnecting() && xmppAccountStore.getDefaultAccount() != null)
+      SWTUtils.runSafeSWTAsync(log, () -> XMPPConnectionSupport.getInstance().connect(true));
   }
 
-  private void showTutorial() {
-    SWTUtils.runSafeSWTAsync(
-        LOG,
-        new Runnable() {
-          @Override
-          public void run() {
-            try {
-              new GettingStartedHandler().execute(new ExecutionEvent());
-            } catch (ExecutionException e) {
-              LOG.warn("failed to execute tutorial handler", e);
-            }
-          }
-        });
-  }
+  private static void openSarosView() {
 
-  private void showSarosView() {
-    SWTUtils.runSafeSWTAsync(
-        LOG,
-        new Runnable() {
-          @Override
-          public void run() {
-            IIntroManager m = PlatformUI.getWorkbench().getIntroManager();
-            IIntroPart i = m.getIntro();
-            /*
-             * if there is a welcome screen, don't activate the SarosView
-             * because it would be maximized and hiding the workbench window
-             */
-            if (i == null) ViewUtils.openSarosView();
-          }
-        });
+    IIntroManager m = PlatformUI.getWorkbench().getIntroManager();
+    IIntroPart i = m.getIntro();
+    /*
+     * if there is a welcome screen, do not open the SarosView
+     * because it would be maximized and hiding the workbench window.
+     */
+    if (i == null) ViewUtils.openSarosView();
   }
 }
