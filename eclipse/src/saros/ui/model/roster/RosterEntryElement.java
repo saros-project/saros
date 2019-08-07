cd51010e13484b0349ba2a92c23cf471e9a1746a
Stefan Moll <stefan@stefaus.de>
2019-08-07 07:53:40 +0200

    [E] Replace Roster logic with XMPPContactsService
    Replace some XMPP logic in the Eclipse saros.ui.model.roster package and
    classes referencing these provided information with XMPPContactsService.

--- eclipse/src/saros/ui/model/roster/RosterEntryElement.java
+++ eclipse/src/saros/ui/model/roster/RosterEntryElement.java
@@ -1,84 +1,50 @@
 package saros.ui.model.roster;
 
+import java.util.Objects;
 import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.graphics.Image;
-import org.jivesoftware.smack.Roster;
-import org.jivesoftware.smack.RosterEntry;
-import org.jivesoftware.smack.packet.Presence;
-import org.jivesoftware.smack.packet.Presence.Mode;
-import org.jivesoftware.smack.packet.RosterPacket;
-import org.jivesoftware.smack.packet.RosterPacket.ItemType;
-import saros.net.util.XMPPUtils;
 import saros.net.xmpp.JID;
+import saros.net.xmpp.contact.ContactStatus;
+import saros.net.xmpp.contact.XMPPContact;
 import saros.ui.ImageManager;
 import saros.ui.Messages;
 import saros.ui.model.ITreeElement;
 import saros.ui.model.TreeElement;
 
-/**
- * Wrapper for {@link RosterEntryElement RosterEntryElements} in use with {@link Viewer Viewers}
- *
- * @author bkahlert
- */
+/** Wrapper for {@link XMPPContact XMPPContacts} in use with {@link Viewer Viewers} */
 public class RosterEntryElement extends TreeElement {
 
-  private final Roster roster;
-  private final JID jid;
+  private final XMPPContact contact;
 
-  private final boolean hasSarosSupport;
-
-  public RosterEntryElement(Roster roster, JID jid, boolean hasSarosSupport) {
-
-    this.roster = roster;
-    this.jid = jid;
-    this.hasSarosSupport = hasSarosSupport;
-  }
-
-  protected RosterEntry getRosterEntry() {
-    if (roster == null) return null;
-
-    return roster.getEntry(jid.getBase());
+  public RosterEntryElement(XMPPContact contact) {
+    this.contact = contact;
   }
 
   @Override
   public StyledString getStyledText() {
-    StyledString styledString = new StyledString();
+    StyledString styledString = new StyledString(contact.getDisplayableName());
 
-    final RosterEntry rosterEntry = getRosterEntry();
-
-    if (rosterEntry == null) {
-      styledString.append(jid.toString());
-      return styledString;
-    }
-
-    styledString.append(XMPPUtils.getDisplayableName(rosterEntry));
-
-    final Presence presence = roster.getPresence(jid.getBase());
-
-    if (rosterEntry.getStatus() == RosterPacket.ItemStatus.SUBSCRIPTION_PENDING) {
-
-      styledString
-          .append(" ")
-          .append( //$NON-NLS-1$
-              Messages.RosterEntryElement_subscription_pending, StyledString.COUNTER_STYLER);
-
-    } else if (rosterEntry.getType() == ItemType.none || rosterEntry.getType() == ItemType.from) {
-
-      /*
-       * see http://xmpp.org/rfcs/rfc3921.html chapter 8.2.1, 8.3.1 and
-       * 8.6
-       */
-
-      styledString
-          .append(" ")
-          .append( //$NON-NLS-1$
-              Messages.RosterEntryElement_subscription_canceled, StyledString.COUNTER_STYLER);
-
-    } else if (presence.isAway()) {
-      styledString.append(
-          " (" + presence.getMode() + ")", // $NON-NLS-1$ //$NON-NLS-2$
-          StyledString.COUNTER_STYLER);
+    ContactStatus status = contact.getStatus();
+    switch (status.getType()) {
+      case SUBSCRIPTION_CANCELED:
+        styledString
+            .append(" ")
+            .append(Messages.RosterEntryElement_subscription_canceled, StyledString.COUNTER_STYLER);
+        break;
+      case SUBSCRIPTION_PENDING:
+        styledString
+            .append(" ")
+            .append(Messages.RosterEntryElement_subscription_pending, StyledString.COUNTER_STYLER);
+        break;
+      case AWAY:
+        status
+            .getMessage()
+            .ifPresent(
+                message -> styledString.append(" (" + message + ")", StyledString.COUNTER_STYLER));
+        break;
+      default:
+        break;
     }
 
     return styledString;
@@ -86,48 +52,35 @@ public class RosterEntryElement extends TreeElement {
 
   @Override
   public Image getImage() {
-    if (roster == null) return null;
-
-    final Presence presence = roster.getPresence(jid.getBase());
-    boolean sarosSupported = isSarosSupported();
-
-    if (!presence.isAvailable()) return ImageManager.ICON_CONTACT_OFFLINE;
-
-    Mode mode = presence.getMode();
-
-    if (mode == null)
-      // see Presence#getMode();
-      mode = Mode.available;
-
-    // TODO add icons for different modes
-    switch (mode) {
-      case away:
-        // $FALL-THROUGH$
-      case dnd:
-        // $FALL-THROUGH$
-      case xa:
-        return sarosSupported ? ImageManager.ICON_USER_SAROS_AWAY : ImageManager.ICON_CONTACT_AWAY;
-      case chat:
-        // $FALL-THROUGH$
-      case available:
-        // $FALL-THROUGH$
+    switch (contact.getStatus().getType()) {
+      case AVAILABLE:
+        return isSarosSupported()
+            ? ImageManager.ICON_CONTACT_SAROS_SUPPORT
+            : ImageManager.ICON_CONTACT;
+      case AWAY:
+        return isSarosSupported()
+            ? ImageManager.ICON_USER_SAROS_AWAY
+            : ImageManager.ICON_CONTACT_AWAY;
       default:
-        return sarosSupported ? ImageManager.ICON_CONTACT_SAROS_SUPPORT : ImageManager.ICON_CONTACT;
+        return ImageManager.ICON_CONTACT_OFFLINE;
     }
   }
 
   public boolean isOnline() {
-    if (roster == null) return false;
-
-    return roster.getPresence(jid.getBase()).isAvailable();
+    return contact.getStatus().isOnline();
   }
 
+  @Deprecated
   public JID getJID() {
-    return jid;
+    return contact.getBareJid();
+  }
+
+  public XMPPContact getContact() {
+    return contact;
   }
 
   public boolean isSarosSupported() {
-    return hasSarosSupport;
+    return contact.hasSarosSupport();
   }
 
   @Override
@@ -137,20 +90,20 @@ public class RosterEntryElement extends TreeElement {
 
   @Override
   public boolean equals(Object obj) {
-    if (obj == this) {
-      return true;
-    }
-
-    if (!(obj instanceof RosterEntryElement)) {
-      return false;
-    }
-
-    RosterEntryElement rosterEntryElement = (RosterEntryElement) obj;
-    return (jid == null ? rosterEntryElement.jid == null : jid.equals(rosterEntryElement.jid));
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (!(obj instanceof RosterEntryElement)) return false;
+    RosterEntryElement other = (RosterEntryElement) obj;
+    return Objects.equals(contact, other.contact);
   }
 
   @Override
   public int hashCode() {
-    return (jid != null) ? jid.hashCode() : 0;
+    return contact.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return "RosterEntryElement [contact=" + contact + "]";
   }
 }
