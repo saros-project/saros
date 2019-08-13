b92d1024c4d9c12d9d5a69a3085fbea6e099b6b6
Stefan Rossbach <srossbach@arcor.de>
2019-08-13 16:12:25 +0200

    [INTERNAL] address broken links in package-info files

--- core/src/saros/concurrent/management/package-info.java
+++ core/src/saros/concurrent/management/package-info.java
@@ -3,13 +3,15 @@
  *
  * <h1>Management Overview</h1>
  *
- * The Management package manages {@link JupiterServer} and {@link JupiterClient} instances and all
- * Jupiter activities. Local Operations on each client site will be transformed into
- * JupiterActivities (by {@link ConcurrentDocumentClient}) and sent to the host ({@link
- * ConcurrentDocumentServer}). The JupiterActivity will be transformed into its corresponding
- * operation and sent to all clients. This process has to be atomic and may not be interrupted.
+ * The Management package manages {@link saros.concurrent.management.JupiterServer} and {@link
+ * saros.concurrent.management.JupiterClient} instances and all Jupiter activities. Local Operations
+ * on each client site will be transformed into JupiterActivities (by {@link
+ * saros.concurrent.management.ConcurrentDocumentClient}) and sent to the host ({@link
+ * saros.concurrent.management.ConcurrentDocumentServer}). The JupiterActivity will be transformed
+ * into its corresponding operation and sent to all clients. This process has to be atomic and may
+ * not be interrupted.
  *
- * <p>The {@link ConcurrentDocumentServer} exists only on the host site of a Saros session, even
- * when a client adds resources to this session.
+ * <p>The {@link saros.concurrent.management.ConcurrentDocumentServer} exists only on the host site
+ * of a Saros session, even when a client adds resources to this session.
  */
 package saros.concurrent.management;
--- eclipse/src/saros/feedback/package-info.java
+++ eclipse/src/saros/feedback/package-info.java
@@ -4,49 +4,47 @@
  * <h1>Feedback Overview</h1>
  *
  * is responsible for the feedback and statistic processes. The main-classes are {@link
- * FeedbackManager} and {@link StatisticManager }
+ * saros.feedback.FeedbackManager} and {@link saros.feedback.StatisticManager }
  *
  * <p>This package comprises of the following subpackages:
  *
  * <ul>
- *   <li>the {@link AbstractFeedbackManager} is the parent class for FeedbackManager and
- *       StatisticManager.
- *   <li>the {@link AbstractStatisticCollector} is the parent class for all kind of statistic
- *       collectors to get information and send them to the {@link StatisticManager}.
- *   <li>the {@link DataTransferCollector} collects information of data transfers (e.g. time, size,
- *       mode)
- *   <li>the {@link ErrorLogManager} upload error logs to the server if the user accepted it.
- *   <li>the {@link FeedbackManager} is a listener to wait for the end of a session and then to ask
- *       the user for feedback using the feedback dialog.
- *   <li>the {@link FileSubmitter} has the basic functions to upload files for feedback to our
- *       server.
- *   <li>the {@link FollowModeCollector} counts amount of times the user enters follow mode and how
- *       long he stays in it.
- *   <li>the {@link JumpFeatureUsageCollector} counts amount of times the user jumps to another
- *       participant and distinguishes between just {@link User.Permission#READONLY_ACCESS} or
- *       {@link User.Permission#WRITE_ACCESS}.
- *   <li>the {@link Messages} is used for access to messages.properties
- *   <li>the {@link ParticipantCollector} collects information about user behavior of participants
- *       in a session.
- *   <li>the {@link PermissionChangeCollector} collects the permission changes and the time each
- *       user spends in each state.
- *   <li>the {@link SelectionCollector} collects the count of selections made by remote users with
- *       {@link User.Permission#READONLY_ACCESS} and also the selection was modified later.
- *   <li>the {@link SessionDataCollector} collects information about the session ID, session time
- *       and the amount of total saros sessions.
- *   <li>the {@link SessionStatistic} creates a properties file with all information of the
- *       collectors and saves it to disc.
- *   <li>the {@link StatisticManager} starts the collectors when a session is initiated. At the end
- *       of the session, the Manager starts {@link SessionStatistic } to write a summary file and
- *       let it upload to server.
- *   <li>the {@link StatisticAggregator} brings together several statistic files independent from
- *       their sessions to one big file.
- *   <li>the {@link TextEditCollector} counts the number of characters typed by the user and also
- *       evaluates if copy & paste was used.
- *   <li>the {@link VOIPCollector} collects the amount of usages of VoIP and also the spent time in
- *       a connection.
- *   <li>the {@link messages.properties} is a mapping of strings to variables which can be called by
- *       {@link Messages}
+ *   <li>the {@link saros.feedback.AbstractFeedbackManager} is the parent class for FeedbackManager
+ *       and StatisticManager.
+ *   <li>the {@link saros.feedback.AbstractStatisticCollector} is the parent class for all kind of
+ *       statistic collectors to get information and send them to the {@link
+ *       saros.feedback.StatisticManager}.
+ *   <li>the {@link saros.feedback.DataTransferCollector} collects information of data transfers
+ *       (e.g. time, size, mode)
+ *   <li>the {@link saros.feedback.ErrorLogManager} upload error logs to the server if the user
+ *       accepted it.
+ *   <li>the {@link saros.feedback.FeedbackManager} is a listener to wait for the end of a session
+ *       and then to ask the user for feedback using the feedback dialog.
+ *   <li>the {@link saros.feedback.FileSubmitter} has the basic functions to upload files for
+ *       feedback to our server.
+ *   <li>the {@link saros.feedback.FollowModeCollector} counts amount of times the user enters
+ *       follow mode and how long he stays in it.
+ *   <li>the {@link saros.feedback.JumpFeatureUsageCollector} counts amount of times the user jumps
+ *       to another participant and distinguishes between just {@link
+ *       saros.session.User.Permission#READONLY_ACCESS} or {@link
+ *       saros.session.User.Permission#WRITE_ACCESS}.
+ *   <li>the {@link saros.feedback.Messages} is used for access to messages.properties
+ *   <li>the {@link saros.feedback.ParticipantCollector} collects information about user behavior of
+ *       participants in a session.
+ *   <li>the {@link saros.feedback.PermissionChangeCollector} collects the permission changes and
+ *       the time each user spends in each state.
+ *   <li>the {@link saros.feedback.SelectionCollector} collects the count of selections made by
+ *       remote users with {@link saros.session.User.Permission#READONLY_ACCESS} and also the
+ *       selection was modified later.
+ *   <li>the {@link saros.feedback.SessionDataCollector} collects information about the session ID,
+ *       session time and the amount of total Saros sessions.
+ *   <li>the {@link saros.feedback.SessionStatistic} creates a properties file with all information
+ *       of the collectors and saves it to disc.
+ *   <li>the {@link saros.feedback.StatisticManager} starts the collectors when a session is
+ *       initiated. At the end of the session, the Manager starts {@link
+ *       saros.feedback.SessionStatistic } to write a summary file and let it upload to server.
+ *   <li>the {@link saros.feedback.TextEditCollector} counts the number of characters typed by the
+ *       user and also evaluates if copy & paste was used.
  * </ul>
  */
 package saros.feedback;
