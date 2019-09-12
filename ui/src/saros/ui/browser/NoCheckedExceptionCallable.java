76fb2343d58007b40f5da322871e846ddf38ea20
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2019-09-12 14:59:01 +0200

    [HTML] Remove ui project

--- ui/src/saros/ui/browser/NoCheckedExceptionCallable.java
+++
@@ -1,9 +0,0 @@
-package saros.ui.browser;
-
-import java.util.concurrent.Callable;
-
-// Adopted from https://github.com/ag-se/swt-browser-improved
-public interface NoCheckedExceptionCallable<V> extends Callable<V> {
-  @Override
-  V call();
-}
