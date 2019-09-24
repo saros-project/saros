899144bfa113192aaf2cc6fcc884da0dee5a09ef
Michael Schäfer <mschaefer@posteo.de>
2019-09-24 19:54:16 +0200

    [FEATURE][VSC] Add basic language client functionality
    The Saros implementation will be based on the Language Server Protocol
    (see https://microsoft.github.io/language-server-protocol) for data exchange.
    In order to enable VS Code to understand that protocol the VS Code language
    client has been added. Furthermore starting of the Saros LSP Server
    has been encapsulated.

---
+++ vscode/src/extension.ts
@@ -0,0 +1,51 @@
+import * as vscode from 'vscode';
+import { Disposable } from 'vscode-jsonrpc';
+import { sarosExtensionInstance } from './core/saros-extension';
+import { activateAccounts } from './account/activator';
+
+/**
+ * Activation function of the extension.
+ *
+ * @export
+ * @param {vscode.ExtensionContext} context - The extension context
+ */
+export function activate(context: vscode.ExtensionContext) {
+
+	sarosExtensionInstance.setContext(context)
+						.init()
+						.then(() => {
+							activateAccounts(sarosExtensionInstance);
+
+							console.log('Extension "Saros" is now active!');
+						})
+						.catch(reason => {
+							vscode.window.showErrorMessage('Saros extension did not start propertly.'
+														+ 'Reason: ' + reason); //TODO: restart feature
+						});	
+	
+	context.subscriptions.push(createStatusBar());
+}
+
+/**
+ * Creates the status bar.
+ *
+ * @returns {Disposable} The status bar item as [disposable](#Disposable)
+ */
+function createStatusBar(): Disposable {
+
+	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, Number.MAX_VALUE);
+    statusBarItem.text = "Saros";
+    statusBarItem.command = "saros.start";
+	statusBarItem.show();
+	
+	return statusBarItem;
+}
+
+/**
+ * Deactivation function of the extension.
+ *
+ * @export
+ */
+export function deactivate() {
+	console.log("deactivated");
+}
