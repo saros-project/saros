3ab5a773352c02a7f3c591775c3ac42143006e28
Michael Schäfer <mschaefer@posteo.de>
2019-09-24 19:54:16 +0200

    [FEATURE][VSC] Add basic language client functionality
    The Saros implementation will be based on the Language Server Protocol
    (see https://microsoft.github.io/language-server-protocol) for data exchange.
    In order to enable VS Code to understand that protocol the VS Code language
    client has been added. Furthermore starting of the Saros LSP Server
    has been encapsulated.

---
+++ vscode/src/account/activator.ts
@@ -0,0 +1,20 @@
+import { SarosExtension } from "../core/saros-extension";
+import { commands } from "vscode";
+
+/**
+ * Activation function of the account module.
+ *
+ * @export
+ * @param {SarosExtension} extension - The instance of the extension
+ */
+export function activateAccounts(extension: SarosExtension) {
+    commands.registerCommand('saros.account.add', () => {
+        extension.onReady()
+        .then(resolve => {
+            return extension.client.addAccount('micha@jabber.com');
+        })
+        .then(r => {
+            console.log('Response was: ' + r.Response);
+        });
+    });
+}
