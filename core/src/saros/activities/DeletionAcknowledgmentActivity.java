25b53e8a1993bd8e8456acbcc05ca3f034546101
Tobias Bouschen <tobias.bouschen@googlemail.com>
2019-10-10 16:54:26 +0200

    [INTERNAL][CORE] Add deletion acknowledgments
    Adds DeletionAcknowledgmentActivity, which can be used to acknowledge
    that a file was successfully deleted locally as a reaction to a received
    activity. This acknowledgment allows us to drop the activity filter for
    the deleted resource once all other session participants have
    acknowledged that they received the deletion.
    
    Adds the DeletionAcknowledgmentDispatcher to automatically dispatch an
    acknowledgment when an activity is received that contains a file
    deletion.

---
+++ core/src/saros/activities/DeletionAcknowledgmentActivity.java
@@ -0,0 +1,28 @@
+package saros.activities;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import saros.session.User;
+import saros.session.internal.DeletionAcknowledgmentDispatcher;
+
+/**
+ * Activity for notifying other participants that a file was successfully deleted locally.
+ *
+ * @see DeletionAcknowledgmentDispatcher
+ */
+@XStreamAlias("deletionAcknowledgementActivity")
+public class DeletionAcknowledgmentActivity extends AbstractResourceActivity {
+
+  public DeletionAcknowledgmentActivity(User user, SPath resource) {
+    super(user, resource);
+  }
+
+  @Override
+  public void dispatch(IActivityReceiver receiver) {
+    receiver.receive(this);
+  }
+
+  @Override
+  public String toString() {
+    return this.getClass().getSimpleName() + " : " + getSource() + " - " + getPath();
+  }
+}
