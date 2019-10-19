bea471ea111eb3d04dfc405bb4470f98f7033ccd
Michael Schäfer <mschaefer@posteo.de>
2019-10-19 04:13:26 +0200

    [FEATURE][VSC] Add basic language server functionality
    This Saros Server implementation is now based on the Language Server
    Protocol (see https://microsoft.github.io/language-server-protocol).
    The bound Saros Server is currently detached from the LSP Server and
    will be reattached upon further commits. The LSP implementation is
    pretty basic and the "Hello Message" will be removed eventually.

---
+++ vscode.java/src/saros/lsp/SarosLanguageServer.java
@@ -0,0 +1,69 @@
+package saros.lsp;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.lsp4j.InitializeParams;
+import org.eclipse.lsp4j.InitializeResult;
+import org.eclipse.lsp4j.MessageParams;
+import org.eclipse.lsp4j.MessageType;
+import org.eclipse.lsp4j.ServerCapabilities;
+import org.eclipse.lsp4j.services.LanguageClient;
+import org.eclipse.lsp4j.services.LanguageClientAware;
+import org.eclipse.lsp4j.services.LanguageServer;
+import org.eclipse.lsp4j.services.TextDocumentService;
+import org.eclipse.lsp4j.services.WorkspaceService;
+
+import saros.lsp.service.SarosDocumentService;
+import saros.lsp.service.SarosWorkspaceService;
+
+public class SarosLanguageServer implements LanguageServer, LanguageClientAware {
+
+    private LanguageClient languageClient;
+
+    public void sendHello() {
+                
+        MessageParams mp = new MessageParams();
+        mp.setMessage("Saros Server connection established.");
+        mp.setType(MessageType.Info);
+        
+        this.languageClient.showMessage(mp);
+    }
+
+    @Override
+    public CompletableFuture<InitializeResult> initialize(InitializeParams params) {        
+        return CompletableFuture.completedFuture(new InitializeResult(this.getCapabilities()));
+    }
+
+    private ServerCapabilities getCapabilities() {
+        ServerCapabilities capabilities = new ServerCapabilities();
+        
+        return capabilities;
+    }
+
+    @Override
+    public CompletableFuture<Object> shutdown() {
+        //System.out.println("shutdown");
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @Override
+    public void exit() {
+        //System.out.println("exit");
+    }
+
+    @Override
+    public TextDocumentService getTextDocumentService() {
+        return new SarosDocumentService();
+    }
+
+    @Override
+    public WorkspaceService getWorkspaceService() {
+        return new SarosWorkspaceService();
+    }
+
+    @Override
+    public void connect(LanguageClient client) {
+        this.languageClient = client;
+    }
+
+}
