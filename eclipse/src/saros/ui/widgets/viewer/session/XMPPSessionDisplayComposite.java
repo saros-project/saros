57c47c3e8f1af7dfe2980038b863ce72b770cdaa
Stefan Moll <stefan@stefaus.de>
2020-01-08 11:36:05 +0100

    [E] Remove Unnecessary Hook to Expand Contact List

--- eclipse/src/saros/ui/widgets/viewer/session/XMPPSessionDisplayComposite.java
+++ eclipse/src/saros/ui/widgets/viewer/session/XMPPSessionDisplayComposite.java
@@ -1,11 +1,7 @@
 package saros.ui.widgets.viewer.session;
 
-import org.apache.log4j.Logger;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
-import saros.communication.connection.ConnectionHandler;
-import saros.communication.connection.IConnectionStateListener;
-import saros.net.ConnectionState;
 import saros.net.xmpp.contact.XMPPContactsService;
 import saros.repackaged.picocontainer.annotations.Inject;
 import saros.session.internal.SarosSession;
@@ -15,7 +11,6 @@ import saros.ui.model.roster.RosterContentProvider;
 import saros.ui.model.session.SessionComparator;
 import saros.ui.model.session.SessionContentProvider;
 import saros.ui.model.session.SessionInput;
-import saros.ui.util.SWTUtils;
 
 /**
  * This {@link Composite} displays the {@link SarosSession} and the Contact list in parallel.
@@ -33,33 +28,13 @@ import saros.ui.util.SWTUtils;
  */
 public final class XMPPSessionDisplayComposite extends SessionDisplayComposite {
 
-  private static final Logger LOG = Logger.getLogger(XMPPSessionDisplayComposite.class);
-
-  @Inject private ConnectionHandler connectionHandler;
   @Inject private XMPPContactsService contactsService;
-
-  private final IConnectionStateListener connectionListener =
-      (state, error) -> {
-        if (state == ConnectionState.CONNECTED || state == ConnectionState.NOT_CONNECTED) {
-          SWTUtils.runSafeSWTAsync(
-              LOG,
-              () -> {
-                if (getViewer().getControl().isDisposed()) return;
-
-                updateViewer();
-                getViewer().expandAll();
-              });
-        }
-      };
 
   public XMPPSessionDisplayComposite(Composite parent, int style) {
     super(parent, style);
-    connectionHandler.addConnectionStateListener(connectionListener);
 
     addDisposeListener(
         e -> {
-          connectionHandler.removeConnectionStateListener(connectionListener);
-          connectionHandler = null;
           contactsService = null;
         });
   }
