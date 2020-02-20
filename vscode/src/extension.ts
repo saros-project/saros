756a06c23e5d4ef109511bed042f8127ea3c0972
Michael Schäfer <mschaefer@posteo.de>
2020-02-20 15:04:06 +0100

    [REFACTOR][VSC] Introduce eslint
    Introduce eslint and modify code accordingly.
    Furthermore gradle has been modified in
    order to seperate config and execute statements.

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
-import { Disposable } from 'vscode-jsonrpc';
-import { sarosExtensionInstance } from './core/saros-extension';
-import { activateAccounts } from './account/activator';
+import {sarosExtensionInstance} from './core';
+import {activateAccounts} from './account';
 
 /**
  * Activation function of the extension.
@@ -10,35 +9,36 @@ import { activateAccounts } from './account/activator';
  * @param {vscode.ExtensionContext} context - The extension context
  */
 export function activate(context: vscode.ExtensionContext) {
+  sarosExtensionInstance.setContext(context)
+      .init()
+      .then(() => {
+        activateAccounts(sarosExtensionInstance);
 
-	sarosExtensionInstance.setContext(context)
-						.init()
-						.then(() => {
-							activateAccounts(sarosExtensionInstance);
+        console.log('Extension "Saros" is now active!');
+      })
+      .catch((reason) => {
+        console.log(reason);
+        vscode.window.showErrorMessage(
+            'Saros extension did not start propertly.' +
+            'Reason: ' + reason);
+      });
 
-							console.log('Extension "Saros" is now active!');
-						})
-						.catch(reason => {
-							console.log(reason);
-							vscode.window.showErrorMessage('Saros extension did not start propertly.'
-														+ 'Reason: ' + reason); //TODO: restart feature
-						});	
-	
-	context.subscriptions.push(createStatusBar());
+  context.subscriptions.push(createStatusBar());
 }
 
 /**
  * Creates the status bar.
  *
- * @returns {Disposable} The status bar item as [disposable](#Disposable)
+ * @return {Disposable} The status bar item as [disposable](#Disposable)
  */
-function createStatusBar(): Disposable {
+function createStatusBar(): vscode.Disposable {
+  const statusBarItem =
+    vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left,
+        Number.MAX_VALUE);
+  statusBarItem.text = 'Saros';
+  statusBarItem.show();
 
-	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, Number.MAX_VALUE);
-    statusBarItem.text = "Saros";
-	statusBarItem.show();
-	
-	return statusBarItem;
+  return statusBarItem;
 }
 
 /**
@@ -47,5 +47,5 @@ function createStatusBar(): Disposable {
  * @export
  */
 export function deactivate() {
-	console.log("deactivated");
+  console.log('deactivated');
 }
