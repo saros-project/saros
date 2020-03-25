ea28f18b829490b8f317c913b84c3594106f84d3
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2020-03-25 13:49:39 +0100

    [INTERNAL][E] Update saros urls

--- eclipse/src/saros/ui/wizards/pages/CreateXMPPAccountWizardPage.java
+++ eclipse/src/saros/ui/wizards/pages/CreateXMPPAccountWizardPage.java
@@ -91,7 +91,7 @@ public class CreateXMPPAccountWizardPage extends WizardPage {
     shell.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
     Browser browser = new Browser(shell, SWT.NONE);
-    browser.setUrl("http://www.saros-project.org/setupXMPP");
+    browser.setUrl("https://www.saros-project.org/documentation/setup-xmpp.html");
     browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
     shell.open();
