3e83277f64816795fee425ca766e6cf8581c0fb5
Stefan Moll <stefan@stefaus.de>
2020-04-03 20:31:22 +0200

    [CORE] Delete RosterTracker
    The RosterTracker is not used anymore and replaced by
    XMPPContactsService.

--- core/test/junit/saros/net/TestSuite.java
+++ core/test/junit/saros/net/TestSuite.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({JIDTest.class, RosterTrackerTest.class, UPnPTest.class})
+@Suite.SuiteClasses({JIDTest.class, UPnPTest.class})
 public class TestSuite {
   // the class remains completely empty,
   // being used only as a holder for the above annotations
