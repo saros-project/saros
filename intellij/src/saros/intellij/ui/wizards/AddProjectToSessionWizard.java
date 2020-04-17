saros-project/saros

commit ccf4111e0f1e785737ad9077e528c52fd2bb6d15

[REFACTOR][I] Refactor usage of ProgressMonitorAdapter
Fixed typo in class name.

Move strings shown to user to messages.properties.

Fix switched usage of message and title.

Remove unnecessary usage of UIUtils.

intellij/src/saros/intellij/ui/wizards/AddProjectToSessionWizard.java
@@ -15,7 +15,6 @@ import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.util.ThrowableComputable;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.ui.UIUtil;
 import java.awt.Dimension;
 import java.awt.Window;
 import java.io.FileNotFoundException;
@@ -40,7 +39,7 @@ import saros.intellij.negotiation.ModuleConfigurationInitializer;
 import saros.intellij.runtime.FilesystemRunner;
 import saros.intellij.ui.Messages;
 import saros.intellij.ui.util.NotificationPanel;
-import saros.intellij.ui.widgets.progress.ProgessMonitorAdapter;
+import saros.intellij.ui.widgets.progress.ProgressMonitorAdapter;
 import saros.intellij.ui.wizards.pages.HeaderPanel;
 import saros.intellij.ui.wizards.pages.PageActionListener;
 import saros.intellij.ui.wizards.pages.TextAreaPage;
@@ -575,22 +574,16 @@ public class AddProjectToSessionWizard extends Wizard {
             + (type.equals(NegotiationTools.CancelLocation.LOCAL)
                 ? "locally "
                 : "remotely by " + peer);
-    UIUtil.invokeLaterIfNeeded(
-        new Runnable() {
-          @Override
-          public void run() {
 
-            /*
-             *  if we already triggered the negotiation the message will
-             *  be displayed in the trigger logic, so do not popup another dialog here
-             */
-            if (!triggered)
-              NotificationPanel.showInformation(
-                  message + (errorMsg != null ? "\n\n" + errorMsg : ""), message);
+    /*
+     *  if we already triggered the negotiation the message will
+     *  be displayed in the trigger logic, so do not popup another dialog here
+     */
+    if (!triggered)
+      NotificationPanel.showInformation(
+          message + (errorMsg != null ? "\n\n" + errorMsg : ""), message);
 
-            close();
-          }
-        });
+    close();
   }
 
   /**
@@ -608,31 +601,32 @@ public class AddProjectToSessionWizard extends Wizard {
     ProgressManager.getInstance()
         .run(
             new Task.Backgroundable(
-                project, "Sharing project...", true, PerformInBackgroundOption.DEAF) {
+                project,
+                Messages.AddProjectToSessionWizard_negotiation_progress_title,
+                true,
+                PerformInBackgroundOption.DEAF) {
 
               @Override
-              public void run(ProgressIndicator indicator) {
+              public void run(@NotNull ProgressIndicator indicator) {
                 final ProjectNegotiation.Status status =
-                    negotiation.run(localProjects, new ProgessMonitorAdapter(indicator));
+                    negotiation.run(localProjects, new ProgressMonitorAdapter(indicator));
 
                 indicator.stop();
 
-                UIUtil.invokeLaterIfNeeded(
-                    new Runnable() {
-                      @Override
-                      public void run() {
-                        if (status == ProjectNegotiation.Status.ERROR) {
-                          NotificationPanel.showError(
-                              "Error during project negotiation",
-                              "The project could not be shared: " + negotiation.getErrorMessage());
-                        } else if (status == ProjectNegotiation.Status.OK) {
-                          NotificationPanel.showInformation(
-                              "Project shared", "Project successfully shared");
-                        } else
-                          NotificationPanel.showError(
-                              "Project negotiation aborted", "Project negotiation was canceled");
-                      }
-                    });
+                if (status == ProjectNegotiation.Status.ERROR) {
+                  NotificationPanel.showError(
+                      MessageFormat.format(
+                          Messages.AddProjectToSessionWizard_negotiation_error_message,
+                          negotiation.getErrorMessage()),
+                      Messages.AddProjectToSessionWizard_negotiation_error_title);
+                } else if (status == ProjectNegotiation.Status.OK) {
+                  NotificationPanel.showInformation(
+                      Messages.AddProjectToSessionWizard_negotiation_successful_message,
+                      Messages.AddProjectToSessionWizard_negotiation_successful_title);
+                } else
+                  NotificationPanel.showError(
+                      Messages.AddProjectToSessionWizard_negotiation_aborted_message,
+                      Messages.AddProjectToSessionWizard_negotiation_aborted_title);
               }
             });
 

intellij/src/saros/intellij/ui/Messages.java
@@ -29,6 +29,13 @@ public class Messages {
   public static String AddProjectToSessionWizard_no_session_message;
   public static String AddProjectToSessionWizard_context_teardown_title;
   public static String AddProjectToSessionWizard_context_teardown_message;
+  public static String AddProjectToSessionWizard_negotiation_progress_title;
+  public static String AddProjectToSessionWizard_negotiation_error_title;
+  public static String AddProjectToSessionWizard_negotiation_error_message;
+  public static String AddProjectToSessionWizard_negotiation_successful_title;
+  public static String AddProjectToSessionWizard_negotiation_successful_message;
+  public static String AddProjectToSessionWizard_negotiation_aborted_title;
+  public static String AddProjectToSessionWizard_negotiation_aborted_message;
 
   public static String CollaborationUtils_confirm_closing_title;
   public static String CollaborationUtils_confirm_closing_message;

intellij/src/saros/intellij/ui/messages.properties
@@ -16,6 +16,13 @@ AddProjectToSessionWizard_no_session_title=No Running Session Found – Negotiat
 AddProjectToSessionWizard_no_session_message=The local session no longer seems to be available. This might be due to network issues that caused the session to end prematurely.
 AddProjectToSessionWizard_context_teardown_title=Session No Longer Valid – Negotiation Aborted
 AddProjectToSessionWizard_context_teardown_message=The local session context no longer seems to be valid. Please create a new session and try again.
+AddProjectToSessionWizard_negotiation_progress_title=Sharing Project...
+AddProjectToSessionWizard_negotiation_error_title=Error during project negotiation
+AddProjectToSessionWizard_negotiation_error_message=The project could not be shared: {0}
+AddProjectToSessionWizard_negotiation_successful_title=Project shared
+AddProjectToSessionWizard_negotiation_successful_message=Project successfully shared
+AddProjectToSessionWizard_negotiation_aborted_title=Project negotiation was canceled
+AddProjectToSessionWizard_negotiation_aborted_message=Project negotiation aborted
 
 CollaborationUtils_confirm_closing_title=Confirm Closing Session
 CollaborationUtils_confirm_closing_message=Are you sure that you want to close this Saros session? Since you are the creator of this session, it will be closed for all participants.

intellij/src/saros/intellij/ui/widgets/progress/ProgressMonitorAdapter.java
@@ -6,7 +6,7 @@ import saros.monitoring.IProgressMonitor;
 /**
  * Adapter class for {@link ProgressIndicator} objects to be used as an {@link IProgressMonitor}.
  */
-public final class ProgessMonitorAdapter implements IProgressMonitor {
+public final class ProgressMonitorAdapter implements IProgressMonitor {
 
   private final ProgressIndicator delegate;
 
@@ -17,8 +17,8 @@ public final class ProgessMonitorAdapter implements IProgressMonitor {
 
   private int percentCompleted = 0;
 
-  public ProgessMonitorAdapter(final ProgressIndicator progessIndicator) {
-    delegate = progessIndicator;
+  public ProgressMonitorAdapter(final ProgressIndicator progressIndicator) {
+    delegate = progressIndicator;
   }
 
   @Override
@@ -66,7 +66,7 @@ public final class ProgessMonitorAdapter implements IProgressMonitor {
   public void setCanceled(final boolean canceled) {
     isCanceled = canceled;
 
-    // TODO there is no way to uncancel the progress
+    // TODO there is no way to un-cancel the progress
     if (canceled) delegate.cancel();
   }
 

commit 049ed4b6de1b0e4433520e3c64beaa899aa936e3

[FIX][I] #923 Fix local project negotiation cancellation
Adjusts the cancellation listener in AddProjectToSessionWizard to only
close the dialog if the cancellation was caused by the remote side.

I am not really sure why the issue #923 occurred, but it seems to be a
race condition caused by CancelListener disposing the dialog at an
inconvenient moment.

The issue was introduced in ccf4111e0f1e785737ad9077e528c52fd2bb6d15 by
removing the UIUtil.invokeLaterIfNeeded(...) block around the close()
call in cancelWizard(...).

This problem is now avoided by checking whether the cancellation was
caused locally as such cases are already handled by other logic.

Fixes #923.

intellij/src/saros/intellij/ui/wizards/AddProjectToSessionWizard.java
@@ -54,6 +54,7 @@ import saros.negotiation.FileList;
 import saros.negotiation.FileListDiff;
 import saros.negotiation.FileListFactory;
 import saros.negotiation.NegotiationTools;
+import saros.negotiation.NegotiationTools.CancelLocation;
 import saros.negotiation.ProjectNegotiation;
 import saros.negotiation.ProjectNegotiationData;
 import saros.net.xmpp.JID;
@@ -507,12 +508,13 @@ public class AddProjectToSessionWizard extends Wizard {
         }
       };
 
+  @SuppressWarnings("FieldCanBeLocal")
   private final CancelListener cancelListener =
       new CancelListener() {
 
         @Override
-        public void canceled(final NegotiationTools.CancelLocation location, final String message) {
-          cancelWizard(peer, message, location);
+        public void canceled(final CancelLocation location, final String message) {
+          if (location != CancelLocation.LOCAL) cancelWizard(peer, message, location);
         }
       };
 
@@ -570,7 +572,7 @@ public class AddProjectToSessionWizard extends Wizard {
   public void cancelWizard(
       final JID peer, final String errorMsg, NegotiationTools.CancelLocation type) {
     final String message =
-        "Wizard canceled "
+        "Project negotiation canceled "
             + (type.equals(NegotiationTools.CancelLocation.LOCAL)
                 ? "locally "
                 : "remotely by " + peer);