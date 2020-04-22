d2d8340156bed9aa094e35245a2f010679228e37
Stefan Moll <stefan@stefaus.de>
2020-04-22 15:55:08 +0200

    [CORE] Future Wait Method for IProgressMonitor
    Method to wait for future object completion while checking monitor
    status.

--- core/src/saros/monitoring/IProgressMonitor.java
+++ core/src/saros/monitoring/IProgressMonitor.java
@@ -1,5 +1,8 @@
 package saros.monitoring;
 
+import java.util.concurrent.Future;
+import saros.exceptions.SarosCancellationException;
+
 /**
  * This interface is under development. It currently equals its Eclipse counterpart. If not
  * mentioned otherwise all offered methods are equivalent to their Eclipse counterpart.
@@ -22,4 +25,25 @@ public interface IProgressMonitor {
   public boolean isCanceled();
 
   public void beginTask(String name, int size);
+
+  /**
+   * Wait till the provided future is done, while continuously checking if this monitor is canceled.
+   *
+   * @param future Future to wait for completion
+   * @throws SarosCancellationException when the monitor is cancelled or the thread interrupted
+   */
+  public default void waitForCompletion(Future<?> future) throws SarosCancellationException {
+    try {
+      while (!future.isDone()) {
+        if (isCanceled()) {
+          throw new SarosCancellationException("Canceled waiting.");
+        }
+
+        Thread.sleep(200);
+      }
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      throw new SarosCancellationException("Interrupted waiting.");
+    }
+  }
 }
