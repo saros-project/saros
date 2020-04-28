1039dfadd69df6d31bbec837b015c1d2c169c4e2
Tobias Bouschen <tobias.bouschen@googlemail.com>
2020-04-28 13:37:23 +0200

    [INTERNAL][CORE] Remove usage of getFullPath() from CreateArchiveTask
    Removes the usage of IResource.getFullPath() from CreateArchiveTask by
    unifying the passed lists of files and paths. Since there now always is
    a one-to-one mapping, the error handling requiring the usage of
    getFullPath() can be dropped.

--- core/src/saros/negotiation/ArchiveOutgoingProjectNegotiation.java
+++ core/src/saros/negotiation/ArchiveOutgoingProjectNegotiation.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.log4j.Logger;
 import saros.editor.IEditorManager;
 import saros.exceptions.LocalCancellationException;
@@ -136,8 +138,7 @@ public class ArchiveOutgoingProjectNegotiation extends AbstractOutgoingProjectNe
 
     checkCancellation(CancelOption.NOTIFY_PEER);
 
-    final List<IFile> filesToCompress = new ArrayList<IFile>(fileCount);
-    final List<String> fileAlias = new ArrayList<String>(fileCount);
+    final List<Pair<IFile, String>> filesToCompress = new ArrayList<>(fileCount);
 
     final List<IResource> projectsToLock = new ArrayList<IResource>();
 
@@ -166,11 +167,14 @@ public class ArchiveOutgoingProjectNegotiation extends AbstractOutgoingProjectNe
       final int prefixLength = aliasBuilder.length();
 
       for (final String path : list.getPaths()) {
-
         // assert path is relative !
-        filesToCompress.add(project.getFile(path));
         aliasBuilder.append(path);
-        fileAlias.add(aliasBuilder.toString());
+
+        IFile file = project.getFile(path);
+        String qualifiedPath = aliasBuilder.toString();
+
+        filesToCompress.add(new ImmutablePair<>(file, qualifiedPath));
+
         aliasBuilder.setLength(prefixLength);
       }
     }
@@ -182,7 +186,7 @@ public class ArchiveOutgoingProjectNegotiation extends AbstractOutgoingProjectNe
     try {
       tempArchive = File.createTempFile("saros_" + getID(), ".zip");
       workspace.run(
-          new CreateArchiveTask(tempArchive, filesToCompress, fileAlias, monitor),
+          new CreateArchiveTask(tempArchive, filesToCompress, monitor),
           projectsToLock.toArray(new IResource[0]));
     } catch (OperationCanceledException e) {
       LocalCancellationException canceled = new LocalCancellationException();
