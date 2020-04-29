33200fd9940ae757a8ef63240880b406c2dae3de
Tobias Bouschen <tobias.bouschen@googlemail.com>
2020-04-29 21:47:06 +0200

    [INTERNAL][I] Add methods to determine text attributes for annotations
    Creates methods in the annotation implementations to obtain the text
    attribute to use for their range highlighters. The text attributes
    define how the highlighting is displayed in the editor (color, border,
    etc.).

--- intellij/src/saros/intellij/editor/annotations/SelectionAnnotation.java
+++ intellij/src/saros/intellij/editor/annotations/SelectionAnnotation.java
@@ -1,10 +1,14 @@
 package saros.intellij.editor.annotations;
 
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.editor.markup.TextAttributes;
 import java.util.List;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import saros.filesystem.IFile;
+import saros.intellij.editor.colorstorage.ColorManager;
+import saros.intellij.editor.colorstorage.ColorManager.ColorKeys;
 import saros.session.User;
 
 /**
@@ -36,5 +40,25 @@ class SelectionAnnotation extends AbstractEditorAnnotation {
       throw new IllegalArgumentException(
           "A selection annotation must have exactly one " + "AnnotationRange.");
     }
+  }
+
+  /**
+   * Returns the text attributes used for selection annotation range highlighters.
+   *
+   * @param editor the editor to which the annotation belongs
+   * @param user the user to whom the annotation belongs
+   * @return the text attributes used for selection annotation range highlighters
+   */
+  // TODO make private once migration is complete
+  static TextAttributes getSelectionTextAttributes(@NotNull Editor editor, @NotNull User user) {
+
+    // Retrieve color keys based on the color ID selected by this user. This will automatically
+    // fall back to default colors, if no colors for the given ID are available.
+    ColorKeys colorKeys = ColorManager.getColorKeys(user.getColorID());
+
+    TextAttributesKey highlightColorKey = colorKeys.getSelectionColorKey();
+
+    // Resolve the correct text attributes based on the currently configured IDE scheme.
+    return editor.getColorsScheme().getAttributes(highlightColorKey);
   }
 }
