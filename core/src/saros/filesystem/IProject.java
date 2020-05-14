fd88a195d67b2c9086db73b003a0756e95dd1d38
Tobias Bouschen <tobias.bouschen@googlemail.com>
2020-05-14 20:20:00 +0200

    [API][CORE] Add default implementations for specific IProject methods
    Overwrites specific methods of IContainer in IProject to specify the
    differing behavior for projects.
    
    Adds default implementations for specific methods in IProjects that have
    a constant behavior.

--- core/src/saros/filesystem/IProject.java
+++ core/src/saros/filesystem/IProject.java
@@ -21,8 +21,65 @@
 
 package saros.filesystem;
 
+import java.io.IOException;
+
 /**
- * This interface is under development. It currently equals its Eclipse counterpart. If not
- * mentioned otherwise all offered methods are equivalent to their Eclipse counterpart.
+ * Represents a project in the local (virtual) file system.
+ *
+ * <p>Defines the project specific default behavior of methods defined by {@link IContainer}.
+ *
+ * <p>This interface is under development. It is currently a placeholder and will be reworked once
+ * the migration to reference points is completed.
  */
-public interface IProject extends IContainer {}
+public interface IProject extends IContainer {
+  /**
+   * Returns an empty path.
+   *
+   * @return an empty path
+   */
+  IPath getProjectRelativePath();
+
+  /**
+   * Always throws an IO exception.
+   *
+   * <p>Deleting a project resource is not supported.
+   *
+   * @throws IOException always
+   */
+  default void delete() throws IOException {
+    throw new IOException("Deleting project resources is not supported - tried to delete " + this);
+  }
+
+  /**
+   * Always returns <code>null</code>.
+   *
+   * <p>Resources above the shared project can not be described through (conventional) relative
+   * paths. Additionally, such resources are not part of the shared scope and should therefore not
+   * be of interest for Saros.
+   *
+   * @return <code>null</code>
+   */
+  default IContainer getParent() {
+    return null;
+  }
+
+  /**
+   * Returns a reference to itself.
+   *
+   * @return a reference to itself
+   */
+  default IProject getProject() {
+    return this;
+  }
+
+  /**
+   * Returns <code>false</code>.
+   *
+   * <p>Projects are the base for all shared resources and can therefore not be ignored.
+   *
+   * @return <code>false</code>
+   */
+  default boolean isIgnored() {
+    return false;
+  }
+}
