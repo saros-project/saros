2581f72121250acabe53c28be4ec11aa949516bf
Tobias Bouschen <tobias.bouschen@googlemail.com>
2020-05-15 01:20:49 +0200

    [INTERNAL][I] Migrate ShareWithUserAction to new resource impls

--- intellij/src/saros/intellij/ui/menu/ShareWithUserAction.java
+++ intellij/src/saros/intellij/ui/menu/ShareWithUserAction.java
@@ -3,9 +3,7 @@ package saros.intellij.ui.menu;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.vfs.VirtualFile;
 import java.text.MessageFormat;
 import java.util.Collections;
@@ -16,8 +14,7 @@ import org.apache.log4j.Logger;
 import saros.core.ui.util.CollaborationUtils;
 import saros.filesystem.IProject;
 import saros.intellij.context.SharedIDEContext;
-import saros.intellij.filesystem.IntelliJProjectImpl;
-import saros.intellij.runtime.FilesystemRunner;
+import saros.intellij.filesystem.IntellijReferencePointImpl;
 import saros.intellij.ui.Messages;
 import saros.intellij.ui.util.IconManager;
 import saros.intellij.ui.util.NotificationPanel;
@@ -28,11 +25,6 @@ import saros.net.xmpp.JID;
  *
  * <p>Calls {@link CollaborationUtils#startSession(Set, List)} with the selected module as
  * parameter.
- *
- * <p>This class assumes that the project is allowed to be shared (at the moment only completely
- * shared projects are implemented) and that the call to {@link
- * ShareWithUserAction#getModuleForVirtualFile(VirtualFile, Project)} is supported for this IDE
- * type.
  */
 public class ShareWithUserAction extends AnAction {
 
@@ -64,42 +56,29 @@ public class ShareWithUserAction extends AnAction {
           "Unable to start session - could not determine virtual file for highlighted resource.");
     }
 
-    IProject module;
+    IProject referencePoint;
 
     try {
-      module = getModuleForVirtualFile(virtualFile, event.getProject());
+      referencePoint = new IntellijReferencePointImpl(project, virtualFile);
 
     } catch (IllegalArgumentException e) {
-      log.error("Tried to share illegal module", e);
+      log.error("Failed to create reference point for " + virtualFile + " in " + project, e);
 
       NotificationPanel.showError(
-          MessageFormat.format(Messages.ShareWithUserAction_illegal_module_message, e.getMessage()),
-          Messages.ShareWithUserAction_illegal_module_title);
+          MessageFormat.format(
+              Messages.ShareWithUserAction_failed_to_share_resource_message, e.getMessage()),
+          Messages.ShareWithUserAction_failed_to_share_resource_title);
 
       return;
     }
 
     Set<IProject> projects = new HashSet<>();
-    projects.add(module);
+    projects.add(referencePoint);
 
     List<JID> contacts = Collections.singletonList(userJID);
 
     SharedIDEContext.preregisterProject(project);
     CollaborationUtils.startSession(projects, contacts);
-  }
-
-  private IProject getModuleForVirtualFile(VirtualFile virtualFile, Project project) {
-    ProjectFileIndex projectFileIndex = ProjectFileIndex.getInstance(project);
-
-    Module module =
-        FilesystemRunner.runReadAction(() -> projectFileIndex.getModuleForFile(virtualFile));
-
-    if (module == null) {
-      // FIXME: Find way to select moduleName for non-module based IDEAs (Webstorm)
-      throw new UnsupportedOperationException();
-    }
-
-    return new IntelliJProjectImpl(module);
   }
 
   @Override
