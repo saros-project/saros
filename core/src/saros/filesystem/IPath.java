e50b99a32c656e96387b21e36d79611431477b02
Tobias Bouschen <tobias.bouschen@googlemail.com>
2020-05-14 14:59:24 +0200

    [NOP][CORE] Do minor cleanup in IPath and IPathFactory
    Removes unnecessary declarations from IPath and IPathFactory.
    
    Adds a TODOs to remove IPath and IPathFactory, linking to issue #797.

--- core/src/saros/filesystem/IPath.java
+++ core/src/saros/filesystem/IPath.java
@@ -1,24 +1,3 @@
-/*
- *
- *  DPP - Serious Distributed Pair Programming
- *  (c) Freie Universität Berlin - Fachbereich Mathematik und Informatik - 2010
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 1, or (at your option)
- *  any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * /
- */
-
 package saros.filesystem;
 
 import java.io.File;
@@ -27,33 +6,34 @@ import java.io.File;
  * This interface is under development. It currently equals its Eclipse counterpart. If not
  * mentioned otherwise all offered methods are equivalent to their Eclipse counterpart.
  */
+// TODO replace with java.nio.file.Path; see #797
 public interface IPath {
 
-  public IPath append(IPath path);
+  IPath append(IPath path);
 
-  public String segment(int index);
+  String segment(int index);
 
-  public String lastSegment();
+  String lastSegment();
 
-  public boolean isPrefixOf(IPath path);
+  boolean isPrefixOf(IPath path);
 
-  public int segmentCount();
+  int segmentCount();
 
-  public IPath removeFirstSegments(int count);
+  IPath removeFirstSegments(int count);
 
-  public IPath removeLastSegments(int count);
+  IPath removeLastSegments(int count);
 
-  public String[] segments();
+  String[] segments();
 
-  public IPath append(String path);
+  IPath append(String path);
 
-  public IPath makeAbsolute();
+  IPath makeAbsolute();
 
-  public boolean isAbsolute();
+  boolean isAbsolute();
 
-  public String toPortableString();
+  String toPortableString();
 
-  public String toOSString();
+  String toOSString();
 
-  public File toFile();
+  File toFile();
 }
