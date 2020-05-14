95ac50bda5cd0e812f5af4702d5a62b8ce5576b3
Tobias Bouschen <tobias.bouschen@googlemail.com>
2020-05-15 01:11:13 +0200

    [INTERNAL][I] Migrate ProjectClosedHandler to new resource impls

--- intellij/src/saros/intellij/eventhandler/project/ProjectClosedHandler.java
+++ intellij/src/saros/intellij/eventhandler/project/ProjectClosedHandler.java
@@ -1,25 +1,22 @@
 package saros.intellij.eventhandler.project;
 
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectManagerListener;
 import com.intellij.util.messages.MessageBusConnection;
 import org.jetbrains.annotations.NotNull;
 import saros.filesystem.IProject;
-import saros.intellij.context.SharedIDEContext;
-import saros.intellij.filesystem.IntelliJProjectImpl;
+import saros.intellij.filesystem.IntellijReferencePointImpl;
 import saros.repackaged.picocontainer.Disposable;
 import saros.session.ISarosSession;
 import saros.session.ISarosSessionManager;
 import saros.session.SessionEndReason;
 
 /**
- * Ends the current session when a project containing shared modules is closed and disables the
- * activity execution for resources of the closed project. This is done to avoid calls on disposed
- * project objects.
+ * Ends the current session when a project containing shared reference points is closed and disables
+ * the activity execution for resources of the closed project. This is done to avoid calls on
+ * disposed project objects.
  *
  * <p>The handler is enabled by default after instantiation.
  */
@@ -35,48 +32,33 @@ public class ProjectClosedHandler implements Disposable {
       new ProjectManagerListener() {
         @Override
         public void projectClosing(@NotNull Project project) {
-          Project sessionProject = sarosSession.getComponent(SharedIDEContext.class).getProject();
+          boolean closedProjectContainedSharedResources = false;
 
-          if (sessionProject.equals(project)) {
-            disableActivityExecutionForProjectModules(project);
+          for (IProject referencePoint : sarosSession.getProjects()) {
+            IntellijReferencePointImpl intellijReferencePoint =
+                (IntellijReferencePointImpl) referencePoint;
 
-            Thread shutdownThread =
-                new Thread(
-                    () -> {
-                      assert !ApplicationManager.getApplication().isDispatchThread()
-                          : "The session must not be shut down from the EDT";
+            if (intellijReferencePoint.getIntellijProject().equals(project)) {
+              sarosSession.setActivityExecution(intellijReferencePoint, false);
 
-                      sarosSessionManager.stopSession(SessionEndReason.LOCAL_USER_LEFT);
-                    });
-
-            shutdownThread.start();
-          }
-        }
-
-        /**
-         * Disables the activity execution for the modules of the given project.
-         *
-         * <p>This method is used to report the modules of a closed project to the session in order
-         * to prevent it from trying to run any activities on disposed resources.
-         *
-         * @param project the project whose modules to disable the activity execution for
-         * @see ISarosSession#setActivityExecution(IProject,boolean)
-         */
-        private void disableActivityExecutionForProjectModules(@NotNull Project project) {
-          for (Module module : ModuleManager.getInstance(project).getModules()) {
-            IProject wrappedModule;
-
-            try {
-              wrappedModule = new IntelliJProjectImpl(module);
-
-            } catch (IllegalArgumentException exception) {
-              continue;
-            }
-
-            if (sarosSession.isShared(wrappedModule)) {
-              sarosSession.setActivityExecution(wrappedModule, false);
+              closedProjectContainedSharedResources = true;
             }
           }
+
+          if (!closedProjectContainedSharedResources) {
+            return;
+          }
+
+          Thread shutdownThread =
+              new Thread(
+                  () -> {
+                    assert !ApplicationManager.getApplication().isDispatchThread()
+                        : "The session must not be shut down from the EDT";
+
+                    sarosSessionManager.stopSession(SessionEndReason.LOCAL_USER_LEFT);
+                  });
+
+          shutdownThread.start();
         }
       };
 
