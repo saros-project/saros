b87e670eb8632edadc06596f7caebfc3ef56dee4
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2020-05-26 17:36:28 +0200

    [INTERNAL][I] Move xmpp error callback into lambda

--- intellij/src/saros/intellij/ui/eventhandler/ConnectingFailureHandler.java
+++ intellij/src/saros/intellij/ui/eventhandler/ConnectingFailureHandler.java
@@ -1,23 +1,17 @@
 package saros.intellij.ui.eventhandler;
 
-import saros.account.XMPPAccount;
 import saros.communication.connection.ConnectionHandler;
-import saros.communication.connection.IConnectingFailureCallback;
 import saros.intellij.ui.util.NotificationPanel;
 import saros.ui.CoreMessages;
 
 /** Callback handler informing the user of a failed XMPP connection attempt. */
 public class ConnectingFailureHandler {
 
-  @SuppressWarnings("FieldCanBeLocal")
-  private final IConnectingFailureCallback connectingFailureCallback = this::handleConnectionFailed;
-
   public ConnectingFailureHandler(ConnectionHandler connectionHandler) {
-    connectionHandler.setCallback(connectingFailureCallback);
-  }
-
-  public void handleConnectionFailed(XMPPAccount account, String errorMessage) {
-    // TODO offer user possibility of adjusting settings and re-connect
-    NotificationPanel.showError(errorMessage, CoreMessages.ConnectingFailureHandler_title);
+    connectionHandler.setCallback(
+        (account, errorMessage) -> {
+          // TODO offer user possibility of adjusting settings and re-connect
+          NotificationPanel.showError(errorMessage, CoreMessages.ConnectingFailureHandler_title);
+        });
   }
 }
