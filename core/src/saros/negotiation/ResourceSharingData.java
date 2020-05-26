e4f5ff0253ebf3d32f1950f0bfe19fc938b207ca
Tobias Bouschen <tobias.bouschen@googlemail.com>
2020-05-26 15:44:19 +0200

    [DOC][CORE] Clean up ResourceSharingData

--- core/src/saros/negotiation/ResourceSharingData.java
+++ core/src/saros/negotiation/ResourceSharingData.java
@@ -7,85 +7,87 @@ import java.util.Map;
 import saros.filesystem.IReferencePoint;
 
 /**
- * Defines which projects and which of their resources) to share during a particular {@link
+ * Defines which reference points (and which of their resources) to share during a particular {@link
  * AbstractOutgoingResourceNegotiation}.
  */
 public class ResourceSharingData implements Iterable<IReferencePoint> {
 
-  private Map<String, IReferencePoint> projectsById = new HashMap<>();
-  private Map<IReferencePoint, String> idsByProject = new HashMap<>();
+  private Map<String, IReferencePoint> referencePointsById = new HashMap<>();
+  private Map<IReferencePoint, String> idsByReferencePoint = new HashMap<>();
 
   /**
-   * Declares that the passed project should be shared with the specified ID.
+   * Declares that the passed reference point should be shared with the specified ID.
    *
-   * @param project project that should be shared
-   * @param projectId session-wide ID assigned to the project
+   * @param referencePoint reference point that should be shared
+   * @param referencePointId session-wide ID assigned to the reference point
    */
-  public void addReferencePoint(IReferencePoint project, String projectId) {
-    projectsById.put(projectId, project);
-    idsByProject.put(project, projectId);
+  public void addReferencePoint(IReferencePoint referencePoint, String referencePointId) {
+    referencePointsById.put(referencePointId, referencePoint);
+    idsByReferencePoint.put(referencePoint, referencePointId);
   }
 
   /**
-   * Returns the to-be-shared project with the passed ID.
+   * Returns the to-be-shared reference point with the passed ID.
    *
-   * @param id project ID
-   * @return project with the ID
+   * @param id reference point ID
+   * @return reference point with the ID
    */
   public IReferencePoint getReferencePoint(String id) {
-    return projectsById.get(id);
+    return referencePointsById.get(id);
   }
 
   /**
-   * Returns if the project is already contained in this collection
+   * Returns if the reference point is already contained in this collection
    *
-   * @param id projectId
-   * @return boolean indicating if the project is already contained in this collection
+   * @param id reference point ID
+   * @return boolean indicating if the reference point is already contained in this collection
    */
   public boolean hasReferencePointById(String id) {
-    return projectsById.containsKey(id);
+    return referencePointsById.containsKey(id);
   }
 
   /**
-   * Returns if the project is already contained in this collection
+   * Returns if the reference point is already contained in this collection
    *
-   * @param project
-   * @return boolean indicating if the project is already contained in this collection
+   * @param referencePoint reference point
+   * @return boolean indicating if the reference point is already contained in this collection
    */
-  public boolean hasReferencePoint(IReferencePoint project) {
-    return idsByProject.containsKey(project);
+  public boolean hasReferencePoint(IReferencePoint referencePoint) {
+    return idsByReferencePoint.containsKey(referencePoint);
   }
 
   /**
-   * Returns the ID of the given to-be-shared project.
+   * Returns the ID of the given to-be-shared reference point.
    *
-   * @param project one of the to-be-shared projects
-   * @return matching project ID
+   * @param referencePoint one of the to-be-shared reference points
+   * @return matching reference point ID
    */
-  public String getReferencePointID(IReferencePoint project) {
-    return idsByProject.get(project);
+  public String getReferencePointID(IReferencePoint referencePoint) {
+    return idsByReferencePoint.get(referencePoint);
   }
 
   /**
-   * Returns the number of to-be-shared projects added to this {@link ResourceSharingData} instance.
+   * Returns the number of to-be-shared reference points added to this {@link ResourceSharingData}
+   * instance.
    *
-   * @return number of projects
+   * @return number of reference points
    */
   public int size() {
-    return projectsById.size();
+    return referencePointsById.size();
   }
 
   /**
-   * Returns whether this {@link ResourceSharingData} instance contains any to-be-shared projects.
+   * Returns whether this {@link ResourceSharingData} instance contains any to-be-shared reference
+   * points.
    *
-   * @return true if there are no projects, false if there are
+   * @return true if there are no reference points, false if there are
    */
   public boolean isEmpty() {
-    return projectsById.isEmpty();
+    return referencePointsById.isEmpty();
   }
 
   @Override
   public Iterator<IReferencePoint> iterator() {
-    return Collections.unmodifiableCollection(projectsById.values()).iterator();
+    return Collections.unmodifiableCollection(referencePointsById.values()).iterator();
   }
 }
