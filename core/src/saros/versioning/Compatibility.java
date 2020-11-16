be3b2de7814b978f013cc84dd13a8b4bcc1f253c
Tobias Bouschen <tobias.bouschen@googlemail.com>
2020-11-16 16:28:46 +0100

    [NOP][CORE] Remove unused capabilities of Compatibility enum
    Removes the additional capabilities of the Compatibility enum (like
    specifying the integer represented by an entry and inverting an entry).
    They were no longer being used.

--- core/src/saros/versioning/Compatibility.java
+++ core/src/saros/versioning/Compatibility.java
@@ -4,76 +4,28 @@ package saros.versioning;
 public enum Compatibility {
 
   /** Versions are (probably) compatible */
-  OK(0) {
-    @Override
-    public Compatibility invert() {
-      return OK;
-    }
-  },
+  OK,
+
   /**
    * The local version is (probably) too old to work with the remote version.
    *
    * <p>The user should be told to upgrade
    */
-  OLDER(1) {
-    @Override
-    public Compatibility invert() {
-      return NEWER;
-    }
-  },
+  OLDER,
+
   /**
    * The local version is (probably) too new to work with the remote version.
    *
    * <p>The user should be told to tell the peer to update.
    */
-  NEWER(2) {
-    @Override
-    public Compatibility invert() {
-      return OLDER;
-    }
-  },
+  NEWER,
 
   /** The compatibility could not be determined. */
-  UNKNOWN(3) {
-    @Override
-    public Compatibility invert() {
-      return UNKNOWN;
-    }
-  },
+  UNKNOWN,
 
   /**
    * At least one of the versions contains a qualifiers and the complete version string does not
    * match.
    */
-  QUALIFIER_MISMATCH(4) {
-    @Override
-    public Compatibility invert() {
-      return QUALIFIER_MISMATCH;
-    }
-  };
-
-  private final int code;
-
-  Compatibility(final int code) {
-    this.code = code;
-  }
-
-  /**
-   * @return <code>TOO_OLD</code> if the initial compatibility was <code>TOO_NEW</code>, <code>
-   *     TOO_NEW</code> if the initial compatibility was <code>TOO_OLD</code>, <code>OK</code>
-   *     otherwise
-   */
-  public abstract Compatibility invert();
-
-  public int getCode() {
-    return code;
-  }
-
-  public static Compatibility fromCode(int code) {
-    for (Compatibility compatibility : Compatibility.values()) {
-      if (compatibility.getCode() == code) return compatibility;
-    }
-
-    return UNKNOWN;
-  }
+  QUALIFIER_MISMATCH
 }
