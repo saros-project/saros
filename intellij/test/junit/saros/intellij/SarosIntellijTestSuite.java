d35c36321c4f26310aadf9c8ee041a2f674dd0ba
Tobias Bouschen <tobias.bouschen@googlemail.com>
2021-02-28 19:21:08 +0100

    [I] Remove LocalDocumentModificationHandlerTest
    Removes LocalDocumentModificationHandlerTest. The test only checks
    whether the base listener is actually registered and de-registered from
    the local IntelliJ IDEA instance.
    
    Removes testing libraries which were only added for this one test.

--- intellij/test/junit/saros/intellij/SarosIntellijTestSuite.java
+++ intellij/test/junit/saros/intellij/SarosIntellijTestSuite.java
@@ -4,11 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-  saros.intellij.context.TestSuite.class,
-  saros.intellij.editor.TestSuite.class,
-  saros.intellij.eventhandler.editor.document.TestSuite.class
-})
+@Suite.SuiteClasses({saros.intellij.context.TestSuite.class, saros.intellij.editor.TestSuite.class})
 public class SarosIntellijTestSuite {
   // the class remains completely empty,
   // being used only as a holder for the above annotations
