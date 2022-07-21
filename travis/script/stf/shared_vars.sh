78e18b694839df042cd14daf2437a9cda8058788
Stefan Moll <stefan@stefaus.de>
2022-07-21 12:06:33 +0200

    [Build] Update STF environment to JDK 11
    * Updates STF test runner to use JDK 11 docker container
    * Remove unsupported -t parameter from timeout command
    * JDK 9 introduced java modules. Now eclipse is asking to create a file
      for this and STF Bot is extended to uncheck the creation of this file.
      Otherwise the create dialog would never finish.

--- travis/script/stf/shared_vars.sh
+++ travis/script/stf/shared_vars.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
-stf_coordinator_image="saros/stf_test_coordinator:0.8"
-stf_worker_image="saros/stf_test_worker:0.8"
-stf_xmpp_image="saros/stf_xmpp_server:0.8"
+# tag 0.9
+stf_coordinator_image="saros/stf_test_coordinator@sha256:9558dc015ae0e0905255d93bdb5170fa9596c81dbe5c1092afc39dbe01747d94"
+stf_worker_image="saros/stf_test_worker@sha256:a3cb6b46827b00b72f7f47a44785dd86121a36b4699fc17735313476acdb2dfd"
+stf_xmpp_image="saros/stf_xmpp_server@sha256:df33af58a040d9e6173ed3a3047bc767f6b54e9104b7ecc91296c778c4d50a91"
 
 stf_coordinator_name="stf_coordinator"
 stf_network_name="stf_test_network"
